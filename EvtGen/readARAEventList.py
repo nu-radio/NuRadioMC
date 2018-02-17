4e4fc4ba01fd24963980ceb05bdfca3a0d1065e4
cg-laser <christian.glaser@uci.edu>
2018-02-17 00:12:24 +0000

    add converter from ARAsim eventlist to hdf5

---
+++ EvtGen/readARAEventList.py
@@ -0,0 +1,36 @@
+from __future__ import absolute_import, division, print_function
+import numpy as np
+import h5py
+import argparse
+from io import BytesIO
+
+VERSION = 0.1
+
+parser = argparse.ArgumentParser(description='Parse ARA event list.')
+parser.add_argument('filename', type=str,
+                    help='path to ARA event list')
+parser.add_argument('outputfilename', type=str,
+                    help='name of hdf5 output filename')
+args = parser.parse_args()
+version = 0
+event_number = 0
+with open(args.filename, 'r') as fin:
+    lines = fin.readlines()
+    data = ""
+    for i, line in enumerate(lines):
+        if line.startswith("VERSION"):
+            ara_version = float(line.split("=")[1])
+        elif line.startswith("EVENT_NUM"):
+            event_number = int(line.split("=")[1])
+        else:
+            data += "{}".format(line)
+    if(ara_version != 0.1):
+        print("version != 0.1 not supported")
+        import sys
+        sys.exit(-1)
+
+    data = np.genfromtxt(BytesIO(data), comments='//', skip_header=3)
+    fout = h5py.File(args.outputfilename, 'w')
+    fout['eventlist'] = data
+    fout.attrs['VERSION'] = VERSION
+    fout.close()
