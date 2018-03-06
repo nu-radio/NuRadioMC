2d54b4409525c0f801adcb5eece08c8c5b784caa
cg-laser <christian.glaser@uci.edu>
2018-03-06 17:36:43 +0000

    prototype analytic ray tracing function

---
+++ SignalProp/analyticraytraycing.py
@@ -0,0 +1,32 @@
+"""
+analytic ray tracing solution
+"""
+
+
+def get_angles(start_point, stop_point):
+    """
+    returns the launch and incoming angle for a ray tracing solution between two points
+
+    Parameters
+    ----------
+    start_point: 3-vector of floats
+        start position of the ray
+    stop_point: 3-vector of floats
+        stop position of the ray
+
+    Returns
+    -------
+    None if no solution was found, otherwise:
+
+    launch_angle: float
+        the launch angle of the ray
+
+    incoming_angle: float
+        the incoming angle of the ray
+
+    distance: float
+        the distance between the two points along the ray path
+
+    """
+    # just prototyping
+    return None
