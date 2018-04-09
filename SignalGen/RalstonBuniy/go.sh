020f26171e641d8095c9c81e6b418b8431468255
cg-laser <christian.glaser@uci.edu>
2018-04-09 21:46:40 +0000

    combine relevant getter functions in one file

--- SignalGen/RalstonBuniy/go.sh
+++ SignalGen/RalstonBuniy/go.sh
@@ -1,12 +1,9 @@
-g++ createAsk.cpp -o createAsk Askaryan.cxx -lfftw3
+# example sh script
+# the executable is compiled 
+
+g++ createAsk.cpp -o createAsk Askaryan.cxx -lfftw3 -I../../utilities/
 ./createAsk 1.0e5 55.8 55.8
 ./createAsk 1.0e5 53.3 53.3
 ./createAsk 1.0e5 50.8 50.8
 ./createAsk 1.0e5 48.3 48.3
 ./createAsk 1.0e5 45.8 45.8
-mv shower*.dat ~/AskaryanPapers/RecentPaperFigures
-#rm ../octaveScripts/shower*.dat
-#mv shower*.dat ../octaveScripts
-#cd ../octaveScripts
-#octave plotSpectra.m
-#evince March*pdf
