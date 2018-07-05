9a5c8df80b616c1503e8db98092652b279752ba8
cg-laser <christian.glaser@uci.edu>
2018-07-05 23:39:33 +0000

    update comparison plotting script

--- SignalGen/tests/T01parmetrizations.py
+++ SignalGen/tests/T01parmetrizations.py
@@ -1,6 +1,6 @@
-import NuRadioMC.SignalGen.RalstonBuniy.create_askaryan as AskaryanModule
+import NuRadioMC.SignalGen.RalstonBuniy.askaryan_module as AskaryanModule
 import NuRadioMC.SignalGen.parametrizations as param
-from NuRadioMC.utilities import units
+from NuRadioMC.utilities import units, fft
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -14,22 +14,66 @@ ff = np.fft.rfftfreq(n_samples, dt)
 tt = np.arange(0, n_samples * dt, dt)
 R = 5 * units.km
 
-spec1 = AskaryanModule.get_frequency_spectrum(E, theta, ff, 0, n_index, R)
-spec2 = param.get_frequency_spectrum(E, theta, ff, 0, n_index, R, model='Alvarez2012') / units.MHz
-spec3 = param.get_frequency_spectrum(E, theta, ff, 0, n_index, R, model='Alvarez2000')
+df = ff[1]  # width of a frequency bin
+
+spec1 = AskaryanModule.get_frequency_spectrum(E, theta, ff, 1, n_index, R)
+spec2 = param.get_frequency_spectrum(E, theta, ff, 1, n_index, R, model='Alvarez2012')
+spec3 = param.get_frequency_spectrum(E, theta, ff, 1, n_index, R, model='Alvarez2000')
 
 fig, ax = plt.subplots(1, 1)
-ax.plot(ff / units.MHz, np.abs(spec1[0]) / units.V * units.m * units.MHz, label='Askaryan module eR')
-ax.plot(ff / units.MHz, np.abs(spec1[1]) / units.V * units.m * units.MHz, label='Askaryan module eTheta')
-ax.plot(ff / units.MHz, np.abs(spec2) / units.V * units.m * units.MHz, label='pyrex Alvarez2012')
-ax.plot(ff / units.MHz, np.abs(spec3) / units.V * units.m * units.MHz, label='shelfmc Alvarez2000')
+ax.plot(ff / units.MHz, np.abs(spec1[0]) / units.V * units.m, 'o', label='Askaryan module eR')
+ax.plot(ff / units.MHz, np.abs(spec1[1]) / units.V * units.m, 'o', label='Askaryan module eTheta')
+ax.plot(ff / units.MHz, np.abs(spec2) / units.V * units.m, 'd', label='pyrex Alvarez2012')
+ax.plot(ff / units.MHz, np.abs(spec3) / units.V * units.m, 's', label='shelfmc Alvarez2000')
 ax.semilogx(True)
 ax.semilogy(True)
-ax.set_xlim(1, 2e3)
-ax.set_ylim(1e-11, 1e-3)
+ax.set_xlim(10, 2e3)
+ax.set_ylim(1e-9, 1e-3)
 ax.set_xlabel("frequency [MHz]")
-ax.set_ylabel("amplitude [V/m/MHz]")
+ax.set_ylabel("amplitude [V/m] per {:.1f}MHz".format(df / units.MHz))
 ax.legend()
 fig.tight_layout()
-fig.savefig("comparison.png")
+fig.savefig("comparison_em.png")
+plt.show()
+
+# test if different models are normalized such that they give the same time trace
+# independent of the bin width
+
+# 1st askaryan module
+fig, ax = plt.subplots(1, 1)
+n_trace = 2 ** 10  # samples of trace
+dt = 0.1 * units.ns
+tt = np.arange(0, n_trace * dt, dt)
+ff = np.fft.rfftfreq(n_trace, dt)
+
+spectrum = AskaryanModule.get_time_trace(E, theta, ff, 0, n=1.78, R=R)
+ax.plot(tt / units.ns, np.abs(spectrum[1]), label="dt = {:.2f}ns, n={}".format(dt / units.ns, n_trace))
+
+n_trace = 2 ** 12  # samples of trace
+dt = 0.1 * units.ns
+tt = np.arange(0, n_trace * dt, dt)
+ff = np.fft.rfftfreq(n_trace, dt)
+spectrum = AskaryanModule.get_time_trace(E, theta, ff, 0, n=1.78, R=R)
+ax.plot(tt / units.ns, np.abs(spectrum[1]), label="dt = {:.2f}ns, n={}".format(dt / units.ns, n_trace))
+ax.legend()
+ax.set_title("askaryan module")
+
+# 2nd pyrex
+fig, ax = plt.subplots(1, 1)
+n_trace = 2 ** 10  # samples of trace
+dt = 0.1 * units.ns
+tt = np.arange(0, n_trace * dt, dt)
+ff = np.fft.rfftfreq(n_trace, dt)
+
+trace = fft.freq2time(param.get_frequency_spectrum(E, theta, ff, 0, n_index, R, model='Alvarez2012'))
+ax.plot(tt / units.ns, trace, label="dt = {:.2f}ns, n={}".format(dt / units.ns, n_trace))
+
+n_trace = 2 ** 12  # samples of trace
+dt = 0.1 * units.ns
+tt = np.arange(0, n_trace * dt, dt)
+ff = np.fft.rfftfreq(n_trace, dt)
+trace = fft.freq2time(param.get_frequency_spectrum(E, theta, ff, 0, n_index, R, model='Alvarez2012'))
+ax.plot(tt / units.ns, trace, '--', label="dt = {:.2f}ns, n={}".format(dt / units.ns, n_trace))
+ax.legend()
+ax.set_title("pyrex")
 plt.show()
