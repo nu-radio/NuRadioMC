29f88edda3fb6598ea2c4438165826e74e083efe
cg-laser <christian.glaser@uci.edu>
2018-07-06 01:12:05 +0000

    modify coreas reader to have the same interface as the readCoREAS reader

--- NuRadioReco/modules/io/coreas/readCoREASStation.py
+++ NuRadioReco/modules/io/coreas/readCoREASStation.py
@@ -6,23 +6,38 @@ from NuRadioReco.modules.io.coreas import coreas
 
 class readCoREASStation:
 
-    def begin(self):
-        pass
+    def begin(self, input_files, station_id):
+        """
+        begin method
 
-    def run(self, input_file, station_id):
+        initialize readCoREAS module
 
-        # read in coreas simulation and copy to event
-        corsika = h5py.File(input_file, "r")
-        weights = coreas.calculate_simulation_weights(corsika["highlevel"]["obsplane_na_na"]["antenna_position"])
+        Parameters
+        ----------
+        input_files: input files
+            list of coreas hdf5 files
+        station_id: station id
+            id number of the ARIANNA station
+        """
+        self.__input_files = input_files
+        self.__station_id = station_id
+        self.__current_input_file = 0
 
-        for i, (name, observer) in enumerate(corsika['CoREAS']['observers'].items()):
-            evt = NuRadioReco.framework.event.Event(corsika['inputs'].attrs['RUNNR'], corsika['inputs'].attrs['EVTNR'])  # create empty event
-            station = NuRadioReco.framework.station.Station(station_id)
-            sim_station = coreas.make_sim_station(station_id, corsika, observer, weights[i])
+    def run(self):
+        for input_file in self.__input_files:
 
-            station.set_sim_station(sim_station)
-            evt.set_station(station)
-            yield evt
+            # read in coreas simulation and copy to event
+            corsika = h5py.File(input_file, "r")
+            weights = coreas.calculate_simulation_weights(corsika["highlevel"].values()[0]["antenna_position"])
+
+            for i, (name, observer) in enumerate(corsika['CoREAS']['observers'].items()):
+                evt = NuRadioReco.framework.event.Event(corsika['inputs'].attrs['RUNNR'], corsika['inputs'].attrs['EVTNR'])  # create empty event
+                station = NuRadioReco.framework.station.Station(self.__station_id)
+                sim_station = coreas.make_sim_station(self.__station_id, corsika, observer, weights[i])
+
+                station.set_sim_station(sim_station)
+                evt.set_station(station)
+                yield evt
 
     def end(self):
         pass
