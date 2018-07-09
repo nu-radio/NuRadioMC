3456859fdd9dde7cf61fcf5475a955996fe9cd80
cg-laser <christian.glaser@uci.edu>
2018-07-09 17:54:10 +0000

    add switch for LPM effect

--- SignalGen/RalstonBuniy/createAsk.h
+++ SignalGen/RalstonBuniy/createAsk.h
@@ -1,10 +1,7 @@
-#include <complex>
-#include <vector>
-
-
 void getFrequencySpectrum2(double*& spectrumRealR, double*& spectrumImagR,
 		double*& spectrumRealTheta, double*& spectrumImagTheta,
 		double*& spectrumRealPhi, double*& spectrumImagPhi,
 		int& size, const double energy, const double theta, double* freqs,
-		int size_f, const bool isEMShower, const double n, const double R);
+		int size_f, const bool isEMShower, const double n, const double R,
+		const bool LPM);
 
