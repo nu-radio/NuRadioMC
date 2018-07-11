10efaa3b8797b65b285e01757a86e5e43d111204
cg-laser <christian.glaser@uci.edu>
2018-07-11 01:55:37 +0000

    add individual sim channels

--- NuRadioReco/framework/sim_station.py
+++ NuRadioReco/framework/sim_station.py
@@ -5,12 +5,33 @@ import cPickle as pickle
 
 class SimStation(NuRadioReco.framework.base_station.BaseStation):
 
-    def __init__(self, station_id, sampling_rate, trace, position):
+    def __init__(self, station_id, sampling_rate=None, trace=None, position=None):
         NuRadioReco.framework.base_station.BaseStation.__init__(self, station_id)
+        self.__channels = {}
         self.__position = position
         self.__magnetic_field_vector = None
         self.__simulation_weight = None
-        self.set_trace(trace, sampling_rate)
+        if(trace is not None and sampling_rate is not None):
+            self.set_trace(trace, sampling_rate)
+
+    def get_channels(self):
+        return self.__channels.values()
+
+    def iter_channels(self, use_channels=None):
+        for channel_id, channel in self.__channels.iteritems():
+            if(use_channels is None):
+                yield channel
+            else:
+                if channel_id in use_channels:
+                    yield channel
+
+    def get_channel(self, channel_id):
+        return self.__channels[channel_id]
+
+    def add_channel(self, channel):
+        if(channel.get_id() not in self.__channels.keys()):
+            self.__channels[channel.get_id()] = []
+        self.__channels[channel.get_id()].append(channel)
 
     def get_position(self):
         return self.__position
@@ -30,9 +51,15 @@ class SimStation(NuRadioReco.framework.base_station.BaseStation):
     def serialize(self, mode):
         base_station_pkl = NuRadioReco.framework.base_station.BaseStation.serialize(self, mode)
 
+        channels_pkl = []
+        for channel1 in self.get_channels():
+            for channel2 in channel1:
+                channels_pkl.append(channel2.serialize(mode))
+
         data = {'__magnetic_field_vector': self.__magnetic_field_vector,
                 '__simulation_weight': self.__simulation_weight,
                 '__position': self.__position,
+                'channels': channels_pkl,
                 'base_station': base_station_pkl}
         return pickle.dumps(data, protocol=2)
 
@@ -42,3 +69,7 @@ class SimStation(NuRadioReco.framework.base_station.BaseStation):
         self.__magnetic_field_vector = data['__magnetic_field_vector']
         self.__simulation_weight = data['__simulation_weight']
         self.__position = data['__position']
+        for channel_pkl in data['channels']:
+            channel = NuRadioReco.framework.channel.Channel(0)
+            channel.deserialize(channel_pkl)
+            self.add_channel(channel)
