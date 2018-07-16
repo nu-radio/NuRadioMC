80dbc4a48dea1b251c31fc03efbdd3743e221888
cg-laser <christian.glaser@uci.edu>
2018-07-16 16:53:51 +0000

    add documentation

--- utilities/fft.py
+++ utilities/fft.py
@@ -1,5 +1,14 @@
 import numpy as np
 
+"""
+the fft utility module implements a real fft with a normalization so that
+Plancherel theorem is valid. This means that the fourier transform must be unitary,
+which is achieved by normalizing both transforms with 1/sqrt(n) and an additional
+factor of sqrt(2) because negative frequencies are omitted in the real FFT.
+This means that the power calculated in the time domain is the same as the
+power calculated in the frequency domain.
+"""
+
 
 def time2freq(trace):
     """
