correct normalization, adapt interface

SignalGen/RalstonBuniy/askaryan_module.py
@@ -3,25 +3,17 @@ from NuRadioMC.utilities import units, fft
 import numpy as np
 
 
-def get_frequency_spectrum(energy, theta, freqs, is_em_shower, n, R, LPM=True, a=None):
+def get_time_trace(energy, theta, N, dt, is_em_shower, n, R, LPM=True, a=None):
     if(a is None):
         a = 0
+    freqs = np.fft.rfftfreq(N, dt)
     eR, eTheta, ePhi = create_askaryan.get_frequency_spectrum(energy, theta, freqs, is_em_shower, n, R, LPM, a)
-    # normalize amplitudes correctly to the bin width of the freqs array
-    # the output of the Askaryan module is normlized to 1MHz
-    df = np.mean(freqs[1:] - freqs[:-1])
-    eR *= (df / units.MHz) ** 0.5
-    eTheta *= (df / units.MHz) ** 0.5
-    ePhi *= (df / units.MHz) ** 0.5
-    return np.array([eR, eTheta, ePhi])
-
-
-def get_time_trace(energy, theta, freqs, is_em_shower, n, R, LPM=True, a=None):
-    eR, eTheta, ePhi = get_frequency_spectrum(energy, theta, freqs, is_em_shower, n, R, LPM, a)
-    length = (len(freqs) - 1) * 2
-    sampling_rate = 2 * freqs[-1]
-#     tt = np.arange(0, length / sampling_rate, 1. / sampling_rate)
-    traceR = fft.freq2time(eR)
-    traceTheta = fft.freq2time(eTheta)
-    tracePhi = fft.freq2time(ePhi)
+    traceR = np.fft.irfft(eR) / dt
+    traceTheta = np.fft.irfft(eTheta) / dt
+    tracePhi = np.fft.irfft(ePhi) / dt
     return np.array([traceR, traceTheta, tracePhi])
+
+
+def get_frequency_spectrum(energy, theta, N, dt, is_em_shower, n, R, LPM=True, a=None):
+    eR, eTheta, ePhi = get_time_trace(energy, theta, N, dt, is_em_shower, n, R, LPM, a)
+    return np.array([fft.time2freq(eR), fft.time2freq(eTheta), fft.time2freq(ePhi)])