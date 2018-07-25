fb11c6b6599859cefd8047424b9a1d505e19d847
Anna Nelles <anna.nelles@gmail.com>
2018-07-25 13:26:06 +0200

    Add documentation.

--- NuRadioReco/modules/channelGenericNoiseAdder.py
+++ NuRadioReco/modules/channelGenericNoiseAdder.py
@@ -6,12 +6,20 @@ logger = logging.getLogger('channelGenericNoiseAdder')
 
 class channelGenericNoiseAdder:
     """
-    Module that generates noise in some generic fashion (not based on measured data), which can be added to data. 
+    Module that generates noise in some generic fashion (not based on measured data), which can be added to data.
+
+
     """
-    
+
     def fftnoise(self,f):
         """
-        Adding random phase information to given amplitude spectrum. 
+        Adding random phase information to given amplitude spectrum.
+
+        Parameters
+        ---------
+
+        f: array of floats
+            Data that random phase is added to.
         """
         f = np.array(f, dtype='complex')
         Np = (len(f) - 1) // 2
@@ -20,70 +28,111 @@ class channelGenericNoiseAdder:
         f[1:Np+1] *= phases
         f[-1:-1-Np:-1] = np.conj(f[1:Np+1])
         return np.fft.ifft(f).real
-    
+
     def bandlimited_noise(self,min_freq, max_freq, n_samples, sampling_rate,amplitude, type='perfect_white'):
         """
-        Generating noise of n_samples in a bandwidth [min_freq,max_freq]. 
-        
-        type = perfect_white: flat frequency spectrum
-        type = white: flat frequency spectrum with random jitter
-        
-        amplitude = desired voltage of noise in spectrum 
+        Generating noise of n_samples in a bandwidth [min_freq,max_freq].
+
+        Parameters
+        ---------
+
+        min_freq: float
+            Minimum frequency of passband for noise generation
+        max_freq: float
+            Maximum frequency of passband for noise generation
+        n_samples: int
+            how many samples of noise should be generated
+        sampling_rate: float
+            desired sampling rate of data
+        amplitude: float
+            desired voltage of noise as V_rms (only roughly, since bandpass limited)
+        type: string
+            perfect_white: flat frequency spectrum
+            white: flat frequency spectrum with random jitter
+
         """
         frequencies = np.abs(np.fft.fftfreq(n_samples, 1/sampling_rate))
         f = np.zeros(n_samples)
         selection = np.where((frequencies>=min_freq)& (frequencies<=max_freq))
         if type == 'perfect_white':
-            f[selection] = amplitude
+            f[selection] = amplitude*np.sqrt(2.*n_samples*2)
         elif type == 'white':
-            jitter = amplitude + np.random.rand(n_samples)*0.05*amplitude 
+            jitter = np.random.rand(n_samples)*0.05*amplitude + amplitude*np.sqrt(2.*n_samples*2)
             f[selection] = jitter[selection]
         else:
             logger.error("Other types of noise not yet implemented.")
-            
+
         return self.fftnoise(f)
-    
+
     def __init__(self):
         pass
-        
+
     def begin(self, debug=False):
         self.__debug = debug
-    
-    def run(self, event, station, detector, 
+
+    def run(self, event, station, detector,
                             amplitude=1*units.mV,
                             min_freq=50*units.MHz,
                             max_freq=2000*units.MHz,
                             type='white'):
-        
+
+        """
+        Add noise to given event.
+
+        Parameters
+        ---------
+
+        event
+
+        station
+
+        detector
+
+        amplitude: float
+            desired voltage of noise as V_rms (only roughly, since bandpass limited)
+        min_freq: float
+            Minimum frequency of passband for noise generation
+        max_freq: float
+            Maximum frequency of passband for noise generation
+        type: string
+            perfect_white: flat frequency spectrum
+            white: flat frequency spectrum with random jitter
+
+        """
+
+
         channels = station.get_channels()
         for channel in channels:
             trace = channel.get_trace()
             sampling_rate = channel.get_sampling_rate()
-            
+
             noise = self.bandlimited_noise(min_freq=min_freq,
                                           max_freq=max_freq,
                                           n_samples=trace.shape[0],
                                           sampling_rate=sampling_rate,
                                           amplitude=amplitude,
                                           type=type)
-            
+
             if self.__debug:
                 new_trace = trace + noise
-            
+
+                print "imput amplitude", amplitude
+                print "voltage RMS", np.sqrt(np.mean(noise**2))
+                #print "test", amplitude/np.sqrt(2*trace.shape[0])
+
                 import matplotlib.pyplot as plt
                 plt.plot(trace)
                 plt.plot(noise)
                 plt.plot(new_trace)
-            
-            
+
                 plt.figure()
                 plt.plot(np.abs(np.fft.rfft(trace)))
                 plt.plot(np.abs(np.fft.rfft(noise)))
                 plt.plot(np.abs(np.fft.rfft(new_trace)))
 
                 plt.show()
-            
+
             trace += noise
-        
+
     def end(self):
         pass
