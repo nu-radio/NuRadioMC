2024a130dd8e05f553f9f11512ce7882bc029f15
cg-laser <christian.glaser@uci.edu>
2018-07-29 01:14:27 +0000

    add plotting scripts to visualize simulation in and output

---
+++ simulation/T04visualize_sim_input.py
@@ -0,0 +1,46 @@
+from __future__ import absolute_import, division, print_function
+import numpy as np
+from radiotools import helper as hp
+from radiotools import plthelpers as php
+from matplotlib import pyplot as plt
+from NuRadioMC.utilities import units
+import h5py
+import argparse
+import json
+import time
+import os
+
+parser = argparse.ArgumentParser(description='Parse ARA event list.')
+parser.add_argument('inputfilename', type=str,
+                    help='path to NuRadioMC hdf5 simulation input')
+# parser.add_argument('outputfilename', type=str,
+#                     help='name of output file storing the electric field traces at detector positions')
+args = parser.parse_args()
+
+fin = h5py.File(args.inputfilename, 'r')
+
+# plot vertex distribution
+fig, ax = plt.subplots(1, 1)
+xx = np.array(fin['xx'])
+yy = np.array(fin['yy'])
+rr = (xx ** 2 + yy ** 2) ** 0.5
+zz = np.array(fin['zz'])
+h = ax.hist2d(rr / units.m, zz / units.m, bins=[np.arange(0, 4000, 100), np.arange(-3000, 0, 100)],
+              cmap=plt.get_cmap('Blues'))
+cb = plt.colorbar(h[3], ax=ax)
+cb.set_label("number of events")
+ax.set_aspect('equal')
+ax.set_xlabel("r [m]")
+ax.set_ylabel("z [m]")
+fig.tight_layout()
+
+# plot incoming direction
+zeniths = np.array(fin['zeniths'])
+azimuths = np.array(fin['azimuths'])
+fig, axs = php.get_histograms([zeniths / units.deg, azimuths / units.deg],
+                              bins=[np.arange(0, 181, 10), np.arange(0, 361, 45)],
+                              xlabels=['zenith [deg]', 'azimuth [deg]'],
+                              stats=False)
+fig.suptitle('neutrino direction')
+
+plt.show()
