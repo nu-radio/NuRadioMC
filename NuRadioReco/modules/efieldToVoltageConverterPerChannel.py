2d2eea9c407b9d4396c0d4dcce02b7279fd382bd
cg-laser <christian.glaser@uci.edu>
2018-08-08 21:53:04 +0000

    catch channels with no ray tracing solution, this commit assures that the trace does not become insanely long

--- NuRadioReco/modules/efieldToVoltageConverterPerChannel.py
+++ NuRadioReco/modules/efieldToVoltageConverterPerChannel.py
@@ -90,8 +90,9 @@ class efieldToVoltageConverterPerChannel:
             for sim_channel2 in sim_channel:
                 cab_delay = det.get_cable_delay(sim_station_id, sim_channel2.get_id())
                 t0 = sim_channel2.get_trace_start_time() + cab_delay
-                times_min.append(t0)
-                times_max.append(t0 + sim_channel2.get_number_of_samples() / sim_channel2.get_sampling_rate())
+                if(not np.isnan(t0)):  # trace start time is None if no ray tracing solution was found and channel contains only zeros
+                    times_min.append(t0)
+                    times_max.append(t0 + sim_channel2.get_number_of_samples() / sim_channel2.get_sampling_rate())
         times_min = np.array(times_min) - self.__pre_pulse_time
         times_max = np.array(times_max) + self.__post_pulse_time
         trace_length = times_max.max() - times_min.min()
@@ -137,9 +138,10 @@ class efieldToVoltageConverterPerChannel:
 
                 new_trace = np.zeros((3, trace_length_samples))
                 # calculate the start bin
-                start_bin = int(round((sim_channel2.get_trace_start_time() - times_min.min()) / self.__time_resolution))
-                logger.debug('channel {}, start time {:.1f} = bin {:d}, ray solution {}'.format(channel_id, sim_channel2.get_trace_start_time(), start_bin, sim_channel2['raypath']))
-                new_trace[:, start_bin:(start_bin + len(trace))] = resampled_efield
+                if(not np.isnan(sim_channel2.get_trace_start_time())):
+                    start_bin = int(round((sim_channel2.get_trace_start_time() - times_min.min()) / self.__time_resolution))
+                    logger.debug('channel {}, start time {:.1f} = bin {:d}, ray solution {}'.format(channel_id, sim_channel2.get_trace_start_time(), start_bin, sim_channel2['raypath']))
+                    new_trace[:, start_bin:(start_bin + len(trace))] = resampled_efield
                 trace_object = NuRadioReco.framework.base_trace.BaseTrace()
                 trace_object.set_trace(new_trace, 1. / self.__time_resolution)
                 trace_object.set_trace_start_time(times_min.min())
