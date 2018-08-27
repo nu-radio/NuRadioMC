3d310d00250f2e8f3af86aae41b572ca584db4b8
cg-laser <jcglaser@gmail.com>
2018-08-27 23:40:01 +0000

    improve and simplify serialization

--- NuRadioReco/framework/trigger.py
+++ NuRadioReco/framework/trigger.py
@@ -4,8 +4,8 @@ import cPickle as pickle
 def deserialize(triggers_pkl):
     triggers = {}
     for data_pkl in triggers_pkl:
-        data = pickle.loads(data_pkl)
-        trigger_type = data['_trigger_type']
+        trigger_type = pickle.loads(data_pkl)['_trigger_type']
+        print(trigger_type)
         if(trigger_type == 'default'):
             trigger = Trigger(None)
             trigger.deserialize(data_pkl)
@@ -79,21 +79,23 @@ class Trigger:
         return self._type
     
     def serialize(self):
-        data = {'_name': self._name,
-                '_channels': self._channels,
-                '_triggered': self._triggered,
-                '_trigger_time': self._trigger_time,
-                '_trigger_type': self._trigger_type}
-        return pickle.dumps(data, protocol=2)
-
+        return pickle.dumps(self.__dict__, protocol=2)
+    
     def deserialize(self, data_pkl):
-        data = pickle.loads(data_pkl)
-        self._name = data['_name']
-        self._channels = data['_channels']
-        self._triggered = data['_triggered']
-        self._trigger_time = data['_trigger_time']
-        self._trigger_type = data['_trigger_type']
+        for key, value in pickle.loads(data_pkl).iteritems():
+            setattr(self, key, value)
 
+    def __str__(self):
+        output = ""
+        for key, value in self.__dict__.iteritems():
+            output +="{}: {}\n".format(key[1:], value)
+        return output
+    
+    def get_trigger_settings(self):
+        output = {}
+        for key, value in self.__dict__.iteritems():
+            output[key[1:]] = value
+        return output
 
 
 class SimpleThresholdTrigger(Trigger):
@@ -118,20 +120,6 @@ class SimpleThresholdTrigger(Trigger):
         Trigger.__init__(self, name, channels, 'simple_threshold')
         self._threshold = threshold
         self._number_of_coincidences = number_of_coincidences
-        
-    def serialize(self):
-        base_trigger_pkl = Trigger.serialize(self)
-
-        data = {'_threshold': self._threshold,
-                '_number_of_coincidences': self._number_of_coincidences,
-                'base_trigger': base_trigger_pkl}
-        return pickle.dumps(data, protocol=2)
-
-    def deserialize(self, data_pkl):
-        data = pickle.loads(data_pkl)
-        Trigger.deserialize(self, data['base_trigger'])
-        self._threshold = data['_threshold']
-        self._number_of_coincidences = data['_number_of_coincidences']
 
 
 class HighLowTrigger(Trigger):
@@ -160,47 +148,12 @@ class HighLowTrigger(Trigger):
             the number of channels that need to fulfill the trigger condition
             default: 1
         """
-        Trigger.__init__(self, name, channels, 'simple_threshold')
+        Trigger.__init__(self, name, channels, 'high_low')
         self._number_of_coincidences = number_of_coincidences
         self._threshold_high = threshold_high
         self._threshold_low = threshold_low
         self._high_low_window = high_low_window
         self._coinc_window = channel_coincidence_window
 
-    def set_trigger_settings(self, threshold_high, threshold_low,
-                             high_low_window, coinc_window, number_concidences):
-        self._threshold_high = threshold_high
-        self._threshold_low = threshold_low
-        self._high_low_window = high_low_window
-        self._coinc_window = coinc_window
-        self._number_concidences = number_concidences
-
-    def get_trigger_settings(self):
-        return {'threshold_high': self._threshold_high,
-                'threshold_low': self._threshold_low,
-                'high_low_window': self._high_low_window,
-                'coinc_window': self._coinc_window,
-                'number_concidences': self._number_concidences}
-        
-        
-    def serialize(self):
-        base_trigger_pkl = Trigger.serialize(self)
-
-        data = {'_number_of_coincidences': self._number_of_coincidences,
-                '_threshold_high': self._threshold_high,
-                '_threshold_low': self._threshold_low,
-                '_high_low_window': self._high_low_window,
-                '_coinc_window': self._coinc_window,
-                'base_trigger': base_trigger_pkl}
-        return pickle.dumps(data, protocol=2)
-
-    def deserialize(self, data_pkl):
-        data = pickle.loads(data_pkl)
-        Trigger.deserialize(self, data['base_trigger'])
-        self._number_of_coincidences = data['_number_of_coincidences']
-        self._threshold_high = data['_threshold_high']
-        self._threshold_low = data['_threshold_low']
-        self._high_low_window = data['_high_low_window']
-        self._coinc_window = data['_coinc_window']
 
 
