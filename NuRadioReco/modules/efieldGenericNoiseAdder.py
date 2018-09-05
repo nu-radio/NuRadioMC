939496ee0ed08e120c355bf001adca9a24ffbf22
Ida Storehaug <idastorehaug@s92dyn218.zeuthen.desy.de>
2018-09-05 15:44:37 +0200

    Adding comments and general cleanup

--- NuRadioReco/modules/efieldGenericNoiseAdder.py
+++ NuRadioReco/modules/efieldGenericNoiseAdder.py
@@ -9,7 +9,9 @@ import scipy as sp
 
 class efieldGenericNoiseAdder:
     """
-    Module that adds noise to the electric field -- such as Galactic emission.
+    Module that adds noise to the electric field -- such as Galactic emission and background from human communication.
+    
+    
     """
 
     def __init__(self):
@@ -19,21 +21,34 @@ class efieldGenericNoiseAdder:
         self.__debug = debug
 
     def run(self, evt, station, det, type, narrowband_freq, narrowband_power, passband):
+        """
+        
+        Parameters
+        ----------
+        event
+        
+        station
+        
+        detector
+        
+        type: string
+            narrowband: single frequency background with FM
+            galactic: galactic emission calculated from temperature plots
+        narrowband_freq: list
+            mean frequency of one or more narrowband transmitters
+        narrowband_power: list
+            must be same size as narrowband_freq list. Each element in the list should be a list consisting of 3 elements: the power of the signal in x,y,z polarization.
+        passband: list
+            galactic background will not be simulated below the lower cutoff
+        """
 
         # access simulated efield and high level parameters
         sim_station = station.get_sim_station()
-        sim_station_id = sim_station.get_id()
-        azimuth = sim_station['azimuth']
-        zenith = sim_station['zenith']
-        event_time = sim_station.get_station_time()
 
-        nChannels = det.get_number_of_channels(sim_station_id)
         sampling_rate = sim_station.get_sampling_rate()
 
-        ff = sim_station.get_frequencies()
-
         efield_fft = copy.copy(sim_station.get_frequency_spectrum())
-        efield = copy.copy(sim_station.get_trace()) # trace of the efield
+        efield = copy.copy(sim_station.get_trace())
         n_samples = efield.shape[1]
 
         if type == 'narrowband':
@@ -45,43 +60,42 @@ class efieldGenericNoiseAdder:
                     pol_noise = pol_power * np.sin(norm_freq * 2 * np.pi * t)
                     efield[i] = efield[i] + pol_noise
 
-            station.set_trace(efield, sim_station.get_sampling_rate())
-            sim_station.set_trace(efield, sim_station.get_sampling_rate()) # To be removed once genericnoiseAdder module is corrected to accont for already configured station
+            logger.warning('You are now changing dataelements in sim. station')
             
-        if type == 'galactic':
+            station.set_trace(efield, sim_station.get_sampling_rate())
+            sim_station.set_trace(efield, sim_station.get_sampling_rate())
+            
+        elif type == 'galactic':
 
-            freq_range_raw = sim_station.get_frequencies() # GHz
+            freq_range_raw = sim_station.get_frequencies()
             lower_lim = freq_range_raw > passband[0]
             freq_range = freq_range_raw[lower_lim]
             noise_idx = int(efield_fft.shape[1]) - int(freq_range.shape[0])
             
-            def GalTemp(x,n=-2.41,k=10**7.88): # expression found by fitting to fig. 3.2 in LOFAR
+            # Power law found by fit to Fig 3.2. "An absolute calibration of the antennas at LOFAR", Tijs Karskens (2015)
+            def GalTemp(x,n=-2.41,k=10**7.88):
                 return k * x**n
             
-            B = np.zeros(efield_fft.shape[1])
-            B[noise_idx:] = 4*np.pi * ((2*sp.constants.Boltzmann)/(sp.constants.speed_of_light**2)) * (freq_range/units.MHz)**2 * GalTemp(freq_range/units.MHz) * 50*units.ohm # unit W m^-2 Hz^-1
-            # GalTemp term gives correct output - checked via plot
-            Bnew = B*sampling_rate
-            for i,x in enumerate(Bnew):
-                if x != 0.: Bnew[i] = np.sqrt(x)
-            
+            S = np.zeros(efield_fft.shape[1])
+            S[noise_idx:] = 4*np.pi * ((2*sp.constants.Boltzmann)/(sp.constants.speed_of_light**2)) * (freq_range/units.MHz)**2 * GalTemp(freq_range/units.MHz) * 50*units.ohm
 
-            logger.warning('Something is wrong with the normalization')
-            const = 10**20
-            #B = B*const
+            S = S*sampling_rate
+            for i,x in enumerate(S):
+                if x != 0.: S[i] = np.sqrt(x)
             
-            # From Timo: Power density spectrum must be converter to fourier transform
-            #fft_amp = (B * (n_samples - 1)) / (2 * sampling_rate)
-            #for i,x in enumerate(fft_amp):
-            #    if x != 0.: fft_amp[i] = np.sqrt(x)
             angle = np.random.uniform(0, 2*np.pi)
-            gal_noise = Bnew * np.exp(angle * 1j)
+            gal_noise = S * np.exp(angle * 1j)
             
-            for i in range(3): # The polarization of the galactic emission is the same in all directions
+            for i in range(3): # The power of the galactic background is assumed to be the same in all polarizations
                 efield_fft[i] = efield_fft[i] + gal_noise
+            
+            logger.warning('You are now changing dataelements in sim. station')
                 
             station.set_frequency_spectrum(efield_fft, sampling_rate)
-            sim_station.set_frequency_spectrum(efield_fft, sampling_rate) # To be removed once genericnoiseAdder module is corrected to account for already configured station
+            sim_station.set_frequency_spectrum(efield_fft, sampling_rate) # To be removed once genericnoiseAdder module is corrected to account for already configured stations
+
+        else:
+            logger.error("Other types of noise not yet implemented.")
             
     def end():
         pass
