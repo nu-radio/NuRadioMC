2f6bc58c430029be6fe327e51d6cf4c989af5221
cg-laser <jcglaser@gmail.com>
2018-09-13 16:29:05 +0000

    add steering files and detector calibration for deltaT analysis

---
+++ studies/deltaT/E01detector_simulation.py
@@ -0,0 +1,61 @@
+from __future__ import absolute_import, division, print_function
+import argparse
+# import detector simulation modules
+import NuRadioReco.modules.efieldToVoltageConverterPerChannel
+import NuRadioReco.modules.ARIANNA.triggerSimulator
+import NuRadioReco.modules.triggerSimulator
+import NuRadioReco.modules.channelResampler
+import NuRadioReco.modules.channelBandPassFilter
+import NuRadioReco.modules.custom.deltaT.calculateAmplitudePerRaySolution
+from NuRadioReco.utilities import units
+from NuRadioMC.studies.deltaT import simulation
+import logging
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger("runstrawman")
+
+# initialize detector sim modules
+efieldToVoltageConverterPerChannel = NuRadioReco.modules.efieldToVoltageConverterPerChannel.efieldToVoltageConverterPerChannel()
+efieldToVoltageConverterPerChannel.begin(debug=False, time_resolution=1 * units.ns,
+                                         pre_pulse_time=0 * units.ns, post_pulse_time=0 * units.ns)
+calculateAmplitudePerRaySolution = NuRadioReco.modules.custom.deltaT.calculateAmplitudePerRaySolution.calculateAmplitudePerRaySolution()
+triggerSimulator = NuRadioReco.modules.triggerSimulator.triggerSimulator()
+triggerSimulatorARIANNA = NuRadioReco.modules.ARIANNA.triggerSimulator.triggerSimulator()
+channelResampler = NuRadioReco.modules.channelResampler.channelResampler()
+channelBandPassFilter = NuRadioReco.modules.channelBandPassFilter.channelBandPassFilter()
+
+
+def detector_simulation(evt, station, det, dt, Vrms):
+    
+    calculateAmplitudePerRaySolution.run(evt, station, det)
+    # start detector simulation
+    efieldToVoltageConverterPerChannel.run(evt, station, det)  # convolve efield with antenna pattern
+    # downsample trace back to detector sampling rate
+    channelResampler.run(evt, station, det, sampling_rate=1. / dt)
+    # bandpass filter trace, the upper bound is higher then the sampling rate which makes it just a highpass filter
+    channelBandPassFilter.run(evt, station, det, passband=[80 * units.MHz, 500 * units.MHz],
+                              filter_type='butter10')
+    triggerSimulator.run(evt, station, det,
+                         threshold=2 * Vrms,
+                         triggered_channels=None,
+                         number_concidences=1,
+                         trigger_name='pre_trigger_2sigma')
+
+
+parser = argparse.ArgumentParser(description='Run NuRadioMC simulation')
+parser.add_argument('inputfilename', type=str,
+                    help='path to NuRadioMC input event list')
+parser.add_argument('detectordescription', type=str,
+                    help='path to file containing the detector description')
+parser.add_argument('outputfilename', type=str,
+                    help='hdf5 output filename')
+parser.add_argument('outputfilenameNuRadioReco', type=str, nargs='?', default=None,
+                    help='outputfilename of NuRadioReco detector sim file')
+args = parser.parse_args()
+
+sim = simulation.simulation(eventlist=args.inputfilename,
+                            outputfilename=args.outputfilename,
+                            detectorfile=args.detectordescription,
+                            station_id=101,
+                            Tnoise=350.,
+                            outputfilenameNuRadioReco=args.outputfilenameNuRadioReco)
+sim.run(detector_simulation=detector_simulation, number_of_triggers=1)
