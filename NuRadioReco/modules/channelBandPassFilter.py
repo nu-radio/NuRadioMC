74fc50c749ca65a4f9005cbf4df573ba2dca1788
cg-laser <jcglaser@gmail.com>
2018-09-24 17:11:35 +0000

    add order of filter as argument to the run method. We only implement a bandpass filter, but a low or high gain filter can be obtained by setting the lower frequency to 0 (or the upper frequency to a very large number)

--- NuRadioReco/modules/channelBandPassFilter.py
+++ NuRadioReco/modules/channelBandPassFilter.py
@@ -12,7 +12,7 @@ class channelBandPassFilter:
         pass
 
     def run(self, evt, station, det, passband=[55 * units.MHz, 1000 * units.MHz],
-            filter_type='rectangular'):
+            filter_type='rectangular', order=2):
         """
         Run the filter
 
@@ -27,6 +27,8 @@ class channelBandPassFilter:
             'rectangular': perfect straight line filter
             'butter10': butterworth filter from scipy
             'butter10abs': absolute of butterworth filter from scipy
+        order: int (optional, default 2)
+            for a butterworth wilter: specifies the order of the filter
 
         """
         channels = station.get_channels()
@@ -37,12 +39,12 @@ class channelBandPassFilter:
             if(filter_type == 'rectangular'):
                 trace_fft[np.where(frequencies < passband[0])] = 0.
                 trace_fft[np.where(frequencies > passband[1])] = 0.
-            elif(filter_type == 'butter10'):
-                b, a = scipy.signal.butter(10, passband, 'bandpass', analog=True)
+            elif(filter_type == 'butter'):
+                b, a = scipy.signal.butter(order, passband, 'bandpass', analog=True)
                 w, h = scipy.signal.freqs(b, a, frequencies)
                 trace_fft *= h
-            elif(filter_type == 'butter10abs'):
-                b, a = scipy.signal.butter(10, passband, 'bandpass', analog=True)
+            elif(filter_type == 'butterabs'):
+                b, a = scipy.signal.butter(order, passband, 'bandpass', analog=True)
                 w, h = scipy.signal.freqs(b, a, frequencies)
                 trace_fft *= np.abs(h)
             channel.set_frequency_spectrum(trace_fft, channel.get_sampling_rate())
