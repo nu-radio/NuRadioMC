378dce381fc0974324e6546266cdeb28cf17a0a4
cg-laser <jcglaser@gmail.com>
2018-10-09 22:47:48 +0000

    remove outdated examples

--- NuRadioMC/examples/dummySignalGenProp.sh
+++
@@ -1,1 +0,0 @@
-python2 ../SignalGenProp/DummySignalGenProp.py ../data/dummyEventList.hdf5 ../data/dummyStationList.json ../data/dummyStationEfields.hdf5
--- NuRadioMC/examples/readARAEventList.sh
+++
@@ -1,1 +0,0 @@
-python2 ../EvtGen/readARAEventList.py ../data/ARASimDummyEventList.txt ../data/dummyEventList.hdf5
