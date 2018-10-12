ea9e64fc124915284b53f6e9fdc6078d346e387e
cg-laser <jcglaser@gmail.com>
2018-10-12 00:56:07 +0000

    change fft normalization

--- NuRadioMC/utilities/fft.py
+++ NuRadioMC/utilities/fft.py
@@ -1,12 +1,12 @@
 import numpy as np
 
 """
-the fft utility module implements a real fft with a normalization so that
-Plancherel theorem is valid. This means that the fourier transform must be unitary,
-which is achieved by normalizing both transforms with 1/sqrt(n) and an additional
-factor of sqrt(2) because negative frequencies are omitted in the real FFT.
-This means that the power calculated in the time domain is the same as the
-power calculated in the frequency domain.
+A wrapper around the numpy fft routines to achive a coherent normalization of the fft
+As we have real valued data in the time domain, we use the 'real ffts' that omit the negative frequencies in Fourier 
+space. To account for the missing power in the frequency domain, we multiply the frequency spectrum by sqrt(2), 
+and divide the iFFT with 1/sqrt(2) accordingly. Then, a calculation of the power leads the same result in 
+the time and frequency domain, i.e.
+np.sum(trace**2) * dt = np.sum(spectrum**2) * df
 """
 
 
@@ -14,11 +14,11 @@ def time2freq(trace):
     """
     performs forward FFT with correct normalization that conserves the power
     """
-    return np.fft.rfft(trace, axis=-1, norm="ortho") * 2 ** 0.5  # an additional sqrt(2) is added because negative frequencies are omitted.
+    return np.fft.rfft(trace, axis=-1) * 2 ** 0.5  # an additional sqrt(2) is added because negative frequencies are omitted.
 
 
 def freq2time(spectrum):
     """
     performs backward FFT with correct normalization that conserves the power
     """
-    return np.fft.irfft(spectrum, axis=-1, norm="ortho") / 2 ** 0.5
+    return np.fft.irfft(spectrum, axis=-1) / 2 ** 0.5
