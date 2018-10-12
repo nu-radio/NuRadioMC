234e2b2272e37edf33812b29a926e7aef8385b7e
cg-laser <jcglaser@gmail.com>
2018-10-12 21:08:26 +0000

    add interface to custom filters

--- NuRadioReco/modules/channelBandPassFilter.py
+++ NuRadioReco/modules/channelBandPassFilter.py
@@ -1,6 +1,7 @@
 import numpy as np
 from NuRadioReco.utilities import units
 import scipy.signal
+from NuRadioReco.detector import filterresponse
 
 
 class channelBandPassFilter:
@@ -27,6 +28,8 @@ class channelBandPassFilter:
             'rectangular': perfect straight line filter
             'butter10': butterworth filter from scipy
             'butter10abs': absolute of butterworth filter from scipy
+            or any filter that is implemented in NuRadioReco.detector.filterresponse. In this case the
+            passband parameter is ignored
         order: int (optional, default 2)
             for a butterworth wilter: specifies the order of the filter
 
@@ -48,7 +51,11 @@ class channelBandPassFilter:
                 mask = frequencies > 0
                 b, a = scipy.signal.butter(order, passband, 'bandpass', analog=True)
                 w, h = scipy.signal.freqs(b, a, frequencies[mask])
-                trace_fft[mask] *= h
+                trace_fft[mask] *= np.abs(h)
+            else:
+                mask = frequencies > 0
+                filt = filterresponse.get_filter_response(frequencies[mask], filter_type)
+                trace_fft[mask] *= filt
             channel.set_frequency_spectrum(trace_fft, channel.get_sampling_rate())
 
     def end(self):
