3037dc0ec6843947376dc4d2875a19900ba17b84
cg-laser <jcglaser@gmail.com>
2018-10-13 00:57:23 +0000

    add 4 sigma curves

--- NuRadioMC/studies/deltaT/A01analyse_simulation.py
+++ NuRadioMC/studies/deltaT/A01analyse_simulation.py
@@ -29,6 +29,7 @@ print("simulated depths ", depths)
 
 eff1 = np.zeros_like(depths)
 eff2 = np.zeros_like(depths)
+eff3 = np.zeros_like(depths)
 
 for iD, depth in enumerate(depths):
     As = np.array(fin['max_amp_ray_solution'][:,iD])
@@ -40,15 +41,18 @@ for iD, depth in enumerate(depths):
     
     C1 = ((As[:, 0] >= 3 * Vrms) | (As[:, 1] >= 3 * Vrms)) & ((As[:, 0] >= 2 * Vrms) & (As[:, 1] >= 2 * Vrms)) & (dTs < 430 * units.ns)
     C2 = ((As[:, 0] >= 3 * Vrms) & (As[:, 1] >= 3 * Vrms)) & (dTs < 430 * units.ns)
+    C4 = ((As[:, 0] >= 4 * Vrms) & (As[:, 1] >= 4 * Vrms)) & (dTs < 430 * units.ns)
     
     eff1[iD] = np.sum(weights[~mask][C1])/ np.sum(weights[~mask][C3])
     eff2[iD] = np.sum(weights[~mask][C2])/ np.sum(weights[~mask][C3])
+    eff3[iD] = np.sum(weights[~mask][C4])/ np.sum(weights[~mask][C3])
     
      
 fig, (ax) = plt.subplots(1, 1)
 sort_mask = np.argsort(depths)
 ax.plot(depths[sort_mask], eff1[sort_mask], 'o-', label='one pulse > 3 sigma, one pulse > 2 sigma')
 ax.plot(depths[sort_mask], eff2[sort_mask], "d--", label="both pulses > 3 sigma")
+ax.plot(depths[sort_mask], eff3[sort_mask], "^:", label="both pulses > 4 sigma")
 ax.set_xlim(0, -100)
 ax.set_ylim(0, 1)
 ax.set_xlabel("depth [m]")
