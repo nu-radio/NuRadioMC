b0c0d862dc620128b473d7a8f78fca32b6d92ec0
christoph <christoph.welling@desy.de>
2018-10-16 15:18:35 +0200

    Prevents crash if get_times is executed on a trace with unset _time_trace. Instead an empty array is returned

--- NuRadioReco/framework/base_trace.py
+++ NuRadioReco/framework/base_trace.py
@@ -74,6 +74,8 @@ class BaseTrace:
         return self._sampling_rate
 
     def get_times(self):
+        if self._time_trace is None:
+            return []
         length = self.get_number_of_samples()
         times = np.arange(0, length / self._sampling_rate - 0.1/self._sampling_rate, 1. / self._sampling_rate) + self._trace_start_time
         if(len(times) != length):
