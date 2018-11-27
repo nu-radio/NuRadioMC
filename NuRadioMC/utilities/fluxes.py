477c6e80cffdd6f1d6b1a17526d19a0ec38ce6b6
cg-laser <jcglaser@gmail.com>
2018-11-27 00:40:09 +0000

    slight modularization to get ready for E1 flux limit, prototyp function for integrated limit (not yet working correctly)

--- NuRadioMC/utilities/fluxes.py
+++ NuRadioMC/utilities/fluxes.py
@@ -60,12 +60,42 @@ def get_interaction_length(energy, cross_section_type = 'ctw'):
     L = 1./(N_A * ice_density * get_nu_cross_section(energy, cross_section_type=cross_section_type))
     return L
 
-
 def get_limit_e2_flux(energy, veff,
                     livetime,
                     signalEff = 1.00,
                     energyBinsPerDecade=1.000,
                     upperLimOnEvents=2.44,
+                    nuCrsScn='ctw'):
+    """
+    Limit from effective volume on E^2 flux plot
+
+    Parameters:
+        --------------
+    energy: array of floats
+        neutrino energy
+    veff: array of floats
+        effective volumes
+    livetime: float
+        time used
+    signalEff: float
+        efficiency of signal reconstruction
+    energyBinsPerDecade: float
+        1 for decade bins, 2 for half-decade bins, etc.
+    upperLimOnEvents: float
+         2.3 for Neyman UL w/ 0 background,
+         2.44 for F-C UL w/ 0 background, etc
+    nuCrsScn: str
+        type of neutrino cross-section
+
+
+    """
+    return energy**2 * get_limit_flux(energy, veff, livetime, signalEff, energyBinsPerDecade, upperLimOnEvents,nuCrsScn)
+
+def get_limit_flux(energy, veff,
+                    livetime,
+                    signalEff = 1.00,
+                    energyBinsPerDecade=1.000,
+                    upperLimOnEvents=2.44, 
                     nuCrsScn='ctw'):
 
     """
@@ -98,9 +128,49 @@ def get_limit_e2_flux(energy, veff,
 
     ul  = upperLimOnEvents / evtsPerFluxPerEnergy
     ul *= energyBinsPerDecade / np.log(10)
-    ul *= energy
+    ul /= energy
 
     return ul
+
+# def get_integrated_limit_flux(energy, veff,
+#                               livetime,
+#                               signalEff = 1.00,
+#                               upperLimOnEvents=2.44, 
+#                               nuCrsScn='ctw'):
+# 
+#     """
+#     Limit from effective volume on E^2 flux plot
+# 
+#     Parameters:
+#         --------------
+#     energy: array of floats
+#         energies (the bin centers), the binning in log10(E) must be equidistant!
+#     veff: array of floats
+#         effective volumes
+#     livetime: float
+#         time used
+#     signalEff: float
+#         efficiency of signal reconstruction
+#     energyBinsPerDecade: float
+#         1 for decade bins, 2 for half-decade bins, etc.
+#     upperLimOnEvents: float
+#          2.3 for Neyman UL w/ 0 background,
+#          2.44 for F-C UL w/ 0 background, etc
+#     nuCrsScn: str
+#         type of neutrino cross-section
+# 
+# 
+#     """
+#     energy = np.array(energy)
+#     veff = np.array(veff)
+#     logE = np.log10(energy)
+#     dlogE = logE[1] - logE[0]
+# 
+#     L = get_interaction_length(energy, cross_section_type = nuCrsScn)
+# #     N = np.sum(veff/L/E) * np.log(10) * livetime * signalEff * dlogE
+#     ul = upperLimOnEvents * dlogE/ (livetime * signalEff * np.log(10)) * np.sum(L/veff * energy) / len(energy)
+# 
+#     return ul
 
 def get_limit_e1_flux(energy, veff,
                     livetime,
