7e029086f8ac5e68d83e57d1f29be4b0312e727f
Christian Glaser <christian.glaser@uci.edu>
2018-12-06 21:26:17 +0000

    add test script of sql detector description

---
+++ NuRadioReco/detector/test_detector_db.py
@@ -0,0 +1,30 @@
+import detector
+import argparse
+import datetime
+import logging
+
+logging.basicConfig(level=logging.INFO)
+
+
+det = detector.Detector(source='sql')
+# print(det.get_antenna_deployment_time(52, 1))
+
+
+parser = argparse.ArgumentParser(description='test DB detector description')
+parser.add_argument('station_number', type=int,
+                    help='the station number')
+parser.add_argument("--time", dest='timestamp', type=str, default=None,
+                    help='the time to evaluate the DB')
+args = parser.parse_args()
+
+det = detector.Detector()
+if(args.timestamp is not None):
+    print("update detector to time {}".format(args.timestamp))
+    print(datetime.datetime.strptime(args.timestamp, "%Y-%m-%d"))
+    det.update(datetime.datetime.strptime(args.timestamp, "%Y-%m-%d"))
+
+# result = det.get_everything(args.station_number)
+result = det.get_relative_positions(args.station_number)
+print(result)
+
+
