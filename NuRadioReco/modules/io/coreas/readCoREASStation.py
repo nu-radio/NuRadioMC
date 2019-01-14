f6db4edb6d5457717b616cec7275f5e9b9e11447
christoph <christoph.welling@desy.de>
2019-01-14 14:25:31 +0100

    Adjust readCoREASStation to new event structure

--- NuRadioReco/modules/io/coreas/readCoREASStation.py
+++ NuRadioReco/modules/io/coreas/readCoREASStation.py
@@ -6,7 +6,7 @@ from NuRadioReco.modules.io.coreas import coreas
 
 class readCoREASStation:
 
-    def begin(self, input_files, station_id, detector):
+    def begin(self, input_files, station_id):
         """
         begin method
 
@@ -21,11 +21,9 @@ class readCoREASStation:
         """
         self.__input_files = input_files
         self.__station_id = station_id
-        self.__current_input_file = 0
-        self.__n_channels = detector.get_number_of_channels(station_id)
-        
+        self.__current_input_file = 0        
 
-    def run(self):
+    def run(self, detector):
         for input_file in self.__input_files:
 
             # read in coreas simulation and copy to event
@@ -35,7 +33,7 @@ class readCoREASStation:
             for i, (name, observer) in enumerate(corsika['CoREAS']['observers'].items()):
                 evt = NuRadioReco.framework.event.Event(corsika['inputs'].attrs['RUNNR'], corsika['inputs'].attrs['EVTNR'])  # create empty event
                 station = NuRadioReco.framework.station.Station(self.__station_id)
-                sim_station = coreas.make_sim_station(self.__station_id, corsika, observer, self.__n_channels, weights[i])
+                sim_station = coreas.make_sim_station(self.__station_id, corsika, observer, detector.get_channel_ids(self.__station_id), weights[i])
 
                 station.set_sim_station(sim_station)
                 evt.set_station(station)
