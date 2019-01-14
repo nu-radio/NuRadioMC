dee3468fcaf6c92dea623d88533b9c467e2a7dba
christoph <christoph.welling@desy.de>
2019-01-14 11:11:34 +0100

    Undoes changes in parameter indices

--- NuRadioReco/framework/parameters.py
+++ NuRadioReco/framework/parameters.py
@@ -18,20 +18,20 @@ class stationParameters(Enum):
     azimuth = 14  # the azimuth angle of the incoming signal direction (WARNING: this parameter is not well defined as the incoming signal direction might be different for different channels)
     zenith_cr_templatefit = 15
     zenith_nu_templatefit = 16
-    cr_xcorrelations = 17 # dict of result of crosscorrelations with cr templates
-    nu_xcorrelations = 18 #  dict of result of crosscorrelations with nu templates
-    station_time = 19
-    cr_energy_em = 20  # the electromagnetic shower energy (the cosmic ray energy that ends up in electrons, positrons and gammas)
+    cr_xcorrelations = 19 # dict of result of crosscorrelations with cr templates
+    nu_xcorrelations = 20 #  dict of result of crosscorrelations with nu templates
+    station_time = 21
+    cr_energy_em = 24  # the electromagnetic shower energy (the cosmic ray energy that ends up in electrons, positrons and gammas)
 
 class channelParameters(Enum):
     zenith = 1  # zenith angle of the incoming signal direction
     azimuth = 2  # azimuth angle of the incoming signal direction
-    maximum_amplitude = 3  # the maximum ampliude of the magnitude of the trace
-    SNR = 4  # an dictionary of various signal-to-noise ratio definitions
-    maximum_amplitude_envelope = 5  # the maximum ampliude of the hilbert envelope of the trace
-    P2P_amplitude = 6  # the peak to peak amplitude
-    cr_xcorrelations = 7 # dict of result of crosscorrelations with cr templates
-    nu_xcorrelations = 8 #  dict of result of crosscorrelations with nu templates
+    maximum_amplitude = 4  # the maximum ampliude of the magnitude of the trace
+    SNR = 5  # an dictionary of various signal-to-noise ratio definitions
+    maximum_amplitude_envelope = 6  # the maximum ampliude of the hilbert envelope of the trace
+    P2P_amplitude = 7  # the peak to peak amplitude
+    cr_xcorrelations = 8 # dict of result of crosscorrelations with cr templates
+    nu_xcorrelations = 9 #  dict of result of crosscorrelations with nu templates
     
 class electricFieldParameters(Enum):
     ray_path_type = 1  # the type of the ray tracing solution ('direct', 'refracted' or 'reflected')
