365c873cdad1df1bf178da6932b6928b814de8de
Christian Glaser <christian.glaser@uci.edu>
2019-02-06 14:49:39 -0800

    Tau event generator (#6)
    This commit adds tau generation to the event generator in an efficient way. Vertices in a large volume are being generated but only interactions in a smaller fiducial volume are being saved. For more details see discussions on pull request
    
    * remove 'new' event file format that will not be adapted
    
    * add functionality to hdf5 writer functions to write a second dataset table
    
    * modify event generator to add some dummy tau second bang interactions,
    add hdf5 testing scripts
    
    * Physics of the tau decay. A beginning.
    
    * Changed an argument.
    
    * fix typo
    
    * insert second vertices into main table
    
    * instead of adding a new table with additional vertices, insert additional vertices into the first table as new rows.
    the hdf5 writer is adjusted to not split a file in the middle of an event. Also the 'number of events' attribute is set correctly to count the number of event and NOT the number of vertices in a file
    
    * Output files with the same number of events each.
    
    * Refactorisation. Tau energy is now correctly calculated using the inelasticity.
    
    * Solved problem that happened while merging.
    
    * save tau decaus only if decay takes place in fiducial volume
    
    * add first step to generate tau decays from nu_tau interactions outside of fiducial volume
    
    * add .settings folder to gitignore
    
    * implement computationally efficient tau second band generation (details will be discussed on pull request)
    
    * add debug output for tau decays

---
+++ NuRadioMC/EvtGen/tests/T01test_additional_interactions.py
@@ -0,0 +1,17 @@
+import numpy as np
+import h5py
+
+from NuRadioMC.EvtGen import generator
+
+datasets = {}
+datasets['event_ids'] = np.arange(100, dtype=np.int)
+datasets['zeniths'] = np.random.uniform(size=100)
+
+additional_datasets = {}
+additional_datasets['event_ids'] = np.array([0, 1, 10, 10, 10])
+additional_datasets['zeniths'] = np.random.uniform(size=5)
+
+generator.write_events_to_hdf5("T01.hdf5", data_sets=datasets, attributes={}, additional_interactions=additional_datasets)
+generator.write_events_to_hdf5("T01_split10.hdf5", data_sets=datasets, attributes={}, additional_interactions=additional_datasets, n_events_per_file=10)
+
+generator.split_hdf5_input_file("T01.hdf5", "T01_split10_later.hdf5", number_of_events_per_file=10)
