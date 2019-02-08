60ddf4c41948abc99c334f2fb5d42c9877bc1fbf
Christian Glaser <christian.glaser@uci.edu>
2019-02-08 00:39:31 +0000

    bugfix

--- NuRadioMC/SignalGen/askaryan.py
+++ NuRadioMC/SignalGen/askaryan.py
@@ -90,7 +90,7 @@ def get_time_trace(energy, theta, N, dt, shower_type, n_index, R, model, interp_
         raise NotImplementedError("model {} unknown".format(model))
 
 
-def get_frequency_spectrum(energy, theta, N, dt, is_em_shower, n_index, R, model):
+def get_frequency_spectrum(energy, theta, N, dt, is_em_shower, n_index, R, model, **kwargs):
     """
     returns the complex amplitudes of the frequency spectrum of the neutrino radio signal
 
@@ -123,4 +123,4 @@ def get_frequency_spectrum(energy, theta, N, dt, is_em_shower, n_index, R, model
         the complex amplitudes for the given frequencies
 
     """
-    return fft.time2freq(get_time_trace(energy, theta, N, dt, is_em_shower, n_index, R, model))
+    return fft.time2freq(get_time_trace(energy, theta, N, dt, is_em_shower, n_index, R, model, **kwargs))
