21b5037a75a9b830324b0249dec65ec99a7c735e
christoph <christoph.welling@desy.de>
2019-02-19 11:31:14 +0100

    Adds trigger properties overview

--- NuRadioReco/eventbrowser/apps/overview.py
+++ NuRadioReco/eventbrowser/apps/overview.py
@@ -66,7 +66,11 @@ layout = html.Div([
         html.Div([
             html.Div('Electric Fields', className='panel-heading'),
             html.Div(id='efield-overview-properties')
-        ], className='panel panel-default', style={'flex':'1'})
+        ], className='panel panel-default', style={'flex':'1'}),
+        html.Div([
+            html.Div('Triggers', className='panel-heading'),
+            html.Div(id='trigger-overview-properties')
+        ], className='panel panel-default', style={'flex': '1'})
     ], style={'display': 'flex'}),
     html.Div([
         html.Div('Correlations', className='panel-heading'),
@@ -350,6 +354,39 @@ def channel_overview_properties(filename, evt_counter, station_id, juser_id):
             html.Div('{}'.format(electric_field.get_channel_ids()), className='custom-table-td custom-table-td-last')
         ], className='custom-table-row'))
         reply.append(html.Div(props, style={'margin': '0 0 30px'}))
+    return reply
+    
+
+@app.callback(Output('trigger-overview-properties', 'children'),
+                [Input('filename', 'value'),
+                Input('event-counter-slider', 'value'),
+                Input('station-id-dropdown', 'value')],
+                [State('user_id', 'children')])
+def trigger_overview_properties(filename, evt_counter, station_id, juser_id):
+    if filename is None or station_id is None:
+        return ''
+    user_id = json.loads(juser_id)
+    ariio = provider.get_arianna_io(user_id, filename)
+    evt = ariio.get_event_i(evt_counter)
+    station = evt.get_station(station_id)
+    if station is None:
+        return []
+    reply = []
+    for trigger_name in station.get_triggers():
+        props = [
+        html.Div([
+            html.Div('{}'.format(trigger_name), className='custom-table-th')
+        ], className='custom-table-row')
+        ]
+        trigger = station.get_trigger(trigger_name)
+        for setting_name in trigger.get_trigger_settings():
+            props.append(
+                html.Div([
+                    html.Div('{}'.format(setting_name), className='custom-table-td'),
+                    html.Div('{}'.format(trigger.get_trigger_settings()[setting_name]), className='custom-table-td custom-table-td-last')
+                ], className='custom-table-row')
+            )
+        reply.append(html.Div(props))
     return reply
 
 
