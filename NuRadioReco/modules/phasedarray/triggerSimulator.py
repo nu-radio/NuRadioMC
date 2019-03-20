728f5fb3cd9252f972db49252655f87a0a874002
daniel-zid <sirzid@gmail.com>
2019-03-20 18:31:11 +0100

    Improvement of the phased trigger and correction of some bugs. It seems to work properly as of now.

--- NuRadioReco/modules/phasedarray/triggerSimulator.py
+++ NuRadioReco/modules/phasedarray/triggerSimulator.py
@@ -1,30 +1,38 @@
 from NuRadioReco.utilities import units
 from NuRadioReco.framework.trigger import SimplePhasedTrigger
 import numpy as np
+from scipy import constants
 import time
 import logging
 logger = logging.getLogger('phasedTriggerSimulator')
 
 cspeed = constants.c * units.m / units.s
 
-default_angles = np.linspace(-45,47,14) * units.deg
+main_low_angle = -53. * units.deg
+main_high_angle = 47 * units.deg
+default_angles = np.arcsin( np.linspace( np.sin(main_low_angle), np.sin(main_high_angle), 15) )
+
+sec_low_angle = main_low_angle/2
+sec_high_angle = main_high_angle/2
+default_sec_angles = np.arcsin(np.linspace( np.sin(sec_low_angle), np.sin(sec_high_angle), 15) )
 
 
 class triggerSimulator:
     """
-    Calculates the trigger for a phased array
+    Calculates the trigger for a phased array with a primary and a secondary beam.
+    The channels that participate in both beams and the pointing angle for each
+    subbeam can be specified.
 
-    explain the module in more detail here
+    See https://arxiv.org/pdf/1809.04573.pdf
     """
 
     def __init__(self):
         self.__t = 0
         self.begin()
 
-
     def begin(self, debug=False, pre_trigger_time=100 * units.ns):
         self.__pre_trigger_time = pre_trigger_time
-	self.__debug = debug
+        self.__debug = debug
 
     def get_vertical_positions(self, station, det, triggered_channels=None):
 
@@ -34,20 +42,25 @@ class triggerSimulator:
 
         return np.array(ant_z)
 
-    def get_beam_rolls(self, station, phasing_angles=default_angles,
-                       sampling_rate=1., ref_index=1.78):
+    def get_beam_rolls(self, station, det, triggered_channels,
+                       phasing_angles=default_angles, ref_index=1.78):
+
+        sampling_rate = station.get_channel(0).get_sampling_rate()
+        time_step = 1./sampling_rate
 
         ant_z = self.get_vertical_positions(station, det, triggered_channels)
         self.check_vertical_string(ant_z)
         beam_rolls = []
-        ref_z = np.max(ant_z)
+        ref_z = (np.max(ant_z)+np.min(ant_z))/2
 
         for angle in phasing_angles:
             subbeam_rolls = {}
-            for z, channel_id in zip(ant_z, station.iter_channels):
-                delay = -(ant_z-ref_z)/cspeed * ref_index * np.sin(angle)
-                roll = int(delay/sampling_rate)
+            for z, channel_id in zip(ant_z, triggered_channels):
+                delay = (z-ref_z)/cspeed * ref_index * np.sin(angle)
+                roll = int(delay/time_step)
                 subbeam_rolls[channel_id] = roll
+            logger.debug("angle:", angle/units.deg)
+            logger.debug(subbeam_rolls)
             beam_rolls.append(subbeam_rolls)
 
         return beam_rolls
@@ -61,23 +74,27 @@ class triggerSimulator:
     def run(self, evt, station, det,
             threshold=60 * units.mV,
             triggered_channels=None,
+            secondary_channels=None,
             trigger_name='simple_phased_threshold',
-            phasing_angles=default_angles):
+            phasing_angles=default_angles,
+            secondary_phasing_angles=default_sec_angles):
         """
         simulates phased array trigger for each event
 
-        describe the run method in more detail here (if neccessary).
-        I left triggered_channels in as an optional parameter, don't know if you
-        need it.
+        Several channels are phased by delaying their signals by an amount given
+        by a pointing angle. Several pointing angles are possible in order to cover
+        the sky. The array triggered_channels controls the channels that are phased,
+        according to the angles phasing_angles. A secondary phasing that is added
+        to this primary phasing is possible, and it is controlled by the parametres
+        secondary_channels and secondary_phasing_angles.
 
         Parameters
         ----------
+        threshold: float
+            threshold above (or below) a trigger is issued, absolute amplitude
         triggered_channels: array of ints
-            channels ids that are triggered on
+            channels ids that form the primary phasing array
         """
-
-        sampling_rate = station.get_channel(0).get_sampling_rate()
-        time_step = 1./sampling_rate
 
         phased_trace = None
 
@@ -85,13 +102,20 @@ class triggerSimulator:
 
     		triggered_channels = [channel._id for channel in station.iter_channels()]
 
-        beam_rolls = self.get_beam_rolls(station, ant_z, phasing_angles, sampling_rate, ref_z)
+        if (secondary_channels == None):
+
+            secondary_channels = triggered_channels[::2]
+
+        logger.debug("primary channels:", triggered_channels)
+        beam_rolls = self.get_beam_rolls(station, det, triggered_channels, phasing_angles)
+        logger.debug("secondary_channels:", secondary_channels)
+        secondary_beam_rolls = self.get_beam_rolls(station, det, secondary_channels, secondary_phasing_angles)
 
         for channel in station.iter_channels():  # loop over all channels (i.e. antennas) of the station
             channel_id = channel.get_id()
             if channel_id not in triggered_channels:  # skip all channels that do not participate in the trigger decision
-		          logger.debug("skipping channel{}".format(channel_id))
-                  continue
+                logger.debug("skipping channel{}".format(channel_id))
+                continue
 
             trace = channel.get_trace()  # get the time trace (i.e. an array of amplitudes)
             #times = channel.get_times()  # get the corresponding time bins
@@ -103,12 +127,16 @@ class triggerSimulator:
                 else:
                     phased_trace += np.roll(trace, subbeam_rolls[channel_id])
 
+            if channel_id in secondary_channels:
+                for secbeam_rolls in secondary_beam_rolls:
+                    phased_trace += np.roll(trace, secbeam_rolls[channel_id])
+
     	is_triggered = False
         if(np.max(np.abs(phased_trace)) > threshold):  # define a simple threshold trigger
             is_triggered = True
     	    logger.debug("Station has triggered")
 
-    	trigger = SimplePhasedTrigger(trigger_name, threshold, triggered_channels)
+    	trigger = SimplePhasedTrigger(trigger_name, threshold, triggered_channels, secondary_channels, phasing_angles, secondary_phasing_angles)
      	trigger.set_triggered(is_triggered)
     	station.set_trigger(trigger)
 
