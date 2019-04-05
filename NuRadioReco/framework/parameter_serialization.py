2d342fd0f4e9a67f5a578278849a3a795b388824
christoph <christoph.welling@desy.de>
2019-04-05 17:28:28 +0200

    Uses strings instead of enums as dict keys when serializing station properties to improve compatibility when parameters are missing

---
+++ NuRadioReco/framework/parameter_serialization.py
@@ -0,0 +1,15 @@
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+def serialize(object):
+    reply = {}
+    for entry in object:
+        reply[str(entry)] = object[entry]
+    return reply
+
+def deserialize(object, parameter_enum):
+    reply = {}
+    for entry in parameter_enum:
+        if str(entry) in object:
+            reply[entry] = object[str(entry)]
+    return reply
+    
