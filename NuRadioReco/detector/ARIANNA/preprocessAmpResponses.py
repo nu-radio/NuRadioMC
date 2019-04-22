91968504ee3d0dd77ab141c7ea4d7aa28184d0fa
Christian Glaser <christian.glaser@uci.edu>
2019-04-22 18:48:41 +0000

    update script that preprocesses hardware responses of series 300 amp measurements

--- NuRadioReco/detector/ARIANNA/preprocessAmpResponses.py
+++ NuRadioReco/detector/ARIANNA/preprocessAmpResponses.py
@@ -1,5 +1,4 @@
 import numpy as np
-from scipy.interpolate import interp1d
 import os
 from radiotools import helper as hp
 from NuRadioReco.utilities import units
@@ -9,129 +8,74 @@ import scipy.signal as ss
 from numpy import pi, diff, unwrap, angle
 import matplotlib.pyplot  as plt
 
-
 logger = logging.getLogger('analog_components')
 
-def save_preprocessed_Amps(response,amp_name):
+
+def save_preprocessed_Amps(response, amp_name):
     output_filename = '{}.pkl'.format(amp_name)
     with open(output_filename, 'wb') as fout:
         logger.info('saving output to {}'.format(output_filename))
         pickle.dump(response, fout, protocol=2)
 
-def checkGroupDelay(h,w,amp_name,path):
-    gd = os.path.join(path, 'GROUPDELAY'+amp_name)
-    amp_gd_discrete = np.loadtxt(gd, skiprows=3, delimiter=',')[:, 1]*1e9
-
-    group_delay = -diff(unwrap(angle(h))) / diff(w) / (2*np.pi) / units.ns
-
-    plt.plot(w,amp_gd_discrete,label='file')
-    plt.plot(w[1:],group_delay,label='calc')
-    plt.legend()
-    plt.show()
-
-def complexNullResponse():
-    path = 'HardwareResponses/300SeriesAmpBoxes/'
-    amp_gain_discrete = np.loadtxt(os.path.join(path, 'LOGMAG_NULL.CSV'),
-                                   skiprows=3, delimiter=',')
-    ph = os.path.join(path, 'PHASE_NULL.CSV')
-    amp_phase_discrete = np.loadtxt(ph, skiprows=3, delimiter=',')
-
-
-    # Convert to GHz and add 60dB/40dB for attenuation in measurement circuit
-    amp_gain_discrete[:, 0] *= units.Hz
-    ff = amp_gain_discrete[:, 0]
-
-    amp_gain_discrete[:, 1] += 40 # 300 series amps
-
-    amp_gain_db_f = interp1d(amp_gain_discrete[:, 0], amp_gain_discrete[:, 1],
-                             bounds_error=False, fill_value=0)  # all requests outside of measurement range are set to 0
-
-    def get_amp_gain(ff):
-        amp_gain_db = amp_gain_db_f(ff)
-        amp_gain = 10 ** (amp_gain_db / 20.)
-        return amp_gain
-
-    amp_phase_discrete[:, 0] *= units.Hz
-
-    amp_phase_f = interp1d(amp_phase_discrete[:, 0], np.unwrap(np.deg2rad(amp_phase_discrete[:, 1])),
-                           bounds_error=False, fill_value=0)  # all requests outside of measurement range are set to 0
-
-    def get_amp_phase(ff):
-        amp_phase = amp_phase_f(ff)
-        return np.exp(1j * amp_phase)
-
-    return get_amp_gain(ff)*get_amp_phase(ff)
-
-
-
-def preprocess_300Amp(amp,channel='',path="HardwareResponses/"):
+def preprocess_300Amp(amp, channel, path="HardwareResponses/"):
     """
-    Read out amplifier gain and phase. Currently only examples have been implemented.
-    Needs a better structure in the future, possibly with database.
+    preprocess individual 300 series measurements
 
     Parameters
     ----------
-    ff: array
-    	array of frequencies to probe the response.
     amp: string
         amp series - box number - and channel. Ex: 300-01
-    channel: string
-        channel id: ex '1'. Default is None fore general 100 and 200 series amps
+    channel: int
+        channel id
     path: string
         directory where amp responses are located. Default is "HardwareResponses/"
     """            
-    # HardwareResponses/300SeriesAmpBoxes/Box300-03/
-
-    amplifier_response = {}
-    path = 'HardwareResponses/300SeriesAmpBoxes/Box' + amp + '/'
-    ch_name = '_CH'+channel+'.CSV'
-    amp_gain_discrete = np.loadtxt(os.path.join(path, 'LOGMAG' + ch_name),
-                                   skiprows=3, delimiter=',')
-    ph = os.path.join(path, 'PHASE'+ch_name)
+    
+    # load null measurement
+    amp_gain_db = np.loadtxt(os.path.join(path, 'LOGMAG_NULL.CSV'), skiprows=3, delimiter=',')
+    ph = os.path.join(path, 'PHASE_NULL.CSV')
     amp_phase_discrete = np.loadtxt(ph, skiprows=3, delimiter=',')
 
-
-    # Convert to GHz and add 60dB/40dB for attenuation in measurement circuit
-    amp_gain_discrete[:, 0] *= units.Hz
-    ff = amp_gain_discrete[:, 0]
-
-    amp_gain_discrete[:, 1] += 40 # 300 series amps
-
-    amp_gain_db_f = interp1d(amp_gain_discrete[:, 0], amp_gain_discrete[:, 1],
-                             bounds_error=False, fill_value=0)  # all requests outside of measurement range are set to 0
-
-    def get_amp_gain(ff):
-        amp_gain_db = amp_gain_db_f(ff)
-        amp_gain = 10 ** (amp_gain_db / 20.)
-        return amp_gain
-
-    # Convert to MHz and broaden range
+    # Convert to GHz and add 40dB for attenuation in measurement circuit
+    amp_gain_db[:, 0] *= units.Hz
     amp_phase_discrete[:, 0] *= units.Hz
+    if(not np.allclose(amp_gain_db[:, 0], amp_phase_discrete[:, 0])):
+        raise ValueError("frequencies of gain and phase measurement are not equal for NULL measurement")
+    amp_gain_db[:, 1] += 40  # 300 series amps
+    gain_lin = 10 ** (amp_gain_db[:, 1] / 20.)
+    response_null = gain_lin * np.exp(1j * amp_phase_discrete[:, 1])
+    
 
-    amp_phase_f = interp1d(amp_phase_discrete[:, 0], np.unwrap(np.deg2rad(amp_phase_discrete[:, 1])),
-                           bounds_error=False, fill_value=0)  # all requests outside of measurement range are set to 0
+    # read in individual amp measurement
+    path = os.path.join(path, 'Box' + amp)
+    ch_name = '_CH{:d}.CSV'.format(channel)
+    amp_gain_db = np.loadtxt(os.path.join(path, 'LOGMAG' + ch_name), skiprows=3, delimiter=',')
+    ph = os.path.join(path, 'PHASE' + ch_name)
+    amp_phase_discrete = np.loadtxt(ph, skiprows=3, delimiter=',')
 
-    def get_amp_phase(ff):
-        amp_phase = amp_phase_f(ff)
-        return np.exp(1j * amp_phase)
+    # Convert to GHz and add 40dB for attenuation in measurement circuit
+    amp_gain_db[:, 0] *= units.Hz
+    amp_phase_discrete[:, 0] *= units.Hz
+    ff = amp_gain_db[:, 0]
+    if(not np.allclose(amp_gain_db[:, 0], amp_phase_discrete[:, 0])):
+        raise ValueError("frequencies of gain and phase measurement are not equal for {}-{:02d}".format(amp, channel))
 
-    amp_name = amp + '-0' + channel
+    amp_gain_db[:, 1] += 40  # 300 series amps
+    
+    gain_lin = 10 ** (amp_gain_db[:, 1] / 20.)
+    r = gain_lin * np.exp(1j * amp_phase_discrete[:, 1])
 
     response = {}
     response['freqs'] = ff
-    response['response'] = (get_amp_gain(ff)*get_amp_phase(ff))/complexNullResponse()
+    response['response'] = r / response_null
+    amplifier_response = {}
+    amp_name = '{}-{:02d}'.format(amp, channel)
     amplifier_response[amp_name] = response
     
-    #checkGroupDelay(get_amp_gain(ff)*get_amp_phase(ff),ff,ch_name,path)
-    save_preprocessed_Amps(amplifier_response,amp_name)
+    # checkGroupDelay(get_amp_gain(ff)*get_amp_phase(ff),ff,ch_name,path)
+    save_preprocessed_Amps(amplifier_response, amp_name)
 
 
-preprocess_300Amp('300-03','0')
-preprocess_300Amp('300-03','1')
-preprocess_300Amp('300-03','2')
-preprocess_300Amp('300-03','3')
-preprocess_300Amp('300-03','4')
-preprocess_300Amp('300-03','5')
-preprocess_300Amp('300-03','6')
-preprocess_300Amp('300-03','7')
+for i in range(8):
+    preprocess_300Amp('300-03', i, path='HardwareResponses/300SeriesAmpBoxes')
 
