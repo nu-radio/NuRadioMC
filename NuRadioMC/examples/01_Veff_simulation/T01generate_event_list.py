da96cb8fa3e1298a0bc03358741a6c6d4213b96c
Christian Glaser <christian.glaser@uci.edu>
2019-05-10 22:09:35 +0000

    set fiducial volume to full volume

--- NuRadioMC/examples/01_Veff_simulation/T01generate_event_list.py
+++ NuRadioMC/examples/01_Veff_simulation/T01generate_event_list.py
@@ -13,4 +13,5 @@ generate_eventlist_cylinder('1e19_n1e3.hdf5', 1e3, 1e19 * units.eV, 1e19 * units
                              full_rmin=rmin, full_rmax=rmax, full_zmin=zmin, full_zmax=zmax)
 
 # generate one event list at 1e18 eV with 10000 neutrinos
-generate_eventlist_cylinder('1e18_n1e4.hdf5', 1e4, 1e18 * units.eV, 1e18 * units.eV, rmin, rmax, zmin, zmax)
+generate_eventlist_cylinder('1e18_n1e4.hdf5', 1e4, 1e18 * units.eV, 1e18 * units.eV, rmin, rmax, zmin, zmax,
+                            full_rmin=rmin, full_rmax=rmax, full_zmin=zmin, full_zmax=zmax)
