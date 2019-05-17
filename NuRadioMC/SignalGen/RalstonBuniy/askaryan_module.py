3b09e4284f6152cea8514609032d70a4fc137b08
daniel-zid <sirzid@gmail.com>
2019-05-17 17:11:17 +0200

    Explanation for the ZHS normalisation factor.

--- NuRadioMC/SignalGen/RalstonBuniy/askaryan_module.py
+++ NuRadioMC/SignalGen/RalstonBuniy/askaryan_module.py
@@ -88,9 +88,10 @@ def get_time_trace(energy, theta, N, dt, is_em_shower, n, R, LPM=True, a=None):
         a = get_N_AskDepthA_2(energy, em=is_em_shower, lpm=True)
     freqs = np.fft.rfftfreq(N, dt)
     eR, eTheta, ePhi = create_askaryan.get_frequency_spectrum(energy, theta, freqs, is_em_shower, n, R, LPM, a)
-    traceR = np.fft.irfft(eR) / dt * 0.5
-    traceTheta = np.fft.irfft(eTheta) / dt * 0.5
-    tracePhi = np.fft.irfft(ePhi) / dt * 0.5
+    ZHS_norm = 2 # ZHS Fourier transform factor
+    traceR = np.fft.irfft(eR) / dt / ZHS_norm
+    traceTheta = np.fft.irfft(eTheta) / dt / ZHS_norm
+    tracePhi = np.fft.irfft(ePhi) / dt / ZHS_norm
     return np.array([traceR, traceTheta, tracePhi])
 
 
