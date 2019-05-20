4174da5fd69445941ea8b59d3b76bbb6b8e95c0c
Christian Glaser <christian.glaser@uci.edu>
2019-05-20 23:59:59 +0000

    replace shower length part with daniels implementation

--- NuRadioMC/SignalGen/RalstonBuniy/hanson2017.py
+++ NuRadioMC/SignalGen/RalstonBuniy/hanson2017.py
@@ -1,6 +1,7 @@
 import numpy as np
-from NuRadioMC.utilities import units
+from NuRadioMC.utilities import units, fft
 from scipy import constants
+from scipy.optimize import curve_fit
 
 
 """
@@ -13,7 +14,7 @@ Astropart. Phys. ????
 c = constants.c
 c= 0.29972
 
-_strictLowFreqLimit = False
+_strictLowFreqLimit = True
 
 NORM = 1.0
 
@@ -99,11 +100,15 @@ def get_E_omega(ff, E, R, theta, n_index, em=True,
     thetaComp *= units.V / units.m / units.MHz
     return rComp, thetaComp
 
+def gauss(x, A, mu, sigma):
+    return A * np.exp(-(x-mu)**2/2/sigma**2)
+
 
 def get_N_AskDepthA(E, em=True, lpm=True):
+    E *= units.GeV
     if em:
-        E_CRIT = 0.073  # GeV
-        max_x = 50.0  # maximum number of radiation lengths
+        E_CRIT = 0.073 * units.GeV  # GeV
+        max_x = 5000.0  # maximum number of radiation lengths
         dx = 0.01  # small enough bin in depth for our purposes.
         x_start = 0.01  # starting radiation length
         # Greissen EM shower profile from Energy E in GeV.
@@ -117,14 +122,13 @@ def get_N_AskDepthA(E, em=True, lpm=True):
 
     else:  # hadronic shower profile
         # Gaisser-Hillas hadronic shower parameterization
-        print("HAD")
-        max_x = 2000.0  # maximum depth in g/cm^2
+        max_x = 200000.0  # maximum depth in g/cm^2
         dx = 1.0  # small enough bin in depth for our purposes.
         x_start = dx  # depth in g/cm^2
         S0 = 0.11842
         X0 = 39.562  # g/cm^2
         l = 113.03  # g/cm^2
-        Ec = 0.17006  # GeV
+        Ec = 0.17006 * units.GeV  # GeV
         Xmax = X0 * np.log(E / Ec)
         x = np.arange(x_start, max_x, dx)
         a = S0 * E / Ec * (Xmax - l) / Xmax * np.exp(Xmax / l - 1)
@@ -133,26 +137,52 @@ def get_N_AskDepthA(E, em=True, lpm=True):
         nx = a * b * c
     # find location of maximum, and charge excess from Fig. 5.9, compare in cm not m.
     n_max = np.argmax(nx)
-    excess = 0.09 + dx * n_max * ICE_RAD_LENGTH / ICE_DENSITY * 1.0e-4
+    if em:
+        excess = 0.09 + dx * n_max * ICE_RAD_LENGTH / ICE_DENSITY * 1.0e-4
+    else:
+        excess = 0.09 + dx * n_max / ICE_DENSITY * 1.0e-4
     Nmax = excess * n_max / 1000.0
-    # find depth, which is really the FWHM of this Greisen formula.
-    i = np.argwhere((nx / nx[n_max]) > 0.606531)[0][0]
-    j = np.argwhere((nx[(i + 1):] / nx[n_max]) < 0.606531)[0]
-    _askaryanDepthA = dx * (j - i) / ICE_DENSITY * ICE_RAD_LENGTH / 100.0  # meters
+
+    fit_region_cut = 0.95 # We want to perform a fit for the regions with an excess charge
+                         # 10% close to the maximum
+    cut_left = np.argwhere((nx[:n_max] / nx[n_max]) > fit_region_cut)[0][0]
+    cut_right = np.argwhere((nx[n_max:] / nx[n_max]) < fit_region_cut)[0][0]+n_max
+    fit_width = cut_right-cut_left
+    max_vicinity = nx[n_max-fit_width:n_max+fit_width]/nx[n_max]
+    x_fit = np.arange(0, len(max_vicinity), 1)
+    sigma = curve_fit(gauss, x_fit, max_vicinity)[0]
+    if em:
+        _askaryanDepthA = dx * sigma[2] / ICE_DENSITY * ICE_RAD_LENGTH / 100.0  # meters
+    else:
+        _askaryanDepthA = dx * sigma[2] / ICE_DENSITY / 100.0  # meters
 
     if(em and lpm):
-        print("LPM")
         p1 = -2.8564e2
         p2 = 7.8140e1
         p3 = -8.3893
         p4 = 4.4175e-1
         p5 = -1.1382e-2
         p6 = 1.1493e-4
-        e = np.log10(E) + 9.0  # log_10 of Energy in eV
+        e = np.log10(E/units.eV)  # log_10 of Energy in eV
         log10_shower_depth = p1 + p2 * e + p3 * e**2 + p4 * e**3 + p5 * e**4 + p6 * e**5
         a = 10.0**log10_shower_depth
         # Right here, record the reduction in n_max that I don't believe in.
         if _strictLowFreqLimit:
+            print(Nmax, a, _askaryanDepthA)
             Nmax = Nmax / (a / _askaryanDepthA)
         _askaryanDepthA = a
     return Nmax, _askaryanDepthA
+
+def get_time_trace(energy, theta, N, dt, is_em_shower, n_index, R, LPM=True, a=None):
+    freqs = np.fft.rfftfreq(N, dt)
+    eR, eTheta = get_E_omega(freqs, energy, R, theta, n_index, is_em_shower, LPM)
+    ZHS_norm = 1 # ZHS Fourier transform factor
+    traceR = np.fft.irfft(eR) / dt / ZHS_norm
+    traceTheta = np.fft.irfft(eTheta) / dt / ZHS_norm
+    return np.array([traceR, traceTheta, np.zeros_like(traceTheta)])
+
+
+def get_frequency_spectrum(energy, theta, N, dt, is_em_shower, n, R, LPM=True, a=None):
+    eR, eTheta, ePhi = get_time_trace(energy, theta, N, dt, is_em_shower, n, R, LPM, a)
+    return np.array([fft.time2freq(eR), fft.time2freq(eTheta), fft.time2freq(ePhi)])
+
