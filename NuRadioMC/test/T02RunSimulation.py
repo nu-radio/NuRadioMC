6b1700bb9346f67eaf2aaba3b7f12a1e6a953c63
Anna Nelles <anna.nelles@gmail.com>
2019-05-21 11:43:40 +0200

    Update the tests to match the new pull request merge.

--- NuRadioMC/test/T02RunSimulation.py
+++ NuRadioMC/test/T02RunSimulation.py
@@ -2,8 +2,8 @@ from __future__ import absolute_import, division, print_function
 import argparse
 # import detector simulation modules
 import NuRadioReco.modules.efieldToVoltageConverter
-import NuRadioReco.modules.ARIANNA.triggerSimulator2
-import NuRadioReco.modules.triggerSimulator
+import NuRadioReco.modules.trigger.highLowThreshold
+import NuRadioReco.modules.trigger.simpleThreshold
 import NuRadioReco.modules.channelResampler
 import NuRadioReco.modules.channelBandPassFilter
 import NuRadioReco.modules.channelGenericNoiseAdder
@@ -16,8 +16,8 @@ logger = logging.getLogger("runstrawman")
 # initialize detector sim modules
 efieldToVoltageConverter = NuRadioReco.modules.efieldToVoltageConverter.efieldToVoltageConverter()
 efieldToVoltageConverter.begin()
-triggerSimulator = NuRadioReco.modules.triggerSimulator.triggerSimulator()
-triggerSimulatorARIANNA = NuRadioReco.modules.ARIANNA.triggerSimulator2.triggerSimulator()
+triggerSimulatorHighLow = NuRadioReco.modules.trigger.highLowThreshold.triggerSimulator()
+triggerSimulatorSimple = NuRadioReco.modules.trigger.simpleThreshold.triggerSimulator()
 channelResampler = NuRadioReco.modules.channelResampler.channelResampler()
 channelBandPassFilter = NuRadioReco.modules.channelBandPassFilter.channelBandPassFilter()
 channelGenericNoiseAdder = NuRadioReco.modules.channelGenericNoiseAdder.channelGenericNoiseAdder()
@@ -29,49 +29,47 @@ class mySimulation(simulation.simulation):
         # start detector simulation
         if(bool(self._cfg['signal']['zerosignal'])):
             self._increase_signal(None, 0)
-        
+
         efieldToVoltageConverter.run(self._evt, self._station, self._det)  # convolve efield with antenna pattern
         # downsample trace to internal simulation sampling rate (the efieldToVoltageConverter upsamples the trace to
-        # 20 GHz by default to achive a good time resolution when the two signals from the two signal paths are added) 
+        # 20 GHz by default to achive a good time resolution when the two signals from the two signal paths are added)
         channelResampler.run(self._evt, self._station, self._det, sampling_rate=1. / self._dt)
-        
+
         if bool(self._cfg['noise']):
             Vrms = self._Vrms / (self._bandwidth /( 2.5 * units.GHz))** 0.5  # normalize noise level to the bandwidth its generated for
             channelGenericNoiseAdder.run(self._evt, self._station, self._det, amplitude=Vrms, min_freq=0 * units.MHz,
                                          max_freq=2.5 * units.GHz, type='rayleigh')
-        
+
         # bandpass filter trace, the upper bound is higher then the sampling rate which makes it just a highpass filter
         channelBandPassFilter.run(self._evt, self._station, self._det, passband=[80 * units.MHz, 1000 * units.GHz],
                                   filter_type='butter', order=2)
         channelBandPassFilter.run(self._evt, self._station, self._det, passband=[0, 500 * units.MHz],
                                   filter_type='butter', order=10)
         # first run a simple threshold trigger
-        triggerSimulator.run(self._evt, self._station, self._det,
+        triggerSimulatorSimple.run(self._evt, self._station, self._det,
                              threshold=3 * self._Vrms,
                              triggered_channels=None,  # run trigger on all channels
                              number_concidences=1,
                              trigger_name='simple_threshold')  # the name of the trigger
-        
+
         # run a high/low trigger on the 4 downward pointing LPDAs
-        triggerSimulatorARIANNA.run(self._evt, self._station, self._det,
+        triggerSimulatorHighLow.run(self._evt, self._station, self._det,
                                     threshold_high=4 * self._Vrms,
                                     threshold_low=-4 * self._Vrms,
                                     triggered_channels=[0, 1, 2, 3],  # select the LPDA channels
                                     number_concidences=2,  # 2/4 majority logic
-                                    cut_trace=False,
                                     trigger_name='LPDA_2of4_4.1sigma',
                                     set_not_triggered=(not self._station.has_triggered("simple_threshold"))) # calculate more time consuming ARIANNA trigger only if station passes simple trigger
-        
-        # run a high/low trigger on the 4 surface dipoles 
-        triggerSimulatorARIANNA.run(self._evt, self._station, self._det,
+
+        # run a high/low trigger on the 4 surface dipoles
+        triggerSimulatorHighLow.run(self._evt, self._station, self._det,
                                     threshold_high=3 * self._Vrms,
                                     threshold_low=-3 * self._Vrms,
                                     triggered_channels=[4, 5, 6, 7], # select the bicone channels
                                     number_concidences=4, # 4/4 majority logic
-                                    cut_trace=False,
                                     trigger_name='surface_dipoles_4of4_3sigma',
                                     set_not_triggered=(not self._station.has_triggered("simple_threshold"))) # calculate more time consuming ARIANNA trigger only if station passes simple trigger
-        
+
         # downsample trace back to detector sampling rate
         channelResampler.run(self._evt, self._station, self._det, sampling_rate=self._sampling_rate_detector)
 
