9b3b39e6857d23064bd9501065cdd82cea81d656
daniel-zid <sirzid@gmail.com>
2019-07-19 15:34:02 +0200

    Indenting

--- NuRadioMC/utilities/Veff.py
+++ NuRadioMC/utilities/Veff.py
@@ -36,9 +36,10 @@ def get_triggered(fin):
         mask_interactions = np.array(fin['event_ids']) == event_id
         multiple_interaction_indexes = np.argwhere( np.array(fin['event_ids']) == event_id )[0]
 
-            for int_index in multiple_interaction_indexes[1:]:
-                triggered[int_index] = False
-            triggered[multiple_interaction_indexes[0]] = True
+        print(multiple_interaction_indexes)
+        for int_index in multiple_interaction_indexes[1:]:
+            triggered[int_index] = False
+        triggered[multiple_interaction_indexes[0]] = True
 
     return triggered
 
