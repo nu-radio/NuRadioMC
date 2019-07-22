28620726f09c1786db8444e1dab2dac5af528471
Anna Nelles <anna.nelles@gmail.com>
2019-07-22 09:18:40 +0200

    Adding documentation and code cosmetics.

--- NuRadioReco/modules/io/araroot/readARAData.py
+++ NuRadioReco/modules/io/araroot/readARAData.py
@@ -26,22 +26,44 @@ libc = CDLL("libRootFftwWrapper.so")
 class readARAData:
 
     """
-    This is the AraReader. Prints out event waveform, unixtime and timestamp.
+    This is the AraReader. Reads ARA data in the ARARoot format.
+
+
     """
     def begin(self,input_file):
+
+    """
+    Begin function of the ARA reader
+
+    Parameters
+    ----------
+    input_file: string
+        path to file to read
+
+    """
+
         self.__id_current_event = -1
         self.__t = time.time()
-        
+
         self.f = ROOT.TFile.Open(input_file)
         self.raw_ptr=ROOT.RawAtriStationEvent()
         self.data_tree = self.f.Get("eventTree")
-        self.data_tree.SetBranchAddress("event",self.raw_ptr)      
+        self.data_tree.SetBranchAddress("event",self.raw_ptr)
         self.n_events = self.data_tree.GetEntries()
 
         return self.n_events
 
-    def run(self):
+    def run(self, n_channels = 16, sampling = 0.625*units.ns):
         """
+        Run function of the ARA reader
+
+        Parameters
+        ----------
+        n_channels: int
+            number of ARA channels to loop over, default 16
+
+        sampling: float (units time)
+            default sampling of ARA detector
 
         """
         while True:
@@ -60,30 +82,36 @@ class readARAData:
             run_number = self.data_tree.run
             evt_number = self.raw_ptr.eventNumber
             station_id = bytearray(self.raw_ptr.stationId)[0]
-            print run_number,evt_number,station_id
+            logger.info("Reading Run: {0}, Event {1}, Station {2}".format(run_number,evt_number,station_id))
+
             evt = NuRadioReco.framework.event.Event(run_number, evt_number)
             station = NuRadioReco.framework.station.Station(station_id)
             real_ptr=ROOT.UsefulAtriStationEvent(self.raw_ptr,ROOT.AraCalType.kLatestCalib)
-            for iCh in xrange(16):
-            #ARA always has 16 channels
+
+            # Loop over all channels in data
+            for iCh in xrange(n_channels):
+
                 channel =NuRadioReco.framework.channel.Channel(iCh)
-                
+
+                # Get data from array via graph method
                 graph_waveform = real_ptr.getGraphFromRFChan(iCh)
                 times = np.array(graph_waveform.GetX())*units.ns
                 voltage = np.array(graph_waveform.GetY())*units.mV
 
                 #interpolate to get equal sampling between data points
                 f_interpolate=interpolate.interp1d(times, voltage)
-                times_new = np.arange(times[0], times[-1], 0.625)
+                times_new = np.arange(times[0], times[-1], sampling)
                 voltage_new=f_interpolate(times_new)
-                
+
                 if voltage_new.shape[0]%2 !=0:
                     voltage_new=voltage_new[:-1]
                 sampling_rate = times_new[1]-times_new[0]
-                #logger.warning("ARA sampling frequency not constant. Need to add the interpolating function here!!!")
+
                 channel.set_trace(voltage_new,sampling_rate)
                 station.add_channel(channel)
                 evt.set_station(station)
             yield evt
+
+
     def end(self):
         pass
