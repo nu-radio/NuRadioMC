9338d8c1abe91860d1046e7be8aa3a3a9dfb1e98
christoph <christoph.welling@desy.de>
2019-08-09 16:10:38 +0200

    Adds unit test scripts for triggers

---
+++ NuRadioReco/test/trigger_tests/trigger_tests.py
@@ -0,0 +1,36 @@
+import NuRadioReco.detector.generic_detector
+import NuRadioReco.modules.io.eventReader
+import NuRadioReco.modules.io.eventWriter
+import NuRadioReco.modules.ARIANNA.hardwareResponseIncorporator
+import NuRadioReco.modules.trigger.highLowThreshold
+import NuRadioReco.modules.trigger.multiHighLowThreshold
+import NuRadioReco.modules.trigger.simpleThreshold
+import NuRadioReco.modules.phasedarray.triggerSimulator
+import NuRadioReco.modules.efieldToVoltageConverter
+from NuRadioReco.utilities import units
+
+det = NuRadioReco.detector.generic_detector.GenericDetector('trigger_test_detector.json', 1, 0)
+event_reader = NuRadioReco.modules.io.eventReader.eventReader()
+event_reader.begin('trigger_test_input.nur')
+event_writer = NuRadioReco.modules.io.eventWriter.eventWriter()
+event_writer.begin('trigger_test_output.nur')
+
+high_low_trigger = NuRadioReco.modules.trigger.highLowThreshold.triggerSimulator()
+multi_high_low_trigger = NuRadioReco.modules.trigger.multiHighLowThreshold.triggerSimulator()
+simple_threshold_trigger = NuRadioReco.modules.trigger.simpleThreshold.triggerSimulator()
+phased_array_trigger = NuRadioReco.modules.phasedarray.triggerSimulator.triggerSimulator()
+efield_to_voltage_converter = NuRadioReco.modules.efieldToVoltageConverter.efieldToVoltageConverter()
+efield_to_voltage_converter.begin()
+hardware_response_incorporator = NuRadioReco.modules.ARIANNA.hardwareResponseIncorporator.hardwareResponseIncorporator()
+
+for event in event_reader.run():
+    station = event.get_station(1)
+    efield_to_voltage_converter.run(event, station, det)
+    hardware_response_incorporator.run(event, station, det, True)
+    high_low_trigger.run(event, station, det)
+    multi_high_low_trigger.run(event, station, det, trigger_name="default_multi_high_low")
+    simple_threshold_trigger.run(event, station, det)
+    phased_array_trigger.run(event, station, det)
+    
+    event_writer.run(event)
+
