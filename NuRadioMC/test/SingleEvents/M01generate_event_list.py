4f2768753f6a0167e6d569ffaec79da161cff95a
Christian Glaser <christian.glaser@uci.edu>
2019-08-22 19:42:46 +0000

    add unit test for MB event-by-event check

---
+++ NuRadioMC/test/SingleEvents/M01generate_event_list.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+from __future__ import absolute_import, division, print_function
+from NuRadioMC.utilities import units
+from NuRadioMC.EvtGen.generator import generate_eventlist_cylinder
+
+# define simulation volume
+zmin = -0.55 * units.km  # Moore's Bay is ~570 m deep
+zmax = 0 * units.km
+rmin = 0 * units.km
+rmax = 2 * units.km
+
+# generate one event list at 1e18 eV with 10000 neutrinos
+generate_eventlist_cylinder('MB_1e18_full.hdf5', 1e3, 1e18 * units.eV, 1e18 * units.eV, rmin, rmax, zmin, zmax,
+                            full_rmin=rmin, full_rmax=rmax, full_zmin=zmin, full_zmax=zmax)
