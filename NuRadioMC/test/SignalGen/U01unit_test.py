1dd51530db44b5c847965b9f2896fed8d901fa10
christoph <christoph.welling@desy.de>
2019-08-28 11:25:50 +0200

    Uses NuRadioReco io_utilities to read in pickles

--- NuRadioMC/test/SignalGen/U01unit_test.py
+++ NuRadioMC/test/SignalGen/U01unit_test.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python
 from NuRadioMC.SignalGen.askaryan import get_time_trace, get_frequency_spectrum
 from NuRadioMC.utilities import units, fft
+from NuRadioReco.utilities import io_utilities
 import numpy as np
 from numpy import testing
-import pickle
 import sys
 
 try:
@@ -30,14 +30,13 @@ Es = 10**np.linspace(15,19,5) * units.eV
 domegas = np.linspace(-5,5,10) * units.deg
 thetas = np.arccos(1./n_index) + domegas
 
-with open(reference_file, "rb") as fin:
-    reference = pickle.load(fin, encoding='latin1')
-    i = -1
-    for model in models:
-        for E in Es:
-            for shower_type in shower_types:
-                for theta in thetas:
-                    i+=1
-                    trace = get_time_trace(E, theta, n_samples, dt, shower_type, n_index, R, model)
-                    testing.assert_almost_equal(trace, reference[i])
+reference = io_utilities.read_pickle(reference_file, encoding='latin1')
+i = -1
+for model in models:
+    for E in Es:
+        for shower_type in shower_types:
+            for theta in thetas:
+                i+=1
+                trace = get_time_trace(E, theta, n_samples, dt, shower_type, n_index, R, model)
+                testing.assert_almost_equal(trace, reference[i])
 print('SignalGen test passed without any issues!')
