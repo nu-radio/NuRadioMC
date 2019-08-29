5938c773d1112fefe7e8e7ebf97b5783a702428b
Felix Schlueter <felix.schlueter@kit.edu>
2019-08-29 14:23:55 +0200

    This avoids calling the function twich, and makes it more flexible

--- NuRadioReco/framework/parameter_serialization.py
+++ NuRadioReco/framework/parameter_serialization.py
@@ -1,15 +1,23 @@
 from __future__ import absolute_import, division, print_function, unicode_literals
 
+
 def serialize(object):
     reply = {}
     for entry in object:
         reply[str(entry)] = object[entry]
     return reply
 
-def deserialize(object, parameter_enum):
+
+def deserialize(object, parameter_enums):
     reply = {}
-    for entry in parameter_enum:
-        if str(entry) in object:
-            reply[entry] = object[str(entry)]
+
+    if not isinstance(parameter_enums, list):
+		parameter_enums = [parameter_enums]
+
+	for parameter_enum in parameter_enums:
+	    for entry in parameter_enum:
+	        if str(entry) in object:
+	            reply[entry] = object[str(entry)]
+    
     return reply
     
