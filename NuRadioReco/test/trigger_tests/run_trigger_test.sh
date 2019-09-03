b14905eebf4d012d3c547a7f14f4761886f8c818
christophwelling <christoph.welling@desy.de>
2019-09-03 13:37:40 +0200

    Stops test script from catching error

--- NuRadioReco/test/trigger_tests/run_trigger_test.sh
+++ NuRadioReco/test/trigger_tests/run_trigger_test.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+set -e
 python NuRadioReco/test/trigger_tests/generate_events.py
 python NuRadioReco/test/trigger_tests/trigger_tests.py
 python NuRadioReco/test/trigger_tests/compare_to_reference.py
