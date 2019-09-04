b8cf3882e0dc1099af447742776f29746279c463
christophwelling <christoph.welling@desy.de>
2019-09-04 15:46:16 +0200

    Changes sphinx style and adds detector to table of contents

--- NuRadioReco/autodoc/conf.py
+++ NuRadioReco/autodoc/conf.py
@@ -19,6 +19,7 @@
 #
 import os
 import sys
+import sphinx_rtd_theme
 sys.path.insert(0, os.path.abspath('.'))
 
 
@@ -31,7 +32,10 @@ sys.path.insert(0, os.path.abspath('.'))
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.autodoc']
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx_rtd_theme'
+    ]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -83,14 +87,15 @@ todo_include_todos = False
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
 #
 html_theme_options = {
-    'collapse_navigation': True
+    'collapse_navigation': False,
+    'stiky_navigation': True
 }
 
 # Add any paths that contain custom static files (such as style sheets) here,
@@ -105,8 +110,8 @@ html_static_path = ['_static']
 # refs: http://alabaster.readthedocs.io/en/latest/installation.html#sidebars
 html_sidebars = {
     '**': [
-        'relations.html',  # needs 'show_related': True theme option to display
-        'searchbox.html',
+        'localtoc.html',
+        'searchbox.html'
     ]
 }
 
