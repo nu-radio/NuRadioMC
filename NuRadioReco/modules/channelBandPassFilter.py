daf548c5c3e8844c871c99d4bf111a49fe9fe8ed
Christian Glaser <christian.glaser@uci.edu>
2019-09-04 22:16:16 +0000

    unify get_filter function

--- NuRadioReco/modules/channelBandPassFilter.py
+++ NuRadioReco/modules/channelBandPassFilter.py
@@ -79,11 +79,11 @@ class channelBandPassFilter:
             return f
         elif(filter_type == 'butter'):
             b, a = scipy.signal.butter(order, passband, 'bandpass', analog=True)
-            w, h = scipy.signal.freqs(b, a)
+            w, h = scipy.signal.freqs(b, a, frequencies)
             return h
         elif(filter_type == 'butterabs'):
             b, a = scipy.signal.butter(order, passband, 'bandpass', analog=True)
-            w, h = scipy.signal.freqs(b, a)
+            w, h = scipy.signal.freqs(b, a, frequencies)
             return np.abs(h)
         elif(filter_type.find('FIR')>=0):
             raise NotImplementedError("FIR filter not yet implemented")
@@ -109,11 +109,11 @@ class channelBandPassFilter:
         isFIR = False
         
         if(filter_type == 'rectangular'):
-            trace_fft *= self.get_filter(frequencies, 0, 0,  passband, filter_type)
+            trace_fft *= self.get_filter(frequencies, 0, 0, None, passband, filter_type)
         elif(filter_type == 'butter'):
-            trace_fft *= self.get_filter(frequencies, 0, 0, passband, filter_type)
+            trace_fft *= self.get_filter(frequencies, 0, 0, None, passband, filter_type, order)
         elif(filter_type == 'butterabs'):
-            trace_fft *= self.get_filter(frequencies, 0, 0, passband, filter_type)
+            trace_fft *= self.get_filter(frequencies, 0, 0, None, passband, filter_type, order)
         elif(filter_type.find('FIR')>=0):
             #print('This is a FIR filter')
             firarray = filter_type.split()
@@ -167,7 +167,7 @@ class channelBandPassFilter:
 #             trace_fft = channel.get_frequency_spectrum()
             isFIR = True
         else:
-            trace_fft *= self.get_filter(frequencies, 0, 0, passband, filter_type)
+            trace_fft *= self.get_filter(frequencies, 0, 0, None, passband, filter_type)
         if isFIR:
             channel.set_trace(trace_fir, sample_rate)
             #print('set trace for fir')
