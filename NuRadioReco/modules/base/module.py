fe38d5c1696fda69a28dd7b534ec5db65c4448b4
Christian Glaser <christian.glaser@uci.edu>
2019-09-04 20:54:38 +0000

    add decorator

--- NuRadioReco/modules/base/module.py
+++ NuRadioReco/modules/base/module.py
@@ -3,7 +3,6 @@ from timeit import default_timer as timer
 
 def run_decorator(run):
     @wraps(run)
-    
     def register_run_method(self, evt, station, det, **kwargs):
         evt.register_module(self, self.__class__.__name__, kwargs)
         start = timer()
@@ -13,10 +12,49 @@ def run_decorator(run):
             register_run_method.time[self] = 0
         register_run_method.time[self] += (end - start)
         return res
+    print(run.__class__)
     register_run_method.time = {}
     return register_run_method
        
-        
+
+
+def time_this(original_function):      
+    print("decorating")
+    def new_function(*args,**kwargs):
+        print("starting timer")
+        import datetime                 
+        before = datetime.datetime.now()                     
+        x = original_function(*args,**kwargs)                
+        after = datetime.datetime.now()                      
+        print("Elapsed Time = {0}".format(after-before))
+        return x                                             
+    return new_function  
+
+def ModuleDecorator(Cls):
+    class NewCls(object):
+        def __init__(self,*args,**kwargs):
+            self.oInstance = Cls(*args,**kwargs)
+        def __getattribute__(self,s):
+            """
+            this is called whenever any attribute of a NewCls object is accessed. This function first tries to 
+            get the attribute off NewCls. If it fails then it tries to fetch the attribute from self.oInstance (an
+            instance of the decorated class). If it manages to fetch the attribute from self.oInstance, and 
+            the attribute is an instance method then `time_this` is applied.
+            """
+            try:    
+                x = super(NewCls,self).__getattribute__(s)
+            except AttributeError:      
+                pass
+            else:
+                return x
+            print(s)
+            x = self.oInstance.__getattribute__(s)
+            print(x)
+            if type(x) == type(self.__init__): # it is an instance method
+                return run_decorator(x)                 # this is equivalent of just decorating the method with time_this
+            else:
+                return x
+    return NewCls
 
 
 # class ModuleDecorator(object):
