459612254f07c6ae07bfd789d70d55c630701e48
christophwelling <christoph.welling@desy.de>
2019-09-09 15:05:01 +0200

    Reverts changes made to parameter_serialization

--- NuRadioReco/framework/parameter_serialization.py
+++ NuRadioReco/framework/parameter_serialization.py
@@ -1,23 +1,14 @@
 from __future__ import absolute_import, division, print_function, unicode_literals
 
-
 def serialize(object):
     reply = {}
     for entry in object:
         reply[str(entry)] = object[entry]
     return reply
 
-
-def deserialize(object, parameter_enums):
+def deserialize(object, parameter_enum):
     reply = {}
-
-    if not isinstance(parameter_enums, list):
-		parameter_enums = [parameter_enums]
-
-	for parameter_enum in parameter_enums:
-	    for entry in parameter_enum:
-	        if str(entry) in object:
-	            reply[entry] = object[str(entry)]
-    
+    for entry in parameter_enum:
+        if str(entry) in object:
+            reply[entry] = object[str(entry)]
     return reply
-    
