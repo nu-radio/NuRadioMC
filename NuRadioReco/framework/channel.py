cbb11889b62eca84157972333c4c1aadec63d765
Anna Nelles <anna.nelles@gmail.com>
2019-09-11 14:09:27 +0200

    Remove superflous efield storing.

--- NuRadioReco/framework/channel.py
+++ NuRadioReco/framework/channel.py
@@ -55,9 +55,6 @@ class Channel(NuRadioReco.framework.base_trace.BaseTrace):
         data = {'parameters': NuRadioReco.framework.parameter_serialization.serialize(self._parameters),
                 'id': self.get_id(),
                 'base_trace': base_trace_pkl}
-        if(self.__electric_field is not None):
-            if(not(mode == 'micro' or mode == 'mini')):
-                data['electric_field'] = self.__electric_field.serialize()
         return pickle.dumps(data, protocol=2)
 
     def deserialize(self, data_pkl):
