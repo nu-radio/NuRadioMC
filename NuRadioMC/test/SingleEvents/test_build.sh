a95a97e3dbf94be1d9523338305658a00efe9631
Anna Nelles <anna.nelles@gmail.com>
2019-09-11 14:08:15 +0200

    Add reference file and test whether nur files are equal.

--- NuRadioMC/test/SingleEvents/test_build.sh
+++ NuRadioMC/test/SingleEvents/test_build.sh
@@ -3,3 +3,5 @@
 NuRadioMC/test/SingleEvents/T02RunSimulation.py NuRadioMC/test/SingleEvents/1e18_output_reference.hdf5 NuRadioMC/test/SingleEvents/surface_station_1GHz.json NuRadioMC/test/SingleEvents/config.yaml NuRadioMC/test/SingleEvents/1e18_output.hdf5 NuRadioMC/test/SingleEvents/1e18_output.nur
 
 NuRadioMC/test/SingleEvents/T03validate.py NuRadioMC/test/SingleEvents/1e18_output.hdf5 NuRadioMC/test/SingleEvents/1e18_output_reference.hdf5
+
+NuRadioMC/test/SingleEvents/T05validate_nur_file.py NuRadioMC/test/SingleEvents/1e18_output.nur NuRadioMC/test/SingleEvents/1e18_output_reference.nur
