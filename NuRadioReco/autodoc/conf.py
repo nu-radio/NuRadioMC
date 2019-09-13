4d641520aa840b498c36e262d14dcff506201463
christophwelling <christoph.welling@desy.de>
2019-09-13 09:10:37 +0200

    Adds logo

--- NuRadioReco/autodoc/conf.py
+++ NuRadioReco/autodoc/conf.py
@@ -98,6 +98,8 @@ html_theme_options = {
     'stiky_navigation': True
 }
 
+html_logo = 'logo_small.png'
+
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
