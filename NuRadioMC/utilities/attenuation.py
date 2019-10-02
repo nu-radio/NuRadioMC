4446def4fa7aec60a82d480baafc5812b09d9bca
Christian Glaser <christian.glaser@uci.edu>
2019-10-02 21:23:11 +0000

    small adjustment of MB attenuation length, increases attenuation length by ~11%, make sure that input points are floats, otherwise int casting leads to inacuracies

--- NuRadioMC/utilities/attenuation.py
+++ NuRadioMC/utilities/attenuation.py
@@ -3,6 +3,7 @@ from NuRadioMC.utilities import units
 
 model_to_int = {"SP1" : 1, "GL1" : 2, "MB1" : 3}
 
+
 def fit_GL1(z):
     """
     Returns the attenuation length at 75 MHz as a function of depth for Greenland
@@ -22,14 +23,15 @@ def fit_GL1(z):
     else:
         att_length = np.zeros_like(z)
     for power, coeff in enumerate(fit_values):
-        att_length += coeff * z**power
+        att_length += coeff * z ** power
 
     if (not hasattr(att_length, '__len__')):
-        if ( att_length < min_length ):
+        if (att_length < min_length):
             att_length = min_length
     else:
         att_length[ att_length < min_length ] = min_length
     return att_length
+
 
 def get_temperature(z):
     """
@@ -43,7 +45,8 @@ def get_temperature(z):
     # from https://icecube.wisc.edu/~araproject/radio/#icetabsorption
 
     z2 = np.abs(z / units.m)
-    return 1.83415e-09 * z2**3 + (-1.59061e-08 * z2**2) + 0.00267687 * z2 + (-51.0696)
+    return 1.83415e-09 * z2 ** 3 + (-1.59061e-08 * z2 ** 2) + 0.00267687 * z2 + (-51.0696)
+
 
 def get_attenuation_length(z, frequency, model):
     """
@@ -89,8 +92,8 @@ def get_attenuation_length(z, frequency, model):
         return 1. / np.exp(a + bb * w)
     elif(model == "GL1"):
 
-        att_length_75 = fit_GL1(z/units.m)
-        att_length_f = att_length_75 - 0.55 * units.m * (frequency/units.MHz - 75)
+        att_length_75 = fit_GL1(z / units.m)
+        att_length_f = att_length_75 - 0.55 * units.m * (frequency / units.MHz - 75)
 
         min_length = 100 * units.m
         if(not hasattr(frequency, '__len__') and not hasattr(z, '__len__')):
@@ -103,21 +106,24 @@ def get_attenuation_length(z, frequency, model):
     elif(model == "MB1"):
         # 10.3189/2015JoG14J214 measured the depth-averaged attenuation length as a function of frequency
         # the derived parameterization assumed a reflection coefficient of 1
-        # however a reflection coefficient of 0.82 was measured which results in a slight increase of the derived 
+        # however a reflection coefficient of 0.82 was measured which results in a slight increase of the derived
         # attenution length (correction factor below)
         R = 0.82
         d_ice = 576 * units.m
-        att_length = 460 * units.m - 180 * units.m /units.GHz * frequency
+        att_length = 460 * units.m - 180 * units.m / units.GHz * frequency
         att_length *= (1 + att_length / (2 * d_ice) * np.log(R)) ** -1  # additional correction for reflection coefficient being less than 1.
 
         # The temperature dependence of the attenuation length is independent of the frequency dependence
         # the relationship between temparature and L is from 10.1063/1.363582
-        # the temparature profile of the Ross Ice shelf is from 10.1126/science.203.4379.433 and rescaled to the shelf 
-        # thickness of the ARIANNA site (almost linear from -28 - -2deg C. 
+        # the temparature profile of the Ross Ice shelf is from 10.1126/science.203.4379.433 and rescaled to the shelf
+        # thickness of the ARIANNA site (almost linear from -28 - -2deg C.
         # this theoretical depth dependence is scaled to match the depth-averaged measurement of the attenuation length.
-        d = -z * 420. / d_ice;
+        d = -z * 420. * units.m / d_ice;
         L = (1250.*0.08886 * np.exp(-0.048827 * (225.6746 - 86.517596 * np.log10(848.870 - (d)))))
-        att_length *= L/262.0
+        # this differs from the equation published in F. Wu PhD thesis UCI.
+        # 262m is supposed to be the depth averaged attenuation length but the
+        # integral (int(1/L, 420, 0)/420) ^ -1 = 231.21m and NOT 262m.
+        att_length *= L / 231.21 * units.m
 
         return att_length
     else:
