6ea27a86b4a163060594fb1c38390825888273f8
Lilly Pyras <lilly@s68dyn245.zeuthen.desy.de>
2019-11-13 17:11:15 +0100

    Updated trace envelope based threshold trigger. Now with coincidence check on multiple channels of a station and coincidence window.

--- NuRadioReco/modules/trigger/envelopeTrigger.py
+++ NuRadioReco/modules/trigger/envelopeTrigger.py
@@ -1,18 +1,19 @@
 from NuRadioReco.modules.base.module import register_run
 from NuRadioReco.utilities import units
-from NuRadioReco.framework.parameters import stationParameters as stnp
+from NuRadioReco.modules.trigger.highLowThreshold import get_majority_logic
 from NuRadioReco.framework.trigger import EnvelopeTrigger
+from scipy.signal import hilbert
 import numpy as np
 import time
 import logging
-from scipy.signal import hilbert
 
 logger = logging.getLogger('envelopeTrigger')
 
 
-def get_envelope_triggers(trace, threshold):
+def get_envelope_triggers(trace, threshold):  # define trigger constraint for each channel
+
     """
-    calculats a Hilbert-envelope based trigger
+    calculates a Hilbert-envelope based trigger
 
     Parameters
     ----------
@@ -25,7 +26,9 @@ def get_envelope_triggers(trace, threshold):
     triggered bins: array of bools
         the bins where the trigger condition is satisfied
     """
-    return np.abs(trace) >= threshold
+
+    return np.abs(hilbert(trace)) > threshold
+
 
 class triggerSimulator:
     """
@@ -42,44 +45,94 @@ class triggerSimulator:
     @register_run()
     def run(self, evt, station, det,
             threshold=60 * units.mV,
+            number_coincidences=2,
             triggered_channels=None,
+            coinc_window=500 * units.ns,
             trigger_name='default_envelope_trigger'):
         """
         simulate simple trigger logic, no time window, just threshold in all channels
 
         Parameters
         ----------
+        evt:
+            event
+        station:
+        det:
         threshold: float
             threshold above (or below) a trigger is issued, absolute amplitude
+        number_coincidences: int
+            number of channels that are required in coincidence to trigger a station
         triggered_channels: array of ints or None
             channels ids that are triggered on, if None trigger will run on all channels
+        coinc_window: float
+            time window in which number_coincidences channels need to trigger
         trigger_name: string
             a unique name of this particular trigger
         """
+        t = time.time()  # absolute time of system
 
         sampling_rate = station.get_channel(0).get_sampling_rate()
         dt = 1. / sampling_rate
 
+        triggered_bins_channels = []
+        channels_that_passed_trigger = []
+        if triggered_channels is None:  # caveat: all channels start at the same time
+            for channel in station.iter_channels():
+                channel_trace_start_time = channel.get_trace_start_time()
+                break
+        else:
+            channel_trace_start_time = station.get_channel(triggered_channels[0]).get_trace_start_time()
+
+        event_id = evt.get_id()
+        for channel in station.iter_channels():  # apply envelope trigger to each channel
+            channel_id = channel.get_id()
+            trace = channel.get_trace()
+            if triggered_channels is not None and channel_id not in triggered_channels:
+                logger.debug("skipping channel {}".format(channel_id))
+                continue
+            if channel.get_trace_start_time() != channel_trace_start_time:
+                logger.warning('Channel has a trace_start_time that differs from '
+                               '        the other channels. The trigger simulator may not work properly')
+
+            triggered_bins = get_envelope_triggers(trace, threshold)
+            triggered_bins_channels.append(triggered_bins)
+
+            if True in triggered_bins:
+                channels_that_passed_trigger.append(channel.get_id())
+
+        # check for coincidences with get_majority_logic(tts, number_of_coincidences=2,
+        # time_coincidence=32 * units.ns, dt=1 * units.ns)
+        # returns:
+        # triggered: bool; returns True if majority logic is fulfilled --> has_triggered
+        # triggered_bins: array of ints; the bins that fulfilled the trigger --> triggered_bins
+        # triggered_times = triggered_bins * dt: array of floats;
+        # the trigger times relative to the trace --> triggered_times
+
+        has_triggered, triggered_bins, triggered_times = get_majority_logic(triggered_bins_channels,
+                                                                            number_coincidences, coinc_window, dt)
+
         # set maximum signal amplitude
         max_signal = 0
-        event_id = evt.get_id()
-        for channel in station.iter_channels():
-            trace = channel.get_trace()
-            amplitude_envelope = np.abs(hilbert(trace))
 
-            max_signal = max(max_signal, amplitude_envelope.max()) # which channels had the highest amplitude
+        trigger = EnvelopeTrigger(trigger_name, threshold, triggered_channels, number_coincidences, coinc_window)
+        trigger.set_triggered_channels(channels_that_passed_trigger)
 
-        trigger = EnvelopeTrigger(trigger_name, threshold, triggered_channels)
-
-        if max_signal < threshold:
-            trigger.set_triggered(False)
-            logger.debug("station has NOT triggered")
-        else:
+        if has_triggered:
             trigger.set_triggered(True)
+            trigger.set_trigger_time(triggered_times.min() + channel_trace_start_time)  # trigger_time = time from the beginning of the trace
             logger.debug("station has triggered")
 
+        else:
+            trigger.set_triggered(False)
+            trigger.set_trigger_time(None)
+            logger.debug("station has NOT triggered")
 
         station.set_trigger(trigger)
+        self.__t += time.time() - t
 
     def end(self):
-        return
+        from datetime import timedelta
+        logger.setLevel(logging.INFO)
+        dt = timedelta(seconds=self.__t)
+        logger.info("total time used by this module is {}".format(dt))
+        return dt
