9c93034ef3bef75d95d26ccc04b9e4c9ea50645a
Christian Glaser <christian.glaser@uci.edu>
2019-11-14 00:06:33 +0000

    import units from NuRadioReco

--- NuRadioReco/examples/PhasedArray/SNR_curves/T01generate_event_list.py
+++ NuRadioReco/examples/PhasedArray/SNR_curves/T01generate_event_list.py
@@ -11,7 +11,7 @@ WARNING: This file needs NuRadioMC installed. https://github.com/nu-radio/NuRadi
 """
 
 from __future__ import absolute_import, division, print_function
-from NuRadioMC.utilities import units
+from NuRadioReco.utilities import units
 from NuRadioMC.EvtGen.generator import generate_eventlist_cylinder
 import numpy as np
 import os
@@ -19,40 +19,40 @@ import os
 z12 = -200 * units.m
 rho = 500 * units.m
 
-obs_angles = np.linspace(-10.,10.,41) * units.deg
+obs_angles = np.linspace(-10., 10., 41) * units.deg
 # Elevation of the vertex point seen from the middle point of the array
 
 for obs_angle in obs_angles:
 	# define simulation volume
-	zmin = z12 + rho*np.tan(obs_angle) - 0.1 * units.m
-	zmax = z12 + rho*np.tan(obs_angle)
+	zmin = z12 + rho * np.tan(obs_angle) - 0.1 * units.m
+	zmax = z12 + rho * np.tan(obs_angle)
 	rmin = rho
 	rmax = rho + 0.1 * units.m
 	print('Neutrino height', zmax)
 
 	theta_c = 55 * units.deg
-	theta_nu = 90*units.deg - theta_c - obs_angle
+	theta_nu = 90 * units.deg - theta_c - obs_angle
 
 	phimin = 0.*units.deg
-	phimax = 0.1*units.deg
+	phimax = 0.1 * units.deg
 
 	if (theta_nu < 0):
 		theta_nu = -theta_nu
-		phimin = 180*units.deg
-		phimax = 180.1*units.deg
+		phimin = 180 * units.deg
+		phimax = 180.1 * units.deg
 
-	thetas = np.array([theta_nu, theta_nu + 0.01*units.deg])
+	thetas = np.array([theta_nu, theta_nu + 0.01 * units.deg])
 	thetamins = thetas[0:-1]
 	thetamaxs = thetas[1:]
 	print(thetamins)
 	print(thetamaxs)
 
-	Es = np.array([18,18.1])
-	Es = 10**Es
+	Es = np.array([18, 18.1])
+	Es = 10 ** Es
 	Emins = Es[0:-1]
 	Emaxs = Es[1:]
 
-	flavours = [12] # no difference between neutrinos and antineutrinos for us
+	flavours = [12]  # no difference between neutrinos and antineutrinos for us
 
 	nevt = 2e3
 	nevt_perfile = 1e3
@@ -61,15 +61,15 @@ for obs_angle in obs_angles:
 
 		for flavour in flavours:
 
-			folder_angle = "{:.1f}deg_{}".format(thetamin/units.deg,flavour)
+			folder_angle = "{:.1f}deg_{}".format(thetamin / units.deg, flavour)
 			try:
 				os.mkdir(folder_angle)
 			except:
 				pass
 
 			folderlist = []
-			for ifolder, Emin,Emax in zip(range(len(Emins)),Emins,Emaxs):
-				folderlist.append("{:.2f}_{}_{}_{:.2e}_{:.2e}".format(thetamin/units.deg,flavour,str(ifolder).zfill(2),Emin,Emax))
+			for ifolder, Emin, Emax in zip(range(len(Emins)), Emins, Emaxs):
+				folderlist.append("{:.2f}_{}_{}_{:.2e}_{:.2e}".format(thetamin / units.deg, flavour, str(ifolder).zfill(2), Emin, Emax))
 			for folder in folderlist:
 				try:
 					os.mkdir(os.path.join(folder_angle, folder))
@@ -83,7 +83,7 @@ for obs_angle in obs_angles:
 					os.mkdir(input_dir)
 				except:
 					pass
-				outname = folder_angle+'/'+folder+'/input/'+folder+'.hdf5'
-				print(Emin/units.PeV, Emax/units.PeV)
+				outname = folder_angle + '/' + folder + '/input/' + folder + '.hdf5'
+				print(Emin / units.PeV, Emax / units.PeV)
 				print(outname)
 				generate_eventlist_cylinder(outname, nevt, Emin, Emax, rmin, rmax, zmin, zmax, full_rmin=rmin, full_rmax=rmax, full_zmin=zmin, full_zmax=zmax, thetamin=thetamin, thetamax=thetamax, phimin=phimin, phimax=phimax, start_event_id=1, flavor=[flavour], n_events_per_file=nevt_perfile, deposited=True)
