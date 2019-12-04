7987ec3de1fc5468f2825907ad686fcfb4f3b7a9
daniel-zid <sirzid@gmail.com>
2019-12-04 17:37:58 +0100

    Adding the first version of the envelope phased array with its examples

---
+++ NuRadioReco/examples/EnvelopePhasedArray/Effective_volume/T02RunPhasedRNO_shortband.py
@@ -0,0 +1,131 @@
+"""
+This file runs a phased array trigger simulation. The phased array configuration
+in this file is similar to one of the proposed ideas for RNO-G: 3 GS/s, 4 antennas
+at a depth of ~100 m, 30 primary phasing directions. In order to run, we need
+a detector file and a configuration file, included in this folder. To run
+the code, type:
+
+python T02RunPhasedRNO_shortband.py input_neutrino_file.hdf5 proposalcompact_100m_1.5GHz.json
+config_RNO.yaml output_NuRadioMC_file.hdf5 output_NuRadioReco_file.nur
+
+The antenna positions can be changed in the detector position. The config file
+defines de bandwidth for the noise RMS calculation. The properties of the phased
+array can be changed in the current file - phasing angles, triggering channels,
+bandpass filter and so on.
+
+This file calculates the effective volume for the (140-700) MHz band
+
+WARNING: this file needs NuRadioMC to be run.
+"""
+
+from __future__ import absolute_import, division, print_function
+import argparse
+# import detector simulation modules
+import NuRadioReco.modules.efieldToVoltageConverter
+import NuRadioReco.modules.trigger.simpleThreshold
+import NuRadioReco.modules.envelope_phasedarray.triggerSimulator
+import NuRadioReco.modules.channelResampler
+import NuRadioReco.modules.channelBandPassFilter
+import NuRadioReco.modules.channelGenericNoiseAdder
+from NuRadioReco.utilities import units
+from NuRadioMC.simulation import simulation
+import NuRadioReco.utilities.diodeSimulator
+import numpy as np
+import logging
+logging.basicConfig(level=logging.WARNING)
+logger = logging.getLogger("runstrawman")
+
+# initialize detector sim modules
+efieldToVoltageConverter = NuRadioReco.modules.efieldToVoltageConverter.efieldToVoltageConverter()
+efieldToVoltageConverter.begin(debug=False)
+triggerSimulator = NuRadioReco.modules.envelope_phasedarray.triggerSimulator.triggerSimulator()
+channelResampler = NuRadioReco.modules.channelResampler.channelResampler()
+channelBandPassFilter = NuRadioReco.modules.channelBandPassFilter.channelBandPassFilter()
+channelGenericNoiseAdder = NuRadioReco.modules.channelGenericNoiseAdder.channelGenericNoiseAdder()
+thresholdSimulator = NuRadioReco.modules.trigger.simpleThreshold.triggerSimulator()
+diodeSimulator = NuRadioReco.utilities.diodeSimulator.diodeSimulator()
+
+main_low_angle = -50 * units.deg
+main_high_angle = 50 * units.deg
+phasing_angles = np.arcsin( np.linspace( np.sin(main_low_angle), np.sin(main_high_angle), 30) )
+
+class mySimulation(simulation.simulation):
+
+    def _detector_simulation(self):
+        # start detector simulation
+        efieldToVoltageConverter.run(self._evt, self._station, self._det)  # convolve efield with antenna pattern
+        # downsample trace to 3 ns
+        new_sampling_rate = 3 * units.GHz
+        channelResampler.run(self._evt, self._station, self._det, sampling_rate=new_sampling_rate)
+
+        threshold_cut = True
+        # Forcing a threshold cut BEFORE adding noise for limiting the noise-induced triggers
+        if threshold_cut:
+
+            thresholdSimulator.run(self._evt, self._station, self._det,
+                                   threshold=0.75 * self._Vrms,
+                                   triggered_channels=None,  # run trigger on all channels
+                                   number_concidences=1,
+                                   trigger_name='simple_threshold')
+
+        # Bool for checking the noise triggering rate
+        check_only_noise = False
+        if check_only_noise:
+
+            for channel in self._station.iter_channels():  # loop over all channels (i.e. antennas) of the station
+
+                trace = channel.get_trace() * 0
+                channel.set_trace(trace, sampling_rate = new_sampling_rate)
+
+        noise = True
+
+        if noise:
+            max_freq = 0.5 / self._dt
+            norm = self._get_noise_normalization(self._station.get_id())  # assuming the same noise level for all stations
+            Vrms = self._Vrms / (norm / (max_freq)) ** 0.5  # normalize noise level to the bandwidth its generated for
+            channelGenericNoiseAdder.run(self._evt, self._station, self._det, amplitude=Vrms, min_freq=0 * units.MHz,
+                                         max_freq=max_freq, type='rayleigh')
+        # bandpass filter trace, the upper bound is higher then the sampling rate which makes it just a highpass filter
+        channelBandPassFilter.run(self._evt, self._station, self._det, passband=[132 * units.MHz, 1150 * units.MHz],
+                                  filter_type='butter', order=2)
+        channelBandPassFilter.run(self._evt, self._station, self._det, passband=[0, 700 * units.MHz],
+                                  filter_type='butter', order=10)
+
+        # run an ARA trigger. REMEMBER TO CHANGE THE FREQUENCIES FOR CALCULATING
+        # THE NOISE PARAMETERS
+        power_mean, power_std = diodeSimulator.calculate_noise_parameters(new_sampling_rate,
+                                                                          132 * units.MHz,
+                                                                          700 * units.MHz,
+                                                                          amplitude=self._Vrms)
+
+        # first run a simple threshold trigger
+        triggerSimulator.run(self._evt, self._station, self._det,
+                             threshold_factor=6.5, # see envelope phased trigger module for explanation
+                             power_mean=power_mean,
+                             power_std=power_std,
+                             triggered_channels=None,  # run trigger on all channels
+                             trigger_name='envelope_phasing', # the name of the trigger
+                             phasing_angles=phasing_angles,
+                             set_not_triggered=(not self._station.has_triggered("simple_threshold")),
+                             ref_index=1.55)
+
+
+parser = argparse.ArgumentParser(description='Run NuRadioMC simulation')
+parser.add_argument('--inputfilename', type=str,
+                    help='path to NuRadioMC input event list', default='0.00_12_00_1.00e+16_1.00e+19.hdf5')
+parser.add_argument('--detectordescription', type=str,
+                    help='path to file containing the detector description', default='proposalcompact_100m_1.5GHz.json')
+parser.add_argument('--config', type=str,
+                    help='NuRadioMC yaml config file', default='config_RNO.yaml')
+parser.add_argument('--outputfilename', type=str,
+                    help='hdf5 output filename', default='output_PA_RNO.hdf5')
+parser.add_argument('--outputfilenameNuRadioReco', type=str, nargs='?', default=None,
+                    help='outputfilename of NuRadioReco detector sim file')
+args = parser.parse_args()
+
+sim = mySimulation(eventlist=args.inputfilename,
+                            outputfilename=args.outputfilename,
+                            detectorfile=args.detectordescription,
+                            outputfilenameNuRadioReco=args.outputfilenameNuRadioReco,
+                            config_file=args.config)
+sim.run()
---
+++ NuRadioReco/examples/EnvelopePhasedArray/Noise_trigger_rate/noise_trigger_rate.py
@@ -0,0 +1,153 @@
+import NuRadioReco.modules.channelGenericNoiseAdder
+from NuRadioReco.utilities import units
+import matplotlib.pyplot as plt
+import numpy as np
+from scipy import constants
+import argparse
+channelGenericNoiseAdder = NuRadioReco.modules.channelGenericNoiseAdder.channelGenericNoiseAdder()
+from NuRadioReco.utilities.diodeSimulator import diodeSimulator
+import NuRadioReco.framework.channel
+from NuRadioReco.utilities.fft import time2freq, freq2time
+from scipy.signal import butter, freqs
+
+"""
+This code calculates the probability per window for the whole phased
+array (not just one beam) of triggering on noise, and the rate of
+noise triggers.
+
+The trigger is an envelope phased array trigger. According
+to the ARA folks, the maximum rate should be around 10 Hz for the whole
+phased array.
+
+This code calculates the delays for the beams, introduces noise in all the
+phased antennas, calculates the tunnel diode envelope and then phases the array
+into the directions given by default_angles. It verifies if there has been a
+trigger and repeats.
+
+Increasing the number of samples is equivalent to increasing the number of
+tries, since the code calculates the probability of randomly triggering a
+single window filled with noise.
+"""
+
+parser = argparse.ArgumentParser(description='calculates noise trigger rate for phased array')
+parser.add_argument('--ntries', type=int, help='number noise traces to which a trigger is applied for each threshold', default=100)
+args = parser.parse_args()
+
+main_low_angle = -50. * units.deg
+main_high_angle = 50. * units.deg
+default_angles = np.arcsin( np.linspace( np.sin(main_low_angle), np.sin(main_high_angle), 15) )
+
+def get_beam_rolls(ant_z, channel_list, phasing_angles=default_angles, time_step=2./3*units.ns, ref_index=1.78):
+    """
+    Calculates the delays needed for phasing the array.
+    """
+
+    beam_rolls = []
+    ref_z = (np.max(ant_z)+np.min(ant_z))/2
+
+    n_phased = len(channel_list)
+
+    for angle in phasing_angles:
+        subbeam_rolls = {}
+        for z, channel_id in zip(ant_z, range(n_phased)):
+            delay = (z-ref_z)/0.3 * ref_index * np.sin(angle)
+            roll = int(delay/time_step)
+            subbeam_rolls[channel_id] = roll
+        beam_rolls.append(subbeam_rolls)
+
+        return beam_rolls
+
+min_freq = 132*units.MHz
+max_freq = 700*units.MHz
+sampling_rate = 3*units.GHz
+window_width = 32
+
+primary_angles = np.arcsin( np.linspace( np.sin(main_low_angle), np.sin(main_high_angle), 30) )
+
+n_samples = 1000000 # number of samples
+time_step = 1./sampling_rate
+bandwidth = max_freq-min_freq
+amplitude = (300 * 50 * constants.k * bandwidth / units.Hz) ** 0.5
+
+Ntries = args.ntries # number of tries
+
+threshold_factors = [3.75]
+
+ratios = []
+
+ant_z_primary = [-98.5, -99.5, -100.5, -101.5] # primary antennas positions
+primary_channels = [0, 1, 2, 3] # channels used for primary beam
+beam_rolls = get_beam_rolls(ant_z_primary, primary_channels, primary_angles, time_step)
+
+n_beams = len(primary_angles)
+
+diode = diodeSimulator()
+power_mean, power_std = diode.calculate_noise_parameters(sampling_rate,
+                                                         min_freq,
+                                                         max_freq,
+                                                         amplitude=amplitude)
+
+# The output of the envelope is filtered between 100 and 200 MHz
+if n_samples % 2 == 0:
+    n_freqs = n_samples/2 + 1
+else:
+    n_freqs = (n_samples+1)/2
+frequencies = np.linspace(0, sampling_rate/2, n_freqs)
+passband = [100*units.MHz, 200*units.MHz]
+order = 6
+b, a = butter(order, passband, 'bandpass', analog=True)
+w, h = freqs(b, a, frequencies)
+
+for threshold_factor in threshold_factors:
+    prob_cross = 0
+
+    n_phased = len(primary_channels)
+    low_trigger  = power_mean - power_std * np.abs(threshold_factor)
+    low_trigger *= n_phased
+
+    for Ntry in range(Ntries):
+        noise_array = []
+
+        for iant in range(len(primary_channels)):
+
+            noise_trace = channelGenericNoiseAdder.bandlimited_noise(min_freq, max_freq, n_samples,
+                                                sampling_rate, amplitude, type='rayleigh')
+            channel = NuRadioReco.framework.channel.Channel(0)
+            channel.set_trace(noise_trace, sampling_rate)
+
+            enveloped_noise = diode.tunnel_diode(channel)
+
+            spectrum = time2freq(enveloped_noise, sampling_rate)
+            passed_enveloped_noise = freq2time(h * spectrum, sampling_rate)
+
+            noise_array.append(passed_enveloped_noise)
+
+        for subbeam_rolls in beam_rolls:
+
+            noise = None
+
+            for channel_id in range(len(primary_channels)):
+
+                if(noise is None):
+                    noise = np.roll(noise_array[channel_id], subbeam_rolls[channel_id])
+                else:
+                    noise += np.roll(noise_array[channel_id], subbeam_rolls[channel_id])
+
+            n_windows = int(n_samples/window_width)
+            n_windows = int(2*n_windows - 1)
+
+            threshold_passed = [ np.any(np.lib.stride_tricks.as_strided(noise[int(i_window*window_width/2):],(window_width,)) < low_trigger) \
+                                 for i_window in np.linspace(0,n_windows-1,n_windows) ]
+            threshold_passed = np.array(threshold_passed)
+
+            # If a phased direction triggers, the whole phased array triggers.
+            # That is why we multiply the probability by the number of beams, or phasing directions.
+            # This is justified as long as the probability is small and each direction triggers
+            # independently of the rest.
+            prob_cross += np.sum( threshold_passed )/n_windows * n_beams
+
+    ratio = float(prob_cross)/Ntries
+    trigger_frequency = 2*ratio/window_width / units.Hz
+    print('Threshold factor: {:.2f}, Fraction of noise triggers: {:.5f}%, Noise trigger rate: {:.2f} Hz'.format(threshold_factor, ratio*100., trigger_frequency))
+
+    ratios.append(ratio)
