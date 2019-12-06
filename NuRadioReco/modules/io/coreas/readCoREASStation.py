da0b6c4009570925120ab7a23beeefd4173bc97a
christophwelling <christoph.welling@desy.de>
2019-12-06 09:54:26 +0100

    Fixes readCoREASStation

--- NuRadioReco/modules/io/coreas/readCoREASStation.py
+++ NuRadioReco/modules/io/coreas/readCoREASStation.py
@@ -27,6 +27,7 @@ class readCoREASStation:
         self.__input_files = input_files
         self.__station_id = station_id
         self.__current_input_file = 0
+        self.__current_event = 0
 
     @register_run()
     def run(self, detector):
@@ -41,24 +42,25 @@ class readCoREASStation:
 
         """
         for input_file in self.__input_files:
-
+            self.__current_event = 0
             with h5py.File(input_file, "r") as corsika:
-                if corsika["highlevel"].values() == []:
+                if list(corsika["highlevel"].values()) == []:
                     logger.warning(" No highlevel quantities in simulated hdf5 files, weights will be one")
                     weights = np.ones(len(corsika['CoREAS']['observers']))
                 else:
-                    weights = coreas.calculate_simulation_weights(corsika["highlevel"].values()[0]["antenna_position"])
+                    weights = coreas.calculate_simulation_weights(list(corsika["highlevel"].values())[0]["antenna_position"])
 
                 for i, (name, observer) in enumerate(corsika['CoREAS']['observers'].items()):
-                    evt = NuRadioReco.framework.event.Event(corsika['inputs'].attrs['RUNNR'],
-                                corsika['inputs'].attrs['EVTNR'])  # create empty event
+                    evt = NuRadioReco.framework.event.Event(self.__current_input_file, self.__current_event)  # create empty event
                     station = NuRadioReco.framework.station.Station(self.__station_id)
                     sim_station = coreas.make_sim_station(self.__station_id, corsika, observer,
                                  detector.get_channel_ids(self.__station_id), weights[i])
 
                     station.set_sim_station(sim_station)
                     evt.set_station(station)
+                    self.__current_event += 1
                     yield evt
+            self.__current_input_file += 1
 
     def end(self):
         pass
