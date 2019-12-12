a76c74ac964f8f9b91ebe84c3de19d9838d6e05e
daniel-zid <sirzid@gmail.com>
2019-12-12 15:04:48 +0100

    Removing the pre-threshold for the phased array examples. Switching to diode windows instead of hardcoded window

--- NuRadioReco/examples/PhasedArray/Effective_volume/T02RunPhasedRNO.py
+++ NuRadioReco/examples/PhasedArray/Effective_volume/T02RunPhasedRNO.py
@@ -27,6 +27,7 @@ import NuRadioReco.modules.channelBandPassFilter
 import NuRadioReco.modules.channelGenericNoiseAdder
 from NuRadioReco.utilities import units
 from NuRadioMC.simulation import simulation
+from NuRadioReco.utilities.traceWindows import get_window_around_maximum
 import numpy as np
 import logging
 logging.basicConfig(level=logging.WARNING)
@@ -45,10 +46,13 @@ main_low_angle = -50 * units.deg
 main_high_angle = 50 * units.deg
 phasing_angles = np.arcsin(np.linspace(np.sin(main_low_angle), np.sin(main_high_angle), 30))
 
+<<<<<<< HEAD
 left_edge_around_max = 20 * units.ns
 right_edge_around_max = 40 * units.ns
 
 
+=======
+>>>>>>> Removing the pre-threshold for the phased array examples. Switching to diode windows instead of hardcoded window
 class mySimulation(simulation.simulation):
 
     def _detector_simulation(self):
@@ -58,27 +62,26 @@ class mySimulation(simulation.simulation):
         new_sampling_rate = 3 * units.GHz
         channelResampler.run(self._evt, self._station, self._det, sampling_rate=new_sampling_rate)
 
-        # Forcing a threshold cut BEFORE adding noise for limiting the noise-induced triggers
-        thresholdSimulator.run(self._evt, self._station, self._det,
-                             threshold=0.75 * self._Vrms,
-                             triggered_channels=None,  # run trigger on all channels
-                             number_concidences=1,
-                             trigger_name='simple_threshold')
-
-        max_times = []
+        cut_times = get_window_around_maximum(self._station)
 
         # Bool for checking the noise triggering rate
         check_only_noise = False
 
+<<<<<<< HEAD
         for channel in self._station.iter_channels():  # loop over all channels (i.e. antennas) of the station
 
             times = channel.get_times()
             argmax = np.argmax(np.abs(channel.get_trace()))
             max_times.append(times[argmax])
             if check_only_noise:
+=======
+        if check_only_noise:
+            for channel in self._station.iter_channels():
+>>>>>>> Removing the pre-threshold for the phased array examples. Switching to diode windows instead of hardcoded window
                 trace = channel.get_trace() * 0
                 channel.set_trace(trace, sampling_rate=new_sampling_rate)
 
+<<<<<<< HEAD
         left_time = np.min(max_times) - left_edge_around_max
         right_time = np.max(max_times) + right_edge_around_max
 
@@ -89,6 +92,8 @@ class mySimulation(simulation.simulation):
             channelGenericNoiseAdder.run(self._evt, self._station, self._det, amplitude=self._Vrms, min_freq=0 * units.MHz,
                                          max_freq=max_freq, type='rayleigh', bandwidth=norm)
 =======
+=======
+>>>>>>> Removing the pre-threshold for the phased array examples. Switching to diode windows instead of hardcoded window
         noise = True
 
         if noise:
@@ -106,6 +111,7 @@ class mySimulation(simulation.simulation):
         channelBandPassFilter.run(self._evt, self._station, self._det, passband=[0, 700 * units.MHz],
                                   filter_type='butter', order=10)
 
+<<<<<<< HEAD
         # Setting the trace values far from the amplitude maxima to zero
         # to reduce the noise trigger rate
         for channel in self._station.iter_channels():
@@ -142,6 +148,10 @@ class mySimulation(simulation.simulation):
         # first run a simple threshold trigger
         trig = triggerSimulator.run(self._evt, self._station, self._det,
 >>>>>>> Changing trigger name
+=======
+        # first run a simple threshold trigger
+        triggerSimulator.run(self._evt, self._station, self._det,
+>>>>>>> Removing the pre-threshold for the phased array examples. Switching to diode windows instead of hardcoded window
                              threshold=2.2 * self._Vrms, # see phased trigger module for explanation
 >>>>>>> Fixing bug regarding the noise amplitude. Now noise is calculated with the correct sampling rate
                              triggered_channels=None,  # run trigger on all channels
@@ -152,12 +162,9 @@ class mySimulation(simulation.simulation):
 >>>>>>> Changing trigger name
                              phasing_angles=phasing_angles,
                              secondary_phasing_angles=None,
-                             set_not_triggered=(not self._station.has_triggered("simple_threshold")),
                              coupled=False,
-                             ref_index=1.75)
-
-        if trig:
-            print('ay mamarl')
+                             ref_index=1.75,
+                             cut_times=cut_times)
 
 
 parser = argparse.ArgumentParser(description='Run NuRadioMC simulation')
