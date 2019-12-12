880609b8f20956085da350f62783443e9eacef50
Christian Glaser <christian.glaser@uci.edu>
2019-12-12 02:05:14 +0000

    start web interface

---
+++ NuRadioReco/detector/webinterface/apps/menu.py
@@ -0,0 +1,11 @@
+import dash_core_components as dcc
+import dash_html_components as html
+from dash.dependencies import Input, Output
+
+from app import app
+
+app.layout = html.Div([
+    html.H3('Welcome to NuRadioReco database web interface'),
+    dcc.Link('Add a new amplifier', href='/apps/add_amps')
+])
+
