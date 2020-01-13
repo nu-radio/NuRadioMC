1f2a6f3c9848ce4d827d51f1c80c7f1844740771
Christian Glaser <christian.glaser@uci.edu>
2020-01-13 17:42:02 +0000

    fix unit conversion. Altitudes are always in meters

--- NuRadioReco/detector/detector.py
+++ NuRadioReco/detector/detector.py
@@ -404,7 +404,7 @@ class Detector(object):
         if(res['pos_northing'] is not None):
             northing = res['pos_northing'] * unit_xy
         if(res['pos_altitude'] is not None):
-            altitude = res['pos_altitude'] * unit_xy
+            altitude = res['pos_altitude']
         return np.array([easting, northing, altitude])
 
     def get_absolute_position_site(self, site):

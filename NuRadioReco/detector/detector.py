fix unit conversion. Altitudes are always in meters

NuRadioReco/detector/detector.py
@@ -404,7 +404,7 @@ class Detector(object):
         if(res['pos_northing'] is not None):
             northing = res['pos_northing'] * unit_xy
         if(res['pos_altitude'] is not None):
-            altitude = res['pos_altitude'] * unit_xy
+            altitude = res['pos_altitude']
         return np.array([easting, northing, altitude])
 
     def get_absolute_position_site(self, site):