1acac4ee2ccc8ddbabb8afc3ac01a100d66f0ac6
lpyras <pyras@posteo.de>
2020-02-04 13:44:11 +0100

    Add data for analoge components for RNO-G and RNO-G files

---
+++ NuRadioReco/detector/RNO-G/analog_components.py
@@ -0,0 +1,84 @@
+import numpy as np
+from scipy.interpolate import interp1d
+import os
+from radiotools import helper as hp
+from NuRadioReco.utilities import units, io_utilities
+import logging
+logger = logging.getLogger('analog_components')
+
+
+def load_amplifier_response(path=os.path.dirname(os.path.realpath(__file__))):  # here we use log data
+    """
+    Read out amplifier gain and phase. Currently only examples have been implemented.
+    Needs a better structure in the future, possibly with database.
+    """
+    amplifier_response = {}
+
+    ph = os.path.join(path, 'HardwareResponses/surface_-60dBm_chan0_LogA_20dB.csv')
+    amp_gain_discrete = np.loadtxt(ph, delimiter=',', skiprows=7, usecols= (0, 5))
+    amp_phase_discrete = np.loadtxt(ph, delimiter=',', skiprows=7, usecols=(0, 6))
+
+
+    # Convert to GHz and add 60dB/40dB for attenuation in measurement circuit
+    amp_gain_discrete[:, 0] *= units.Hz
+    amp_gain_discrete[:, 1] += 20
+
+    amp_gain_db_f = interp1d(amp_gain_discrete[:, 0], amp_gain_discrete[:, 1],
+                             bounds_error=False, fill_value=0)  # all requests outside of measurement range are set to 0
+
+    def get_amp_gain(ff):
+        amp_gain_db = amp_gain_db_f(ff)
+        amp_gain = 10 ** (amp_gain_db / 20.)
+        return amp_gain
+
+    # Convert to MHz and broaden range
+    amp_phase_discrete[:, 0] *= units.Hz
+
+    amp_phase_f = interp1d(amp_phase_discrete[:, 0], np.unwrap(np.deg2rad(amp_phase_discrete[:, 1])),
+                           bounds_error=False, fill_value=0)  # all requests outside of measurement range are set to 0
+
+    def get_amp_phase(ff):
+        amp_phase = amp_phase_f(ff)
+        return np.exp(1j * amp_phase)
+
+    amplifier_response['gain'] = get_amp_gain
+    amplifier_response['phase'] = get_amp_phase
+
+    return amplifier_response
+
+def load_amp_measurement(amp_measurement='surface_-60dBm_chan0_RI_20dB'):  # here we use Real and Imaginary
+    """
+    load individual amp measurement from file and buffer interpolation function
+    """
+    filename = os.path.join(os.path.dirname(__file__), 'HardwareResponses/', amp_measurement+".csv")
+    ff = np.loadtxt(filename, delimiter=',', skiprows=7, usecols=(0)) # frequency
+    response = data[amp_measurement]['response']
+    gain = np.loadtxt(filename, delimiter=',', skiprows=7, usecols=(0))
+    phase = np.unwrap(np.angle(response))
+    amp_phase_f = interp1d(ff, phase, bounds_error=False, fill_value=0)  # all requests outside of measurement range are set to 0
+    amp_gain_f = interp1d(ff, gain, bounds_error=False, fill_value=1)  # all requests outside of measurement range are set to 1
+    
+    def get_response(ff):
+        return amp_gain_f(ff) * np.exp(1j * amp_phase_f(ff))
+    
+    amp_measurements[amp_measurement] = get_response
+
+# amp responses do not occupy a lot of memory, pre load all responses
+amplifier_response = {}
+for amp_type in ['100', '200', '300']:
+    amplifier_response[amp_type] = load_amplifier_response(amp_type)
+    
+amp_measurements = {}  # buffer for amp measurements
+
+
+def get_amplifier_response(ff, amp_type, amp_measurement=None):
+    if(amp_measurement is not None):
+        if amp_measurement not in amp_measurements:
+            load_amp_measurement(amp_measurement)
+        return amp_measurements[amp_measurement](ff)
+    elif(amp_type in amplifier_response.keys()):
+        return amplifier_response[amp_type]['gain'](ff) * amplifier_response[amp_type]['phase'](ff)
+    else:
+        logger.error("Amplifier response for type {} not implemented, returning None".format(amp_type))
+        return None
+
---
+++ NuRadioReco/modules/RNO-G/hardwareResponseIncorporator.py
@@ -0,0 +1,196 @@
+from NuRadioReco.modules.base.module import register_run
+from NuRadioReco.detector.ARIANNA import analog_components
+import numpy as np
+from NuRadioReco.utilities import units, fft
+import time
+import logging
+
+
+
+
+class hardwareResponseIncorporator:
+    """
+    Incorporates the gain and phase induced by the ARIANNA hardware.
+
+
+    """
+
+    def __init__(self):
+        self.logger = logging.getLogger("NuRadioReco.hardwareResponseIncorporator")
+        self.__debug = False
+        self.__time_delays = {}
+        self.__t = 0
+        self.__mingainlin = None
+        self.begin()
+
+
+    def begin(self, debug=False):
+        self.__debug = debug
+        #analog_components
+
+    def get_filter(self, frequencies, station_id, channel_id, det, sim_to_data=False, phase_only=False, mode=None, mingainlin=None):
+        """
+        helper function to return the filter that the module applies.
+
+        Parameters
+        -----------
+        frequencies: array of floats
+            the frequency array for which the filter should be returned
+        station_id: int
+            the station id
+        channel_id: int
+            the channel id
+        det: detector instance
+            the detector
+        the remaining arguments are the arguments of the run method!
+
+        Returns: array of complex floats
+            the complex filter amplitudes
+        """
+        amp_type = det.get_amplifier_type(station_id, channel_id)
+        amp_measurement = det.get_amplifier_measurement(station_id, channel_id)
+        amp_response = analog_components.get_amplifier_response(frequencies, amp_type=amp_type,
+                                                                    amp_measurement=amp_measurement)
+
+        if mingainlin!=None:
+            mingainlin = float(mingainlin)
+            ampmax= np.max(np.abs(amp_response))
+            iamp_gain_low = np.where(np.abs(amp_response)<(mingainlin*ampmax))
+            amp_response[iamp_gain_low]=(mingainlin*ampmax) * np.exp(1j * np.angle(amp_response[iamp_gain_low]))
+
+        cable_response = analog_components.get_cable_response_parametrized(frequencies,
+                                   *det.get_cable_type_and_length(station_id, channel_id))
+        if(mode=='phase_only'):
+            cable_response = np.ones_like(cable_response) * np.exp(1j * np.angle(cable_response))
+            amp_response = np.ones_like(amp_response) * np.angle(amp_response)
+        elif(mode=='relative'):
+            ampmax= np.max(np.abs(amp_response))
+            amp_response /= ampmax
+
+        if sim_to_data:
+            return amp_response * cable_response
+        else:
+            return 1./ (amp_response * cable_response)
+
+    @register_run()
+    def run(self, evt, station, det, sim_to_data=False, phase_only=False,mode=None,mingainlin=None):
+        """
+        Switch sim_to_data to go from simulation to data or otherwise.
+        The option zero_noise can be used to zero the noise around the pulse. It is unclear, how useful this is.
+
+        Parameters
+        -----------
+        sim_to_data: bool (default False)
+            if False, deconvolve the hardware response
+            if True, convolve with the hardware response
+        phase_only: bool (default False)
+            if True, only the phases response is applied but not the amplitude response
+        mode: string
+            'phase_only': only the phases response is applied but not the amplitude response
+                (identical to phase_only=True )
+            'relativ': gain of amp is divided by maximum of the gain, i.e. at the maximum of the
+                filter response is 1 (before applying cable response). This makes it easier
+                to compare the filtered to unfiltered signal
+            None : default, gain and phase effects are applied 'normally'
+        mingainlin: float
+            In frequency ranges where the gain gets very small, the reconstruction of the original signal (obtained by
+            dividing the measured signal by the gain) leads to excessively high values, due to the effect of
+            post-amplifier noise. In order to mitigate this effect, a minimum gain (linear scale!) as fraction of the
+            maximum gain can be defined. If specified, any gain value smaller than mingainlin will be replaced by mingainlin.
+            Note: The adjustment to the minimal gain is NOT visible when getting the amp response from
+            analog_components.get_amplifier_response()
+        """
+
+        self.__mingainlin = mingainlin
+
+        if (phase_only):
+            mode='phase_only'
+            self.logger.warning('Please use option mode=''phase_only'' in the future, use of option phase_only will be phased out')
+
+        t = time.time()
+
+        for channel in station.iter_channels():
+            frequencies = channel.get_frequencies()
+            trace_fft = channel.get_frequency_spectrum()
+
+            trace_fft *= self.get_filter(frequencies, station.get_id(), channel.get_id(), det, sim_to_data, phase_only, mode, mingainlin)
+            # zero first bins to avoid DC offset
+            trace_fft[0] = 0
+
+            # hardwareResponse incorporator should always be used in conjunction with bandpassfilter
+            # otherwise, noise will be blown up
+            channel.set_frequency_spectrum(trace_fft, channel.get_sampling_rate())
+
+            if not sim_to_data:
+                # Include cable delays
+                cable_delay = det.get_cable_delay(station.get_id(), channel.get_id())
+                self.logger.debug("cable delay of channel {} is {}ns".format(channel.get_id(), cable_delay / units.ns))
+                channel.add_trace_start_time(-cable_delay)
+
+
+        self.__t += time.time() - t
+
+    def end(self):
+        from datetime import timedelta
+        self.logger.setLevel(logging.INFO)
+        dt = timedelta(seconds=self.__t)
+        self.logger.info("total time used by this module is {}".format(dt))
+        return dt
+
+    def __calculate_time_delays_cable(self):
+        """
+        helper function to calculate the time delay of the amp for a delta pulse
+        """
+        sampling_rate = 10 * units.GHz  # assume a huge sampling rate to have a good time resolution
+        n = 2 ** 12
+        trace = np.zeros(n)
+        trace[n // 2] = 1
+        max_time = trace.argmax() / sampling_rate
+        spec = fft.time2freq(trace, sampling_rate)
+        ff = np.fft.rfftfreq(n, 1. / sampling_rate)
+        response = analog_components.get_cable_response(ff)
+        response_gain = response['gain']
+        response_phase = response['phase']
+        trace2 = fft.freq2time(spec * response_gain * response_phase, sampling_rate)
+#         import matplotlib.pyplot as plt
+#         fig, ax = plt.subplots(1, 1)
+#         ax.plot(trace)
+#         ax.plot(trace2)
+#         plt.show()
+        max_time2 = np.abs(trace2).argmax() / sampling_rate
+        return max_time2 - max_time
+
+    def __calculate_time_delays_amp(self, amp_type):
+        """
+        helper function to calculate the time delay of the amp for a delta pulse
+        """
+        amp_response_f = analog_components.amplifier_response[amp_type]
+        sampling_rate = 10 * units.GHz  # assume a huge sampling rate to have a good time resolution
+        n = 2 ** 12
+        trace = np.zeros(n)
+        trace[n // 2] = 1
+        max_time = trace.argmax() / sampling_rate
+        spec = fft.time2freq(trace, sampling_rate)
+        ff = np.fft.rfftfreq(n, 1. / sampling_rate)
+        amp_response_gain = amp_response_f['gain'](ff)
+        amp_response_phase = amp_response_f['phase'](ff)
+        mask = (ff < 70 * units.MHz) & (ff > 40 * units.MHz)
+        spec[~mask] = 0
+        trace2 = fft.freq2time(spec * amp_response_gain * amp_response_phase, sampling_rate)
+#         import matplotlib.pyplot as plt
+#         fig, ax = plt.subplots(1, 1)
+#         ax.plot(trace)
+#         ax.plot(trace2)
+#         plt.show()
+        max_time2 = np.abs(trace2).argmax() / sampling_rate
+        return max_time2 - max_time
+
+    def get_time_delay(self, amp_type):
+        if(amp_type not in self.__time_delays.keys()):
+            # not yet calculated -> calculate the time delay
+            self.__time_delays[amp_type] = self.__calculate_time_delays_amp(amp_type)
+            self.logger.info("time delays of amp {} have not yet been calculated -> calculating -> time delay is {:.2f} ns".format(amp_type, self.__time_delays[amp_type] / units.ns))
+        return self.__time_delays[amp_type]
+
+    def get_mingainlin(self):
+        return self.__mingainlin
