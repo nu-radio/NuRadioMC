0e330b0357248afcd316bb3d70c6c1ece1b0a262
Christian Glaser <christian.glaser@uci.edu>
2020-02-20 00:56:04 +0000

    change parameters

--- NuRadioReco/framework/parameters.py
+++ NuRadioReco/framework/parameters.py
@@ -56,8 +56,7 @@ class electricFieldParameters(Enum):
     reflection_coefficient_theta = 14  # for reflected rays: the complex Fresnel reflection coefficient of the eTheta component
     reflection_coefficient_phi = 15  # for reflected rays: the complex Fresnel reflection coefficient of the ePhi component
     cr_spectrum_quadratic_term = 16  # result of the second order correction to the spectrum fitted by the voltageToAnalyticEfieldConverter
-    charge_excess_profile_index = 17  # a dictionary of the indizes of the used charge excess profiles per shower type (applies for ARZ models)
-    shower_energy = 18  # a dictionary of the shower energy per shower type (EM or HAD)
+    shower_id = 17  # reference to the shower corresponding for this efield
 
 
 class ARIANNAParameters(Enum):  # this class stores parameters specific to the ARIANNA data taking
@@ -91,6 +90,9 @@ class showerParameters(Enum):
     magnetic_field_rotation = 102
     magnetic_field_vector = 103  # magnetic field used in simulation in local coordinate system
     observation_level = 104  # altitude a.s.l where the particles are stored
+
+    charge_excess_profile_id = 105  # the id of the charge-excess profile used in the ARZ Askaryan calculation
+    type = 106  # for neutrino induces showers in ice: can be "HAD" or "EM"
 
 
 class eventParameters(Enum):
