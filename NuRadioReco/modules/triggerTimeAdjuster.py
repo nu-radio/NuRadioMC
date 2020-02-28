a163854825b8e1630128a988a68d1076a6b8125c
Christian Glaser <christian.glaser@uci.edu>
2020-02-28 20:46:51 +0000

    remove print statements

--- NuRadioReco/modules/triggerTimeAdjuster.py
+++ NuRadioReco/modules/triggerTimeAdjuster.py
@@ -50,8 +50,6 @@ class triggerTimeAdjuster:
             trigger_time = trigger.get_trigger_time()
             for channel in station.iter_channels():
                 trigger_time_channel = trigger_time - channel.get_trace_start_time()
-                print(f"trace start time {channel.get_trace_start_time():.1f}")
-                print(f"trigger time {trigger_time:.1f}")
 
                 trace = channel.get_trace()
                 trace_length = len(trace)
@@ -66,7 +64,6 @@ class triggerTimeAdjuster:
                     samples_before_trigger = int(self.__pre_trigger_time * sampling_rate)
                     rel_station_time_samples = 0
                     cut_samples_beginning = 0
-                    print(samples_before_trigger, trigger_time_sample)
                     if(samples_before_trigger < trigger_time_sample):
                         cut_samples_beginning = trigger_time_sample - samples_before_trigger
                         roll_by = 0
