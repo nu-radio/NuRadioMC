f29ef52465630442d44e97c4d197b7cc501e7c63
Christian Glaser <christian.glaser@uci.edu>
2020-03-06 17:48:27 +0000

    update exampe, remove ice-water correction in debug output

--- NuRadioMC/simulation/simulation.py
+++ NuRadioMC/simulation/simulation.py
@@ -1001,9 +1001,6 @@ class simulation():
 
     def calculate_Veff(self):
         # calculate effective
-        density_ice = 0.9167 * units.g / units.cm ** 3
-        density_water = 1000 * units.kg / units.m ** 3
-
         n_triggered = np.sum(self._mout['triggered'])
         n_triggered_weighted = np.sum(self._mout['weights'][self._mout['triggered']])
         logger.warning(f'fraction of triggered events = {n_triggered:.0f}/{self._n_events:.0f} = {n_triggered / self._n_events:.3f} (sum of weights = {n_triggered_weighted:.2f})')
@@ -1019,7 +1016,7 @@ class simulation():
             rmax = self._fin_attrs['rmax']
             dZ = self._fin_attrs['zmax'] - self._fin_attrs['zmin']
             V = np.pi * (rmax ** 2 - rmin ** 2) * dZ
-        Veff = V * density_ice / density_water * 4 * np.pi * n_triggered_weighted / self._n_events
+        Veff = V * 4 * np.pi * n_triggered_weighted / self._n_events
         logger.warning("Veff = {:.4g} km^3 sr".format(Veff / units.km ** 3))
 
     def _get_em_had_fraction(self, inelasticity, inttype, flavor):
