81774f1a6d058c211773bceecc64553c1cef2e74
Geoffrey <ggaswint@uci.edu>
2020-03-10 16:04:20 -0700

    force polarization to be entirely eTheta or ePhi in efield reconstruction using VoltageToEfieldConverter

--- NuRadioReco/modules/voltageToEfieldConverter.py
+++ NuRadioReco/modules/voltageToEfieldConverter.py
@@ -120,7 +120,7 @@ class voltageToEfieldConverter:
         pass
 
     @register_run()
-    def run(self, evt, station, det, debug=False, debug_plotpath=None, use_channels=[0, 1, 2, 3], use_MC_direction=False, force_Polarization=False):
+    def run(self, evt, station, det, debug=False, debug_plotpath=None, use_channels=[0, 1, 2, 3], use_MC_direction=False, force_Polarization=''):
         """
         run method. This function is executed for each event
 
@@ -137,8 +137,9 @@ class voltageToEfieldConverter:
         use_channels: array of ints
             the channel ids to use for the electric field reconstruction
         use_MC_direction:
-        force_Polarization:
-            Only reconstructs eTheta polarization of electric field, assuming ePhi is 0
+        force_Polarization: str
+            if eTheta or ePhi, then only reconstructs chosen polarization of electric field,
+            assuming the other is 0. Otherwise, reconstructs electric field for both eTheta and ePhi
         """
         event_time = station.get_station_time()
         station_id = station.get_id()
@@ -168,8 +169,10 @@ class voltageToEfieldConverter:
         E2[mask] = (V[-1] - efield_antenna_factor[-1][0] * E1)[mask] / efield_antenna_factor[-1][1][mask]
         # solve it in a vectorized way
         efield3_f = np.zeros((2, n_frequencies), dtype=np.complex)
-        if force_Polarization:
+        if force_Polarization == 'eTheta':
             efield3_f[:1, mask] = np.moveaxis(stacked_lstsq(np.moveaxis(efield_antenna_factor[:, 0, mask], 1, 0)[:,:,np.newaxis], np.moveaxis(V[:, mask], 1, 0)), 0, 1)
+        elif force_Polarization == 'ePhi':
+            efield3_f[1:, mask] = np.moveaxis(stacked_lstsq(np.moveaxis(efield_antenna_factor[:, 1, mask], 1, 0)[:,:,np.newaxis], np.moveaxis(V[:, mask], 1, 0)), 0, 1)
         else:
             efield3_f[:, mask] = np.moveaxis(stacked_lstsq(np.moveaxis(efield_antenna_factor[:, :, mask], 2, 0), np.moveaxis(V[:, mask], 1, 0)), 0, 1)
         # add eR direction
