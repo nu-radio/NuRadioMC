310603c7430d716fc71f37d0173fda6586216d9c
Christian Glaser <christian.glaser@uci.edu>
2020-03-12 21:52:43 +0000

    implement adding of DRAB units

--- NuRadioReco/detector/webinterface/apps/menu.py
+++ NuRadioReco/detector/webinterface/apps/menu.py
@@ -3,6 +3,8 @@ import dash_html_components as html
 
 layout = html.Div([
     html.H3('Welcome to NuRadioReco database web interface'),
-    dcc.Link('Add S parameter measurement of surface board', href='/apps/add_surface_board')
+    dcc.Link('Add S parameter measurement of surface board', href='/apps/add_surface_board'),
+    html.Br(),
+    dcc.Link('Add another DRAB unit measurement', href='/apps/add_DRAB')
 ])
 
