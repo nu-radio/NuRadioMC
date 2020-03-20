b34738d5c9326558a57d3cd7d8b687c69bd40158
christophwelling <christoph.welling@desy.de>
2020-03-20 12:31:06 +0100

    Adds tooltips

---
+++ NuRadioMC/examples/SignalVisualization/antennas.py
@@ -0,0 +1,20 @@
+
+antenna_options = [
+    {
+        'label': 'Bicone',
+        'value': 'bicone_v8_InfFirn',
+        'description': 'Bicone antenna used by the ARA experiment'},
+    {
+        'label': 'LPDA',
+        'value': 'createLPDA_100MHz_InfFirn',
+        'description': ('Logarithmic-periodic dipole antenna used by ARIANNA'
+            'and the surface component of RNO-G')},
+    {
+        'label': 'RNO V-pol',
+        'value': 'greenland_vpol_InfFirn',
+        'description': 'Vertical polarization antenna used by RNO-G'},
+    {
+        'label': 'RNO Fourslot',
+        'value': 'fourslot_InfFirn',
+        'description': 'Fourslot horizontal polarization antenna considered for RNO-G'}
+]
