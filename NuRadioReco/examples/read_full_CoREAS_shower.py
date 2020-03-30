aa0a87cbe9e70e45c1144b650f73dbf54e35661e
christophwelling <christoph.welling@desy.de>
2020-03-30 15:02:28 +0200

    Adds eventTypeIdentifier to read_full_CoREAS_shower example

--- NuRadioReco/examples/read_full_CoREAS_shower.py
+++ NuRadioReco/examples/read_full_CoREAS_shower.py
@@ -1,6 +1,7 @@
 import NuRadioReco.modules.io.coreas.readCoREASShower
 import NuRadioReco.modules.io.eventWriter
 import NuRadioReco.modules.efieldToVoltageConverter
+import NuRadioReco.modules.eventTypeIdentifier
 from NuRadioReco.utilities import units
 from NuRadioReco.framework.parameters import showerParameters as shP
 
@@ -36,12 +37,14 @@ readCoREASShower = NuRadioReco.modules.io.coreas.readCoREASShower.readCoREASShow
 readCoREASShower.begin(args.inputfilename, det, set_ascending_run_and_event_number=args.set_run_number)
 efieldToVoltageConverter = NuRadioReco.modules.efieldToVoltageConverter.efieldToVoltageConverter()
 efieldToVoltageConverter.begin()
+eventTypeIdentifier = NuRadioReco.modules.eventTypeIdentifier.eventTypeIdentifier()
 eventWriter = NuRadioReco.modules.io.eventWriter.eventWriter()
 eventWriter.begin(args.output_filename)
 
 for event, gen_det in readCoREASShower.run():
     print('Event {} {}'.format(event.get_run_number(), event.get_id()))
     for station in event.get_stations():
+        eventTypeIdentifier.run(event, station, 'forced', 'cosmic_ray')
         efieldToVoltageConverter.run(event, station, gen_det)
     eventWriter.run(event, gen_det)
 
