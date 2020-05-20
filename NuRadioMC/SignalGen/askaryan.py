d5082c1cd0706a23b8d320fa1b0419087d5f4910
Christian Glaser <christian.glaser@uci.edu>
2020-05-20 14:46:13 +0000

    change that allows to retrive a specific random realization of a shower. The ARZ and Alvarez2009 module accept a variable as input that specifies the shower realization. All modules can return a dictionary with additional information (which is used to return the information about which random shower was selected)

--- NuRadioMC/SignalGen/askaryan.py
+++ NuRadioMC/SignalGen/askaryan.py
@@ -13,7 +13,7 @@ def set_log_level(level):
 
 
 def get_time_trace(energy, theta, N, dt, shower_type, n_index, R, model, interp_factor=None, interp_factor2=None,
-                   same_shower=False, seed=None, **kwargs):
+                   same_shower=False, seed=None, full_output=False, **kwargs):
     """
     returns the Askaryan pulse in the time domain of the eTheta component
 
@@ -58,17 +58,27 @@ def get_time_trace(energy, theta, N, dt, shower_type, n_index, R, model, interp_
         more details
     seed: None or int
         the random seed for the Askaryan modules
+    full_output: bool (default False)    
+        if True, askaryan modules can return additional output
 
     Returns
     -------
     time trace: array
         the amplitudes for each time bin
+    additional information: dict
+        only available if `full_output` enabled
 
     """
+    shower_type = shower_type.upper()  # some convenience to be sloppy with upper/lower case
+    tmp = None
+    additional_output = {}
     if(energy == 0):
-        return np.zeros(N)
+        tmp = np.zeros(N)
     if model in par.get_parametrizations():
-        return par.get_time_trace(energy, theta, N, dt, shower_type, n_index, R, model, seed=seed, same_shower=same_shower)
+        tmp = par.get_time_trace(energy, theta, N, dt, shower_type, n_index, R, model, seed=seed, same_shower=same_shower,
+                                     full_output=full_output)
+        if(full_output):
+            tmp, additional_output = tmp
     elif(model == 'HCRB2017'):
         from NuRadioMC.SignalGen import HCRB2017
         is_em_shower = None
@@ -84,7 +94,7 @@ def get_time_trace(energy, theta, N, dt, shower_type, n_index, R, model, interp_
             LPM = kwargs['LPM']
         if('a' in kwargs):
             a = kwargs['a']
-        return HCRB2017.get_time_trace(energy, theta, N, dt, is_em_shower, n_index, R, LPM, a)[1]
+        tmp = HCRB2017.get_time_trace(energy, theta, N, dt, is_em_shower, n_index, R, LPM, a)[1]
     elif(model == 'ARZ2019' or model == 'ARZ2020'):
         from NuRadioMC.SignalGen.ARZ import ARZ
         gARZ = ARZ.ARZ(arz_version=model, seed=seed)
@@ -93,18 +103,23 @@ def get_time_trace(energy, theta, N, dt, shower_type, n_index, R, model, interp_
 
         if(interp_factor2 is not None):
             gARZ.set_interpolation_factor2(interp_factor2)
-        return gARZ.get_time_trace(energy, theta, N, dt, shower_type, n_index, R, same_shower=same_shower, **kwargs)[1]
+        tmp = gARZ.get_time_trace(energy, theta, N, dt, shower_type, n_index, R, same_shower=same_shower, **kwargs)[1]
+        additional_output['iN'] = gARZ.get_last_shower_profile_id()[shower_type]
 
     elif(model == 'spherical'):
         amplitude = 1. * energy / R
         trace = np.zeros(N)
         trace[N // 2] = amplitude
-        return trace
+        tmp = trace
     else:
         raise NotImplementedError("model {} unknown".format(model))
+    if(full_output):
+        return tmp, additional_output
+    else:
+        return tmp
 
 
-def get_frequency_spectrum(energy, theta, N, dt, shower_type, n_index, R, model, **kwargs):
+def get_frequency_spectrum(energy, theta, N, dt, shower_type, n_index, R, model, full_output=False, **kwargs):
     """
     returns the complex amplitudes of the frequency spectrum of the neutrino radio signal
 
@@ -133,10 +148,18 @@ def get_frequency_spectrum(energy, theta, N, dt, shower_type, n_index, R, model,
         * Alvarez2009: parameterization based on ZHS from J. Alvarez-Muniz, W. R. Carvalho, M. Tueros, and E. Zas, Coherent cherenkov radio pulses fromhadronic showers up to EeV energies, Astroparticle Physics 35 (2012), no. 6 287 – 299 and J. Alvarez-Muniz, C. James, R. Protheroe, and E. Zas, Thinned simulations of extremely energeticshowers in dense media for radio applications, Astroparticle Physics 32 (2009), no. 2 100 – 111
         * HCRB2017: analytic model from J. Hanson, A. Connolly Astroparticle Physics 91 (2017) 75-89
         * ARZ2019 semi MC time domain model from Alvarez-Muñiz, J., Romero-Wolf, A., & Zas, E. (2011). Practical and accurate calculations of Askaryan radiation. Physical Review D - Particles, Fields, Gravitation and Cosmology, 84(10). https://doi.org/10.1103/PhysRevD.84.103003
+    full_output: bool (default False)    
+        if True, askaryan modules can return additional output
     Returns
     -------
     spectrum: array
         the complex amplitudes for the given frequencies
+    additional information: dict
+        only available if `full_output` enabled
 
     """
-    return fft.time2freq(get_time_trace(energy, theta, N, dt, shower_type, n_index, R, model, **kwargs), 1 / dt)
+    tmp = get_time_trace(energy, theta, N, dt, shower_type, n_index, R, model, full_output=full_output, **kwargs)
+    if(full_output):
+        return fft.time2freq(tmp[0], 1 / dt), tmp[1]
+    else:
+        return fft.time2freq(tmp, 1 / dt)
