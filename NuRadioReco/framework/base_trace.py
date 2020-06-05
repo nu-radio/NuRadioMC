3b267c8444d596ddaea6d8f3c75502eb979bf3ab
Christian Glaser <christian.glaser@uci.edu>
2020-06-05 13:58:00 +0000

    fix typo

--- NuRadioReco/framework/base_trace.py
+++ NuRadioReco/framework/base_trace.py
@@ -1,4 +1,4 @@
-from __future__ import absolute_import, division, print_functionmv
+from __future__ import absolute_import, division, print_function
 import numpy as np
 import logging
 from NuRadioReco.utilities import fft
