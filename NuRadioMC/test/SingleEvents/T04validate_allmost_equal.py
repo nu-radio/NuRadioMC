4c12547b09a813dbb9f36d28a79156bce5864ed0
Christian Glaser <christian.glaser@uci.edu>
2020-06-18 07:46:45 +0000

    improve output

--- NuRadioMC/test/SingleEvents/T04validate_allmost_equal.py
+++ NuRadioMC/test/SingleEvents/T04validate_allmost_equal.py
@@ -83,6 +83,7 @@ def test_almost_equal_station_keys(keys, fin1=fin1, fin2=fin2, error=error):
                 print("\n attribute {} not almost equal".format(key))
                 print(np.abs((arr1[i] - arr2[i]) / arr2[i]))
                 print(arr1[i])
+                print(arr2[i])
                 error = -1
             # now test zero entries for equality
             if not np.all(arr1[i][zero_mask] == arr2[i][zero_mask]):
