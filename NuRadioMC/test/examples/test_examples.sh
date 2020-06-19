ab38c8c79b7a4af527a75665cc0ed5da4d100bb6
Christian Glaser <christian.glaser@uci.edu>
2020-06-19 13:11:11 +0000

    temporarily disable DnR example

--- NuRadioMC/test/examples/test_examples.sh
+++ NuRadioMC/test/examples/test_examples.sh
@@ -5,9 +5,9 @@ python3 T01generate_event_list.py
 python3 T02RunSimulation.py 1e19_n1e3.hdf5 surface_station_1GHz.json config.yaml output/output.hdf5 output.nur
 python3 T03visualizeVeff.py
 
-cd ../02_DnR
-python3 E01detector_simulation.py event_input/1e19_n1e3comparison1.hdf5 detector/string_to_100m.json config.yaml output.hdf5
-python3 A01analyse_simulation.py output.hdf5
+#cd ../02_DnR
+#python3 E01detector_simulation.py event_input/1e19_n1e3comparison1.hdf5 detector/string_to_100m.json config.yaml output.hdf5
+#python3 A01analyse_simulation.py output.hdf5
 
 cd ../05_pulser_calibration_measurement
 python3 A01generate_pulser_events.py
