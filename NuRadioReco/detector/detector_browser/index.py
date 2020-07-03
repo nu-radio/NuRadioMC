c8c45bff4544df84df4f780782576ad95abc1dd6
christophwelling <christoph.welling@desy.de>
2020-07-03 11:24:16 +0200

    Sets up file reading menu

---
+++ NuRadioReco/detector/detector_browser/index.py
@@ -0,0 +1,45 @@
+import numpy as np
+from dash.dependencies import Input, Output, State
+import dash_core_components as dcc
+import dash_html_components as html
+import dash
+import plotly.graph_objs as go
+import NuRadioReco.detector.detector
+import NuRadioReco.detector.generic_detector
+import open_file
+import argparse
+import os
+from app import app
+
+argparser = argparse.ArgumentParser(description="Visualization for the detector")
+argparser.add_argument('file_location', type=str, help="Path of folder or filename.")
+parsed_args = argparser.parse_args()
+data_folder = os.path.dirname(parsed_args.file_location)
+
+app.title = 'Detector Browser'
+
+app.layout = html.Div([
+    html.Div([
+        html.Div([
+            html.Div(id='folder-dummy', style={'display': 'none'}, children=data_folder),
+            open_file.layout
+        ], style={'flex': '1'}),
+        html.Div([
+            html.Div([
+                html.Div('Title', className='panel panel-heading'),
+                html.Div([
+                    html.Div('Panel')
+                ], className='panel panel-body')
+            ], className='panel panel-default')
+        ], style={'flex': '2'})
+    ], style={'display': 'flex'})
+])
+
+
+
+
+if __name__ == '__main__':
+    if int(dash.__version__.split('.')[0]) <= 1:
+        if int(dash.__version__.split('.')[1]) < 0:
+            print('WARNING: Dash version 0.39.0 or newer is required, you are running version {}. Please update.'.format(dash.__version__))
+    app.run_server(debug=False, port=8080)
