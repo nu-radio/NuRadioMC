398f366bdbf5ffa90baeb4f39c373b089c9e9a71
christophwelling <christoph.welling@desy.de>
2020-09-02 15:33:58 +0200

    Fixes raytracing tests

--- NuRadioMC/test/SignalProp/run_signal_test.sh
+++ NuRadioMC/test/SignalProp/run_signal_test.sh
@@ -1,7 +1,7 @@
 set -e
 cd NuRadioMC/test/SignalProp/
-python T01test_python_vs_cpp.py
-python T02test_analytic_D_T.py
-python T04MooresBay.py
-python T05unit_test_C0_SP.py
-python T06unit_test_C0_mooresbay.py
+python3 T01test_python_vs_cpp.py
+python3 T02test_analytic_D_T.py
+python3 T04MooresBay.py
+python3 T05unit_test_C0_SP.py
+python3 T06unit_test_C0_mooresbay.py
