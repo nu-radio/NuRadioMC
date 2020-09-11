640dafbc5f8c358b6b64b72e75ca0b7111cfde66
christophwelling <christoph.welling@desy.de>
2020-09-11 14:47:46 +0200

    Code cleanup

--- NuRadioReco/framework/parameters.py
+++ NuRadioReco/framework/parameters.py
@@ -26,7 +26,7 @@ class stationParameters(Enum):
     chi2_efield_time_direction_fit = 26  # the chi2 of the direction fitter that used the maximum pulse times of the efields
     ndf_efield_time_direction_fit = 27  # the number of degrees of freedom of the direction fitter that used the maximum pulse times of the efields
     cr_xmax = 28  # Depth of shower maximum of the air shower
-    vertex_2D_fit = 29 #horizontal distance and z coordinate of the reconstructed vertex of the neutrino
+    vertex_2D_fit = 29  # horizontal distance and z coordinate of the reconstructed vertex of the neutrino
 
 
 class channelParameters(Enum):
