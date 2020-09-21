61a538aae01a84a23e7e259d089119dadbbba65a
christophwelling <christoph.welling@desy.de>
2020-09-21 12:41:27 +0200

    Defines __add__ method for baseTrace class

--- NuRadioReco/framework/base_trace.py
+++ NuRadioReco/framework/base_trace.py
@@ -1,7 +1,10 @@
 from __future__ import absolute_import, division, print_function
 import numpy as np
 import logging
-from NuRadioReco.utilities import fft
+import fractions
+from NuRadioReco.utilities import fft, trace_utilities
+import scipy.signal
+import copy
 try:
     import cPickle as pickle
 except ImportError:
@@ -120,7 +123,7 @@ class BaseTrace:
             length = (self._frequency_spectrum.shape[-1] - 1) * 2
         return length
 
-    def apply_time_shift(self, delta_t):
+    def apply_time_shift(self, delta_t, silent=False):
         """
         Uses the fourier shift theorem to apply a time shift to the trace
         Note that this is a cyclic shift, which means the trace will wrap
@@ -130,8 +133,12 @@ class BaseTrace:
         --------------------
         delta_t: float
             Time by which the trace should be shifted
+        silent: boolean (default:False)
+            Turn off warnings if time shift is larger than 10% of trace length
+            Only use this option if you are sure that your trace is long enough
+            to acommodate the time shift
         """
-        if delta_t > .1 * self.get_number_of_samples() / self.get_sampling_rate():
+        if delta_t > .1 * self.get_number_of_samples() / self.get_sampling_rate() and not silent:
             logger.warning('Trace is shifted by more than 10% of its length')
         spec = self.get_frequency_spectrum()
         spec *= np.exp(-2.j * np.pi * delta_t * self.get_frequencies())
@@ -148,3 +155,73 @@ class BaseTrace:
         self.set_trace(data['time_trace'], data['sampling_rate'])
         if('trace_start_time' in data.keys()):
             self.set_trace_start_time(data['trace_start_time'])
+
+    def __add__(self, x):
+        if not isinstance(x, BaseTrace):
+            raise TypeError('+ operator is only defined for 2 BaseTrace objects')
+        if self.get_trace() is None or x.get_trace() is None:
+            raise ValueError('One of the trace objects has no trace set')
+        if self.get_trace().ndim != x.get_trace().ndim:
+            raise ValueError('Traces have different dimensions')
+        trace_1 = copy.copy(self.get_trace())
+        trace_2 = copy.copy(x.get_trace())
+        if self.get_sampling_rate() != x.get_sampling_rate():
+            if self.get_sampling_rate() > x.get_sampling_rate():
+                sampling_rate = self.get_sampling_rate()
+                resampling_factor = fractions.Fraction(self.get_sampling_rate() / x.get_sampling_rate())
+                if (resampling_factor.numerator != 1):
+                    trace_2 = scipy.signal.resample(trace_2, resampling_factor.numerator * len(trace_2))
+                if(resampling_factor.denominator != 1):
+                    trace_2 = scipy.signal.resample(trace_2, len(trace_2) // resampling_factor.denominator)
+            else:
+                sampling_rate = x.get_sampling_rate()
+                resampling_factor = fractions.Fraction(x.get_sampling_rate() / self.get_sampling_rate())
+                if (resampling_factor.numerator != 1):
+                    trace_1 = scipy.signal.resample(trace_1, resampling_factor.numerator * len(trace_1))
+                if(resampling_factor.denominator != 1):
+                    trace_1 = scipy.signal.resample(trace_1, len(trace_1) // resampling_factor.denominator)
+        else:
+            sampling_rate = self.get_sampling_rate()
+        if self.get_trace_start_time() <= x.get_trace_start_time():
+            trace_start = self.get_trace_start_time()
+            time_offset = x.get_trace_start_time() - self.get_trace_start_time()
+            i_start = int(round(time_offset * sampling_rate))
+            if trace_1.ndim == 1:
+                trace_length = max(trace_1.shape[0], i_start + trace_2.shape[0])
+                trace_length += trace_length % 2
+                early_trace = np.zeros(trace_length)
+                early_trace[:trace_1.shape[0]] = trace_1
+                late_trace = np.zeros(trace_length)
+                late_trace[:trace_2.shape[0]] = trace_2
+            else:
+                trace_length = max(trace_1.shape[1], i_start + trace_2.shape[1])
+                trace_length += trace_length % 2
+                early_trace = np.zeros((trace_1.shape[0], trace_length))
+                early_trace[:, :trace_1.shape[1]] = trace_1
+                late_trace = np.zeros((trace_2.shape[0], trace_length))
+                late_trace[:, :trace_2.shape[1]] = trace_2
+        else:
+            trace_start = x.get_trace_start_time()
+            time_offset = self.get_trace_start_time() - x.get_trace_start_time()
+            i_start = int(round(time_offset * sampling_rate))
+            if trace_1.ndim == 1:
+                trace_length = max(trace_2.shape[0], i_start + trace_1.shape[0])
+                trace_length += trace_length % 2
+                early_trace = np.zeros(trace_length)
+                early_trace[:trace_2.shape[0]] = trace_2
+                late_trace = np.zeros(trace_length)
+                late_trace[:trace_1.shape[0]] = trace_1
+            else:
+                trace_length = max(trace_2.shape[1], i_start + trace_1.shape[1])
+                trace_length += trace_length % 2
+                early_trace = np.zeros((trace_2.shape[0], trace_length))
+                early_trace[:, :trace_2.shape[1]] = trace_2
+                late_trace = np.zeros((trace_1.shape[0], trace_length))
+                late_trace[:, :trace_1.shape[1]] = trace_1
+        late_trace_object = BaseTrace()
+        late_trace_object.set_trace(late_trace, sampling_rate)
+        late_trace_object.apply_time_shift(time_offset, True)
+        new_trace = BaseTrace()
+        new_trace.set_trace(early_trace + late_trace_object.get_trace(), sampling_rate)
+        new_trace.set_trace_start_time(trace_start)
+        return new_trace
