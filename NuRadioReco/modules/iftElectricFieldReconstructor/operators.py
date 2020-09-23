5efcf2eb148a6b1c9f349bda6c86547f44286cbe
christophwelling <christoph.welling@desy.de>
2020-09-23 12:22:17 +0200

    Code cleanup

--- NuRadioReco/modules/iftElectricFieldReconstructor/operators.py
+++ NuRadioReco/modules/iftElectricFieldReconstructor/operators.py
@@ -1,14 +1,12 @@
 import numpy as np
 import nifty5 as ift
 
+
 class LinearSlopeOperator(ift.LinearOperator):
     def __init__(self, target):
         self._target = ift.DomainTuple.make(target)
         self._domain = ift.DomainTuple.make(ift.UnstructuredDomain((2,)))
         self._capability = self.TIMES | self.ADJOINT_TIMES
-        pos = np.zeros_like(self.target[0].get_k_length_array().val)
-        #pos[:len(pos)//2+1] = self.target[0].get_k_length_array().val[:len(pos)//2+1]
-        #pos[len(pos)//2+1:] = -1.*self.target[0].get_k_length_array().val[len(pos)//2+1:]
         pos = self.target[0].get_k_length_array().val
 
         self._pos = pos
@@ -18,20 +16,19 @@ class LinearSlopeOperator(ift.LinearOperator):
         inp = x.to_global_data()
         if mode == self.TIMES:
             res = np.empty(self.target.shape, dtype=x.dtype)
-            #res[0] = 0
-            res = inp[1] + inp[0]*self._pos
+            res = inp[1] + inp[0] * self._pos
         else:
             res = np.array(
-                [np.sum(self._pos*inp),
+                [np.sum(self._pos * inp),
                  np.sum(inp[1:])], dtype=x.dtype)
         return ift.Field.from_global_data(self._tgt(mode), res)
+
 
 def SlopeSpectrumOperator(target, m=0, n=0, sigma_m=.1, sigma_n=.1):
     codomain = target.get_default_codomain()
 
     pos_diagonals = np.ones(target.shape[0])
-    pos_diagonals[target.shape[0]//2+1:] = -1
-    #pos_diagonals[0] = 0
+    pos_diagonals[target.shape[0] // 2 + 1:] = -1
     flipper = ift.DiagonalOperator(ift.Field(ift.DomainTuple.make(codomain), pos_diagonals))
     slope = LinearSlopeOperator(target.get_default_codomain())
     mean = np.array([m, n])
@@ -41,18 +38,20 @@ def SlopeSpectrumOperator(target, m=0, n=0, sigma_m=.1, sigma_n=.1):
     linear_operator = flipper @ slope @ ift.Adder(mean) @ ift.makeOp(sig)
     return linear_operator.ducktape('slope')
 
+
 class Inserter(ift.LinearOperator):
-    def __init__(self,target):
+    def __init__(self, target):
         self._domain = ift.makeDomain(ift.UnstructuredDomain(1))
         self._target = ift.makeDomain(target)
-        self._capability= self.TIMES | self.ADJOINT_TIMES
+        self._capability = self.TIMES | self.ADJOINT_TIMES
 
-    def apply(self,x,mode):
-        self._check_input(x,mode)
+    def apply(self, x, mode):
+        self._check_input(x, mode)
         x = x.to_global_data()
         if mode == self.TIMES:
-            return ift.full(self.target,x[0])
-        return ift.full(self.domain,x.sum())
+            return ift.full(self.target, x[0])
+        return ift.full(self.domain, x.sum())
+
 
 class DomainFlipper(ift.LinearOperator):
     """
@@ -79,6 +78,7 @@ class DomainFlipper(ift.LinearOperator):
             y = ift.from_global_data(self._target, x.to_global_data())
         return y
 
+
 class SymmetrizingOperator(ift.EndomorphicOperator):
     """Adds the field axes-wise in reverse order to itself.
 
@@ -98,8 +98,8 @@ class SymmetrizingOperator(ift.EndomorphicOperator):
         self._check_input(x, mode)
         v = x.val.copy()
         for i in self._domain.axes[self._space]:
-            lead = (slice(None),)*i
+            lead = (slice(None),) * i
             v, loc = ift.dobj.ensure_not_distributed(v, (i,))
-            loc[lead+(slice(None),)] += loc[lead+(slice(None, None, -1),)]
+            loc[lead + (slice(None),)] += loc[lead + (slice(None, None, -1),)]
             loc /= 2
         return ift.Field(self.target, ift.dobj.ensure_default_distributed(v))
