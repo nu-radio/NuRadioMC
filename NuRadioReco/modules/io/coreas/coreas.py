fix weighting if it is not set in highlevel files. Now it is performed in the shower plane and the projected to ground. This is necessary for symmetry reasons.

NuRadioReco/modules/io/coreas/coreas.py
@@ -1,4 +1,5 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
 from radiotools import helper as hp
 from radiotools import coordinatesystems
@@ -8,6 +9,7 @@ import NuRadioReco.framework.sim_station
 import NuRadioReco.framework.base_trace
 import NuRadioReco.framework.electric_field
 import NuRadioReco.framework.radio_shower
+import radiotools.coordinatesystems
 
 from NuRadioReco.framework.parameters import stationParameters as stnp
 from NuRadioReco.framework.parameters import electricFieldParameters as efp
@@ -38,16 +40,72 @@ def get_angles(corsika):
     return zenith, azimuth, magnetic_field_vector
 
 
-def calculate_simulation_weights(positions):
+def calculate_simulation_weights(positions, zenith, azimuth, site='summit'):
     """Calculate weights according to the area that one simulated position represents.
     Weights are therefore given in units of area.
     Note: The volume of a 2d convex hull is the area."""
 
     import scipy.spatial as spatial
+
+    positions = np.array(positions)
+    cstrafo = radiotools.coordinatesystems.cstrafo(zenith=zenith, azimuth=azimuth, magnetic_field_vector=None,
+                                                   site=site)
+    x_trafo_from_shower = cstrafo.transform_from_vxB_vxvxB(station_position=np.array([1, 0, 0]))
+    y_trafo_from_shower = cstrafo.transform_from_vxB_vxvxB(station_position=np.array([0, 1, 0]))
+    z_trafo_from_shower = cstrafo.transform_from_vxB_vxvxB(station_position=np.array([0, 0, 1]))
+
+    # voronoi has to be calculated in the shower plane due to symmetry reasons
+    shower = cstrafo.transform_to_vxB_vxvxB(station_position=positions)
+    vor = spatial.Voronoi(shower[:, :2])  # algorithm will find no solution if flat simulation is given in 3d.
+
+    #fig1, ax1 = plt.subplots()
+    #spatial.voronoi_plot_2d(vor, ax1)
+    #ax1.set_aspect('equal')
+    #ax1.set_title('Positions of stations (blue) and vertices (orange) in shower plane, zenith = {:.2f}'.format(zenith/units.degree))
+
+    fig2, ax2 = plt.subplots()
+
     weights = np.zeros_like(positions[:, 0])
-    vor = spatial.Voronoi(positions[:, :2])  # algorithm will find no solution if flat simulation is given in 3d.
-    for p in range(0, weights.shape[0]):
-        weights[p] = spatial.ConvexHull(vor.vertices[vor.regions[vor.point_region[p]]]).volume
+    for p in range(0, weights.shape[0]): # loop over all 240 station positions
+        vertices_shower_2d = vor.vertices[vor.regions[vor.point_region[p]]]
+
+        # x_vertice_ground = x_trafo_from_shower[0] * x_vertice_shower + y_trafo_from_shower[0] * y_vertice_shower + z_trafo_from_shower[0] * z_vertice_shower
+        # y_vertice_ground = x_trafo_from_shower[1] * x_vertice_shower + y_trafo_from_shower[1] * y_vertice_shower + z_trafo_from_shower[1] * z_vertice_shower
+        # z_vertice_ground = x_trafo_from_shower[2] * x_vertice_shower + y_trafo_from_shower[2] * y_vertice_shower + z_trafo_from_shower[2] * z_vertice_shower
+
+        x_vertice_shower = vertices_shower_2d[:, 0]
+        y_vertice_shower = vertices_shower_2d[:, 1]
+        z_vertice_shower = -(x_trafo_from_shower[2] * x_vertice_shower + y_trafo_from_shower[2] * y_vertice_shower) / z_trafo_from_shower[2]
+
+        vertices_shower_3d = []
+        for iter in range(len(x_vertice_shower)):
+            vertices_shower_3d.append([x_vertice_shower[iter],y_vertice_shower[iter],z_vertice_shower[iter]])
+        vertices_shower_3d = np.array(vertices_shower_3d)
+        vertices_ground = cstrafo.transform_from_vxB_vxvxB(station_position=vertices_shower_3d)
+
+        n_arms = 8  ##mask last stations of each arm
+        length_shower = np.sqrt(shower[:, 0] ** 2 + shower[:, 1] ** 2)
+        ind = np.argpartition(length_shower, -n_arms)[-n_arms:]
+
+        weight = spatial.ConvexHull(vertices_ground[:, :2])
+        weights[p] = weight.volume # volume of a 2d dataset is the area, area of a 2d data set is the perimeter
+        weights[ind] = 0
+
+        #ax2.scatter(vertices_ground[:, 0], vertices_ground[:, 1])
+    #ax2.scatter(positions[:, 0], positions[:, 1], c='k', alpha=0.1)
+    #ax2.set_aspect('equal')
+    #ax2.set_title('Vertices of voronoi in shower projected to ground')
+
+    #fig3 = plt.figure(figsize=[12,4])
+    #ax4 = fig3.add_subplot(121)
+    #ax5 = fig3.add_subplot(122)
+
+    #ax4.hist(weights)
+
+    #ax5.scatter(length_shower**2, weights)
+    #ax5.set_xlabel('length^2')
+    #ax5.set_ylabel('weight')
+
     return weights
 
 