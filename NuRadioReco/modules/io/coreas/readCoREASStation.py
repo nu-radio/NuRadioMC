fix weighting if it is not set in highlevel files. Now it is performed in the shower plane and the projected to ground. This is necessary for symmetry reasons.

NuRadioReco/modules/io/coreas/readCoREASStation.py
@@ -4,6 +4,8 @@ import numpy as np
 import NuRadioReco.framework.event
 import NuRadioReco.framework.station
 from NuRadioReco.modules.io.coreas import coreas
+import matplotlib.pyplot as plt
+from NuRadioReco.utilities import units
 
 import logging
 logger = logging.getLogger('readCoREASStation')
@@ -44,9 +46,25 @@ class readCoREASStation:
         for input_file in self.__input_files:
             self.__current_event = 0
             with h5py.File(input_file, "r") as corsika:
+                #if "highlevel" not in corsika.keys() or list(corsika["highlevel"].values()) == []:
+                 #   logger.warning(" No highlevel quantities in simulated hdf5 files, weights will be one")
+                  #  weights = np.ones(len(corsika['CoREAS']['observers']))
                 if "highlevel" not in corsika.keys() or list(corsika["highlevel"].values()) == []:
-                    logger.warning(" No highlevel quantities in simulated hdf5 files, weights will be one")
-                    weights = np.ones(len(corsika['CoREAS']['observers']))
+                    logger.warning(" No highlevel quantities in simulated hdf5 files, weights will be taken from station position")
+                    positions = []
+                    for observer in corsika['CoREAS']['observers'].values():
+                        position = observer.attrs['position']
+                        positions.append(np.array([-position[1], position[0], 0]) * units.cm)
+                    positions = np.array(positions)
+                    zenith, azimuth, magnetic_field_vector = coreas.get_angles(corsika)
+                    weights = coreas.calculate_simulation_weights(positions, zenith, azimuth)
+
+                    #fig, ax = plt.subplots()
+                    #im = ax.scatter(positions[:,0], positions[:,1], c=weights)
+                    #fig.colorbar(im, ax=ax)
+                    #plt.gca().set_aspect('equal')
+                    #plt.show()
+
                 else:
                     weights = coreas.calculate_simulation_weights(list(corsika["highlevel"].values())[0]["antenna_position"])
 