43708f4516b4129eb56b538ff4ad0fab85d6a2e6
Daniel Smith <danielsmith@uchicago.edu>
2020-10-14 22:08:01 -0500

    Debugging to make sure signals have proper noise

--- NuRadioReco/examples/PhasedArray/Noise_trigger_rate/T01MeasureNoiselevel.py
+++ NuRadioReco/examples/PhasedArray/Noise_trigger_rate/T01MeasureNoiselevel.py
@@ -1,29 +1,28 @@
-import sys
-sys.path.append('/home/danielsmith/icecube_gen2/NuRadioReco')
-
+import argparse
+import time
+import copy
+from astropy.time import Time
+from scipy import constants
+from itertools import product
+from matplotlib import pyplot as plt
+import numpy as np
+from multiprocessing import Pool as ThreadPool
 import NuRadioReco.modules.channelGenericNoiseAdder
 import NuRadioReco.modules.channelBandPassFilter
-from NuRadioReco.utilities import units
-import numpy as np
-from scipy import constants
-import argparse
 import NuRadioReco.modules.phasedarray.triggerSimulator
 import NuRadioReco.modules.trigger.simpleThreshold
 import NuRadioReco.framework.station
 import NuRadioReco.framework.channel
+from NuRadioReco.utilities import units
 from NuRadioReco.utilities import fft
 from NuRadioReco.detector import detector
-from matplotlib import pyplot as plt
-from astropy.time import Time
-import time
-import copy
 
 det = detector.Detector(json_filename="../Effective_volume/8antennas_100m_0.5GHz.json")
 det.update(Time.now())
 
 channelBandPassFilter = NuRadioReco.modules.channelBandPassFilter.channelBandPassFilter()
 
-n_channels = 8
+n_channels = 1
 
 main_low_angle = np.deg2rad(-59.55)
 main_high_angle = np.deg2rad(59.55)
@@ -33,18 +32,25 @@ if(n_channels == 4):
     upsampling_factor = 2
     default_angles = np.arcsin(np.linspace(np.sin(main_low_angle), np.sin(main_high_angle), 11))
     channels = np.arange(4, 8)
+    phase = True
 elif(n_channels == 8):
     upsampling_factor = 4
     default_angles = np.arcsin(np.linspace(np.sin(main_low_angle), np.sin(main_high_angle), 21))
     channels = None
+    phase = True
+elif(n_channels == 1):
+    upsampling_factor = 1
+    default_angles = []
+    channels = [4]
+    phase = False
 else:
     print("wrong n_channels!")
     exit()
 
-station_id = 101
-thresholds = np.arange(3.5, 5.5, 0.1)
+thresholds = (np.arange(1.5, 3.5, 0.1))
+channel_ids = np.arange(8)
 Vrms = 1
-
+station_id = 1
 n_samples = det.get_number_of_samples(station_id, 1)  # we assume that all channels have the same parameters
 
 sampling_rate = det.get_sampling_frequency(station_id, 1)
@@ -52,7 +58,6 @@ print(f"sampling rate = {sampling_rate/units.MHz}MHz, {n_samples} samples")
 
 dt = 1 / sampling_rate
 ff = np.fft.rfftfreq(n_samples, dt)
-
 filt1 = channelBandPassFilter.get_filter(ff, 0, 0, None, passband=[0, 240 * units.MHz], filter_type="cheby1", order=9, rp=.1)
 filt2 = channelBandPassFilter.get_filter(ff, 0, 0, None, passband=[80 * units.MHz, 230 * units.MHz], filter_type="cheby1", order=4, rp=.1)
 filt = filt1 * filt2
@@ -69,19 +74,15 @@ bandwidth = np.trapz(np.abs(filt_highres) ** 2, fff)
 Vrms_ratio = np.sqrt(bandwidth / (max_freq - min_freq))
 amplitude = Vrms / Vrms_ratio
 
-channel_ids = np.arange(8) #n_channels)
-
 pattern = f"pa_trigger_rate_{n_channels:d}channels_{upsampling_factor}xupsampiling"
 
 triggerSimulator = NuRadioReco.modules.phasedarray.triggerSimulator.triggerSimulator()
 thresholdSimulator = NuRadioReco.modules.trigger.simpleThreshold.triggerSimulator()
 
-phase = True
-
 def loop(zipped):
 
     threshold = float(zipped[0])
-    seed = int(float(zipped[1] * np.power(2.0, 32)))
+    seed = int(zipped[1])
 
     station = NuRadioReco.framework.station.Station(station_id)
     evt = NuRadioReco.framework.event.Event(0, 0)
@@ -89,44 +90,48 @@ def loop(zipped):
     channelGenericNoiseAdder = NuRadioReco.modules.channelGenericNoiseAdder.channelGenericNoiseAdder()
     channelGenericNoiseAdder.begin(seed = seed)
 
+    dt = 1.0 / sampling_rate
+
     for channel_id in channel_ids:
         # Noise rms is amplified to greater than Vrms so that, after filtering, its the Vrms we expect
         spectrum = channelGenericNoiseAdder.bandlimited_noise(min_freq, max_freq, n_samples, sampling_rate, amplitude,
                                                                type="rayleigh", time_domain=False)
-        trace = fft.freq2time(spectrum * filt, sampling_rate)        
+
+        trace = fft.freq2time(spectrum * filt, sampling_rate)
 
         channel = NuRadioReco.framework.channel.Channel(channel_id)
         channel.set_trace(trace, sampling_rate)
         station.add_channel(channel)
 
     if(phase):
-        triggered = triggerSimulator.run(evt, station, det, 
-                                         Vrms, #self._Vrms,
-                                         threshold, 
+        triggered = triggerSimulator.run(evt, station, det,
+                                         Vrms,
+                                         threshold * Vrms,
                                          triggered_channels=channels,
                                          phasing_angles=default_angles,
                                          ref_index=1.75,
                                          trigger_name='primary_phasing',
                                          trigger_adc=False,
                                          adc_output='voltage',
-                                         nyquist_zone=None,
-                                         bandwidth_edge=20 * units.MHz, 
-                                         upsampling_factor=upsampling_factor, 
-                                         window=int(16 / (sampling_rate*upsampling_factor) ), 
-                                         step = int(8  / (sampling_rate*upsampling_factor) ))
+                                         trigger_filter=None,
+                                         upsampling_factor=upsampling_factor,
+                                         window=int(16 * sampling_rate * upsampling_factor),
+                                         step = int(8 * sampling_rate * upsampling_factor))
     else:
-        thresholdSimulator.run(evt, station, det,
-                               threshold=threshold,
-                               triggered_channels=[4],  # run trigger on all channels
-                               number_concidences=1,
-                               trigger_name='simple_threshold')
+        # code for a fake single antenna, integrated antenna
 
-        triggered = station.get_trigger('simple_threshold').has_triggered()
-        
+        original_trace = np.array(station.get_channel(4).get_trace())
+
+        squared_mean, num_frames = triggerSimulator.powerSum(coh_sum=original_trace,
+                                                             window=int(16 * sampling_rate),
+                                                             step=int(8 * sampling_rate),
+                                                             adc_output=f'voltage')
+
+        squared_mean_threshold = 1.0 * np.power(threshold, 2.0)
+
+        triggered = (True in (squared_mean > squared_mean_threshold))
+
     return triggered
-
-from multiprocessing import Pool as ThreadPool
-from itertools import product
 
 pool = ThreadPool(20)
 
@@ -136,32 +141,20 @@ for threshold in thresholds:
     t00 = time.time()
     t0 = time.time()
 
-    while i < 10000: #True:
+    while i < 10000:
+        n_pool = 1000
 
         print("Events:", i, " Delta t=", time.time() - t00, " N_triggers =", n_triggers)
-        i += 1000
+        i += n_pool
         t00 = time.time()
 
-        #results = loop(0)
-
-        results = pool.map(loop, zip(threshold * np.ones(1000), np.random.uniform(0.0, 1.0, 1000)))
+        results = pool.map(loop, zip(threshold * np.ones(n_pool), i + np.arange(n_pool)))
 
         n_triggers += np.sum(results)
         rate = 1. * n_triggers / (i * n_samples * dt)
 
-        '''
-        if(n_triggers >= 10):
-            rate = 1. * n_triggers / (i * n_samples * dt)
-            with open(f"{pattern}.txt", "a") as fout:
-                fout.write(f"{threshold}\t{n_triggers}\t{i*n_samples*dt}\t{rate}\n")
-                fout.close()
-            print(f"threshold = {threshold:.3f}: n_triggers = {n_triggers} -> rate = {rate/units.Hz:.0f} Hz, {(time.time() -  t0)/i*1000:.1f}ms per event -> {(time.time() -  t0)/n_triggers*100/60:.1f}min for 100 triggered events")
-            break
-        '''
-
     rate = 1. * n_triggers / (i * n_samples * dt)
-    with open(f"{pattern}_halfwindow.txt", "a") as fout:
+    with open(f"{pattern}.txt", "a") as fout:
         fout.write(f"{threshold}\t{n_triggers}\t{i*n_samples*dt}\t{rate}\n")
         fout.close()
     print(f"threshold = {threshold:.3f}: n_triggers = {n_triggers} -> rate = {rate/units.Hz:.0f} Hz, {(time.time() -  t0)/i*1000:.1f}ms per event -> {(time.time() -  t0)/n_triggers*100/60:.1f}min for 100 triggered events")
-
