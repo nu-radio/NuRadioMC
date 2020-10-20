a0eab19596e6d8de47b13933cf890dc7ec35cc1f
lpyras <pyras@posteo.de>
2020-10-20 17:20:50 +0200

    nicer plot labels for linter

--- NuRadioReco/modules/io/coreas/readCoREASStation.py
+++ NuRadioReco/modules/io/coreas/readCoREASStation.py
@@ -59,8 +59,10 @@ class readCoREASStation:
                     if self.__debug:
                         import matplotlib.pyplot as plt
                         fig, ax = plt.subplots()
-                        im = ax.scatter(positions[:,0], positions[:,1], c=weights)
-                        fig.colorbar(im, ax=ax)
+                        im = ax.scatter(positions[:,0], positions[:, 1], c=weights)
+                        fig.colorbar(im, ax=ax).set_label(label=r'Area $[m^2]$')
+                        plt.xlabel('East [m]')
+                        plt.ylabel('West [m]')
                         plt.title('Final weighting')
                         plt.gca().set_aspect('equal')
                         plt.show()
