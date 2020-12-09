fb4fe0b41c40e8a5126a2d15d4bde07f0541c472
Christian Glaser <christian.glaser@uci.edu>
2020-12-09 17:12:15 +0000

    change order of tests

--- run_all_tests.sh
+++ run_all_tests.sh
@@ -6,9 +6,9 @@ NuRadioMC/test/SingleEvents/validate_MB.sh
 NuRadioMC/test/SingleEvents/validate_ARZ.sh
 NuRadioMC/test/SignalGen/test_build.sh
 NuRadioMC/test/SignalProp/run_signal_test.sh
-NuRadioMC/test/examples/test_examples.sh
 NuRadioMC/test/Veff/1e18eV/test_build.sh
 NuRadioMC/test/atmospheric_Aeff/1e18eV/test_build.sh
+NuRadioMC/test/examples/test_examples.sh
 NuRadioReco/test/tiny_reconstruction/testTinyReconstruction.sh
 NuRadioReco/test/trigger_tests/run_trigger_test.sh
 NuRadioReco/test/test_examples.sh
