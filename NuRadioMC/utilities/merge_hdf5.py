53992a6863f90a7aa0b276389dfc237c16ee64cf
Christian Glaser <christian.glaser@uci.edu>
2020-12-10 13:15:17 +0000

    use nan_to_num

--- NuRadioMC/utilities/merge_hdf5.py
+++ NuRadioMC/utilities/merge_hdf5.py
@@ -69,14 +69,13 @@ def merge2(filenames, output_filename):
                 attrs[key] = fin.attrs[key]
             else:
                 if(key != 'trigger_names'):
-                    if(not np.all(attrs[key] == fin.attrs[key])):
+                    if(not np.all(np.nan_to_num(attrs[key] == fin.attrs[key]))):
                         if(key == "n_events"):
                             logger.warning(f"number of events in file {filenames[0]} and {f} are different ({attrs[key]} vs. {fin.attrs[key]}. We keep track of the total number of events, but in case the simulation was performed with different settings per file (e.g. different zenith angle bins), the averaging might be effected.")
                         elif(key == "start_event_id"):
                             continue
                         else:
-                            if(not (math.isnan(attrs[key])  and math.isnan(fin.attrs[key]))):
-                                logger.warning(f"attribute {key} of file {filenames[0]} and {f} are different ({attrs[key]} vs. {fin.attrs[key]}. Using attribute value of first file, but you have been warned!")
+                            logger.warning(f"attribute {key} of file {filenames[0]} and {f} are different ({attrs[key]} vs. {fin.attrs[key]}. Using attribute value of first file, but you have been warned!")
             if((('trigger_names' not in attrs) or (len(attrs['trigger_names']) == 0)) and 'trigger_names' in fin.attrs):
                 attrs['trigger_names'] = fin.attrs['trigger_names']
         fin.close()
