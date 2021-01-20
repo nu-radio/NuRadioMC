a9a9dbe4aa7664e108ce173452ab91730fb93029
Bob Oeyen <bob.oeyen@ugent.be>
2021-01-20 11:36:13 +0100

    New medium definition

--- NuRadioMC/utilities/medium.py
+++ NuRadioMC/utilities/medium.py
@@ -2,10 +2,9 @@ from NuRadioMC.utilities.medium_base import*
 
 """
 1) When implementing a new model it should at least inherit from
-'IceModel' from the module 'medium_base'. Overwrite all the function 
-concerning the refractive index. Inheritance from daughter classes
-like 'IceModel_Exponential' is also possible and overwriting function
-may not be neede in this case.
+'IceModel' from the module 'medium_base'. Overwrite all the function. 
+Inheritance from daughter classes like 'IceModel_Exponential' is also 
+possible and overwriting function may not be needed in this case.
 
 2) When implementing a new model and using the radiopropa numerical
 tracer, do not forget to implement the ice model also in the c++
@@ -28,58 +27,72 @@ class southpole_simple(IceModel_Exponential):
     def __init__(self):
         # from https://doi.org/10.1088/1475-7516/2018/07/055 RICE2014/SP model
         # define model parameters (RICE 2014/southpole)
-        IceModel_Exponential.__init__(self, z_bottom = -2500*units.meter, 
-                                        n_ice = 1.78, z_0 = 71.*units.meter, 
-                                        delta_n = 0.426)
+        super().__init__(
+            z_bottom = -2820*units.meter, 
+            n_ice = 1.78, 
+            z_0 = 71.*units.meter, 
+            delta_n = 0.426)
 
 
 class southpole_2015(IceModel_Exponential):
     def __init__(self):
         # from https://doi.org/10.1088/1475-7516/2018/07/055 SPICE2015/SP model
-        IceModel_Exponential.__init__(self, z_bottom = -2500*units.meter, 
-                                        n_ice = 1.78, z_0 = 77.*units.meter, 
-                                        delta_n = 0.423)
+        super().__init__(
+            z_bottom = -2820*units.meter, 
+            n_ice = 1.78, 
+            z_0 = 77.*units.meter, 
+            delta_n = 0.423)
 
 
 class ARAsim_southpole(IceModel_Exponential):
     def __init__(self):
         # define model parameters (SPICE 2015/southpole)
-        IceModel_Exponential.__init__(self, z_bottom = -2500*units.meter, 
-                                        n_ice = 1.78, z_0 = 75.75757575757576*units.meter, 
-                                        delta_n = 0.43)
+        super().__init__(
+            z_bottom = -2820*units.meter, 
+            n_ice = 1.78, 
+            z_0 = 75.75757575757576*units.meter, 
+            delta_n = 0.43)
 
 
-class mooresbay_simple(IceModel_Exponential,ReflectiveBottom):
+class mooresbay_simple(IceModel_Exponential):
     def __init__(self):
-        # from https://doi.org/10.3189/2015JoG14J214
-        ReflectiveBottom.__init__(self, z_refl = -576*units.m, 
-                                            refl_coef = 0.82, refl_phase_shift = 180*units.deg)
-
         # from https://doi.org/10.1088/1475-7516/2018/07/055 MB1 model
-        IceModel_Exponential.__init__(self, z_bottom = self.reflection-1*units.m, 
-                                        n_ice = 1.78, z_0 = 34.5*units.meter, 
-                                        delta_n = 0.46)
+        super().__init__(
+            n_ice = 1.78, 
+            z_0 = 34.5*units.meter, 
+            delta_n = 0.46)
 
-
-class mooresbay_simple_2(IceModel_Exponential,ReflectiveBottom):
-    def __init__(self):\
         # from https://doi.org/10.3189/2015JoG14J214
-        ReflectiveBottom.__init__(self,z_refl = -576*units.m,
-                                            refl_coef = 0.82,refl_phase_shift = 180*units.deg)
+        self.add_reflective_bottom( 
+            z_refl = -576*units.m, 
+            refl_coef = 0.82, 
+            refl_phase_shift = 180*units.deg)
 
+
+class mooresbay_simple_2(IceModel_Exponential):
+    def __init__(self):\
         # from https://doi.org/10.1088/1475-7516/2018/07/055 MB2 model
-        IceModel_Exponential.__init__(self,z_bottom = self.reflection-1*units.m, 
-                                        n_ice = 1.78, z_0 = 37*units.meter, 
-                                        delta_n = 0.481)
+        super().__init__(
+            n_ice = 1.78, 
+            z_0 = 37*units.meter, 
+            delta_n = 0.481)
+
+        # from https://doi.org/10.3189/2015JoG14J214
+        self.add_reflective_bottom( 
+            z_refl = -576*units.m, 
+            refl_coef = 0.82, 
+            refl_phase_shift = 180*units.deg)
 
 
 class greenland_simple(IceModel_Exponential):
     def __init__(self):
         # from C. Deaconu, fit to data from Hawley '08, Alley '88
         # rho(z) = 917 - 602 * exp (-z/37.25), using n = 1 + 0.78 rho(z)/rho_0
-        IceModel_Exponential.__init__(self,z_bottom = -3000*units.meter, 
-                                        n_ice = 1.78, z_0 = 37.25*units.meter, 
-                                        delta_n = 0.51)
+        super().__init__(
+            z_bottom = -3000*units.meter, 
+            n_ice = 1.78, 
+            z_0 = 37.25*units.meter, 
+            delta_n = 0.51)
 
 
 
