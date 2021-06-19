516d70180c3fc96ae122855435ae5ad7a850ded9
lpyras <pyras@posteo.de>
2021-06-19 12:46:43 +0200

    add Auger data and script to plot expected cr number and aeff

--- NuRadioReco/examples/cr_efficiency_analysis/helper_cr_eff.py
+++ NuRadioReco/examples/cr_efficiency_analysis/helper_cr_eff.py
@@ -22,6 +22,11 @@ class NumpyEncoder(json.JSONEncoder):
         return json.JSONEncoder.default(self, obj)
 
 
+def find_nearest(array, value):
+    array = np.asarray(array)
+    idx = (np.abs(array - value)).argmin()
+    return array[idx]
+
 def calculate_thermal_noise_Vrms(Tnoise, T_noise_max_freq, T_noise_min_freq):
     Vrms_thermal_noise = (((scipy.constants.Boltzmann * units.joule / units.kelvin) * Tnoise *
                            (T_noise_max_freq - T_noise_min_freq) * 50 * units.ohm) ** 0.5)
@@ -83,3 +88,33 @@ def set_random_station_time(station):
         station.set_station_time(astropy.time.Time('2019-01-01T{}:00:00'.format(hour)))
 
     return station
+
+def get_auger_cr_flux(energy):
+    import scipy.interpolate as interpolate
+    data = np.loadtxt('Auger_combined spectrum_ICRC_2019.txt', skiprows=3)
+    E = 10**(data[:, 0]) * units.eV
+    E_J = data[:, 1] * units.m**-2 * units.second**-1 * units.steradian**-1
+    J = E_J / E
+    Err_up = data[:, 2] * units.m**-2 * units.second**-1 * units.steradian**-1 / E
+    Err_low = data[:, 3] * units.m**-2 * units.second**-1 * units.steradian**-1 / E
+
+    print(J)
+    get_flux = interpolate.interp1d(E, J, fill_value=0, bounds_error=False)
+
+    return get_flux(energy)
+
+
+def get_auger_flux_per_energy_bin(bin_edge_low, bin_edge_high):
+    import scipy.interpolate as interpolate
+    from scipy.integrate import quad
+
+    data = np.loadtxt('Auger_combined spectrum_ICRC_2019.txt', skiprows=3)
+    E = 10**(data[:, 0]) * units.eV
+    E_J = data[:, 1] * units.m**-2 * units.second**-1 * units.steradian**-1
+    J = E_J / E
+
+    flux = interpolate.interp1d(E, J, fill_value=0, bounds_error=False)
+    int_flux = quad(flux, bin_edge_low, bin_edge_high, limit=2 * E.shape[0], points=E)
+
+    return int_flux[0]
+
