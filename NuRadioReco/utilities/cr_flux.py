97969ad9fc47fcb8fcfb0470b73aafcf7b888c2f
lpyras <pyras@posteo.de>
2021-10-27 12:56:25 +0200

    tidy up the cr flux fuctions and add data from TA

--- NuRadioReco/utilities/cr_flux.py
+++ NuRadioReco/utilities/cr_flux.py
@@ -2,96 +2,105 @@ import numpy as np
 from NuRadioReco.utilities import units
 import scipy.interpolate as interpolate
 from scipy.integrate import quad
+import os.path
 
-def get_cr_flux(log10_energy, type='auger_19'):
+# References
+# Auger ICRC 2019, https://www.auger.org/document-centre-public?task=download.send&id=5045&catid=78&m=0
+# TA ICRC 2021, data provided by van Vliet
+
+def get_measured_data(type):
     """
-    Returns an scipy interpolation of the measured data. Output
-    units are 1/(eV m^2 sr ns) for cosmic-ray energy in (in log10(E / eV)))
-    :param log10_energy: Input energies (in log10(E / eV)))
-    :param mode:
-    :return: scipy interpolation of data in 1/(eV m^2 sr ns) (NuRadio base units)
+    Helper function get data points in NuRadio base units
+    :param type: choose between auger_ICRC2019 and TA_ICRC2021
+    :return: E, J, Err_up, Err_low in NuRadio base units
     """
-    if type == 'auger_19':
-        data = np.loadt_xt('data/Auger_combined_spectrum_ICRC_2019.txt', skiprows=3)
+    if type == 'auger_ICRC2019':
+        abspath = os.path.dirname(os.path.abspath(__file__))
+        data = np.loadtxt(os.path.join(abspath, 'data/Auger_combined_spectrum_ICRC_2019.txt'), skiprows=3)
         # from PRL paper 2020 based on ICRC 2019
-        E = 10**(data[:, 0]) * units.eV
-        E_J = data[:, 1] * units.m**-2 * units.second**-1 * units.steradian**-1
+        E = 10 ** (data[:, 0]) * units.eV
+        E_J = data[:, 1] * units.m ** -2 * units.second ** -1 * units.steradian ** -1
         J = E_J / E
-        Err_up = data[:, 2] * units.m**-2 * units.second**-1 * units.steradian**-1 / E
-        Err_low = data[:, 3] * units.m**-2 * units.second**-1 * units.steradian**-1 / E
+        J_err_low = data[:, 3] * units.m ** -2 * units.second ** -1 * units.steradian ** -1 / E
+        J_err_up = data[:, 2] * units.m ** -2 * units.second ** -1 * units.steradian ** -1 / E
 
-    get_flux = interpolate.interp1d(E, J, fill_value=0, bounds_error=False)
+    if type == 'TA_ICRC2021':
+        abspath = os.path.dirname(os.path.abspath(__file__))
+        data = np.loadtxt(os.path.join(abspath, 'data/TA_combined_spectrum_ICRC_2021.txt'), skiprows=10)
+        E = 10 ** (data[:, 0]) * units.eV
+        J = data[:, 2] * units.eV ** -1 * units.m ** -2 * units.second ** -1 * units.steradian ** -1
+        J_band_low = data[:, 3] * units.eV ** -1 * units.m ** -2 * units.second ** -1 * units.steradian ** -1
+        J_band_up = data[:, 4] * units.eV ** -1 * units.m ** -2 * units.second ** -1 * units.steradian ** -1
+        J_err_low = J - J_band_low
+        J_err_up = J_band_up - J
 
-    return get_flux(10**log10_energy)
+    return E, J, J_err_low, J_err_up
 
-
-def get_flux_per_energy_bin(log10_bin_edge_low, log10_bin_edge_high, type='auger_19'):
+def get_flux_interpolation(type):
     """
-    Returns an scipy integration of the measured data over given interval. Output
-    units are 1/(eV m^2 sr ns) for cosmic-ray energy in (in log10(E / eV)))
+    Helper function to interpolate the measured data
+    :param type: choose between auger_ICRC2019 and TA_ICRC2021
+    :return: scipy interpolation of data in NuRadio base units (which are 1/(eV m^2 sr ns) )
+    """
+    E, J, J_err_low, J_err_up = get_measured_data(type)
+    return interpolate.interp1d(E, J, fill_value=0, bounds_error=False)
+
+
+def get_cr_flux(log10_energy, type='auger_ICRC2019'):
+    """
+    Returns an scipy interpolation of the measured data.
+    :param log10_energy: Input energies (in log10(E / eV)))
+    :param type: choose between auger_ICRC2019 and TA_ICRC2021
+    :return: scipy interpolation of data in NuRadio base units (which are 1/(eV m^2 sr ns) )
+    """
+    flux_interpolation = get_flux_interpolation(type=type)
+
+    return flux_interpolation(10**log10_energy)
+
+
+def get_flux_per_energy_bin(log10_bin_edge_low, log10_bin_edge_high, type='auger_ICRC2019'):
+    """
+    Returns an scipy integration of the measured data over given interval.
     :param log10_bin_edge_low: Input energy lower bound (in log10(E / eV)))
     :param log10_bin_edge_high: Input energy upper bound (in log10(E / eV)))
-    :param mode:
-    :return: scipy intergration of data in 1/(eV m^2 sr ns) (NuRadio base units)
+    :param type: choose between auger_ICRC2019 and TA_ICRC2021
+    :return: scipy integration of data in NuRadio base units (which are 1/(eV m^2 sr ns))
     """
-    if type == 'auger_19':
-        data = np.loadtxt('data/Auger_combined_spectrum_ICRC_2019.txt', skiprows=3)
-        # from PRL paper 2020 based on ICRC 2019
-        E = 10**(data[:, 0]) * units.eV
-        E_J = data[:, 1] * units.m**-2 * units.second**-1 * units.steradian**-1
-        J = E_J / E
+    E, J, J_err_low, J_err_up = get_measured_data(type)
 
-    flux = interpolate.interp1d(E, J, fill_value=0, bounds_error=False)
-    int_flux = quad(flux, 10**log10_bin_edge_low, 10**log10_bin_edge_high, limit=2 * E.shape[0], points=E)
+    flux_interpolation = get_flux_interpolation(type)
 
-    return int_flux[0]
+    integrated_flux = quad(flux_interpolation, 10**log10_bin_edge_low, 10**log10_bin_edge_high, limit=2 * E.shape[0], points=E)
 
-def get_analytic_cr_spectrum(log10_energy, type="auger_19"):
+    return integrated_flux[0]
+
+def plot_measured_spectrum(ax=None, scale=2.7, type='auger_ICRC2019'):
     """
-    Returns a analytic parametrization of the Auger energy spectrum
-    units are 1/(eV m^2 sr ns)  for cosmic-ray energy in log10(E / eV)
-    :param log10_energy: Input energies (in log10(E / eV))
-    :type: take auger_17, auger_19 or TA_19
-    :return: analytic parametrization of spectrum for given input energies in 1/(eV m^2 sr ns) (NuRadio base units)
+    Plot measured spectrum.
+    :param ax: axis on which the data is plotted
+    :param scale: scale factor for energy, default = 2.7
+    :param type: choose between auger_ICRC2019 and TA_ICRC2021
+    :return: plot of data without plt.show()
     """
-    # from https://astro.pages.rwth-aachen.de/astrotools/_modules/auger.html#spectrum_analyticq
+    import matplotlib.pyplot as plt
+    if ax is None:
+        fig = plt.figure()
+        ax = fig.add_subplot(111)
 
-    DSPECTRUM_ANALYTIC_15 = np.array([3.3e-19, 4.82e18, 42.09e18, 3.29, 2.6, 3.14])
-    DSPECTRUM_ANALYTIC_17 = np.array([2.8e-19, 5.08e18, 39e18, 3.293, 2.53, 2.5])
-    DSPECTRUM_ANALYTIC_19 = np.array([3.46e12, 1.5e17, 6.2e18, 12e18, 50e18, 2.92, 3.27, 2.2, 3.2, 5.4])
-    SPECTRA_DICT_ANA = {15: DSPECTRUM_ANALYTIC_15, 17: DSPECTRUM_ANALYTIC_17, 19: DSPECTRUM_ANALYTIC_19}
+    E, J, J_err_low, J_err_up = get_measured_data(type)
 
-    energy = 10 ** log10_energy  # type: np.ndarray
+    E_J_scale_units = units.m**-2 * units.year**-1 * units.steradian**-1 * units.eV**(scale - 1)
 
-    if type == "auger_17":
-        p = SPECTRA_DICT_ANA[17]  # type: np.ndarray
-        return (np.where(energy < p[1],
-                        p[0] * (energy / p[1]) ** (-p[3]),
-                        p[0] * (energy / p[1]) ** (-p[4]) * (1 + (p[1] / p[2]) ** p[5])
-                        * (1 + (energy / p[2]) ** p[5]) ** -1)) * units.year * units.km**2
+    ax.errorbar(E, E**scale * J /E_J_scale_units,
+                yerr=[(E**scale * J_err_low) / E_J_scale_units, (E**scale * J_err_up)/E_J_scale_units],
+                marker='x', linewidth=1, markersize=8, ls='None', label='Measured by {}'.format(type))
 
-    elif type == "auger_19":
-        p = SPECTRA_DICT_ANA[19]  # type: np.ndarray
-        return ((energy / p[0]) ** (-p[5]) * \
-               (1 + (energy / p[1]) ** p[5]) / (1 + (energy / p[1]) ** p[6]) * \
-               (1 + (energy / p[2]) ** p[6]) / (1 + (energy / p[2]) ** p[7]) * \
-               (1 + (energy / p[3]) ** p[7]) / (1 + (energy / p[3]) ** p[8]) * \
-               (1 + (energy / p[4]) ** p[8]) / (1 + (energy / p[4]) ** p[9]) ) * units.year * units.km**2
 
-    elif type == "TA_19":
-        p1 = -3.28
-        p2 = -2.68
-        p3 = -4.84
-        E1 = 10 ** 18.69
-        E2 = 10 ** 19.81
-        c = 2.24e-30
-        c1 = c * (E1 / 1e18) ** p1
-        c2 = c1 * (E2 / E1) ** p2
-        yy = np.where(energy < E1,
-                 c * (energy / 1e18) ** p1,
-                 np.where(energy < E2,
-                          c1 * (energy / E1) ** p2,
-                          c2 * (energy / E2) ** p3))
-        # convert 1/m**2 to 1/km**2 abd second to year
-        return (yy * 1e6 * 3.154 * 10 ** 7) * units.year * units.km**2
-
+    yl = r'$J(E)$ [m$^{-2}$ yr$^{-1}$ sr$^{-1}$ eV$^{%g}$]' % (scale - 1)
+    if scale != 0:
+        yl = r'$E^{%g}\,$' % scale + yl
+    ax.set_ylabel(yl)
+    ax.set_xlabel(r'$E$ [eV]')
+    ax.set_xscale('log')
+    ax.set_yscale('log')
+    plt.legend()
