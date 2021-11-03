c28cf39a57a987c26c7c737485e0f3e2e9aa2f01
Christoph <christoph.welling@desy.de>
2021-11-03 13:39:44 +0100

    Prevent error when printing error message

--- NuRadioMC/test/SingleEvents/validate_MB.sh
+++ NuRadioMC/test/SingleEvents/validate_MB.sh
@@ -1,6 +1,5 @@
-#!/bin/bash
-set -e
+# #!/bin/bash
+# set -e
 
-NuRadioMC/test/SingleEvents/T02RunSimulation.py NuRadioMC/test/SingleEvents/MB_1e18_reference.hdf5 NuRadioMC/test/SingleEvents/surface_station_1GHz.json NuRadioMC/test/SingleEvents/config_MB.yaml NuRadioMC/test/SingleEvents/MB_1e18_output.hdf5
-
-NuRadioMC/test/SingleEvents/T04validate_allmost_equal.py NuRadioMC/test/SingleEvents/MB_1e18_output.hdf5 NuRadioMC/test/SingleEvents/MB_1e18_reference.hdf5
+python3 NuRadioMC/test/SingleEvents/T02RunSimulation.py NuRadioMC/test/SingleEvents/MB_1e18_reference.hdf5 NuRadioMC/test/SingleEvents/surface_station_1GHz.json NuRadioMC/test/SingleEvents/config_MB.yaml NuRadioMC/test/SingleEvents/MB_1e18_output.hdf5
+python3 NuRadioMC/test/SingleEvents/T04validate_allmost_equal.py NuRadioMC/test/SingleEvents/MB_1e18_output.hdf5 NuRadioMC/test/SingleEvents/MB_1e18_reference.hdf5
