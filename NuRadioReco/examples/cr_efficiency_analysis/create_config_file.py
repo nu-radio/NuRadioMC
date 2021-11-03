c88562269b2f21c4257f6f822215ab3b810bb9cf
lpyras <pyras@posteo.de>
2021-11-03 16:35:46 +0100

    add helper function to calculate only one antenna in case of a coincidence of several same antennas. reorganise trigger rate and trigger threshold calculation.

---
+++ NuRadioReco/examples/cr_efficiency_analysis/create_config_file.py
@@ -0,0 +1,157 @@
+import numpy as np
+import helper_cr_eff as hcr
+import json
+import os
+from NuRadioReco.utilities import units
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--output_path', type=os.path.abspath, nargs='?', default='',
+                    help='Path to save output, most likely the path to the cr_efficiency_analysis directory')
+parser.add_argument('--detector_file', type=str, nargs='?',
+                    default='/Users/lilly/Software/NuRadioMC/NuRadioReco/examples/'
+                            'cr_efficiency_analysis/LPDA_Southpole.json',
+                    help='file with one antenna at and the geographic location, change triggered channels accordingly')
+parser.add_argument('--target_global_trigger_rate', type=float, nargs='?', default=1,
+                    help='trigger rate for all channels in Hz')
+parser.add_argument('--trigger_name', type=str, nargs='?', default='high_low',
+                    help='name of the trigger, high_low or envelope')
+parser.add_argument('--default_station', type=int, nargs='?', default=101,
+                    help='default station id')
+parser.add_argument('--trace_samples', type=int, nargs='?', default=1024,
+                    help='elements in the array of one trace')
+parser.add_argument('--sampling_rate', type=int, nargs='?', default=1,
+                    help='sampling rate in GHz')
+parser.add_argument('--triggered_channels', type=np.ndarray, nargs='?', default=np.array([1]),
+                    help='channel on which the trigger is applied')
+parser.add_argument('--total_number_triggered_channels', type=int, nargs='?', default=3,
+                    help='number ot channels that trigger.')
+parser.add_argument('--number_coincidences', type=int, nargs='?', default=2,
+                    help='number coincidences of true trigger within on station of the detector')
+parser.add_argument('--coinc_window', type=int, nargs='?', default=80,
+                    help='coincidence window within the number coincidence has to occur. In ns')
+parser.add_argument('--passband_low', type=int, nargs='?', default=80,
+                    help='lower bound of the passband used for the trigger in MHz')
+parser.add_argument('--passband_high', type=int, nargs='?', default=180,
+                    help='higher bound of the passband used for the trigger in MHz')
+parser.add_argument('--order_trigger', type=int, nargs='?', default=10,
+                    help='order of the filter used in the trigger')
+parser.add_argument('--Tnoise', type=int, nargs='?', default=300,
+                    help='Temperature of thermal noise in K')
+parser.add_argument('--T_noise_min_freq', type=int, nargs='?', default=50,
+                    help='min freq of thermal noise in MHz')
+parser.add_argument('--T_noise_max_freq', type=int, nargs='?', default=800,
+                    help='max freq of thermal noise in MHz')
+parser.add_argument('--galactic_noise_n_side', type=int, nargs='?', default=4,
+                    help='The n_side parameter of the healpix map. Has to be power of 2, basicly the resolution')
+parser.add_argument('--galactic_noise_interpolation_frequencies_start', type=int, nargs='?', default=10,
+                    help='start frequency the galactic noise is interpolated over in MHz')
+parser.add_argument('galactic_noise_interpolation_frequencies_stop', type=int, nargs='?', default=1100,
+                    help='stop frequency the galactic noise is interpolated over in MHz')
+parser.add_argument('--galactic_noise_interpolation_frequencies_step', type=int, nargs='?', default=100,
+                    help='frequency steps the galactic noise is interpolated over in MHz')
+parser.add_argument('--n_random_phase', type=int, nargs='?', default=10,
+                    help='for computing time reasons one galactic noise amplitude is reused '
+                         'n_random_phase times, each time a random phase is added')
+parser.add_argument('--threshold_start', type=int, nargs='?',
+                    help='value of the first tested threshold in Volt')
+parser.add_argument('--threshold_step', type=int, nargs='?',
+                    help='value of the threshold step in Volt')
+parser.add_argument('--station_time', type=str, nargs='?', default='2019-01-01T00:00:00',
+                    help='station time for calculation of galactic noise')
+parser.add_argument('--station_time_random', type=bool, nargs='?', default=True,
+                    help='choose if the station time should be random or not')
+parser.add_argument('--hardware_response', type=bool, nargs='?', default=False,
+                    help='choose if the hardware response (amp) should be True or False')
+parser.add_argument('--iterations_per_job', type=bool, nargs='?', default=200,
+                    help='choose if the hardware response (amp) should be True or False')
+parser.add_argument('--number_of_allowed_trigger', type=bool, nargs='?', default=3,
+                    help='The number of iterations is calculated to yield a trigger rate')
+
+args = parser.parse_args()
+output_path = args.output_path
+trace_samples = args.trace_samples
+target_global_trigger_rate = args.target_global_trigger_rate * units.Hz
+n_random_phase = args.n_random_phase
+abs_output_path = os.path.abspath(args.output_path)
+trigger_name = args.trigger_name
+passband_low = args.passband_low * units.megahertz
+passband_high = args.passband_high * units.megahertz
+passband_trigger = np.array([passband_low, passband_high])
+detector_file = args.detector_file
+triggered_channels = args.triggered_channels
+default_station = args.default_station
+sampling_rate = args.sampling_rate * units.gigahertz
+total_number_triggered_channels = args.total_number_triggered_channels
+coinc_window = args.coinc_window * units.ns
+number_coincidences = args.number_coincidences
+order_trigger = args.order_trigger
+Tnoise = args.Tnoise * units.kelvin
+T_noise_min_freq = args.T_noise_min_freq * units.megahertz
+T_noise_max_freq = args.T_noise_max_freq * units.megahertz
+galactic_noise_n_side = args.galactic_noise_n_side
+galactic_noise_interpolation_frequencies_start = args.galactic_noise_interpolation_frequencies_start * units.MHz
+galactic_noise_interpolation_frequencies_stop = args.galactic_noise_interpolation_frequencies_stop * units.MHz
+galactic_noise_interpolation_frequencies_step = args.galactic_noise_interpolation_frequencies_step * units.MHz
+threshold_start = args.threshold_start
+threshold_step = args.threshold_step
+station_time = args.station_time
+station_time_random = args.station_time_random
+hardware_response = args.hardware_response
+iterations_per_job = args.iterations_per_job
+number_of_allowed_trigger = args.number_of_allowed_trigger
+
+trace_length = trace_samples / sampling_rate
+target_single_trigger_rate = hcr.get_single_channel_trigger_rate(
+    target_global_trigger_rate, total_number_triggered_channels, number_coincidences, coinc_window)
+n_iteration_for_one_allowed_trigger = (trace_length * target_single_trigger_rate) ** -1
+
+n_iterations = int(n_iteration_for_one_allowed_trigger * number_of_allowed_trigger / n_random_phase)
+resolution = (n_iteration_for_one_allowed_trigger * number_of_allowed_trigger * trace_length) ** -1
+
+number_of_jobs = n_iterations / iterations_per_job
+number_of_jobs = int(np.ceil(number_of_jobs))
+Vrms_thermal_noise = hcr.calculate_thermal_noise_Vrms(Tnoise, T_noise_max_freq, T_noise_min_freq)
+
+if threshold_start is None:
+    if hardware_response:
+        threshold_start = 1e3 * Vrms_thermal_noise
+    elif not hardware_response:
+        threshold_start = 1.8 * Vrms_thermal_noise
+    else:
+        threshold_start = threshold_start * units.volt
+
+if threshold_step is None:
+    if hardware_response:
+        threshold_step = 1e-3 * units.volt
+    elif not hardware_response:
+        threshold_step = 1e-6 * units.volt
+    else:
+        threshold_step = threshold_step * units.volt
+
+dic = {'T_noise': Tnoise, 'Vrms_thermal_noise': Vrms_thermal_noise, 'n_iterations_total': n_iterations,
+       'number_of_allowed_trigger': number_of_allowed_trigger, 'iterations_per_job': iterations_per_job,
+       'number_of_jobs': number_of_jobs, 'target_single_trigger_rate': target_single_trigger_rate,
+       'target_global_trigger_rate': target_global_trigger_rate, 'resolution': resolution, 'trigger_name': trigger_name,
+       'passband_trigger': passband_trigger, 'total_number_triggered_channels': total_number_triggered_channels,
+       'number_coincidences': number_coincidences, 'triggered_channels': triggered_channels,
+       'coinc_window': coinc_window, 'order_trigger': order_trigger, 'detector_file': detector_file,
+       'default_station': default_station, 'trace_samples': trace_samples, 'sampling_rate': sampling_rate,
+       'trace_length': trace_length, 'T_noise_min_freq': T_noise_min_freq, 'T_noise_max_freq': T_noise_max_freq,
+       'galactic_noise_n_side': galactic_noise_n_side,
+       'galactic_noise_interpolation_frequencies_start': galactic_noise_interpolation_frequencies_start,
+       'galactic_noise_interpolation_frequencies_stop': galactic_noise_interpolation_frequencies_stop,
+       'galactic_noise_interpolation_frequencies_step': galactic_noise_interpolation_frequencies_step,
+       'station_time': station_time, 'station_time_random': station_time_random, 'hardware_response': hardware_response,
+       'n_random_phase': n_random_phase, 'threshold_start': threshold_start, 'threshold_step': threshold_step}
+
+
+os.makedirs(os.path.join(abs_output_path,'config/'), exist_ok=True)
+
+output_file = 'config/config_{}_trigger_pb_{:.0f}_{:.0f}.json'.format(
+    trigger_name, passband_trigger[0] / units.MHz, passband_trigger[1] / units.MHz)
+
+abs_path_output_file = os.path.normpath(os.path.join(abs_output_path, output_file))
+
+with open(abs_path_output_file, 'w') as outfile:
+    json.dump(dic, outfile, cls=hcr.NumpyEncoder, indent=4)
--- NuRadioReco/examples/cr_efficiency_analysis/helper_cr_eff.py
+++ NuRadioReco/examples/cr_efficiency_analysis/helper_cr_eff.py
@@ -27,14 +27,16 @@ def find_nearest(array, value):
     idx = (np.abs(array - value)).argmin()
     return array[idx]
 
-def calculate_thermal_noise_Vrms(Tnoise, T_noise_max_freq, T_noise_min_freq):
-    Vrms_thermal_noise = (((scipy.constants.Boltzmann * units.joule / units.kelvin) * Tnoise *
+
+def calculate_thermal_noise_Vrms(T_noise, T_noise_max_freq, T_noise_min_freq):
+    Vrms_thermal_noise = (((scipy.constants.Boltzmann * units.joule / units.kelvin) * T_noise *
                            (T_noise_max_freq - T_noise_min_freq) * 50 * units.ohm) ** 0.5)
 
     return Vrms_thermal_noise
 
 
-def create_empty_event(det, station_time='2019-01-01T00:00:00', station_time_random=True, sampling_rate=1 * units.GHz):
+def create_empty_event(det, trace_samples, station_time='2019-01-01T00:00:00', station_time_random=True,
+                       sampling_rate=1 * units.GHz):
     station_ids = det.get_station_ids()
     station_id = station_ids[0]
     channel_ids = det.get_channel_ids(station_id)
@@ -44,7 +46,7 @@ def create_empty_event(det, station_time='2019-01-01T00:00:00', station_time_ran
 
     station.set_station_time(astropy.time.Time(station_time))
 
-    if station_time_random == True:
+    if station_time_random:
         random_generator_hour = np.random.RandomState()
         hour = random_generator_hour.randint(0, 24)
         if hour < 10:
@@ -54,22 +56,22 @@ def create_empty_event(det, station_time='2019-01-01T00:00:00', station_time_ran
 
     for channel_id in channel_ids:  # take some channel id that match your detector
         channel = Channel(channel_id)
-        default_trace = np.zeros(1024)
+        default_trace = np.zeros(trace_samples)
         channel.set_trace(trace=default_trace, sampling_rate=sampling_rate)
         station.add_channel(channel)
 
     return event, station, channel
 
 
-def create_empty_channel_trace(station, sampling_rate=1 *units.GHz):
+def create_empty_channel_trace(station, trace_samples, sampling_rate=1 * units.GHz):
     for channel in station.iter_channels():
-        default_trace = np.zeros(1024)
+        default_trace = np.zeros(trace_samples)
         channel.set_trace(trace=default_trace, sampling_rate=sampling_rate)
 
     return channel
 
 
-def add_random_phase(station, sampling_rate=1 *units.GHz):
+def add_random_phase(station, sampling_rate=1 * units.GHz):
     for channel in station.iter_channels():
         freq_specs = channel.get_frequency_spectrum()
         rand_phase = np.random.uniform(low=0, high=2 * np.pi, size=len(freq_specs))
@@ -89,14 +91,15 @@ def set_random_station_time(station):
 
     return station
 
+
 def get_auger_cr_flux(energy):
     import scipy.interpolate as interpolate
     data = np.loadtxt('Auger_combined spectrum_ICRC_2019.txt', skiprows=3)
-    E = 10**(data[:, 0]) * units.eV
-    E_J = data[:, 1] * units.m**-2 * units.second**-1 * units.steradian**-1
+    E = 10 ** (data[:, 0]) * units.eV
+    E_J = data[:, 1] * units.m ** -2 * units.second ** -1 * units.steradian ** -1
     J = E_J / E
-    Err_up = data[:, 2] * units.m**-2 * units.second**-1 * units.steradian**-1 / E
-    Err_low = data[:, 3] * units.m**-2 * units.second**-1 * units.steradian**-1 / E
+    Err_up = data[:, 2] * units.m ** -2 * units.second ** -1 * units.steradian ** -1 / E
+    Err_low = data[:, 3] * units.m ** -2 * units.second ** -1 * units.steradian ** -1 / E
 
     print(J)
     get_flux = interpolate.interp1d(E, J, fill_value=0, bounds_error=False)
@@ -109,8 +112,8 @@ def get_auger_flux_per_energy_bin(bin_edge_low, bin_edge_high):
     from scipy.integrate import quad
 
     data = np.loadtxt('Auger_combined spectrum_ICRC_2019.txt', skiprows=3)
-    E = 10**(data[:, 0]) * units.eV
-    E_J = data[:, 1] * units.m**-2 * units.second**-1 * units.steradian**-1
+    E = 10 ** (data[:, 0]) * units.eV
+    E_J = data[:, 1] * units.m ** -2 * units.second ** -1 * units.steradian ** -1
     J = E_J / E
 
     flux = interpolate.interp1d(E, J, fill_value=0, bounds_error=False)
@@ -118,3 +121,15 @@ def get_auger_flux_per_energy_bin(bin_edge_low, bin_edge_high):
 
     return int_flux[0]
 
+
+def get_global_trigger_rate(single_rate, n_channels, number_coincidences, coinc_window):
+    return number_coincidences * scipy.special.comb(n_channels, number_coincidences) \
+           * single_rate ** number_coincidences * coinc_window ** (number_coincidences - 1)
+
+
+def get_single_channel_trigger_rate(global_rate, n_channels, number_coincidences, coinc_window):
+    r_single_pow_n_coincidences = global_rate / (
+            number_coincidences * scipy.special.comb(n_channels, number_coincidences) *
+            coinc_window ** (number_coincidences - 1))
+    r_single = r_single_pow_n_coincidences ** (1 / float(number_coincidences))
+    return r_single
