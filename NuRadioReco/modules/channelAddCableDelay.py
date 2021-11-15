b96995b9404faeed5a7076d4c1c3d6aba76ed98a
iplaisier <43960131+iplaisier@users.noreply.github.com>
2021-11-15 21:16:32 +0100

    add option to correct for cable delays in channelAddCableDelay (#339)
    Co-authored-by: Ilse Plaisier <plaisier@wgs15.zeuthen.desy.de>

--- NuRadioReco/modules/channelAddCableDelay.py
+++ NuRadioReco/modules/channelAddCableDelay.py
@@ -12,11 +12,14 @@ class channelAddCableDelay:
         self.logger = logging.getLogger("NuRadioReco.channelApplyCableDelay")
 
     @register_run()
-    def run(self, evt, station, det):
+    def run(self, evt, station, det, mode = 'add'):
         """
         Adds cable delays to channels
+        mode: str
+            options: 'add' or 'subtract'. 
         """
         for channel in station.iter_channels():
             cable_delay = det.get_cable_delay(station.get_id(), channel.get_id())
             self.logger.debug("cable delay of channel {} is {}ns".format(channel.get_id(), cable_delay / units.ns))
-            channel.add_trace_start_time(cable_delay)
+            if mode == 'add': channel.add_trace_start_time(cable_delay)
+            if mode == 'subtract': channel.add_trace_start_time(-1*cable_delay)
