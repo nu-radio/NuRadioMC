6c07bf226cd42ca5c340188e31dec4032fe65913
Bob Oeyen <bob.oeyen@ugent.be>
2021-11-30 10:32:50 +0100

    Refer to available modules

--- NuRadioMC/SignalProp/propagation.py
+++ NuRadioMC/SignalProp/propagation.py
@@ -5,7 +5,9 @@ solution_types = {1: 'direct',
                       3: 'reflected'}
 solution_types_revert = {v:k for k, v in solution_types.items()}
 
-available_modules = ['analytic', 'radiopropa', 'direct_ray']
+available_modules = ['analytic',
+                     'radiopropa',
+                     'direct_ray']
 
 reflection_case = {1: 'upwards launch vector',
                    2: 'downward launch vector'}
@@ -31,13 +33,13 @@ def get_propagation_module(name=None):
     if name is None:
         from NuRadioMC.SignalProp.propagation_base_class import ray_tracing_base
         return ray_tracing_base
-    elif(name=='analytic'):
+    elif(name==available_modules[0]):
         from NuRadioMC.SignalProp.analyticraytracing import ray_tracing
         return ray_tracing
-    elif(name=='direct_ray'):
+    elif(name==available_modules[2]):
         from NuRadioMC.SignalProp.directRayTracing import direct_ray_tracing
         return direct_ray_tracing
-    elif(name=='radiopropa'):
+    elif(name==available_modules[1]):
         from NuRadioMC.SignalProp.radioproparaytracing import radiopropa_ray_tracing
         return radiopropa_ray_tracing
         
