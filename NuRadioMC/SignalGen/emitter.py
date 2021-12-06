949e03a28a62feb8fb745669fc0171293232f37c
Pawan Giri <pgiri4@huskers.unl.edu>
2021-12-06 10:45:20 -0600

    pull changes from remote

--- NuRadioMC/SignalGen/emitter.py
+++ NuRadioMC/SignalGen/emitter.py
@@ -7,6 +7,7 @@ import logging
 logger = logging.getLogger("SignalGen.emitter")
 import os
 
+
 def get_time_trace(amplitude, N, dt, model, full_output=False, **kwargs):
     """
     returns the voltage trace of an emitter
@@ -50,57 +51,57 @@ def get_time_trace(amplitude, N, dt, model, full_output=False, **kwargs):
     additional_output = {}
     if(amplitude == 0):
         trace = np.zeros(3, N)
-    if(model == 'delta_pulse'):         # this takes delta signal as input voltage
+    if(model == 'delta_pulse'):  # this takes delta signal as input voltage
         trace = np.zeros(N)
         trace[N // 2] = amplitude
-    elif(model == 'cw'):              # generates a sine wave of given frequency 
-        time = np.linspace(-(N/2) * dt, ((N - 1) - N/2) * dt, N)
+    elif(model == 'cw'):  # generates a sine wave of given frequency
+        time = np.linspace(-(N / 2) * dt, ((N - 1) - N / 2) * dt, N)
         trace = amplitude * np.sin(2 * np.pi * emitter_frequency * time)
-    elif(model == 'square' or model == 'tone_burst' ):     # generates a rectangular or tone_burst signal of given width and frequency 
-        if(half_width > int(N/2)):
-            raise NotImplementedError(" half_width {} should be < half of the number of samples N " . format( half_width ) )
-        time = np.linspace(-(N/2) * dt, ((N - 1) - N/2) * dt, N)
+    elif(model == 'square' or model == 'tone_burst'):  # generates a rectangular or tone_burst signal of given width and frequency
+        if(half_width > int(N / 2)):
+            raise NotImplementedError(" half_width {} should be < half of the number of samples N " . format(half_width))
+        time = np.linspace(-(N / 2) * dt, ((N - 1) - N / 2) * dt, N)
         voltage = np.zeros(N)
-        for i in range(0,N):
-            if time[i] >= - half_width and time[i] <= half_width:
+        for i in range(0, N):
+            if time[i] >= -half_width and time[i] <= half_width:
                 voltage[i] = amplitude
         if(model == 'square'):
             trace = voltage
         else:
-            trace = voltage * np.sin(2 * np.pi * emitter_frequency * time) 
-    elif(model == 'gaussian'):          # generates gaussian pulse where half_width represents the half width at half maximum
-        time = np.linspace(-(N/2) * dt, ((N - 1) - N/2) * dt, N)
-        sigma = half_width/( np.sqrt( 2 * np.log(2) ) )          
-        trace = 1/( sigma * np.sqrt( 2 * np.pi ) ) * np.exp(-1/2 * (( time - 500)/sigma ) ** 2 )
-        trace = amplitude * 1/np.max(np.abs(trace)) * trace
-    elif(model == 'idl' or model == 'hvsp2'):            # the idl & hvsp2 waveforms gemerated in KU Lab stored in hdf5 file
+            trace = voltage * np.sin(2 * np.pi * emitter_frequency * time)
+    elif(model == 'gaussian'):  # generates gaussian pulse where half_width represents the half width at half maximum
+        time = np.linspace(-(N / 2) * dt, ((N - 1) - N / 2) * dt, N)
+        sigma = half_width / (np.sqrt(2 * np.log(2)))
+        trace = 1 / (sigma * np.sqrt(2 * np.pi)) * np.exp(-1 / 2 * ((time - 500) / sigma) ** 2)
+        trace = amplitude * 1 / np.max(np.abs(trace)) * trace
+    elif(model == 'idl' or model == 'hvsp2'):  # the idl & hvsp2 waveforms gemerated in KU Lab stored in hdf5 file
         path = os.path.dirname(os.path.dirname(__file__))
         if(model == 'idl'):
-            input_file = os.path.join(path , 'data/idl_data.hdf5')    
+            input_file = os.path.join(path, 'data/idl_data.hdf5')
         else:
-            input_file = os.path.join(path , 'data/hvsp2_data.hdf5')
+            input_file = os.path.join(path, 'data/hvsp2_data.hdf5')
         read_file = h5py.File(input_file, 'r')
-        time_original = read_file.get('time') 
-        voltage_original =  read_file.get('voltage')
-        time_new = np.linspace( time_original[0], time_original[len(time_original) - 1], (int((time_original[len(time_original) - 1] - time_original[0]) / dt) + 1))
-        interpolation = interp1d(time_original,voltage_original,kind='cubic')
+        time_original = read_file.get('time')
+        voltage_original = read_file.get('voltage')
+        time_new = np.linspace(time_original[0], time_original[len(time_original) - 1], (int((time_original[len(time_original) - 1] - time_original[0]) / dt) + 1))
+        interpolation = interp1d(time_original, voltage_original, kind='cubic')
         voltage_new = interpolation(time_new)
         # if the interpolated waveform has larger sample size than N , it will truncate the data keeping peak amplitude at center
-        if len(voltage_new)>N:
-            peak_amplitude_index = np.where( np.abs( voltage_new ) == np.max( np.abs( voltage_new ) ) )[0][0]
-            voltage_new = np.roll( voltage_new, int(len(voltage_new)/2) - peak_amplitude_index)
-            lower_index = int(len(voltage_new)/2 - N/2)
-            trace = voltage_new[lower_index : lower_index + N]   # this truncate data making trace lenght of N
+        if len(voltage_new) > N:
+            peak_amplitude_index = np.where(np.abs(voltage_new) == np.max(np.abs(voltage_new)))[0][0]
+            voltage_new = np.roll(voltage_new, int(len(voltage_new) / 2) - peak_amplitude_index)
+            lower_index = int(len(voltage_new) / 2 - N / 2)
+            trace = voltage_new[lower_index: lower_index + N]  # this truncate data making trace lenght of N
         # for the case with larger N, trace size will be adjusted depending on whether the number (N + len(voltage_new)) is even or odd
         else:
-            add_zeros = int(( N-len(voltage_new)) /2)
+            add_zeros = int((N - len(voltage_new)) / 2)
             adjustment = 0
-            if (( N + len(voltage_new)) %2 != 0):
+            if ((N + len(voltage_new)) % 2 != 0):
                 adjustment = 1
             trace = np.pad(voltage_new, (add_zeros + adjustment, add_zeros), 'constant', constant_values=(0, 0))
-        trace = amplitude * trace / np.max(np.abs( trace ))                    # trace now has dimension of amplitude given from event generation file
-        peak_amplitude_index_new = np.where( np.abs( trace ) == np.max( np.abs( trace ) ) )[0][0]
-        trace = np.roll( trace, int(N/2) - peak_amplitude_index_new )             # this rolls the array(trace) to keep peak amplitude at center
+        trace = amplitude * trace / np.max(np.abs(trace))  # trace now has dimension of amplitude given from event generation file
+        peak_amplitude_index_new = np.where(np.abs(trace) == np.max(np.abs(trace)))[0][0]
+        trace = np.roll(trace, int(N / 2) - peak_amplitude_index_new)  # this rolls the array(trace) to keep peak amplitude at center
 
     else:
         raise NotImplementedError("model {} unknown".format(model))
--- NuRadioMC/examples/05_pulser_calibration_measurement/radioEmitting_pulser_calibration/runARA02.py
+++ NuRadioMC/examples/05_pulser_calibration_measurement/radioEmitting_pulser_calibration/runARA02.py
@@ -1,12 +1,7 @@
 from __future__ import absolute_import, division, print_function
 import argparse
-import h5py
-import numpy as np
-import datetime
 import NuRadioReco.modules.triggerTimeAdjuster
-import NuRadioReco.modules.ARIANNA.hardwareResponseIncorporator
 import NuRadioReco.modules.efieldToVoltageConverter
-import NuRadioReco.modules.voltageToEfieldConverter
 import NuRadioReco.modules.trigger.simpleThreshold
 import NuRadioReco.modules.trigger.highLowThreshold
 import NuRadioReco.modules.trigger.powerIntegration
@@ -16,8 +11,6 @@ import NuRadioReco.modules.channelGenericNoiseAdder
 import NuRadioReco.modules.custom.deltaT.calculateAmplitudePerRaySolution
 from NuRadioReco.utilities import units
 from NuRadioMC.simulation import simulation as simulation
-from NuRadioReco.framework.parameters import channelParameters as chp
-from NuRadioReco.framework.trigger import Trigger
 import logging
 logging.basicConfig(level=logging.WARNING)
 logger = logging.getLogger("runARA02")
@@ -34,16 +27,17 @@ calculateAmplitudePerRaySolution = NuRadioReco.modules.custom.deltaT.calculateAm
 triggerSimulator = NuRadioReco.modules.trigger.highLowThreshold.triggerSimulator()
 triggerTimeAdjuster = NuRadioReco.modules.triggerTimeAdjuster.triggerTimeAdjuster()
 triggerSimulator.begin(log_level=logging.WARNING)
+
+
 class mySimulation(simulation.simulation):
 
     def _detector_simulation_filter_amp(self, evt, station, det):
         # bandpass filter trace, the upper bound is higher then the sampling rate which makes it just a highpass filter
-         channelBandPassFilter.run(evt, station, det, passband=[80 * units.MHz, 1000 * units.GHz],
+        channelBandPassFilter.run(evt, station, det, passband=[80 * units.MHz, 1000 * units.GHz],
                                   filter_type='butter', order=2)
-         channelBandPassFilter.run(evt, station, det, passband=[0, 500 * units.MHz],
+        channelBandPassFilter.run(evt, station, det, passband=[0, 500 * units.MHz],
                                   filter_type='butter', order=10)
 
- 
     def _detector_simulation_trigger(self, evt, station, det):
         # save the amplitudes to output hdf5 file
         # save amplitudes per ray tracing solution to hdf5 data output
@@ -54,20 +48,21 @@ class mySimulation(simulation.simulation):
                            high_low_window=50 * units.ns,
                            coinc_window=170 * units.ns,
                            number_concidences=4,
-                           trigger_name='highlow_2sigma',
-                           triggered_channels=[0,1,2,3,4,5,6,7])
-                
+                           trigger_name='highlow_2sigma_Vpol',
+                           triggered_channels=[0, 1, 2, 3, 4, 5, 6, 7])
+
         triggerSimulator.run(evt, station, det,
                            threshold_high=1e-6 * self._Vrms,
                            threshold_low=-1e-6 * self._Vrms,
                            high_low_window=50 * units.ns,
                            coinc_window=170 * units.ns,
                            number_concidences=4,
-                           trigger_name='highlow_2sigma',
-                           triggered_channels=[8,9,10,11,12,13,14,15])       
-        
+                           trigger_name='highlow_2sigma_Hpol',
+                           triggered_channels=[8, 9, 10, 11, 12, 13, 14, 15])
+
         triggerTimeAdjuster.run(evt, station, det)
-           
+
+
 parser = argparse.ArgumentParser(description='Run NuRadioMC simulation')
 parser.add_argument('inputfilename', type=str,
                     help='path to NuRadioMC input event list')
@@ -81,13 +76,11 @@ parser.add_argument('outputfilenameNuRadioReco', type=str, nargs='?', default=No
                     help='outputfilename of NuRadioReco detector sim file')
 args = parser.parse_args()
 
-sim = mySimulation(inputfilename = args.inputfilename,
-                            outputfilename = args.outputfilename,
-                            detectorfile = args.detectordescription,
-                            outputfilenameNuRadioReco = args.outputfilenameNuRadioReco,
-                            config_file = args.config,
-                            file_overwrite = True)
+sim = mySimulation(inputfilename=args.inputfilename,
+                   outputfilename=args.outputfilename,
+                   detectorfile=args.detectordescription,
+                   outputfilenameNuRadioReco=args.outputfilenameNuRadioReco,
+                   config_file=args.config,
+                   file_overwrite=True)
 
 sim.run()
-
-
