ec8c4266f5ebe0e4cd12eadc668f83537923f1c7
Pawan Giri <pgiri4@huskers.unl.edu>
2021-12-09 14:40:15 -0600

    add validate almost equal for radio_emitter test

--- NuRadioMC/test/examples/test_radio_emitting_pulser_example.sh
+++ NuRadioMC/test/examples/test_radio_emitting_pulser_example.sh
@@ -2,7 +2,7 @@
 set -e
 python NuRadioMC/examples/05_pulser_calibration_measurement/radioEmitting_pulser_calibration/A01generate_pulser_events.py
 python NuRadioMC/examples/05_pulser_calibration_measurement/radioEmitting_pulser_calibration/runARA02.py emitter_event_list.hdf5 NuRadioMC/examples/05_pulser_calibration_measurement/radioEmitting_pulser_calibration/ARA02Alt.json NuRadioMC/examples/05_pulser_calibration_measurement/radioEmitting_pulser_calibration/config.yaml output.hdf5 output.nur
-python NuRadioMC/test/SingleEvents/T04validate_allmost_equal.py output.hdf5 NuRadioMC/test/examples/radio_emitter_output_reference.hdf5 
+python NuRadioMC/test/examples/validate_radio_emitter_allmost_equal.py  output.hdf5 NuRadioMC/test/examples/radio_emitter_output_reference.hdf5 
 rm emitter_event_list.hdf5
 rm output.hdf5
 rm output.nur
