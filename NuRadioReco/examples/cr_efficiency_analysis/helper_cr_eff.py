c019d58980634f3cd5c1a44de1470f72c5363e0c
lpyras <pyras@posteo.de>
2021-12-16 21:32:33 +0100

    add doc strings

--- NuRadioReco/examples/cr_efficiency_analysis/helper_cr_eff.py
+++ NuRadioReco/examples/cr_efficiency_analysis/helper_cr_eff.py
@@ -23,20 +23,58 @@ class NumpyEncoder(json.JSONEncoder):
 
 
 def find_nearest(array, value):
+    """ function to find the nearest value in an array to a given value
+    Parameters
+    ----------
+    array: array
+        array in which a certain value needs to be found
+    value: float
+        value to which the closest value should be calulcated
+
+    Returns
+    -------
+    closest value in array to given value
+    """
     array = np.asarray(array)
     idx = (np.abs(array - value)).argmin()
     return array[idx]
 
 
 def calculate_thermal_noise_Vrms(T_noise, T_noise_max_freq, T_noise_min_freq):
+    """ calculates thermal noise amplitude for a given temperature and frequency range.
+    Parameters
+    ----------
+    T_noise: float
+        temperature in Kelvin
+    T_noise_max_freq: float
+        max frequency in GHz
+    T_noise_min_freq: float
+        min frequency in GHz
+
+    Returns
+    -------
+    amplitude of thermal noise
+    """
+
     Vrms_thermal_noise = (((scipy.constants.Boltzmann * units.joule / units.kelvin) * T_noise *
                            (T_noise_max_freq - T_noise_min_freq) * 50 * units.ohm) ** 0.5)
 
     return Vrms_thermal_noise
 
 
-def set_random_station_time(station, station_time):
-    '''this function gives a random hour for the choosen date'''
+def set_random_station_time(station, station_time='2019-01-01T00:00:00'):
+    '''this function sets a random hour for the set date
+     Parameters
+    ----------
+    station: generator_object
+        station object
+    station_time: iso
+        station time and date
+
+    Returns
+    -------
+    generator_object
+    '''
     import datetime
     date = datetime.datetime.date(datetime.datetime.strptime(station_time, "%Y-%m-%dT%H:%M:%S"))
     random_generator_hour = np.random.RandomState()
@@ -49,8 +87,26 @@ def set_random_station_time(station, station_time):
     return station
 
 
-def create_empty_event(det, trace_samples, station_time='2019-01-01T00:00:00', station_time_random=True,
+def create_empty_event(det, trace_samples=1024, station_time='2019-01-01T00:00:00', station_time_random=True,
                        sampling_rate=1 * units.GHz):
+    '''create an empty event for a given detector
+    Parameters
+    ----------
+    det: generator_object
+        detector object
+    trace_samples: int
+        trace length
+    station_time: iso
+        time and date in the station
+    station_time_random: bool
+        set time random: True or False
+    sampling_rate: float
+
+    Returns
+    -------
+    event, station, channel
+    '''
+
     import datetime
     station_ids = det.get_station_ids()
     station_id = station_ids[0]
@@ -74,6 +130,20 @@ def create_empty_event(det, trace_samples, station_time='2019-01-01T00:00:00', s
 
 
 def create_empty_channel_trace(station, trace_samples, sampling_rate=1 * units.GHz):
+    '''creates an trace of zeros for a certain channel
+    Parameters
+    ----------
+    station: generator_object
+        station object
+    trace_samples: int
+        trace length
+
+    sampling_rate: float
+
+    Returns
+    -------
+    channel
+    '''
     for channel in station.iter_channels():
         default_trace = np.zeros(trace_samples)
         channel.set_trace(trace=default_trace, sampling_rate=sampling_rate)
@@ -82,6 +152,17 @@ def create_empty_channel_trace(station, trace_samples, sampling_rate=1 * units.G
 
 
 def add_random_phase(station, sampling_rate=1 * units.GHz):
+    '''changes the signal phase in a random uniform distribution in the range 0 to 2pi Parameters
+    ----------
+    station: generator_object
+        station object
+
+    sampling_rate: float
+
+    Returns
+    -------
+    channel
+    '''
     for channel in station.iter_channels():
         freq_specs = channel.get_frequency_spectrum()
         rand_phase = np.random.uniform(low=0, high=2 * np.pi, size=len(freq_specs))
@@ -92,11 +173,51 @@ def add_random_phase(station, sampling_rate=1 * units.GHz):
 
 
 def get_global_trigger_rate(single_rate, n_channels, number_coincidences, coinc_window):
+    '''calculates the global trigger rate with a given trigger rate from one channel including
+     number of channels with trigger (n_channels), number_coincidences and the time coinc_window
+     of the coincidence (coinc_window)
+
+     Parameters
+    ----------
+    single_rate: float or array
+        trigger rate of a single antenna
+    n_channels: int
+        number of channels with trigger
+    number_coincidences: int
+        number of coincidences with antennas
+    coinc_window: float
+        time window of coincidence in ns
+
+
+    Returns
+    -------
+    global trigger rate of all trigger channels combined
+    '''
+
     return number_coincidences * scipy.special.comb(n_channels, number_coincidences) \
            * single_rate ** number_coincidences * coinc_window ** (number_coincidences - 1)
 
 
 def get_single_channel_trigger_rate(global_rate, n_channels, number_coincidences, coinc_window):
+    '''calculates the trigger rate of a singel channel for a global trigger rate including
+     number of channels with trigger (n_channels), number_coincidences and the time coinc_window
+     of the coincidence (coinc_window)
+
+     Parameters
+        ----------
+    global_rate: float or array
+        trigger rate of a all antenna together
+    n_channels: int
+        number of channels with trigger
+    number_coincidences: int
+        number of coincidences with antennas
+    coinc_window: float
+        time window of coincidence in ns
+
+    Returns
+    -------
+    single trigger rate of one channel
+    '''
     r_single_pow_n_coincidences = global_rate / (
             number_coincidences * scipy.special.comb(n_channels, number_coincidences) *
             coinc_window ** (number_coincidences - 1))
