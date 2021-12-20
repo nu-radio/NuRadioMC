95a14ae8b648026e8cf154a4f93d35b9c762cfe1
lpyras <pyras@posteo.de>
2021-12-20 12:32:33 +0100

    make flux a function of cos(zenith) instead of zenith, change variable names and plotting script.

--- NuRadioReco/utilities/muon_flux.py
+++ NuRadioReco/utilities/muon_flux.py
@@ -4,8 +4,6 @@ import scipy.interpolate as interpolate
 from scipy.integrate import quad
 from scipy.integrate import dblquad
 
-plot_flux = False
-
 def get_flux(energy, file_surface_mu_flux='data/muon_flux_E_SIBYLL23c_GSF.pickle'):
     '''
     provides muon flux over the full zenith band as calculated with MCEq at a given energy. The CR model used
@@ -83,13 +81,12 @@ def get_flux_per_energy_and_zenith(energy, zenith, file_surface_mu_flux='data/mu
     J_e3 = np.array(data_surface_mu_flux['mu_total']) * units.GeV**2 * units.cm**-2 * units.s**-1
     E_data = np.array(data_surface_mu_flux['e_grid']) * units.GeV
     zen = np.array(data_surface_mu_flux['zen_grid']) * units.deg
-
-    def get_flux_norm(E, theta):
-        flux_E3 = interpolate.interp2d(E_data, zen, J_e3)
-        flux = flux_E3(E, theta)/E**3
+    def get_flux_norm(E, cos_theta):
+        flux_E3 = interpolate.interp2d(E_data, np.cos(zen), J_e3)
+        flux = flux_E3(E, cos_theta)/E**3
         return flux
 
-    return get_flux_norm(energy, zenith)
+    return get_flux_norm(energy, np.cos(zenith))
 
 def get_flux_per_energy_and_zenith_bin(energy_bin_edge_low, energy_bin_edge_high, zenith_bin_edge_low, zenith_bin_edge_high):
     '''
@@ -112,39 +109,49 @@ def get_flux_per_energy_and_zenith_bin(energy_bin_edge_low, energy_bin_edge_high
     integrated flux of muon in NuRadio units.
     '''
 
-    def flux(E, theta):
-        return get_flux_per_energy_and_zenith(E, theta)
+    def flux(E, cos_theta):
+        return get_flux_per_energy_and_zenith(E, cos_theta)
 
     int_flux = dblquad(flux, np.cos(zenith_bin_edge_high), np.cos(zenith_bin_edge_low), lambda x: energy_bin_edge_low, lambda x: energy_bin_edge_high)
     return int_flux[0]
 
-
-if plot_flux == True:
+if __name__ == "__main__":
     units_flux = units.GeV**2.7 * units.cm**-2 * units.s**-1 * units.sr**-1
 
     import matplotlib.pyplot as plt
+    plt.style.use({'figure.facecolor':'white'})
+
     file_surface_mu_flux='data/muon_flux_E_SIBYLL23c_GSF.pickle'
     data_surface_mu_flux = io_utilities.read_pickle(file_surface_mu_flux, encoding='latin1')
     J_e3 = np.array(data_surface_mu_flux['mu_total']) * units.GeV**2 * units.cm**-2 * units.s**-1 *units.sr**-1
     E_data = np.array(data_surface_mu_flux['e_grid']) * units.GeV
 
-    plt.plot(E_data, J_e3/E_data**3)
-    plt.xscale('log')
-    plt.yscale('log')
-    plt.ylabel(r'Flux data',fontsize=14)
-    plt.xlabel('Energy [eV]',fontsize=14)
-    #plt.show()
-    #plt.close()
+    energy_bins = np.logspace(14, 19.5, 24)
+    energy_bins_low = energy_bins[0:-1]
+    energy_bins_high = energy_bins[1:]
+    energy_bins_center = 0.5*(energy_bins_low+energy_bins_high)
 
-    plt.style.use({'figure.facecolor':'white'})
-    plt.plot(1e18, get_flux_per_energy_and_zenith_bin(1e18, 1e19, 0*units.deg, 90*units.deg), marker='x', color='k', label='test 2d')
-    plt.plot(1e18, get_flux_per_energy_bin(1e18, 1e19), marker='+', color='k', label='test 1d')
+    flux_1d = []
+    flux_2d = []
+    for i in np.arange(len(energy_bins_low)):
+       flux_1d.append(get_flux_per_energy_bin(energy_bins_low[i], energy_bins_high[i]))
+       flux_2d.append(get_flux_per_energy_and_zenith_bin(energy_bins_low[i], energy_bins_high[i], 0*units.deg, 90*units.deg))
 
-    plt.legend()
-    plt.xscale('log')
-    plt.yscale('log')
-    plt.ylabel(r'Flux',fontsize=14)
-    plt.xlabel('Energy [eV]',fontsize=14)
-    plt.tight_layout()
+    fig, ((ax1, ax2), (ax3, ax4)) = plt.subplots(2, 2)
+    ax1.plot(E_data, (J_e3/E_data**3) * E_data**3.7 / units_flux, label='Scaled muon flux from MCEq')
+    ax2.plot(E_data, J_e3/E_data**3, label='Unscaled muon flux from MCEq')
+    ax2.plot(1e16, get_flux(1e16), marker='x', label='test')
+    ax3.step(energy_bins_center, flux_1d, label='integrated flux 1D')
+    ax4.step(energy_bins_center, flux_2d, label='integrated flux 2D')
+    ax1.set_ylabel(r'$E^{3.7}$ J [$GeV^{2.7}$ $cm^{-2}$ $s^{-1}$ $sr^{-1}$]')
+    ax2.set_ylabel(r'J [$GeV^{-1}$ $cm^{-2}$ $s^{-1}$ $sr^{-1}$]')
+    ax3.set_ylabel(r'J [$GeV^{-1}$ $cm^{-2}$ $s^{-1}$ $sr^{-1}$]')
+    ax4.set_ylabel(r'J [$GeV^{-1}$ $cm^{-2}$ $s^{-1}$ $sr^{-1}$]')
+
+    for ax in fig.get_axes():
+        ax.set_xscale('log')
+        ax.set_yscale('log')
+        ax.set_xlabel('Muon energy [eV]')
+        ax.legend()
     plt.show()
     plt.close()
