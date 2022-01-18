650841d8fb4d1b7de007fb4285b43bdc803b3f2b
Christian Glaser <christian.glaser@uci.edu>
2022-01-18 14:46:11 +0000

    save trigger attributes correctly

--- NuRadioReco/modules/phasedarray/triggerSimulator.py
+++ NuRadioReco/modules/phasedarray/triggerSimulator.py
@@ -553,7 +553,8 @@ class triggerSimulator:
                                                                              step=step)
 
         # Create a trigger object to be returned to the station
-        trigger = SimplePhasedTrigger(trigger_name, threshold, triggered_channels, phasing_angles, trigger_delays)
+        trigger = SimplePhasedTrigger(trigger_name, threshold, channels=triggered_channels,
+                                      primary_angle=phasing_angles, trigger_delays=trigger_delays)
 
         trigger.set_triggered(is_triggered)
 
