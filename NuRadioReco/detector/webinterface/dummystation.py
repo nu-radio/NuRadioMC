568f9d13301fa12f83dba3f382a82ed07f2cde4b
ZackfromEarth <zmeyers@gmail.com>
2022-03-11 15:52:02 +0100

    dummy station

---
+++ NuRadioReco/detector/webinterface/dummystation.py
@@ -0,0 +1,57 @@
+from pymongo import MongoClient
+import six
+import os
+import pymongo
+import datetime
+from astropy.time import Time
+from pprint import pprint
+import logging
+import urllib.parse
+import json
+from bson import json_util #bson dicts are used by pymongo
+import numpy as np
+import pandas as pd
+from NuRadioReco.utilities import units
+import NuRadioReco.utilities.metaclasses
+from NuRadioReco.detector import detector_mongo
+from NuRadioReco.detector import detector
+
+
+mongo_detector = detector_mongo.Detector(database_connection="test")
+
+input_detector = detector.Detector(json_filename = "RNO_G/RNO_season_2021.json")
+
+input_detector.update(Time.now())
+
+station = input_detector.get_station(input_detector.get_station_ids()[1])
+
+mongo_detector.add_station(97,"dummy3",(station['pos_altitude'], station["pos_easting"], station["pos_northing"]),
+            station["commission_time"], station["decommission_time"])
+
+
+
+def add_dummy_channels(input_detector, station_id = 97):
+    for channel_id in input_detector.get_channel_ids(input_detector.get_station_ids()[2]):
+        channel = input_detector.get_channel(input_detector.get_station_ids()[2], channel_id)
+        amp = channel["amp_type"]
+        if amp == "rno_surface":
+            signal_chain = [ "Golden_Coax", "Golden_Surface"]
+        elif amp == "iglu":
+            signal_chain = ["Golden_IGLU", "Golden_Fiber", "Golden_DRAB"]
+        else:
+            print(f"unknown amp type {amp}")
+            continue
+
+        mongo_detector.add_channel_to_station(station_id,
+                                   channel_id,
+                                   signal_chain = signal_chain,
+                                   ant_name = channel["ant_comment"],
+                                   ant_ori_theta = channel["ant_orientation_theta"],
+                                   ant_ori_phi = channel["ant_orientation_phi"],
+                                   ant_rot_theta = channel["ant_rotation_theta"],
+                                   ant_rot_phi = channel["ant_rotation_phi"],
+                                   ant_position = [channel["ant_position_x"],channel["ant_position_y"],channel["ant_position_z"]],
+                                   channel_type = channel["ant_type"],
+                                   commission_time = channel["commission_time"])
+
+add_dummy_channels(input_detector)
