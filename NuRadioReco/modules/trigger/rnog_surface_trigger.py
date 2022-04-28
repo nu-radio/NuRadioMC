c63f726bba0eb01183bc2f3fb74da7b5394b2af7
lpyras <pyras@posteo.de>
2022-04-28 14:11:21 +0200

    first draft of RNO-G surface trigger

---
+++ NuRadioReco/modules/trigger/rnog_surface_trigger.py
@@ -0,0 +1,209 @@
+from NuRadioReco.modules.base.module import register_run
+from NuRadioReco.modules.trigger.highLowThreshold import get_majority_logic
+from NuRadioReco.framework.trigger import RNOGSurfaceTrigger
+import NuRadioReco.utilities.fft
+import numpy as np
+import scipy.signal
+import copy
+import time
+import logging
+
+logger = logging.getLogger('rnog_surface_trigger')
+
+def schottky_diode(trace, threshold, temp=250*units.kelvin, Vbias=2*units.volt):
+    '''returns the absolute maximum of the diode response depending on the maximum input signal.
+    Options for temperature are [300K, 273K, 250K], for the bias voltages [2, 1.5, 1, 0.5]V.
+    The triggerpath reduces the voltage by -10dB, hence the threshold is small. The linear fit is optained from
+    measurements done at DESY Zeuthen.
+    Parameters
+    ----------
+    trace: array of floats
+        the signal trace
+    threshold: float
+        the threshold
+    temp: float
+        temperature of the board with diode
+    Vbias: float
+        applied bias voltage to the diode board
+    Returns
+    -------
+    trigger: bool
+       if signal is above threshold
+        '''
+
+    def func(x, a, b):
+        return a * x + b
+
+    if temp == 300 *units.kelvin:  #measurements taken at 300K
+        if Vbias == 2*units.volt:
+            a = 0.03766068
+            b = 0.70800604
+        if Vbias == 1.5*units.volt: #measurements taken at 1.6V
+            a = 0.03461924
+            b = 0.07128205
+        if Vbias == 1*units.volt:
+            a = 0.0246676
+            b = -1.03562532
+        if Vbias == 0.5*units.volt:  #measurements taken at 0.6V
+            a = 0.0160004
+            b = -0.58376456
+
+    if temp == 273*units.kelvin: #measurements taken at 273K
+        if Vbias == 2*units.volt:
+            a = 0.0457741
+            b = 0.30626021
+        if Vbias == 1.5*units.volt:
+            a = 0.03880661
+            b = -0.29508052
+        if Vbias == 1*units.volt:
+            a = 0.02945406
+            b = -0.7667915
+        if Vbias == 0.5*units.volt:
+            a = 0.01541898
+            b = -0.51015485
+
+    if temp == 250*units.kelvin: #measurements taken at 248K
+        if Vbias == 2*units.volt:
+            a = 0.05493735
+            b = 0.13760984
+        if Vbias == 1.5*units.volt:
+            a = 0.04931924
+            b = -1.67120145
+        if Vbias == 1*units.volt:
+            a = 0.03743406
+            b = -0.97467304
+        if Vbias == 0.5*units.volt:
+            a = 0.01745737
+            b = -0.59006738
+
+    v_in = np.max(np.abs(trace))**2
+    v_out = func(v_in, a, b)
+
+    return np.abs(v_out) > threshold
+
+class triggerSimulator:
+    """
+    Calculates the RNO_G surface trigger with a bandpass filter, a -10db attenuator and a schottky_diode.
+    """
+
+    def __init__(self):
+        self.__t = 0
+        self.begin()
+
+    def begin(self):
+        return
+
+    @register_run()
+    def run(self, evt, station, det, threshold, coinc_window=80, number_coincidences=1, triggered_channels=[13, 16, 19], temp=250*units.kelvin, Vbias=2*units.volt, trigger_name='rnog_surface_trigger'):
+        """
+        Parameters
+        ----------
+        evt: Event
+            Event to run the module on
+        station: Station
+            Station to run the module on
+        det: Detector
+            The detector description
+        threshold: float or dict of floats
+            threshold above (or below) a trigger is issued, absolute amplitude
+            a dict can be used to specify a different threshold per channel where the key is the channel id
+        number_coincidences: int
+            number of channels that are required in coincidence to trigger a station
+        triggered_channels: array of ints or None
+            channels ids that are triggered on, if None trigger will run on all channels
+        coinc_window: float
+            time window in which number_coincidences channels need to trigger
+        temp: float
+            temperature of the board with diode in Kelvin
+        Vbias: float
+            applied bias voltage to the diode board in Volt
+        trigger_name: string
+            a unique name of this particular trigger
+        """
+        t = time.time()  # absolute time of system
+
+        sampling_rate = station.get_channel(det.get_channel_ids(station.get_id())[0]).get_sampling_rate()
+        dt = 1. / sampling_rate
+
+        triggered_bins_channels = []
+        channels_that_passed_trigger = []
+
+        if triggered_channels is None:  # caveat: all channels start at the same time
+            for channel in station.iter_channels():
+                channel_trace_start_time = channel.get_trace_start_time()
+                break
+        else:
+            channel_trace_start_time = station.get_channel(triggered_channels[0]).get_trace_start_time()
+
+        for channel in station.iter_channels():
+            frequencies = channel.get_frequencies()
+
+            #here we apply a bandpass filter which is in the signal chain
+            f = np.zeros_like(frequencies, dtype=complex)
+            mask = frequencies > 0
+            b, a = scipy.signal.cheby1(3, 5, [80 * units.MHz, 180 * units.MHz], 'bandpass', analog=True)  # Numerator (b) and denominator (a) polynomials of the IIR filter
+            #b, a = scipy.signal.butter(10, [80 * units.MHz, 180 * units.MHz], 'bandpass', analog=True)  # Numerator (b) and denominator (a) polynomials of the IIR filter
+            w, h = scipy.signal.freqs(b, a, frequencies[mask])  # w :The angular frequencies at which h was computed. h :The frequency response.
+            f[mask] = h
+
+            freq_spectrum_fft = channel.get_frequency_spectrum()
+            freq_spectrum_fft_copy = copy.copy(freq_spectrum_fft)  # copy spectrum so it is only changed within the trigger module
+            sampling_rate = channel.get_sampling_rate()
+
+            freq_spectrum_fft_copy *= f
+            trace_filtered = NuRadioReco.utilities.fft.freq2time(freq_spectrum_fft_copy, sampling_rate)
+
+            # apply -10dB attenuator of signal chain
+            trace_filtered *= 10**(-10/20)
+
+            channel_id = channel.get_id()
+            trace = trace_filtered
+            if triggered_channels is not None and channel_id not in triggered_channels:
+                logger.debug("skipping channel {}".format(channel_id))
+                continue
+            if channel.get_trace_start_time() != channel_trace_start_time:
+                logger.warning('Channel has a trace_start_time that differs from '
+                               '        the other channels. The trigger simulator may not work properly')
+
+            if(isinstance(threshold, dict)):
+                threshold_tmp = threshold[channel_id]
+            else:
+                threshold_tmp = threshold
+            triggered_bins = schottky_diode(trace, threshold_tmp)
+            triggered_bins_channels.append(triggered_bins)
+
+            if True in triggered_bins:
+                channels_that_passed_trigger.append(channel.get_id())
+
+        # check for coincidences with get_majority_logic(tts, number_of_coincidences, time_coincidence, dt)
+        # returns:
+        # triggered: bool; returns True if majority logic is fulfilled --> has_triggered
+        # triggered_bins: array of ints; the bins that fulfilled the trigger --> triggered_bins
+        # triggered_times = triggered_bins * dt: array of floats;
+        # the trigger times relative to the trace --> triggered_times
+
+        has_triggered, triggered_bins, triggered_times = get_majority_logic(triggered_bins_channels,
+                                                                            number_coincidences, coinc_window, dt)
+
+        trigger = RNOGSurfaceTrigger(trigger_name, threshold, number_coincidences, coinc_window, triggered_channels)
+        trigger.set_triggered_channels(channels_that_passed_trigger)
+
+        if has_triggered:
+            trigger.set_triggered(True)
+            trigger.set_trigger_time(triggered_times.min() + channel_trace_start_time)  # trigger_time = time from the beginning of the trace
+            logger.debug("station has triggered")
+
+        else:
+            trigger.set_triggered(False)
+            trigger.set_trigger_time(None)
+            logger.debug("station has NOT triggered")
+
+        station.set_trigger(trigger)
+        self.__t += time.time() - t
+
+    def end(self):
+        from datetime import timedelta
+        logger.setLevel(logging.INFO)
+        dt = timedelta(seconds=self.__t)
+        logger.info("total time used by this module is {}".format(dt))
+        return dt
