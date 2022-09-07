d76a5fd195b7a6c57f47584db9fc2f409cc9768d
Felix Schlueter <fschluter@icecube.wisc.edu>
2022-09-07 05:28:19 -0500

    Clean up output files of automatic testing after validation

--- NuRadioMC/test/SingleEvents/validate_ARZ.sh
+++ NuRadioMC/test/SingleEvents/validate_ARZ.sh
@@ -2,3 +2,6 @@
 set -e
 NuRadioMC/test/SingleEvents/T02RunSimulation.py NuRadioMC/test/SingleEvents/1e18_output_ARZ_reference.hdf5 NuRadioMC/test/SingleEvents/surface_station_1GHz.json NuRadioMC/test/SingleEvents/config_ARZ.yaml NuRadioMC/test/SingleEvents/1e18_output_ARZ.hdf5
 NuRadioMC/test/SingleEvents/T04validate_allmost_equal.py NuRadioMC/test/SingleEvents/1e18_output_ARZ.hdf5 NuRadioMC/test/SingleEvents/1e18_output_ARZ_reference.hdf5
+
+# cleanup 
+rm -v 1e18_output_ARZ.hdf5
