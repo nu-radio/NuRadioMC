ffeb64195c3822387fe7c3dc4503e4fb99b57f62
Felix Schlueter <fschluter@icecube.wisc.edu>
2022-09-07 05:28:19 -0500

    Clean up output files of automatic testing after validation

--- NuRadioMC/test/SingleEvents/test_build.sh
+++ NuRadioMC/test/SingleEvents/test_build.sh
@@ -6,3 +6,6 @@ python3 NuRadioMC/test/SingleEvents/T04validate_allmost_equal.py NuRadioMC/test/
 python3 NuRadioMC/test/SingleEvents/T05validate_nur_file.py NuRadioMC/test/SingleEvents/1e18_output.nur NuRadioMC/test/SingleEvents/1e18_output_reference.nur
 python3 NuRadioMC/test/SingleEvents/T02RunSimulation.py NuRadioMC/test/SingleEvents/1e18_output_reference.hdf5 NuRadioMC/test/SingleEvents/surface_station_1GHz.json NuRadioMC/test/SingleEvents/config_noise.yaml NuRadioMC/test/SingleEvents/1e18_output_noise.hdf5
 python3 NuRadioMC/test/SingleEvents/T04validate_allmost_equal.py NuRadioMC/test/SingleEvents/1e18_output_noise.hdf5 NuRadioMC/test/SingleEvents/1e18_output_noise_reference.hdf5
+
+# cleanup 
+rm -v 1e18_output_noise.hdf5 1e18_output.hdf5 1e18_output.nur
--- NuRadioMC/test/SingleEvents/validate_MB.sh
+++ NuRadioMC/test/SingleEvents/validate_MB.sh
@@ -4,3 +4,6 @@ set -e
 NuRadioMC/test/SingleEvents/T02RunSimulation.py NuRadioMC/test/SingleEvents/MB_1e18_reference.hdf5 NuRadioMC/test/SingleEvents/surface_station_1GHz.json NuRadioMC/test/SingleEvents/config_MB.yaml NuRadioMC/test/SingleEvents/MB_1e18_output.hdf5
 
 NuRadioMC/test/SingleEvents/T04validate_allmost_equal.py NuRadioMC/test/SingleEvents/MB_1e18_output.hdf5 NuRadioMC/test/SingleEvents/MB_1e18_reference.hdf5
+
+# cleanup 
+rm -v MB_1e18_output.hdf5
