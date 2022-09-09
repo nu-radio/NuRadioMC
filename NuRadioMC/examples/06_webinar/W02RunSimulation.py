ba5044eb9581621d78be5256ad879ee9cd6971f2
Felix Schlueter <fschluter@icecube.wisc.edu>
2022-09-09 04:27:53 -0500

    Remove deprecated default_station/channel arguments from webseminar

--- NuRadioMC/examples/06_webinar/W02RunSimulation.py
+++ NuRadioMC/examples/06_webinar/W02RunSimulation.py
@@ -144,7 +144,5 @@ if __name__ == "__main__":
                        outputfilename=args.outputfilename,
                        detectorfile=args.detectordescription,
                        outputfilenameNuRadioReco=args.outputfilenameNuRadioReco,
-                       config_file=args.config,
-                       default_detector_station=101,
-                       default_detector_channel=0)
+                       config_file=args.config)
     sim.run()
