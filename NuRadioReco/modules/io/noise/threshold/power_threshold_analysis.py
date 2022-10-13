f03b3ae8f6bceb32563df92c6c48260b5d92d11c
DHjeelm <dnl1@live.se>
2022-10-13 15:29:53 +0200

    Threshold analysis folder

--- NuRadioReco/modules/io/noise/threshold/power_threshold_analysis.py
+++ NuRadioReco/modules/io/noise/threshold/power_threshold_analysis.py
@@ -17,6 +17,10 @@ from NuRadioReco.utilities import units
 import uproot
 from astropy.time import Time, TimeDelta
 
+import os, sys
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath("generate_current_noise.py"))))
+from generate_current_noise import empty_event, get_sim_noise
+
 #disable trigger overridden warning
 import logging
 logging.getLogger('BaseStation').setLevel(logging.ERROR)
@@ -61,13 +65,29 @@ def get_generated_noise(generator, dataset):
     noise = noise*dataset.std() + dataset.mean()
     return noise[0]
 
+def get_current_noise_scaled(dataset, trace_length):
+    '''Function that generates noise used in current simulations'''
+
+    # Get event
+    evt = get_sim_noise()
+    channel = evt.get_station(11).get_channel(13)
+    event = channel.get_trace()[0:trace_length]
+
+    # Scale event
+    event = event * np.mean(np.abs(dataset))/np.mean(np.abs(event))
+
+    
+    return event
+
+
+
 def threshold(path_to_files, lower, higher):
 
     file_names = get_file_names(path_to_files, 24)
 
     dataset = np.load('../data.npy')
 
-    generator = keras.models.load_model('stft_generator')
+    generator = keras.models.load_model('../kapre_lstm_generator_4')
 
     # Detector
     det = detector.Detector(json_filename="RNO_G/RNO_season_2021.json", antenna_by_depth=False)
@@ -80,7 +100,7 @@ def threshold(path_to_files, lower, higher):
     trigger.begin()
 
     # trigger path, has a ~11 ns integration and bandpass
-    trigger_integration_window = 11 * units.ns
+    trigger_integration_window = 30 * units.ns
     trigger_passband = [lower*units.MHz, higher*units.MHz]
     bandpass = channelBandPassFilter()
 
@@ -98,7 +118,7 @@ def threshold(path_to_files, lower, higher):
 
 
     for rf_i, run_file in enumerate(file_names):
-        if rf_i > 100:
+        if rf_i > 50:
             break
         reader.begin(run_file)
         for i, event in enumerate(reader.run()):
@@ -110,6 +130,7 @@ def threshold(path_to_files, lower, higher):
             channel = station.get_channel(surface_channel)
             trace = channel.get_trace()[0:512]
             channel.set_trace(trace, 3.2*units.GHz)
+            
             station.add_channel(channel)
             
             bandpass.run(event, station, det, passband=trigger_passband,
@@ -144,6 +165,7 @@ def threshold(path_to_files, lower, higher):
         
             triggered_any_gen = False
             triggered_threshold_gen = 0
+
             
             for threshold in thresholds:
                 if not triggered_any_gen:
@@ -158,52 +180,86 @@ def threshold(path_to_files, lower, higher):
                         triggered_threshold_gen = threshold
                 else:
                     break
+
+            
+            # Current
+            station = event.get_station(event.get_station_ids()[0])
+            channel = station.get_channel(surface_channel)
+            current_trace = get_current_noise_scaled(dataset, 512)/1000
+            channel.set_trace(current_trace, 3.2*units.GHz)
+            station.add_channel(channel)
+            
+            bandpass.run(event, station, det, passband=trigger_passband,
+                        filter_type='butter', order=10, rp=None)
+        
+            triggered_any_current = False
+            triggered_threshold_current = 0
+
+            
+            for threshold in thresholds:
+                if not triggered_any_current:
+                    trigger.run(event, station, det, threshold=threshold,
+                            integration_window=trigger_integration_window,
+                            triggered_channels=[surface_channel],
+                            trigger_name="power_integration_trigger")
+                    tt = station.get_trigger('power_integration_trigger')
+                    # set to triggered
+                    triggered_any_current = tt.has_triggered()
+                    if tt.has_triggered():
+                        triggered_threshold_current = threshold
+                else:
+                    break
             
             
             
             print(f"Data event {i}, triggered at threshold {triggered_threshold_any}")
             print(f"Generated event {i}, triggered at threshold {triggered_threshold_gen}")
+            print(f"Current event {i}, triggered at threshold {triggered_threshold_current}")
             result = {"station_number": event.get_station_ids()[0],
                 "run_number": event.get_run_number(),
                 "event_number": event.get_id(),
                 "event_i": i,
                 "threshold": triggered_threshold_any, "threshold_generator": triggered_threshold_gen,
-                "triggered": triggered_any}
+                "threshold_current": triggered_threshold_current, "triggered": triggered_any}
             results.append(result)
             
     df = pd.DataFrame(results)
-    df.to_hdf("trigger_threshold_scan.hdf5", "data")
+    df.to_hdf("power_trigger_threshold_scan.hdf5", "data")
 
 def analyze_threshold(lower, higher):
     if lower < 1:
         lower = 0
-    data = pd.read_hdf("trigger_threshold_scan.hdf5")
+    data = pd.read_hdf("power_trigger_threshold_scan.hdf5")
 
     thresholds = np.logspace(-4,-1,200)
 
     n_triggers = np.zeros_like(thresholds)
     n_triggers_gen = np.zeros_like(thresholds)
+    n_triggers_current = np.zeros_like(thresholds) 
 
     for i, thres in enumerate(thresholds):
         n_triggers[i] = np.sum(data.threshold>thres)
         n_triggers_gen[i] = np.sum(data.threshold_generator>thres)
+        n_triggers_current[i] = np.sum(data.threshold_current>thres)
 
     plt.plot(thresholds, n_triggers/len(data.threshold), label = "Data")
     plt.plot(thresholds, n_triggers_gen/len(data.threshold_generator), label = "Generated")
+    plt.plot(thresholds, n_triggers_current/len(data.threshold_current), label = "Current")
     plt.xlabel("Trigger threshold")
     plt.ylabel("Triggered event fraction")
     plt.legend()
-    plt.title(f"Trigger analysis with bandpass filter ({lower}-{higher} MHz)")
+    plt.title(f"Power trigger analysis with filter ({lower}-{higher} MHz)")
     plt.semilogx()
+    plt.show()
     
     # plt.savefig(f"threshold_{lower}_{higher}.png")
-    plt.savefig("STFT")
+    # plt.savefig(f"threshold_kapre_{lower}_{higher}.png")
     
 
 if __name__ == "__main__":
     path_to_files = "../../shallman/data/rno_g/forced_triggers/inbox/"
     lower = 0.0000001
-    higher = 200
+    higher = 1600
     threshold(path_to_files, lower, higher)
     analyze_threshold(lower, higher)
     
