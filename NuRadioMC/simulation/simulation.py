339c3c851ed2331ea7bc410607d21673958b7949
lpyras <pyras@posteo.de>
2022-10-17 15:36:22 +0200

    fix typo

--- NuRadioMC/simulation/simulation.py
+++ NuRadioMC/simulation/simulation.py
@@ -290,7 +290,7 @@ class simulation():
             self._tt = np.arange(0, self._n_samples * self._dt, self._dt)
 
             self._create_sim_station()
-            for i_ch, channel_id in enumerate(self._det.get_channel_ids(self._station_id)):
+            for channel_id in self._det.get_channel_ids(self._station_id):
                 electric_field = NuRadioReco.framework.electric_field.ElectricField([channel_id], self._det.get_relative_position(self._sim_station.get_id(), channel_id))
                 trace = np.zeros_like(self._tt)
                 trace[self._n_samples // 2] = 100 * units.V  # set a signal that will satisfy any high/low trigger
@@ -310,7 +310,7 @@ class simulation():
             self._detector_simulation_filter_amp(self._evt, self._station, self._det)
             self._bandwidth_per_channel[self._station_id] = {}
             self._amplification_per_channel[self._station_id] = {}
-            for channel_id in self._det.get_channel_ids(self._station_id):
+            for i_ch, channel_id in enumerate(self._det.get_channel_ids(self._station_id)):
                 ff = np.linspace(0, 0.5 / self._dt, 10000)
                 filt = np.ones_like(ff, dtype=np.complex)
                 for i, (name, instance, kwargs) in enumerate(self._evt.iter_modules(self._station_id)):
