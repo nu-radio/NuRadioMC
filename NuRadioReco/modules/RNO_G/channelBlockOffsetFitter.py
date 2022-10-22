016019abcb17dfb0676c2e50a4ca9820b443bb10
sjoerd-bouma <sjoerdbouma@xs4all.nl>
2022-10-22 13:41:29 +0200

    changed block offset fit function to standalone

--- NuRadioReco/modules/RNO_G/channelBlockOffsetFitter.py
+++ NuRadioReco/modules/RNO_G/channelBlockOffsetFitter.py
@@ -1,4 +1,4 @@
-from NuRadioReco.utilities import units
+from NuRadioReco.utilities import units, fft
 from NuRadioReco.framework.base_trace import BaseTrace
 # from iminuit import Minuit
 import numpy as np
@@ -24,7 +24,6 @@ class channelBlockOffsets:
         """
         self.sampling_rate = None
         self.block_size = block_size # the size (in samples) of the blocks
-        self._offset_guess = dict()
         self._offset_fit = dict()
         self._offset_inject = dict()
         self._max_frequency = max_frequency
@@ -148,61 +147,10 @@ class channelBlockOffsets:
             channel = station.get_channel(channel_id)
             trace = channel.get_trace()
 
-            # we use the bandpass-filtered trace to get a first estimate of
-            # the block offsets, by simply averaging over each block.
-            filtered_trace = channel.get_filtered_trace([0, self._max_frequency], 'rectangular')
-            self.sampling_rate = channel.get_sampling_rate()
-            n_blocks = len(trace) // block_size
-
-            # obtain guesses for block offsets
-            a_guess = np.array([
-                np.mean(filtered_trace[i*block_size:(i+1)*block_size])
-                for i in range(n_blocks)
-            ])
-            self._offset_guess[channel_id] = a_guess
-            # we can get rid of one parameter through a global shift
-            a_guess = a_guess[:-1] - a_guess[-1]
-            frequencies = channel.get_frequencies()
-            spectrum = channel.get_frequency_spectrum()
-
-            # we perform the fit out-of-band, in order to avoid
-            # distorting any actual signal
-            mask = (frequencies > 0) & (frequencies < self._max_frequency)
-
-            frequencies_oob = frequencies[mask]
-            spectrum_oob = spectrum[mask]
-
-            ns = self.block_size
-            dt = 1/self.sampling_rate
-
-            # most of the terms in the fit depend only on the frequencies,
-            # sampling rate and number of blocks. We therefore calculate these
-            # only once, outside the fit function.
-            pre_factor_exponent = np.array([
-                -2.j * np.pi * frequencies_oob * dt * ((j+.5) * ns - .5)
-                 for j in range(len(a_guess))
-            ])
-            self._const_fft_term = (
-                 1 / self.sampling_rate * np.sqrt(2) # NuRadio FFT normalization
-                * np.exp(pre_factor_exponent)
-                * np.sin(np.pi*frequencies_oob*ns*dt)[None]
-                / np.sin(np.pi*frequencies_oob*dt)[None]
+            block_offsets = fit_block_offsets(
+                trace, block_size,
+                channel.get_sampling_rate(), self._max_frequency
             )
-            self._spectrum = spectrum_oob
-            res = scipy.optimize.fmin(
-                self._pedestal_fit, a_guess, disp=0)
-            ### Minuit seems a lot quicker than scipy - not sure why?
-            # m = Minuit(self._pedestal_fit_minuit, a_guess * nufft_conversion_factor)
-            # m.errordef = 1
-            # m.errors = 0.01 * np.ones_like(a_guess)
-            # m.migrad(ncall=20000)
-            # res = m.values
-            block_offsets = np.zeros(len(res) + 1)
-            block_offsets[:-1] = res
-            # the fit is not sensitive to an overall shift,
-            # so we include the zero-meaning here
-            block_offsets += np.mean(trace) - np.mean(block_offsets)
-
             self._offset_fit[channel_id] = block_offsets
 
     def get_offsets(self, channel_id, offset_type='fit'):
@@ -217,7 +165,6 @@ class channelBlockOffsets:
             Options:
 
             - 'fit': return the fitted block offsets
-            - 'guess': return the first guess for the block offsets
             - 'injected': return the block offsets that were injected
               using the ``add_offsets`` method.
 
@@ -231,8 +178,6 @@ class channelBlockOffsets:
         trace = BaseTrace()
         if offset_type == 'fit':
             trace.set_trace(np.repeat(self._offset_fit[channel_id], self.block_size), self.sampling_rate)
-        elif offset_type == 'guess':
-            trace.set_trace(np.repeat(self._offset_guess[channel_id], self.block_size), self.sampling_rate)
         elif offset_type == 'injected':
             trace.set_trace(np.repeat(self._offset_inject[channel_id], self.block_size), self.sampling_rate)
         return trace
@@ -241,3 +186,116 @@ class channelBlockOffsets:
         fit = np.sum(a[:, None] * self._const_fft_term, axis=0)
         chi2 = np.sum(np.abs(fit-self._spectrum)**2)
         return chi2
+
+def fit_block_offsets(
+        trace, block_size=128, sampling_rate=3.2*units.GHz,
+        max_frequency=50*units.MHz, return_trace = False,
+        xtol=1e-6, maxiter=100000):
+    """
+    Fit 'block' offsets for a voltage trace
+
+    Fit block offsets ('rect'-shaped offsets from a baseline)
+    using a fit to the out-of-band spectrum of a voltage trace.
+
+    Parameters
+    ----------
+    trace: numpy Array
+        the voltage trace
+    block_size: int (default: 128)
+        the number of samples in one block
+    sampling_rate: float (default: 3.2 GHz)
+        the sampling rate of the trace
+    max_frequency: float (default: 50 MHz)
+        the fit to the block offsets is performed
+        in the frequency domain, in the band up to
+        max_frequency
+    return_trace: bool (default: False)
+        if True, return the tuple (offsets, output_trace)
+        where the output_trace is the input trace with
+        fitted block offsets removed
+
+    Returns
+    -------
+    block_offsets: numpy array
+        The fitted block offsets.
+    output_trace: numpy array or None
+        The input trace with the fitted block offsets removed.
+        Returned only if return_trace=True
+
+    Other Parameters
+    ----------------
+    xtol: float (default: 1e-6)
+        tolerance parameter passed on to scipy.optimize.fmin
+    maxiter: int (default: 100000)
+        maximum number of iterations for scipy.optimize.fmin
+
+    """
+    dt = 1. / sampling_rate
+    spectrum = fft.time2freq(trace, sampling_rate)
+    frequencies = np.fft.rfftfreq(len(trace), dt)
+    n_blocks = len(trace) // block_size
+
+    mask = (frequencies > 0) & (frequencies < max_frequency) #  a simple rectangular filter
+    frequencies_oob = frequencies[mask]
+    spectrum_oob = spectrum[mask]
+
+    # we use the bandpass-filtered trace to get a first estimate of
+    # the block offsets, by simply averaging over each block.
+    filtered_trace_fft = np.copy(spectrum)
+    filtered_trace_fft[~mask] = 0
+    filtered_trace = fft.freq2time(filtered_trace_fft, dt)
+
+    # obtain guesses for block offsets
+    a_guess = np.array([
+        np.mean(filtered_trace[i*block_size:(i+1)*block_size])
+        for i in range(n_blocks)
+    ])
+    # self._offset_guess[channel_id] = a_guess
+    # we can get rid of one parameter through a global shift
+    a_guess = a_guess[:-1] - a_guess[-1]
+
+    # we perform the fit out-of-band, in order to avoid
+    # distorting any actual signal
+
+    # most of the terms in the fit depend only on the frequencies,
+    # sampling rate and number of blocks. We therefore calculate these
+    # only once, outside the fit function.
+    pre_factor_exponent = np.array([
+        -2.j * np.pi * frequencies_oob * dt * ((j+.5) * block_size - .5)
+            for j in range(len(a_guess))
+    ])
+    const_fft_term = (
+            1 / sampling_rate * np.sqrt(2) # NuRadio FFT normalization
+        * np.exp(pre_factor_exponent)
+        * np.sin(np.pi*frequencies_oob*block_size*dt)[None]
+        / np.sin(np.pi*frequencies_oob*dt)[None]
+    )
+
+    def pedestal_fit(self, a):
+        fit = np.sum(a[:, None] * const_fft_term, axis=0)
+        chi2 = np.sum(np.abs(fit-spectrum_oob)**2)
+        return chi2
+
+    # self._spectrum = spectrum_oob
+    res = scipy.optimize.fmin(
+        pedestal_fit, a_guess, disp=0,
+        xtol=xtol, maxiter=maxiter)
+    ### maybe TODO - include option to use Minuit, which seems a lot quicker?
+    # m = Minuit(self._pedestal_fit_minuit, a_guess * nufft_conversion_factor)
+    # m.errordef = 1
+    # m.errors = 0.01 * np.ones_like(a_guess)
+    # m.migrad(ncall=20000)
+    # res = m.values
+
+    block_offsets = np.zeros(len(res) + 1)
+    block_offsets[:-1] = res
+
+    # the fit is not sensitive to an overall shift,
+    # so we include the zero-meaning here
+    block_offsets += np.mean(trace) - np.mean(block_offsets)
+
+    if return_trace:
+        output_trace = trace - np.repeat(block_offsets, block_size)
+        return block_offsets, output_trace
+
+    return block_offsets
