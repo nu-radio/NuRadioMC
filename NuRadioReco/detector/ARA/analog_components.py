89f5e59a4c7d48bef0fb878a3b6e311c6f5e56c1
Pawan Giri <pgiri4@login.crane.hcc.unl.edu>
2022-11-04 12:54:49 -0500

    modify analog_components and hardwareResponseIncorporator file

--- NuRadioReco/detector/ARA/analog_components.py
+++ NuRadioReco/detector/ARA/analog_components.py
@@ -1,10 +1,14 @@
 import numpy as np
 import os
+import datetime
 from NuRadioReco.utilities import units
 from scipy.interpolate import interp1d
+from NuRadioReco.detector.ARA import ARA_stations_configuration
+import logging
 
+logger = logging.getLogger("analog_components")
 
-def load_system_response(path=os.path.dirname(os.path.realpath(__file__))):
+def load_system_response(path=os.path.dirname(os.path.realpath(__file__)), **kwargs):
     """
     Default file was imported from:
     https://github.com/bhokansonfasig/pyrex/tree/master/pyrex/custom/ara/data
@@ -15,29 +19,66 @@ def load_system_response(path=os.path.dirname(os.path.realpath(__file__))):
         Path to the file containing the system response
     """
 
-    data = np.loadtxt(os.path.join(path, "HardwareResponses/ARA_Electronics_TotalGain_TwoFilters.txt"),
-                      skiprows=3, delimiter=',')
+    station_id = kwargs.get("station_id")
+    ARA_config_id  = kwargs.get("ARA_config_id")
+    if ARA_config_id == 'C0':
+       data = np.loadtxt(os.path.join(path, "HardwareResponses/ARA_default_configuration.txt"),
+                      skiprows=3, delimiter=' ') # Proceed with default gain (ARAsim default gain)
+    else:
+       data = np.loadtxt(os.path.join(path, "HardwareResponses/ARA_"+str(station_id)+"_configuration_"+str(ARA_config_id)+".txt"),
+               skiprows=3, delimiter=' ') # individual hardware response for A2 and A3 channels taken from[https://github.com/MyoungchulK/MF_filters/tree/main/data/in_situ_sc_gain]
+    
+    number_of_columns = data.shape[1]
     default = {}
+    default['gain'] = []
+    default['phase'] = []
     default['frequencies'] = data[:, 0] * units.MHz
-    default['gain'] = data[:, 1]  # unitless
-    default['phase'] = data[:, 2] * units.rad  # rad
+    for i in range((number_of_columns)):
+        if i%2 !=0 :    ###### odd numbered columns contain gain values ######
+           per_channel_gain = data[:, int(i)]  # unitless
+           default['gain'].append(per_channel_gain)
+        elif i%2 ==0 and i!=0:    ###### even numbered columns contain phase values ######
+           per_channel_phase = data[:, int(i)] 
+           default['phase'].append(per_channel_phase)
 
     return default
 
 
 system_response = {}
-system_response['default'] = load_system_response()
+collect_station_id = []
+system_response_collection = []
 
 
-def get_system_response(frequencies):
-    orig_frequencies = system_response['default']['frequencies']
+def get_system_response(frequencies, station, evt_time):
+    station_id = station.get_id()
+    channels = station.iter_channels()
+    global ARA_config_id
+
+    ## Check if stationi_id is already saved or not and read ARA_config_id only once for given station
+    if station_id in collect_station_id:
+       system_response['default'] = system_response_collection[collect_station_id.index(station_id)]
+    else:
+       ARA_config_id = ARA_stations_configuration.get_ARA_hardware_configuration(station_id, evt_time)
+       system_response['default'] = load_system_response(station_id= station_id,  ARA_config_id = ARA_config_id)
+       collect_station_id.append(station_id)
+       system_response_collection.append(system_response['default'])
+       logger.warning(" Processing with ARA station {} and ARA_configuration {}".format(station_id,ARA_config_id)) 
+
+    default_frequencies = system_response['default']['frequencies']
     phase = system_response['default']['phase']
     gain = system_response['default']['gain']
-
-    interp_phase = interp1d(orig_frequencies, np.unwrap(phase), bounds_error=False, fill_value=0)
-    interp_gain = interp1d(orig_frequencies, gain, bounds_error=False, fill_value=0)
-
+    
     system = {}
-    system['gain'] = interp_gain(frequencies)
-    system['phase'] = np.exp(1j * interp_phase(frequencies))
+    system['gain'] = []
+    system['phase'] = []
+
+    for channel in channels:
+        channel_id = channel.get_id()
+        interp_gain = interp1d(default_frequencies, gain[channel_id], bounds_error=False, fill_value=0)
+        interp_phase = interp1d(default_frequencies, np.unwrap(phase[channel_id]), bounds_error=False, fill_value=0) 
+        gain_value = interp_gain(frequencies)
+        phase_value = np.exp(1j * interp_phase(frequencies))
+        system['gain'].append(gain_value)
+        system['phase'].append(phase_value)
+
     return system
