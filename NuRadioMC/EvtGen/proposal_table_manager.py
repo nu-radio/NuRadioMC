c38a40cccf2b11511981947c8f071862f95e61c9
Steffen Hallmann <steffen.hallmann@desy.de>
2023-01-30 17:39:32 +0100

    improve info printout and readme

--- NuRadioMC/EvtGen/proposal_table_manager.py
+++ NuRadioMC/EvtGen/proposal_table_manager.py
@@ -9,7 +9,16 @@ import logging
 logger = logging.getLogger('ProposalTablesManager')
 
 def produce_proposal_tables(config_file, tables_path=None):
+    """
+    produce proposal tables for all relevant flavors
 
+    Parameters
+    ----------
+    config_file: string
+        one of the default PROPOSAL configurations ['InfIce','SouthPole', 'MooresBay', 'Greenland']
+    tables_path: string
+        output path for the generated tables
+    """
     proposal_func = ProposalFunctions(config_file=config_file, tables_path=tables_path)
 
     for particle_code in [-15, -13, 13, 15]:
@@ -17,12 +26,33 @@ def produce_proposal_tables(config_file, tables_path=None):
         proposal_func._ProposalFunctions__get_propagator(particle_code=particle_code)
 
 
+def get_compiler():
+    # proposal (as of V 7.5.0) generates tables with different hashes for clang and gcc
+    # infer which of the precalculation is relevant here
+    compiler = "gcc"
+    if "clang" in sys.version.lower():
+        compiler = "clang"
+    return compiler
+
 def produce_proposal_tables_tarball(config_file, tables_path=None):
+    """
+    produce proposal tables tarball for all relevant flavors
+    Note: the produced tarballs need to be placed in the
+          NuRadioMC/proposal_tables/{version}/{compiler} directory on the desy cluster.
+
+    Parameters
+    ----------
+    config_file: string
+        one of the default PROPOSAL configurations ['InfIce','SouthPole', 'MooresBay', 'Greenland']
+    tables_path: string
+        output path for the generated tables
+    """    
+
     if tables_path is None:
         proposal_func = ProposalFunctions(config_file=config_file)
         tables_path = proposal_func._ProposalFunctions__tables_path
 
-    outdir = f"./{tables_path}/v{proposal.__version__}"
+    outdir = f"./{tables_path}/v{proposal.__version__}/{get_compiler()}"
     if not os.path.exists(outdir):
         os.makedirs(outdir)
 
@@ -41,6 +71,18 @@ def produce_proposal_tables_tarball(config_file, tables_path=None):
                 'gztar', tables_path)
 
 def download_proposal_tables(config_file, tables_path=None):
+    """
+    download precalculated proposal tables for all relevant flavors
+    from the NuRadioMC data storage
+
+    Parameters
+    ----------
+    config_file: string
+        one of the default PROPOSAL configurations ['InfIce','SouthPole', 'MooresBay', 'Greenland']
+    tables_path: string
+        output path for the generated tables
+    """
+
     if tables_path is None:
         proposal_func = ProposalFunctions(config_file=config_file)
         tables_path = proposal_func._ProposalFunctions__tables_path
@@ -50,7 +92,7 @@ def download_proposal_tables(config_file, tables_path=None):
         # does not exist yet -> download file
         import requests
         proposal_version = proposal.__version__
-        URL = f'https://rnog-data.zeuthen.desy.de/proposal_tables/v{proposal_version}/{config_file}.tar.gz'
+        URL = f'https://rnog-data.zeuthen.desy.de/proposal_tables/v{proposal_version}/{get_compiler()}/{config_file}.tar.gz'
 
         folder = tables_path #os.path.dirname(tables_path)
         if not os.path.exists(folder):
@@ -74,12 +116,15 @@ def download_proposal_tables(config_file, tables_path=None):
 if __name__ == "__main__":
     parser = argparse.ArgumentParser("NuRadioProposal tables I/O")
     parser.add_argument("option", choices=["create", "download"])
-    parser.add_argument('config_file')
-    parser.add_argument('-t', '--tables_path', default=None)
+    parser.add_argument('config_file', help="one of the default configurations ['InfIce','SouthPole', 'MooresBay', 'Greenland'] or 'all'")
+    parser.add_argument('-t', '--tables_path', default=None, help="target path for table creation/download")
 
     args = parser.parse_args()
 
+    logger.warning(f"Your compiler type is {get_compiler()}")
+
     if args.option == "create":
+        logger.warning(f"Creating proposal tables for {args.option}")
         if args.config_file == "all":
             cfgs = ['InfIce','SouthPole', 'MooresBay', 'Greenland']
             for cfg in cfgs:
@@ -88,4 +133,5 @@ if __name__ == "__main__":
             produce_proposal_tables_tarball(args.config_file, args.tables_path)
 
     elif args.option == "download":
+        logger.warning(f"Downloading proposal tables for {args.option}")
         download_proposal_tables(args.config_file, args.tables_path)
