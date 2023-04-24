b6dff4d5956b8c47f6718c0ad325bc5e525c111c
Felix Schlüter <fschlueter@icecube.wisc.edu>
2023-04-24 16:31:35 +0200

    Implement more getter functions. Add docstrings

--- NuRadioReco/detector/RNO_G/rnog_detector.py
+++ NuRadioReco/detector/RNO_G/rnog_detector.py
@@ -5,7 +5,7 @@ import numpy as np
 from radiotools import helper
 from scipy import interpolate
 
-from NuRadioReco.detector.db_mongo_read import Database
+from NuRadioReco.detector.db_mongo_read import Database, filtered_keys
 import NuRadioReco.framework.base_trace
 from NuRadioReco.utilities import units
 import collections
@@ -267,40 +267,194 @@ class RNOG_Detector():
         return response
 
 
-    def get_station_id(self):
-        pass      
+    def get_station_ids(self):
+        """ 
+        Returns a list of all commissioned station ids. Queries basic station information of all stations
+        and add stations to buffer.
+        
+        Simple command to get all configured station ids (without checking commission time):
+            station_ids = self.__db.get_station_ids("station_rnog")
+        
+        What is missing sofar: When do we need to update the buffer.
+        
+        Returns
+        -------
+        
+        ids: list
+            List of all station ids.
+        """
+        
+        # I.e., when the buffer is empty
+        if not len(self.__buffered_stations.keys()):
+
+            time_filter = [{"$match": {
+                'commission_time': {"$lte": self.__detector_time},
+                'decommission_time': {"$gte": self.__detector_time}}}]
+
+            # get all stations which fit the filter
+            stations_information = list(self.__db.db["station_rnog"].aggregate(time_filter))
             
+            for station_info in stations_information:
+                station_id = station_info["id"]
+                for key in filtered_keys(station_info, "id"):
+                    self.__buffered_stations[station_id][key] = station_info[key]
+        
+        return list(self.__buffered_stations.keys())
+    
+    
+    def has_station(self, station_id):
+        """
+        Returns true if the station is commission. First checks buffer. If not in buffer, queries (and buffers)
+        the basic information of all stations and checks if station is among them. 
+        
+        Parameters
+        ----------
+        
+        station_id: int
+            Station id which uniquely idendifies a station
+        
+        Returns
+        -------
+        
+        has_station: bool
+            Returns True if the station could be found. A found station will be in the buffer.
+        """
+        # Check if station is in buffer ...
+        if station_id not in self.__buffered_stations:
+            
+            # ... if not check DB (this will also store them in self.__buffered_stations)
+            station_ids = self.get_station_ids()
+
+            return station_id in station_ids
+        else:
+            return True
+
+
     def get_number_of_channels(self, station_id):
-        pass
+        """
+        Get number of channels for a particlular station. It will query the basic information of all stations in the 
+        Database if necessary. Raises an error if the station is not commission. 
+        
+        Parameters
+        ----------
+        
+        station_id: int
+            Station id which uniquely idendifies a station
+        
+        Returns
+        -------
+        
+        channel_ids: int
+            Number of all channels for the requested station
+        """
+        
+        if not self.has_station(station_id):
+            err = f"Station id {station_id} not commission at {self.__detector_time}"
+            self.logger.error(err)
+            raise ValueError(err)
+            
+        # now station is in buffer (or an error was raised)
+        channels = self.__buffered_stations[station_id]["channels"]
+                        
+        return len(channels)
+    
     
     def get_channel_ids(self, station_id):
-        pass
+        """
+        Get channel ids for a particlular station. It will query the basic information of all stations in the 
+        Database if necessary. Raises an error if the station is not commission. 
+        
+        Parameters
+        ----------
+        
+        station_id: int
+            Station id which uniquely idendifies a station
+        
+        Returns
+        -------
+        
+        channel_ids: list(int)
+            A list of all channel ids for the requested station
+        """
+
+        if not self.has_station(station_id):
+            err = f"Station id {station_id} not commission at {self.__detector_time}"
+            self.logger.error(err)
+            raise ValueError(err)
+            
+        # now station is in buffer (or an error was raised)
+        channels = self.__buffered_stations[station_id]["channels"]
+        
+        return [ele["id"] for ele in channels]
     
     def get_number_of_samples(self, station_id, channel_id):
-        pass
+        """ Get number of samples per station / channel """
+        self.logger.warn("Return a hard-coded value of 2048 samples. This information is not (yet) implemented in the DB.")
+        return 2048
     
     def get_sampling_frequency(self, station_id, channel_id):
-        pass
+        """ Get sampling frequency per station / channel """
+        self.logger.warn("Return a hard-coded value of 3.2 GHz. This information is not (yet) implemented in the DB.")
+        return 3.2 * units.GHz
+        
     
     def get_noise_temperature(self, station_id, channel_id):
         pass
     
+    
     def is_channel_noiseless(self, station_id, channel_id):
         pass
+
             
     def get_cable_delay(self, station_id, channel_id):
-        pass
+        self.logger.error("The cable delay is not yet implemented in the DB.")
+        raise NotImplementedError("The cable delay is not yet implemented in the DB.")
+    
     
     def get_site(self, station_id):
-        pass
+        """
+        This detector class is exclusive for the RNO-G detector at Summit Greenland.
+        
+        Returns
+        -------
+        
+        site: str
+            Returns "summit"
+        """
+        return "summit"
+            
             
     def db(self):
         return self.__db
     
     
 class Response:
+    """
+    This class provides an interface to read-in and apply the complex response functions of the
+    various components of the signal chain of a RNO-G channel. The class allows to combine the 
+    response of several components into one response by multiplying them.
+    """
     
     def __init__(self, frequency, y, y_unit, name="default"):
+        """
+        Parameters
+        ----------
+        
+        frequency: list(float)
+            The frequency vector at which the response is measured. Unit has to be GHz.
+            
+        y: [list(float), list(float)]
+            The measured response. First entry is the vector of the measured amplitude, the second entry is the measured phase.
+            The unit of both entries is specified with the next argument.
+            
+        y_unit: [str, str]
+            The first entry specifies the unit of the measured amplitude. Options are "dB", "MAG" and "mag".
+            The second entry specifies the unit of the measured phase. Options are "rad" and "deg".
+
+        name: str
+            Give the response a name. This is only use for printing purposes. (Default: "default")        
+        """
+        
         
         self.__names = [name]
         
@@ -334,20 +488,46 @@ class Response:
 
 
     def __call__(self, freq):
+        """
+        Returns the complex response for a given frequency.
+        
+        Parameters
+        ----------
+        
+        freq: list(float)
+            The frequencies for which to get the response.
+            
+        Returns
+        
+        response: np.array(np.complex128)
+            The complex response at the desired frequencies
+        """
         response = np.ones_like(freq, dtype=np.complex128)
         for gain, phase in zip(self.__gains, self.__phases):
-            response *= gain(freq) * np.exp(1j * phase(freq))
+            response *= gain(freq / units.GHz) * np.exp(1j * phase(freq / units.GHz))
 
         return response
     
     
     def get_names(self):
+        """ Get list of the names of all individual responses """
         return self.__names
 
 
     def __mul__(self, other):
+        """ 
+        Define multiplication operator for 
+            - Other objects of the same class
+            - Objects of type NuRadioReco.framework.base_trace
+            
+        TODO: Multiplication with base_trace will just work when the spectrum is one dimensional. Check if
+        there is a case where the spectrum could be 3 (or more) dimensional and adjust the class
+        
+        """
         
         if isinstance(other, Response):
+            # Store each response individually: append/concatenate lists of gains and phases.
+            # The multiplication happens in __call__. 
             self.__names += other.__names
             self.__gains += other.__gains
             self.__phases += other.__phases
@@ -356,7 +536,7 @@ class Response:
         elif isinstance(other, NuRadioReco.framework.base_trace):
             spec = other.get_frequency_spectrum()
             freqs = other.get_frequencies()
-            spec *= self(freqs)
+            spec *= self(freqs)  # __call__
             return other
         
         else:
@@ -364,6 +544,7 @@ class Response:
         
 
     def __rmul__(self, other):
+        """ Same as mul """
         return self.__mul__(other)
 
 
@@ -373,13 +554,16 @@ class Response:
 
 if __name__ == "__main__":
     det = RNOG_Detector()
-    
+    det.update(datetime.datetime(2022, 8, 2, 0, 0))  # datetime.datetime.utcnow())
     # det.db().get_collection_information("station_position", 11, "tape_measurement")
+    ids = det.get_number_of_channels(11)
+    print(ids)
+    print(det.get_channel_ids(11))
     # print(det.get_relative_position_device(11, None))
     # print(det.get_relative_position(11, 1))
     # det.get_full_station_from_buffer(11)
-    print(det.get_channel_signal_chain(11, 11))
-    print(det.get_signal_chain_response(11, 11))
+    # print(det.get_channel_signal_chain(11, 11))
+    # print(det.get_signal_chain_response(11, 11))
     # print(det.get_relative_position(11, 11))
     # print(det.get_channel_orientation(11, 11))
     # det.db().get_complete_channel_information(11, 0)
