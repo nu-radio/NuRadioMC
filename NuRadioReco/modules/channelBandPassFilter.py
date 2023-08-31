f9772d544de2408a5d2323a6422896928c711ec8
Mitja Desmet <mitja.desmet@vub.be>
2023-08-31 16:39:46 +0200

    Implemented Gaussian tapered and Hann tapered filters

--- NuRadioReco/modules/channelBandPassFilter.py
+++ NuRadioReco/modules/channelBandPassFilter.py
@@ -2,7 +2,7 @@ import numpy as np
 from scipy import signal
 import logging
 from NuRadioReco.modules.base.module import register_run
-from NuRadioReco.utilities import units, bandpass_filter
+from NuRadioReco.utilities import units, bandpass_filter, signal_processing
 
 
 class channelBandPassFilter:
@@ -18,31 +18,44 @@ class channelBandPassFilter:
     def begin(self):
         pass
 
-    def get_filter_arguments(self, channel_id, passband, filter_type, order=2, rp=None):
-        if(isinstance(passband, dict)):
+    @staticmethod
+    def get_filter_arguments(channel_id, passband, filter_type, order=2,
+                             rp=None, roll_width=None, half_hann_percent=None):
+        if isinstance(passband, dict):
             tmp_passband = passband[channel_id]
         else:
             tmp_passband = passband
 
-        if(isinstance(order, dict)):
+        if isinstance(order, dict):
             tmp_order = order[channel_id]
         else:
             tmp_order = order
 
-        if(isinstance(filter_type, dict)):
+        if isinstance(filter_type, dict):
             tmp_filter_type = filter_type[channel_id]
         else:
             tmp_filter_type = filter_type
 
-        if(isinstance(rp, dict)):
+        if isinstance(rp, dict):
             tmp_rp = rp[channel_id]
         else:
             tmp_rp = rp
-        return tmp_passband, tmp_order, tmp_filter_type, tmp_rp
+
+        if isinstance(roll_width, dict):
+            tmp_roll_width = roll_width[channel_id]
+        else:
+            tmp_roll_width = roll_width
+
+        if isinstance(half_hann_percent, dict):
+            tmp_half_hann_percent = half_hann_percent[channel_id]
+        else:
+            tmp_half_hann_percent = half_hann_percent
+        return tmp_passband, tmp_order, tmp_filter_type, tmp_rp, tmp_roll_width, tmp_half_hann_percent
 
     @register_run()
     def run(self, evt, station, det, passband=None,
-            filter_type='rectangular', order=2, rp=None):
+            filter_type='rectangular', order=2, rp=None,
+            roll_width=2.5 * units.MHz, half_hann_percent=0.1):
         """
         Run the filter
 
@@ -58,8 +71,11 @@ class channelBandPassFilter:
             'rectangular': perfect straight line filter
             'butter': butterworth filter from scipy
             'butterabs': absolute of butterworth filter from scipy
+            'gaussian_tapered' : a rectangular bandpass filter convolved with a Gaussian
             or any filter that is implemented in NuRadioReco.detector.filterresponse. In this case the
             passband parameter is ignored
+            or 'hann_tapered' : a rectangular bandpass filter with the ends replaced by a half-Hann window to applied
+            in the time domain. In this case the passband parameter is ignored
             or 'FIR <type> <parameter>' - see below for FIR filter options
 
             a dict can be used to specify a different bandwidth per channel, the key is the channel_id
@@ -70,6 +86,15 @@ class channelBandPassFilter:
             (for chebyshev filter)
 
             a dict can be used to specify a different bandwidth per channel, the key is the channel_id
+        roll_width : float, default=2.5MHz
+            Determines the sigma of the Gaussian to be used in the convolution of the rectangular filter.
+            (Relevant for the Gaussian tapered filter)
+        half_hann_percent : float, default=0.1
+            The size of the half-Hann window expressed as a percentage of the length of the trace.
+            (Relevant for the Hann tapered filter)
+
+        Notes
+        -----
         Added Jan-07-2018 by robert.lahmann@fau.de:
         FIR filter:
         see https://docs.scipy.org/doc/scipy/reference/generated/scipy.signal.get_window.html
@@ -80,7 +105,7 @@ class channelBandPassFilter:
         filter_type='FIR kaiser 10' : Use Kaiser window with beta parameter 10
         filter_type='FIR kaiser' : Use Kaiser window with default beta parameter 6
 
-        In principal, window names are just passed on to signal.firwin(), but if parameters
+        In principle, window names are just passed on to signal.firwin(), but if parameters
         are required, then these cases must be explicitly implemented in the code below.
 
         The four main filter types can be implemented:
@@ -93,10 +118,15 @@ class channelBandPassFilter:
         if passband is None:
             passband = [55 * units.MHz, 1000 * units.MHz]
         for channel in station.iter_channels():
-            tmp_passband, tmp_order, tmp_filter_type, tmp_rp = self.get_filter_arguments(channel.get_id(), passband, filter_type, order, rp)
-            self._apply_filter(channel, tmp_passband, tmp_filter_type, tmp_order, tmp_rp, False)
+            tmp_passband, tmp_order, tmp_filter_type, tmp_rp, tmp_roll_width, tmp_half_hann_percent = \
+                self.get_filter_arguments(
+                    channel.get_id(), passband, filter_type, order, rp, roll_width, half_hann_percent
+                )
+            self._apply_filter(channel, tmp_passband, tmp_filter_type, tmp_order, tmp_rp, tmp_roll_width,
+                               tmp_half_hann_percent, False)
 
-    def get_filter(self, frequencies, station_id, channel_id, det, passband, filter_type, order=2, rp=None):
+    def get_filter(self, frequencies, station_id, channel_id, det, passband, filter_type,
+                   order=2, rp=None, roll_width=2.5 * units.MHz):
         """
         helper function to return the filter that the module applies.
 
@@ -120,8 +150,11 @@ class channelBandPassFilter:
             'rectangular': perfect straight line filter
             'butter': butterworth filter from scipy
             'butterabs': absolute of butterworth filter from scipy
+            'gaussian_tapered' : Gaussian tapered version of the rectangular filter
             or any filter that is implemented in NuRadioReco.detector.filterresponse. In this case the
             passband parameter is ignored
+            or 'hann_tapered' : a rectangular bandpass filter with the ends replaced by a half-Hann window to applied
+            in the time domain. In this case the passband parameter is ignored
             or 'FIR <type> <parameter>' - see below for FIR filter options
 
             a dict can be used to specify a different bandwidth per channel, the key is the channel_id
@@ -133,18 +166,23 @@ class channelBandPassFilter:
             (for chebyshev filter)
 
             a dict can be used to specify a different bandwidth per channel, the key is the channel_id
-
-
+        roll_width : float, default=2.5MHz
+            Determines the sigma of the Gaussian to be used in the convolution of the rectangular filter.
+            (Relevant for the Gaussian tapered filter)
 
         Returns
         -------
          array of complex floats
             the complex filter amplitudes
         """
-        tmp_passband, tmp_order, tmp_filter_type, tmp_rp = self.get_filter_arguments(channel_id, passband, filter_type, order, rp)
-        return bandpass_filter.get_filter_response(frequencies, tmp_passband, tmp_filter_type, tmp_order, tmp_rp)
+        tmp_passband, tmp_order, tmp_filter_type, tmp_rp, tmp_roll_width, _ = \
+            self.get_filter_arguments(channel_id, passband, filter_type, order, rp, roll_width)
+        return bandpass_filter.get_filter_response(
+            frequencies, tmp_passband, tmp_filter_type, tmp_order, tmp_rp, tmp_roll_width
+        )
 
-    def _apply_filter(self, channel, passband, filter_type, order, rp=None, is_efield=False):
+    def _apply_filter(self, channel, passband, filter_type, order,
+                      rp=None, roll_width=None, half_hann_percent=None, is_efield=False):
 
         frequencies = channel.get_frequencies()
         trace_fft = channel.get_frequency_spectrum()
@@ -158,18 +196,25 @@ class channelBandPassFilter:
 
         isFIR = False
 
-        if(filter_type == 'rectangular'):
+        if filter_type == 'rectangular':
             trace_fft *= self.get_filter(frequencies, 0, 0, None, passband, filter_type)
-        elif(filter_type == 'butter'):
+        elif filter_type == 'butter':
             trace_fft *= self.get_filter(frequencies, 0, 0, None, passband, filter_type, order)
-        elif(filter_type == 'butterabs'):
+        elif filter_type == 'butterabs':
             trace_fft *= self.get_filter(frequencies, 0, 0, None, passband, filter_type, order)
-        elif(filter_type == 'cheby1'):
+        elif filter_type == 'cheby1':
             trace_fft *= self.get_filter(frequencies, 0, 0, None, passband, filter_type, order, rp)
-        elif(filter_type.find('FIR') >= 0):
+        elif filter_type == 'gaussian_tapered':
+            trace_fft *= self.get_filter(frequencies, 0, 0, None, passband, filter_type, order, rp, roll_width)
+        elif filter_type == 'hann_tapered':
+            trace_fir = channel.get_trace()
+            trace_fir *= signal_processing.half_hann_window(len(trace_fir), half_percent=half_hann_percent)
+            # This filter is applied in the time domain, so use the isFIR variable to set the time trace back
+            isFIR = True
+        elif filter_type.find('FIR') >= 0:
             # print('This is a FIR filter')
             firarray = filter_type.split()
-            if (len(firarray) == 1):
+            if len(firarray) == 1:
                 wtype = 'hamming'
                 # print('hamming window')
             else:
@@ -182,19 +227,19 @@ class channelBandPassFilter:
                     wtype = ('kaiser', beta)
             # print('window type: ', wtype)
             Nfir = order + 1
-            if (passband[0] is None):
+            if passband[0] is None:
                 # this is a low pass filter
                 pass_zero = True
                 fcut = passband[1]
-            elif (passband[1] is None or passband[1] / sample_rate >= 0.5):
+            elif passband[1] is None or passband[1] / sample_rate >= 0.5:
                 # this is a high pass filter
                 pass_zero = False
                 fcut = passband[0]
-            elif (passband[1] > passband[0]):
+            elif passband[1] > passband[0]:
                 # this is a bandpass filter
                 pass_zero = False
                 fcut = passband
-            elif(passband[0] > passband[1]):
+            elif passband[0] > passband[1]:
                 # this is a bandstop filter
                 pass_zero = True
                 fcut = [passband[1], passband[0]]
@@ -203,7 +248,7 @@ class channelBandPassFilter:
                 print("Error, could not define filter type")
             taps = signal.firwin(Nfir, fcut, window=wtype, scale=False, pass_zero=pass_zero, fs=sample_rate)
 
-            if ((Nfir // 2) * 2 == Nfir):
+            if (Nfir // 2) * 2 == Nfir:
                 print("odd filter order, rolling is off by T_s/2")
 
             ndelay = int(0.5 * (Nfir - 1))
