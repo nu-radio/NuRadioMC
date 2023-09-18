d2cb3a6204648253989cf020c23294b02890b32a
Alan Coleman <alan.coleman@physics.uu.se>
2023-09-18 20:12:26 +0200

    Updated to chunked trigger

--- NuRadioReco/modules/trigger/chunkedTrigger.py
+++ NuRadioReco/modules/trigger/chunkedTrigger.py
@@ -7,55 +7,9 @@ import numpy as np
 import time
 import torch
 
-
-def CalculateTimeOverThresholdIntervals(threshold, tot_bins, amplitudes):
-    """
-    Calculates the bin-intervals for which the amplitudes are above `threshold`
-    for at least `tot_bins`. Returns a list of pairs corresponding to the first
-    bin that is above the threshold and the first bin which drops below the threshold
-
-    Parameters
-    ----------
-    threshold: float
-        threshold above which the TOT is considered
-    tot_bins: int
-        the number of consecutive bins for which the `amplitudes` must be above `threshold`
-    amplitudes: np.ndarray or torch.Tensor of floats
-        values on which the algorithm will be run
-
-    Returns
-    -------
-    passing bin intervals: list of pairs of ints
-        pairs corresponding to the first bin that is above `threshold` and the first bin
-        when the `amplitudes` drop below the `threshold`
-
-    """
-
-    logger = logging.getLogger("CalculateTimeOverThresholdIntervals")
-
-    if isinstance(amplitudes, np.ndarray):
-        # Calculate intervals over which the values are above threshold
-        intervals = np.where(np.diff(amplitudes > threshold, prepend=0, append=0))[0].reshape(-1, 2)
-        # Select only those which are long enough
-        intervals = intervals[np.subtract(*intervals.T) <= -tot_bins]
-    elif isinstance(amplitudes, torch.Tensor):
-        # Calculate intervals over which the values are above threshold
-        intervals = torch.where(
-            torch.diff(amplitudes.squeeze() > threshold, prepend=torch.Tensor([0]), append=torch.Tensor([0]))
-        )[0].reshape(-1, 2)
-        # Select only those which are long enough
-        intervals = intervals[torch.subtract(*intervals.T) <= -tot_bins].cpu().numpy()
-    else:
-        msg = f"Supplied amplitudes of type {type(amplitudes)}. Must be np.ndarray or torch.Tensor"
-        logger.error(msg)
-        raise TypeError(msg)
-
-    return intervals
-
-
 class triggerSimulator:
     """
-    Calculates a GRU-based triggering algorithm using a time-over-threshold algorithm on the netowrk output
+    Calculates a CNN-based triggering algorithm that looks as overlapping snapshots of time
     """
 
     def __init__(self):
@@ -67,7 +21,7 @@ class triggerSimulator:
         self._device = None
         self.__h = None
 
-    def begin(self, model, device=None, h=None, log_level=None):
+    def begin(self, model, device=None, log_level=None):
         """
         Parameters
         ----------
@@ -81,11 +35,10 @@ class triggerSimulator:
 
         self._model = model
         self._device = device
-        self.__h = h
-        
+
         ## TODO: remove hardcoding here
-        self.trigger_waveform_length=256
-        self.skip_step_size=200
+        self.trigger_waveform_length = 256
+        self.skip_step_size = 200
 
         # Find if a GPU is found by the PyTorch
         if self._device is None:
@@ -101,9 +54,7 @@ class triggerSimulator:
         self._model.eval()
 
     @register_run()
-    def run(
-        self, evt, station, det, threshold, triggered_channels, vrms_per_channel, trigger_name="default_chunked_trigger"
-    ):
+    def run(self, evt, station, det, threshold, triggered_channels, vrms_per_channel, trigger_name="default_chunked_trigger"):
         """
         Applies the GRU to the waveforms and calculates TOT condition on the output
 
@@ -177,61 +128,47 @@ class triggerSimulator:
         assert torch_tensor.shape[0] == 1  # First dim is the unsqueezed part
         assert torch_tensor.shape[2] == len(prediction_traces[0])  # Waveform length
         assert torch_tensor.shape[1] == len(triggered_channels)  # The N channels are the features
-        
-        print("tensor shape .... !", torch_tensor.shape)
-        torch_tensor=torch_tensor.to(self._device)
-        ## maximum number of applies for the chunked network
 
-        all_preds=[]
-        has_triggered=False
+        torch_tensor = torch_tensor.to(self._device)
+
+        all_preds = []
+        has_triggered = False
         sampling_rate = station.get_channel(triggered_channels[0]).get_sampling_rate()
-      
-        trigger_times=[]
-        first_index=np.random.randint(self.skip_step_size)
-        print("first index ", first_index)
-        next_to_last_index=first_index+self.trigger_waveform_length
+
+        trigger_times = []
+        first_index = np.random.randint(self.skip_step_size)
+        next_to_last_index = first_index + self.trigger_waveform_length
 
         with torch.no_grad():
-            
-            pred_ind=0
-            while(next_to_last_index<=len(prediction_traces[0])):
+
+            pred_ind = 0
+            while next_to_last_index <= len(prediction_traces[0]):
 
                 pred = self._model(torch_tensor[:, :, first_index:next_to_last_index])[0].cpu().item()
-                print(pred_ind,"PRED ", pred)
-                #all_preds.append(pred.cpu().item())
-                if(pred>threshold):
+                # all_preds.append(pred.cpu().item())
+                if pred > threshold:
                     ## we trigger
-                    has_triggered=True
+                    has_triggered = True
 
                     ## setting trigger time in center
-                    trigger_times.append(start_time+1.0/sampling_rate*(self.skip_step_size*pred_ind+int(0.5*self.trigger_waveform_length)))
-                    
+                    trigger_times.append(
+                        start_time
+                        + 1.0 / sampling_rate * (self.skip_step_size * pred_ind + int(0.5 * self.trigger_waveform_length))
+                    )
 
-                first_index+=self.skip_step_size
-                next_to_last_index=first_index+self.trigger_waveform_length
+                first_index += self.skip_step_size
+                next_to_last_index = first_index + self.trigger_waveform_length
 
-                pred_ind+=1
-                
-            #tot_intervals = CalculateTimeOverThresholdIntervals(threshold, tot_bins, yhat.cpu())
-            #has_triggered = len(tot_intervals) > 0  # Any number of tot passings constitute a global success for this waveform
-            #self.logger.debug(f"[{trigger_name}] Found {len(tot_intervals)} instances of the trigger being passed")
-        print("FIRST CHANNEL")
-        print(prediction_traces[0,:50])
+                pred_ind += 1
+
         # Set the trigger results and times
-        #trigger = TimeOverThresholdTrigger(trigger_name, threshold, tot_bins, channels=triggered_channels)
         trigger = SimpleThresholdTrigger(trigger_name, threshold, channels=triggered_channels)
         trigger.set_triggered_channels(triggered_channels)
         trigger.set_triggered(has_triggered)
 
         if has_triggered:
-        
             trigger.set_trigger_time(trigger_times[0])
             trigger.set_trigger_times(trigger_times)
-            #self.logger.debug(f"[{trigger_name}] Station has passed trigger, trigger times are {trigger_times / units.ns} ns")
-            #self.logger.debug(f"[{trigger_name}] \t--> Time since waveform start: {trigger_times - start_time / units.ns} ns")
-            #self.logger.debug(f"[{trigger_name}] \t--> trigger bins: {tot_intervals[:, 0]}, of {yhat.shape[1]} bins")
-        else:
-            #self.logger.debug(f"[{trigger_name}] Station has NOT passed trigger")
 
         station.set_trigger(trigger)
 
