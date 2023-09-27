Fix effective volume calculation

NuRadioMC/simulation/output_writer_hdf5.py
@@ -199,17 +199,6 @@ class outputWriterHDF5:
         output_file.attrs['n_samples'] = n_samples
         output_file.attrs['config'] = yaml.dump(self.__config)
 
-        # copy over data from input file
-        # for key in self.__input_data.keys():
-        #     print('!', key)
-        #     
-        #     if not key.startswith('station_') and not key in output_file.keys():
-        #         if np.array(self.__input_data[key]).dtype.char == 'U':
-        #             output_file[key] = np.array(self.__input_data[key], dtype=h5py.string_dtype(encoding='utf-8'))[saved_events_mask]
-# 
-        #         else:
-        #             output_file[key] = np.array(self.__input_data[key])[saved_events_mask]
-        #             
         for key in self.__input_attributes.keys():
             if not key in output_file.attrs.keys():  # only save atrributes sets that havn't been recomputed and saved already
                 if key not in ["trigger_names", "Tnoise", "Vrms", "bandwidth", "n_samples", "dt", "detector", "config"]:  # don't write trigger names from input to output file, this will lead to problems with incompatible trigger names when merging output files
@@ -379,6 +368,9 @@ class outputWriterHDF5:
             )
         self.__output_triggered_station[station_id].append(np.any(multiple_triggers))
 
+    def get_trigger_status(self):
+        return self.__output['triggered']
 
-
+    def get_weights(self):
+        return self.__output['weights']
 

NuRadioMC/simulation/simulation.py
@@ -608,12 +608,14 @@ class simulation():
 
     def calculate_Veff(self):
         # calculate effective
-        triggered = remove_duplicate_triggers(self._mout['triggered'], self.__fin['event_group_ids'])
+        trigger_status = self.__output_writer_hdf5.get_trigger_status()
+        triggered = remove_duplicate_triggers(trigger_status, self.__fin['event_group_ids'])
         n_triggered = np.sum(triggered)
-        n_triggered_weighted = np.sum(self._mout['weights'][triggered])
+        weights = self.__output_writer_hdf5.get_weights()
+        n_triggered_weighted = np.sum(weights[triggered])
         n_events = self.__fin_attrs['n_events']
         logger.status(f'fraction of triggered events = {n_triggered:.0f}/{n_events:.0f} = {n_triggered / self._n_showers:.3f} (sum of weights = {n_triggered_weighted:.2f})')
-
+        print(self.__fin_attrs.keys())
         V = self.__fin_attrs['volume']
         Veff = V * n_triggered_weighted / n_events
         logger.status(f"Veff = {Veff / units.km ** 3:.4g} km^3, Veffsr = {Veff * 4 * np.pi/units.km**3:.4g} km^3 sr")