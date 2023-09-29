989a87eae5d047ec7fae80918f1e7917700a7e2c
Sjoerd Bouma <sjoerdbouma@xs4all.nl>
2023-09-29 14:52:42 +0200

    add thread-locking to prevent multiple file_handlers with same id

--- NuRadioReco/eventbrowser/dataprovider.py
+++ NuRadioReco/eventbrowser/dataprovider.py
@@ -1,6 +1,12 @@
 import NuRadioReco.eventbrowser.dataprovider_root
 import NuRadioReco.eventbrowser.dataprovider_nur
+import threading
+import logging
+logging.basicConfig()
+logger = logging.getLogger('eventbrowser.dataprovider')
+logger.setLevel(logging.INFO)
 
+LOCK = threading.Lock()
 
 class DataProvider(object):
     __instance = None
@@ -20,4 +26,12 @@ class DataProvider(object):
             self.__data_provider = NuRadioReco.eventbrowser.dataprovider_nur.DataProvider()
 
     def get_file_handler(self, user_id, filename):
-        return self.__data_provider.get_file_handler(user_id, filename)
+        thread = threading.get_ident()
+        total_threads = threading.active_count()
+        logger.debug(f"Thread {thread} out of total {total_threads} requesting file_handler for user {user_id}")
+        LOCK.acquire()
+        logger.debug(f"Thread {thread} locked, getting file_handler for user {user_id}...")
+        file_handler = self.__data_provider.get_file_handler(user_id, filename)
+        LOCK.release()
+        logger.debug(f"Returning file_handler and releasing lock")
+        return file_handler
