d69d3becd409eb723da88a65ca4c229e0eed572c
Felix Schlüter <felix.schluter@icecube.wisc.edu>
2024-02-05 15:13:15 +0100

    Fix filters

--- NuRadioReco/utilities/bandpass_filter.py
+++ NuRadioReco/utilities/bandpass_filter.py
@@ -35,6 +35,9 @@ def get_filter_response(frequencies, passband, filter_type, order, rp=None):
 
     """
 
+    if filter_type == 'rectangular':
+        return np.where(passband[0] <= frequencies <= passband[1], 1, 0)
+
     # we need to specify if we have a lowpass filter
     # otherwise scipy>=1.8.0 raises an error
     if passband[0] == 0:
@@ -42,26 +45,26 @@ def get_filter_response(frequencies, passband, filter_type, order, rp=None):
     else:
         scipy_args = [passband, 'bandpass']
 
-    if filter_type == 'rectangular':
-        return np.where(passband[0] <= frequencies <= passband[1], 1, 0)
+    filt = np.zeros_like(frequencies, dtype=complex)
+    mask = frequencies > 0
 
-    elif filter_type == 'butter':
-        mask = frequencies > 0
+    if filter_type == 'butter':
         b, a = scipy.signal.butter(order, *scipy_args, analog=True)
         w, h = scipy.signal.freqs(b, a, frequencies[mask])
-        return np.where(mask, h, 0)
+        filt[mask] = h
+        return filt
 
     elif filter_type == 'butterabs':
-        mask = frequencies > 0
         b, a = scipy.signal.butter(order, *scipy_args, analog=True)
         w, h = scipy.signal.freqs(b, a, frequencies[mask])
-        return np.abs(np.where(mask, h, 0))
+        filt[mask] = h
+        return np.abs(filt)
 
     elif filter_type == 'cheby1':
-        mask = frequencies > 0
         b, a = scipy.signal.cheby1(order, rp, *scipy_args, analog=True)
         w, h = scipy.signal.freqs(b, a, frequencies[mask])
-        return np.where(mask, h, 0)
+        filt[mask] = h
+        return filt
 
     elif filter_type.find('FIR') >= 0:
         raise NotImplementedError("FIR filter not yet implemented")
