76c3d56a7a2cf64836a2a2f5d7dc7534cc8470ad
Martin Langgård Ravn <martin@ravn.dk>
2024-03-12 17:00:59 +0100

    Use pseudoinverse of covariance matrix

--- NuRadioReco/utilities/noise_model.py
+++ NuRadioReco/utilities/noise_model.py
@@ -21,33 +21,52 @@ class NoiseModel:
         spectra : numpy.ndarray, optional
             Spectra of the noise in the antennas. Has dimensions [n_antennas,n_samples/2+1]. Default value is None, 
             and can instead be calculated using NoiseModel.calculate_spectra_from_data.
-        add_white_noise : float, optional
-            Calculating the inverse of a (covariance) matrix is numerically unstable and is impossible if the matrix 
-            is not full rank. By adding a small component to the diagonal of the covariance matrix (corresponding to 
-            white noise) it is guaranteed to be full rank, and the inverse can be calculated. This parameter should 
-            neither be too large or too small.
+        matrix_inversion_method : str, optional
+            If the covariance matrix is not full rank, the inverse, which is used to calculate the multivariate normal, 
+            does not exist. This is the case if any frequency amplitudes in the spectra are zero, which corresponds to
+            a distribution in a sub-space whih has lower number of dimensions than n_samples, i.e. some dimensions are
+            degenerate.  The correct mathematical way to describe the degenerate case is by using the pseudo-inverse
+            and pseudo-determinant to calculate the multivariate normal PDF. This method is used if this parameters is 
+            set to "pseudo_inv", and is numerically stable but slow. We thus also provide the option to use the regular 
+            inverse by setting this parameter to "regular_inv", which works for full rank matrices and sometimes for
+            lower rank matrices (see increase_cov_diagonal).
+        threshold_pdet : float, optional
+            The pseudo-determinant of a matrix is the product of all non-zero eigenvalues. This is the threshold for what
+            is considered non-zero.
+        increase_cov_diagonal : float, optional
+            Only used if matrix_inversion_method is "regular_inv". Calculating the inverse of a (covariance) matrix is 
+            numerically unstable, and it does not exist if the matrix is not full rank. By adding a small component to the 
+            diagonal when the inverse increases the rank and can improve stability for lower rank matrices. The parameter is
+            what fraction of the variance should be added to the diagonal of the covariance matrix only when calculating 
+            the inverse.
+        ignore_normalization : bool, optional
+            We are generally only interrested in likelihood ratios or delta log likelihood. In this case the normalization of
+            the distribution cancels out and can just be set to 1. This speeds up initializing the class/covariance matrices.
     """
 
-    def __init__(self, n_antennas, n_samples, sampling_rate, spectra=None, add_white_noise=1e-12):
+    def __init__(self, n_antennas, n_samples, sampling_rate, spectra=None, matrix_inversion_method="pseudo_inv", threshold_pdet=1e-12, increase_cov_diagonal=0, ignore_normalization=True):
         self.n_antennas = n_antennas
         self.n_samples = n_samples
         self.sampling_rate = sampling_rate
         self.spectra = spectra
+        self.matrix_inversion_method = matrix_inversion_method
+        self.threshold_pdet = threshold_pdet
+        self.increase_cov_diagonal = increase_cov_diagonal
+        self.ignore_normalization = ignore_normalization
         self.frequencies = np.fft.rfftfreq(n_samples, 1.0/sampling_rate)
         self.n_frequencies = len(self.frequencies)
         self.cov = None
         self.cov_inv = None
-        self.cov_det = None
-        self.add_white_noise = add_white_noise
+        self.cov_log_det = None
         self.t_array = np.arange(n_samples) * 1.0 / sampling_rate
 
         if spectra is not None:
-            assert np.shape(spectra)[1] == self.n_frequencies, "The dimensionality of the provided spectrum does not match the number of samples per trace"
+            assert np.shape(spectra)[1] == self.n_frequencies, "The dimensionality of the provided spectra does not match the number of samples per trace"
 
     def _set_covariance_matrices(self, cov):
         """
-        Sets the covariance matrices, their inverses, and log-determinants (for optimimization reasons)
-
+        Sets the covariance matrices, their (pseudo-)inverses, and log-determinants.
+        
         Parameters
         ----------
             cov : numpy.ndarray
@@ -55,10 +74,20 @@ class NoiseModel:
         """
         self.cov = cov
         self.cov_inv = np.zeros([self.n_antennas, self.n_samples, self.n_samples])
-        self.cov_det = np.zeros(self.n_antennas)
+        self.cov_log_det = np.zeros(self.n_antennas)
         for i in range(self.n_antennas):
-            self.cov_inv[i,:,:] = np.linalg.inv(cov[i,:,:] + np.diag(np.ones(self.n_samples) * self.add_white_noise))
-            self.cov_det[i] = np.linalg.slogdet(cov[i,:,:])[0]
+            if self.matrix_inversion_method == "pseudo_inv":
+                self.cov_inv[i,:,:] = scp.linalg.pinvh(cov[i,:,:])
+                if not self.ignore_normalization:
+                    eigen_values = np.linalg.eigvalsh(cov[i,:,:])
+                    self.cov_log_det[i] = np.sum(np.log(eigen_values[eigen_values > self.threshold_pdet]))
+                else:
+                    self.cov_log_det[i] = 1
+            elif self.matrix_inversion_method == "regular_inv":
+                self.cov_inv[i,:,:] = np.linalg.inv(cov[i,:,:] + np.diag(np.ones(self.n_samples) * cov[i,0,0] * self.increase_cov_diagonal))
+                self.cov_log_det[i] = np.linalg.det() if not self.ignore_normalization else 1
+            else:
+                raise Exception("""matrix_inversion_method not recognized. Choose "pseudo_inv" or "regular_inv" """)
 
     def calculate_spectra_from_data(self, data):
         """
@@ -144,9 +173,9 @@ class NoiseModel:
 
         self._set_covariance_matrices(covariance_matrices)
 
-    def _log_multivariate_normal(self, x, mu, cov_inv, cov_det):
+    def _log_multivariate_normal(self, x, mu, cov_inv, cov_log_det):
         """
-        Calculates the multivariate normal probability of vector x, given means mu and covariance matrix 
+        Calculates the multivariate normal probability (PDF) of vector x, given means mu and covariance matrix 
         inverse and determinant.
 
         Parameters
@@ -157,25 +186,25 @@ class NoiseModel:
                 Means of the distribution with dimensions [n_samples]
             cov_inv : numpy.ndarray
                 Inverse covariance matrix of the distribution with dimensions [n_samples,n_samples]
-            cov_det : float
+            cov_log_det : float
                 Determinant of covariance matrix of the distribution
 
         Returns
         -------
             float
-                Natural logarithm to the PDF value of the vector x given the multivariate normal distribution described by mu, cov_inv, and cov_det
+                Natural logarithm to the PDF value of the vector x given the multivariate normal distribution described by mu, cov_inv, and cov_log_det
         """
         n = len(x)
         term_1 = -0.5 * n * np.log(2*np.pi)
-        term_2 = -0.5 * cov_det
+        term_2 = -0.5 * cov_log_det
         # np.matmul sometimes returns a np.matrix instead of np.array, which needs at least 2 dimensions. So we need to cast and flatten it:
         term_3_temp = np.array(np.matmul(cov_inv, x-mu)).flatten()
         term_3 = -0.5 * np.matmul(x-mu, term_3_temp)
         return term_1 + term_2 + term_3
 
-    def calculate_llh(self, data, signal=None):
+    def calculate_delta_llh(self, data, signal=None):
         """
-        Calculates delta log likelihood for the datasets relative to the most probable noise
+        Calculates delta log likelihood for the datasets relative to the most probable trace
 
         Parameters
         ----------
@@ -197,13 +226,13 @@ class NoiseModel:
 
         # If only one dataset is given, add dimension along axis=0 ([1,n_antennas,n_samples]):
         if len(np.shape(data)) == 2:
-            data = np.array([data])
+            data = data[np.newaxis,:,:]
 
         n_datasets = len(data)
 
         LLH_best = 0
         for i in range(self.n_antennas):
-            LLH_best += self._log_multivariate_normal(x=means[i,:], mu=means[i,:], cov_inv=self.cov_inv[i,:,:], cov_det=self.cov_det[i])
+            LLH_best += self._log_multivariate_normal(x=means[i,:], mu=means[i,:], cov_inv=self.cov_inv[i,:,:], cov_log_det=self.cov_log_det[i])
 
         LLH_array = np.zeros(n_datasets)
 
@@ -211,12 +240,12 @@ class NoiseModel:
             # Sum over likelihood for all antennas:
             LLH = 0
             for j in range(self.n_antennas):
-                LLH += self._log_multivariate_normal(x=data[i,j,:], mu=means[j,:], cov_inv=self.cov_inv[j,:,:], cov_det=self.cov_det[j])
+                LLH += self._log_multivariate_normal(x=data[i,j,:], mu=means[j,:], cov_inv=self.cov_inv[j,:,:], cov_log_det=self.cov_log_det[j])
             LLH_array[i] = LLH
 
         return LLH_array - LLH_best
     
-    def calculate_minus_two_llh(self, data, signal=None):
+    def calculate_minus_two_delta_llh(self, data, signal=None):
         """
         Calculates the minus two delta log likelihood for the datasets relative to the most probable noise
 
@@ -233,7 +262,7 @@ class NoiseModel:
             np.array
                 Minus two delta log likelihood for the data given the noise model
         """
-        return -2*self.calculate_llh(data, signal=signal)
+        return -2*self.calculate_delta_llh(data, signal=signal)
 
     def get_minus_two_delta_llh_function(self, data_to_fit, signal_function):
         """
@@ -249,19 +278,17 @@ class NoiseModel:
 
         Returns
         -------
-            llh_func : function
+            minus_two_delta_llh_func : function
                 Function that returns a minus two delta log likelihood for a set of parameters given a data realization and the noise model
         """
 
-        LLH_best = self.calculate_llh(data_to_fit, data_to_fit)
-
-        def llh_func(params):
+        def minus_two_delta_llh_func(params):
             signal = signal_function(params)
-            LLH_signal = self.calculate_llh(signal, data_to_fit)
-            minus_two_delta_llh_signal = -2 * (LLH_signal - LLH_best)
+            LLH_signal = self.calculate_delta_llh(signal, data_to_fit)
+            minus_two_delta_llh_signal = -2 * LLH_signal
             return minus_two_delta_llh_signal
 
-        return llh_func
+        return minus_two_delta_llh_func
     
     def save_covariance_matrix(self, antenna, filename):
         """
@@ -469,7 +496,7 @@ class NoiseModel:
         plt.ylabel(f"Cov$(\Delta t)$")
         plt.tight_layout()
 
-    def plot_llh_distribution(self, data, make_new_figure = True):
+    def plot_llh_distribution(self, data, n_dof=None, make_new_figure = True):
         """
         Calculate the llh values for many datasets and plot the distribution alongside a chi2 
         distribution with dof equal to the number of samples
@@ -478,13 +505,17 @@ class NoiseModel:
         ----------
             data : numpy.array
                 Array containing data with dimensions [n_samples]
+            n_dof : int, optional
+                Number of degrees of freedom for the chi2 distribution. If not provided, it is set equal to
+                the number of samples
             make_new_figure : bool
                 If True create a new figure
         """
-        LLH_array = self.calculate_llh(data)
+        LLH_array = self.calculate_delta_llh(data)
 
         n_datasets = len(data)
-        n_dof = int(self.n_antennas * self.n_samples)
+        if n_dof is None:
+            n_dof = int(self.n_antennas * self.n_samples)
 
         if make_new_figure:
             plt.figure(figsize=[4.2,3])
