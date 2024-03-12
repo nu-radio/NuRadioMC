7800c56da2dfbb55c99ce6f4cfd76c7a3b0dc93f
Sjoerd Bouma <sjoerdbouma@xs4all.nl>
2024-03-12 15:15:48 +0100

    correct description of fft power normalization

--- NuRadioReco/utilities/fft.py
+++ NuRadioReco/utilities/fft.py
@@ -1,17 +1,24 @@
-import numpy as np
-
 """
 A wrapper around the numpy fft routines to achive a coherent normalization of the fft
+
 As we have real valued data in the time domain, we use the 'real ffts' that omit the negative frequencies in Fourier
 space. To account for the missing power in the frequency domain, we multiply the frequency spectrum by sqrt(2),
 and divide the iFFT with 1/sqrt(2) accordingly. The frequency spectrum is divided by the sampling rate so that the
 units if the spectrum are volts/GHz instead of volts/bin and the voltage in the frequency domain is independent of the
 sampling rate.
-Then, a calculation of the power leads the same result in
+
+Then, a calculation of the energy leads the same result in
 the time and frequency domain, i.e.
-np.sum(trace**2) * dt = np.sum(spectrum**2/dt**2) * df
+
+.. code-block::
+
+    np.sum(trace**2) * dt = np.sum(spectrum**2) * df
+
+Note that this will have units of :math:`V^2/GHz`.
+In order to obtain the correct units (i.e. energy in eV) one additionally has to divide by the impedance ``Z``.
 """
 
+import numpy as np
 
 def time2freq(trace, sampling_rate):
     """
