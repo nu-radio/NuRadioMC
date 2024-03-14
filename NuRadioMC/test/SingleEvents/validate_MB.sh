ff3425f3f56d78ad6435e7de2cd519b2360b7d15
Christian Glaser <christian.glaser@uci.edu>
2024-03-14 04:49:15 +0000

    report test as failed

--- NuRadioMC/test/SingleEvents/validate_MB.sh
+++ NuRadioMC/test/SingleEvents/validate_MB.sh
@@ -1,5 +1,5 @@
 # #!/bin/bash
-# set -e
+set -e
 
 
 NuRadioMC/test/SingleEvents/T02RunSimulation.py NuRadioMC/test/SingleEvents/MB_1e18_reference.hdf5 NuRadioMC/test/SingleEvents/surface_station_1GHz.json NuRadioMC/test/SingleEvents/config_MB.yaml NuRadioMC/test/SingleEvents/MB_1e18_output.hdf5
