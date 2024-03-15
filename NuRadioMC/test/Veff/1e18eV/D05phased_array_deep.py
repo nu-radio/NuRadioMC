333aef4d37ed3766832db16831c23aa35d68e8eb
Christian Glaser <christian.glaser@uci.edu>
2024-03-15 03:44:23 +0000

    add time logger

--- NuRadioMC/test/Veff/1e18eV/D05phased_array_deep.py
+++ NuRadioMC/test/Veff/1e18eV/D05phased_array_deep.py
@@ -64,8 +64,10 @@ class mySimulation(simulation.simulation):
 
         # run the 4 phased trigger
         # x2 for upsampling
-        window_4ant = int(16 * units.ns * self._sampling_rate_detector * 2.0)
-        step_4ant = int(8 * units.ns * self._sampling_rate_detector * 2.0)
+        cid = det.get_channel_ids(station.get_id())[0]
+        sampling_rate = det.get_sampling_frequency(station.get_id(), cid)
+        window_4ant = int(16 * units.ns * sampling_rate * 2.0)
+        step_4ant = int(8 * units.ns * sampling_rate * 2.0)
 
         phasedArrayTrigger.run(evt, station, det,
                                Vrms=Vrms,
@@ -100,5 +102,6 @@ sim = mySimulation(inputfilename=args.inputfilename,
                             detectorfile=args.detectordescription,
                             outputfilenameNuRadioReco=args.outputfilenameNuRadioReco,
                             config_file=args.config,
-                            default_detector_station=1)
+                            default_detector_station=1,
+                            file_overwrite=True)
 sim.run()
