4233ba87a0b41f0384d3f92eca28837ea7da324d
Christian Glaser <christian.glaser@uci.edu>
2024-03-15 22:38:09 +0000

    add distance cut

--- NuRadioMC/EvtGen/generator.py
+++ NuRadioMC/EvtGen/generator.py
@@ -11,7 +11,7 @@ import time
 import NuRadioMC
 from NuRadioReco.utilities import units, version, particle_names
 from NuRadioMC.utilities import inelasticities
-from NuRadioMC.simulation.simulation import pretty_time_delta
+from NuRadioMC.simulation.time_logger import pretty_time_delta
 
 
 logger = logging.getLogger("NuRadioMC.EvtGen")
