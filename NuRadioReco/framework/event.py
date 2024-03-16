6293e6eb0b4c895cdc8960c8dea94e73acb1f50e
Christian Glaser <christian.glaser@uci.edu>
2024-03-16 00:52:24 +0000

    correctly implement distance cut, do not return iterator but list of showers

--- NuRadioReco/framework/event.py
+++ NuRadioReco/framework/event.py
@@ -382,8 +382,9 @@ class Event:
         """
         Get an iterator over all simulated showers in the event
         """
-        for shower in self.__sim_showers.values():
-            yield shower
+        return self.__sim_showers.values()
+        # for shower in self.__sim_showers.values():
+        #     yield shower
 
     def get_sim_shower(self, shower_id):
         """
