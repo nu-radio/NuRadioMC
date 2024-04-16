04df09f0c59272bea578af80def54ab9a1a85308
Karen Terveer <94610272+karenterveer@users.noreply.github.com>
2024-04-16 12:14:26 +0200

    corrected indexing and added correct path

--- NuRadioReco/detector/LOFAR/analog_components.py
+++ NuRadioReco/detector/LOFAR/analog_components.py
@@ -6,9 +6,9 @@ from scipy.interpolate import interp1d
 from radiotools import helper as hp
 
 logger = logging.getLogger("NuRadioReco.LOFAR_analog_components")
-logger.setLevel(logger.INFO)
 
-def load_cable_response(cable_length, path="/vol/astro7/lofar/kterveer/software/NuRadioMC/NuRadioReco/detector/LOFAR/signalchain"):
+
+def load_cable_response(cable_length, path="signalchain"):
     """
     Parameters
     ----------
@@ -18,10 +18,10 @@ def load_cable_response(cable_length, path="/vol/astro7/lofar/kterveer/software/
         length of the coax cable of the corresponding channel
     """
 
-    data = np.loadtxt(f"/vol/astro7/lofar/kterveer/software/NuRadioMC/NuRadioReco/detector/LOFAR/signalchain/attenuation_RG58_{cable_length}m.txt")
+    data = np.loadtxt(f"{path}/attenuation_RG58_{cable_length}m.txt")
     default = {}
-    default['frequencies'] = np.arange(30,80) * units.MHz
-    default['gain'] = -1*hp.dB_to_linear((data)) * units.dB   #cable loss is negative. data is in dB
+    default['frequencies'] = np.arange(30,81) * units.MHz
+    default['gain'] = hp.dB_to_linear(data)   #data is in dB
     return default
 
 def get_cable_response(frequencies, cable_length):
@@ -32,14 +32,14 @@ def get_cable_response(frequencies, cable_length):
     orig_frequencies = cable_response['default']['frequencies']
     gain = cable_response['default']['gain']
 
-    interp_gain = interp1d(orig_frequencies, gain, bounds_error=False, fill_value=0)
+    interp_gain = interp1d(orig_frequencies, gain[:,1], bounds_error=False, fill_value=0)
 
     cable = {}
     cable['gain'] = interp_gain(frequencies)
     return cable
 
 
-def load_RCU_response(path="/vol/astro7/lofar/kterveer/software/NuRadioMC/NuRadioReco/detector/LOFAR/signalchain"):
+def load_RCU_response(path="signalchain"):
     """
     Parameters
     ----------
@@ -47,24 +47,23 @@ def load_RCU_response(path="/vol/astro7/lofar/kterveer/software/NuRadioMC/NuRadi
         Path to the folder containing the RCU response
     """
 
-    data = np.loadtxt("/vol/astro7/lofar/kterveer/software/NuRadioMC/NuRadioReco/detector/LOFAR/signalchain/RCU_gain_new_5.txt")
+    data = np.loadtxt(f"{path}/RCU_gain_new_5.txt")
     default = {}
-    default['frequencies'] = np.arange(30,80) * units.MHz
-    default['gain'] = hp.dB_to_linear(data)       # data is in dB
+    default['frequencies'] = np.arange(30,81) * units.MHz
+    default['gain'] = hp.dB_to_linear(data)   # data is in dB
 
     return default
 
-def get_RCU_response(frequencies, cable_length):
+def get_RCU_response(frequencies):
 
 
     RCU_response = {}
     RCU_response['default'] = load_RCU_response()
-
     orig_frequencies = RCU_response['default']['frequencies']
 
     gain = RCU_response['default']['gain']
 
-    interp_gain = interp1d(orig_frequencies, gain, bounds_error=False, fill_value=0)
+    interp_gain = interp1d(orig_frequencies, gain[:,1], bounds_error=False, fill_value=0)
 
     system = {}
     system['gain'] = interp_gain(frequencies)
