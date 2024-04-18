1c346ad979b35a86d5ab0a4a606482db97edcf7e
Karen Terveer <94610272+karenterveer@users.noreply.github.com>
2024-04-18 15:54:48 +0200

    corrected paths

--- NuRadioReco/detector/LOFAR/analog_components.py
+++ NuRadioReco/detector/LOFAR/analog_components.py
@@ -8,7 +8,7 @@ from radiotools import helper as hp
 logger = logging.getLogger("NuRadioReco.LOFAR_analog_components")
 
 
-def load_cable_response(cable_length, path="signalchain"):
+def load_cable_response(cable_length):
     """
     Parameters
     ----------
@@ -17,11 +17,14 @@ def load_cable_response(cable_length, path="signalchain"):
     cable_length: int
         length of the coax cable of the corresponding channel
     """
+    module_dir = os.path.dirname(__file__)
+    file_path = os.path.join(module_dir, "signalchain", f"attenuation_RG58_{cable_length}m.txt")
 
-    data = np.loadtxt(f"{path}/attenuation_RG58_{cable_length}m.txt")
+    data = np.loadtxt(file_path)
+
     default = {}
     default['frequencies'] = np.arange(30,81) * units.MHz
-    default['gain'] = hp.dB_to_linear(data)   #data is in dB
+    default['attenuation'] = -1*data  #data is in dB
     return default
 
 def get_cable_response(frequencies, cable_length):
@@ -30,16 +33,16 @@ def get_cable_response(frequencies, cable_length):
     cable_response['default'] = load_cable_response(cable_length=cable_length)
 
     orig_frequencies = cable_response['default']['frequencies']
-    gain = cable_response['default']['gain']
+    gain = cable_response['default']['attenuation']
 
     interp_gain = interp1d(orig_frequencies, gain[:,1], bounds_error=False, fill_value=0)
 
     cable = {}
-    cable['gain'] = interp_gain(frequencies)
+    cable['attenuation'] = interp_gain(frequencies)
     return cable
 
 
-def load_RCU_response(path="signalchain"):
+def load_RCU_response():
     """
     Parameters
     ----------
@@ -47,10 +50,14 @@ def load_RCU_response(path="signalchain"):
         Path to the folder containing the RCU response
     """
 
-    data = np.loadtxt(f"{path}/RCU_gain_new_5.txt")
+    module_dir = os.path.dirname(__file__)
+    file_path = os.path.join(module_dir, "signalchain/RCU_gain_new_5.txt")
+
+    data = np.loadtxt(file_path)
+
     default = {}
     default['frequencies'] = np.arange(30,81) * units.MHz
-    default['gain'] = hp.dB_to_linear(data)   # data is in dB
+    default['gain'] = data   # data is in dB
 
     return default
 
@@ -63,7 +70,7 @@ def get_RCU_response(frequencies):
 
     gain = RCU_response['default']['gain']
 
-    interp_gain = interp1d(orig_frequencies, gain[:,1], bounds_error=False, fill_value=0)
+    interp_gain = interp1d(orig_frequencies, gain, bounds_error=False, fill_value=0)
 
     system = {}
     system['gain'] = interp_gain(frequencies)
