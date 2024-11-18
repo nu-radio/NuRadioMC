2721700783762db08d811b44d1be702a6a9422a5
Aishwarya Vijai <123188229+avijai2000@users.noreply.github.com>
2024-11-18 16:37:31 -0500

    Add files via upload

---
+++ NuRadioMC/examples/01_Veff_simulation_Aishwarya/T01generate_event_list.py
@@ -0,0 +1,20 @@
+from __future__ import absolute_import, division, print_function
+from NuRadioReco.utilities import units
+from NuRadioMC.EvtGen.generator import generate_eventlist_cylinder
+
+# Setup logging
+from NuRadioReco.utilities.logging import setup_logger
+logger = setup_logger(name="")
+
+# define simulation volume
+volume = {
+'fiducial_zmin':-2.7 * units.km,  # the ice sheet at South Pole is 2.7km deep
+'fiducial_zmax': 0 * units.km,
+'fiducial_rmin': 0 * units.km,
+'fiducial_rmax': 4 * units.km}
+
+# generate one event list at 1e19 eV with 1000 neutrinos
+generate_eventlist_cylinder('1e19_n1e3.hdf5', 1e3, 1e19 * units.eV, 1e19 * units.eV, volume)
+
+# generate one event list at 1e18 eV with 10000 neutrinos
+generate_eventlist_cylinder('1e18_n1e4.hdf5', 1e4, 1e18 * units.eV, 1e18 * units.eV, volume)
