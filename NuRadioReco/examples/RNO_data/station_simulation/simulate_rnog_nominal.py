fb586de6ea86c11bb11fff70368726771a94893f
Felix Schlüter <30903175+fschlueter@users.noreply.github.com>
2024-11-19 13:44:56 +0100

    Update simulate_rnog_nominal.py: Fix variable name

--- NuRadioReco/examples/RNO_data/station_simulation/simulate_rnog_nominal.py
+++ NuRadioReco/examples/RNO_data/station_simulation/simulate_rnog_nominal.py
@@ -155,7 +155,7 @@ def task(q, iSim, energy_min, energy_max, detectordescription, config, output_fi
                 evt, station_copy, det, requested_channels=pa_channels, vrms=vrms_input_to_adc, digitize_trace=True
             )  # Digitization will be done in the trigger module
 
-            pa_channel = det.get_channel(station_copy.get_id(), deep_trigger_channels[0])
+            pa_channel = det.get_channel(station_copy.get_id(), pa_channels[0])
             pa_sampling_rate = pa_channel["trigger_adc_sampling_frequency"] * units.GHz
 
             # integration window size and stride
