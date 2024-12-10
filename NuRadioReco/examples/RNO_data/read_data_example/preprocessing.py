97ca80077632c3db1973784dc24002084d482d03
Aishwarya Vijai <123188229+avijai2000@users.noreply.github.com>
2024-12-10 05:55:23 -0500

    Delete NuRadioReco/examples/RNO_data/read_data_example directory

--- NuRadioReco/examples/RNO_data/read_data_example/preprocessing.py
+++
@@ -1,8 +0,0 @@
-from scipy.signal import hilbert
-import numpy as np
-
-def envelope(channel_signals):
-    for channel in channel_signals.keys():
-        channel_signals[channel] = np.abs(hilbert(channel_signals[channel]))
-
-    return channel_signals
