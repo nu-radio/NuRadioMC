9fa097694421b27f60c70d65666512f286f8d6b8
Felix Schlüter <felix.schluter@icecube.wisc.edu>
2025-01-17 10:24:41 +0100

    Minor modifications. Remove one function

--- NuRadioReco/examples/RNOG/rnog_standard_data_processing.py
+++ NuRadioReco/examples/RNOG/rnog_standard_data_processing.py
@@ -15,10 +15,33 @@ import yaml
 
 
 def use_module(name, config):
+    """
+    Simple helper function returns True if module is supposed to be used
+
+    Parameters
+    ----------
+    name : str
+        Name of the module
+    config : dict
+        (Yaml) configuration
+
+    Returns
+    -------
+    use : bool
+        True if module is supposed to be used
+    """
     return name in config and config[name]["use"]
 
 
 def process_data(config):
+    """
+    Processing of RNO-G data.
+
+    Parameters
+    ----------
+    config : dict
+        (Yaml) configuration
+    """
 
     # Initialize modules
 
@@ -39,8 +62,22 @@ def process_data(config):
     hardwareResponseIncorporator = NuRadioReco.modules.RNO_G.hardwareResponseIncorporator.hardwareResponseIncorporator()
     hardwareResponseIncorporator.begin()
 
-    def process_event(evt, det, config):
+    paths = config["readRNOGDataMattak"]["args"].pop("filenames")
+    readRNOGDataMattak = NuRadioReco.modules.io.RNO_G.readRNOGDataMattak.readRNOGData()
+    readRNOGDataMattak.begin(
+        paths, **config["readRNOGDataMattak"]["args"]
+    )
 
+    # Initialize detector class
+    detector = NuRadioReco.detector.RNO_G.rnog_detector.Detector(
+        **config["detector"]["args"]
+    )
+
+    # Loop over all events (the reader module has options to select events -
+    # see class documentation or module arguements in config file)
+    for event in readRNOGDataMattak.run():
+
+        # Loop over all stations.
         for station in evt.get_stations():
 
             detector.update(station.get_station_time())
@@ -67,29 +104,16 @@ def process_data(config):
             if use_module("channelBandPassFilter", config):
                 channelBandPassFilter.run(evt, station, det, **config["channelBandPassFilter"]["args"])
 
-            # Write event
+            # Write event - the RNO-G detector class is not stored within the nur files.
             eventWriter.run(evt, det=None, mode=config["eventWriter"]["args"]['mode'])
 
-
-    detector = NuRadioReco.detector.RNO_G.rnog_detector.Detector(
-        **config["detector"]["args"]
-    )
-
-    paths = config["readRNOGDataMattak"]["args"].pop("filenames")
-    readRNOGDataMattak = NuRadioReco.modules.io.RNO_G.readRNOGDataMattak.readRNOGData()
-    readRNOGDataMattak.begin(
-        paths, **config["readRNOGDataMattak"]["args"]
-    )
-
-    for event in readRNOGDataMattak.run():
-        process_event(event, detector, config)
 
 if __name__ == "__main__":
 
     parser = argparse.ArgumentParser(description='Run standard RNO-G data processing')
 
-    parser.add_argument('config', type=str, help='Yaml config file')
-    parser.add_argument('--rootfiles', type=str, nargs="*", help='Specify to root files to read & process')
+    parser.add_argument('config', type=str, help='Yaml config file to steer the data procession and reconstruction')
+    parser.add_argument('--filenames', type=str, nargs="*", help='Specify root data files if not specified in the config file')
 
     args = parser.parse_args()
 
@@ -97,6 +121,6 @@ if __name__ == "__main__":
         config = yaml.safe_load(f)
 
     if config["readRNOGDataMattak"]["args"]["filenames"] is None:
-        config["readRNOGDataMattak"]["args"]["filenames"] = args.rootfiles
+        config["readRNOGDataMattak"]["args"]["filenames"] = args.filenames
 
     process_data(config)
