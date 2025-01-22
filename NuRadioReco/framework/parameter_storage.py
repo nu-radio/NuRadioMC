ea36feb50713f5bc3e7b77d7ef95a0088d59605b
Felix Schlüter <felix.schluter@icecube.wisc.edu>
2025-01-22 13:08:56 +0100

    Remove decorator. Sjoerd knew all along that this was a bad idea.

--- NuRadioReco/framework/parameter_storage.py
+++ NuRadioReco/framework/parameter_storage.py
@@ -3,19 +3,6 @@ logger = logging.getLogger('NuRadioReco.framework.parameter_storage')
 
 
 
-def check_key(func):
-
-    def wrapper(*args):
-        key = args[1]
-        parameter_class = args[0]._parameter_type
-        if not isinstance(key, parameter_class):
-            logger.error(f"Parameter {key} needs to be of type {parameter_class}")
-            raise ValueError(f"Parameter {key} needs to be of type {parameter_class}")
-        return func(*args)
-
-    return wrapper
-
-
 class ParameterStorage:
 
     def __init__(self, parameter_type):
@@ -29,28 +16,33 @@ class ParameterStorage:
     def __getitem__(self, key):
         return self.get_parameter(key)
 
-    @check_key
+    def _check_key(self, key):
+        if not isinstance(key, self._parameter_type):
+            logger.error(f"Parameter {key} needs to be of type {self._parameter_type}")
+            raise ValueError(f"Parameter {key} needs to be of type {self._parameter_type}")
+
     def get_parameter(self, key):
+        self._check_key(key)
         return self._parameters[key]
 
-    @check_key
     def has_parameter(self, key):
+        self._check_key(key)
         return key in self._parameters.keys()
 
-    @check_key
     def set_parameter(self, key, value):
+        self._check_key(key)
         self._parameters[key] = value
 
-    @check_key
     def set_parameter_error(self, key, value):
+        self._check_key(key)
         self._parameter_covariances[(key, key)] = value ** 2
 
-    @check_key
     def get_parameter_error(self, key):
+        self._check_key(key)
         return self._parameter_covariances[(key, key)] ** 0.5
 
-    @check_key
     def remove_parameter(self, key):
+        self._check_key(key)
         self._parameters.pop(key, None)
 
     def get_parameters(self):
