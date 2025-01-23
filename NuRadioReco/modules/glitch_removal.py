ef6227292f0e8e83de254d3a64f64110f0ced9a7
Aishwarya Vijai <123188229+avijai2000@users.noreply.github.com>
2025-01-23 17:07:58 -0500

    Add files via upload

---
+++ NuRadioReco/modules/glitch_removal.py
@@ -0,0 +1,14 @@
+class GlitchFinder:
+
+    def run(self, event, station):
+
+        for ch in station.iter_channels():
+            trace = ch.get_trace() * 4095/2.5
+            times = ch.get_times()
+
+            for i in range(len(trace) - 1):
+                diff = trace[i+1] - trace[i]
+                if (diff < -2000 or diff > 2000):
+                    return True
+        return False 
+
