84c24bc121100b2f58f0cf1bac097d01f7d7d3ac
Cosmin Deaconu <cozzyd@kicp.uchicago.edu>
2025-01-29 14:55:57 -0600

    Add check_modules script
    This script will check if all run methods defined in a class in the
    module directory have been registered. It takes advantage of the fact
    that a time attribute is added to the run function by the decorator.
    
    Since each module must be imported as part of this check, it also as a
    byproduct finds broken modules (though they may only be broken for the
    user running it, if e.g. an optional dependency is required).
    
    Probably this is not the right place for the script but I didn't want to
    figure out relative paths :)

---
+++ check_modules.py
@@ -0,0 +1,58 @@
+#! /usr/bin/env python3
+
+
+
+# module and try to import (possibly failing if for example, you don't have all
+# the optional things for each module)
+
+# Then it will find all classes defined in
+# the module with a run method, and complain if the time attribute is not
+# present (this is an attribute added by the register_run decorator)
+
+import os
+import inspect
+import importlib
+
+
+broken = []
+unregistered_runs = []
+
+for dirpath,folders,files in os.walk('NuRadioReco/modules'):
+    for f in files:
+        if not f.endswith(".py") or f == "__init__.py":
+            continue
+
+
+        try:
+            mname = dirpath.replace('/','.')+'.'+f[:-3]
+            print("Trying ", mname)
+
+            m = importlib.import_module(mname)
+
+            for name,obj in inspect.getmembers(m, lambda member: inspect.isclass(member) and member.__module__ == mname):
+                print("Found class ",name, obj)
+                if hasattr(obj,'run') and not hasattr(obj.run,'time'):
+                    print('Has run method but not registered properly! Public flogging will be scheduled.')
+                    unregistered_runs.append(mname + '.' +name)
+
+        except Exception as e:
+            print(e)
+            print("Couldn't load module... maybe it's broken, oh well")
+            broken.append(mname)
+
+
+
+print("\n\n\n.........................................\n")
+print ("Broken modules: ", broken)
+print()
+print ("Unregistered runs: ", unregistered_runs)
+
+
+
+
+
+
+
+
+
+
