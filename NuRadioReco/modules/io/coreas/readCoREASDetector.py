6ea6e95a86d8e121eeaad669fa4e00b3bb96dea9
Mitja Desmet <mitja.desmet@vub.be>
2025-01-29 17:01:22 +0100

    fixup! Use helper function

--- NuRadioReco/modules/io/coreas/readCoREASDetector.py
+++ NuRadioReco/modules/io/coreas/readCoREASDetector.py
@@ -242,7 +242,7 @@ class readCoREASDetector:
                     smooth_res_efield = apply_hanning(res_efield)
 
                     # Store the trace in an ElecticField object
-                    add_electric_field_to_sim_station(
+                    coreas.add_electric_field_to_sim_station(
                         sim_station, channel_ids_for_group_id, smooth_res_efield.T, res_trace_start_time,
                         sim_shower[shp.zenith], sim_shower[shp.azimuth], self.coreas_interpolator.sampling_rate)
 
