f10a9b5df12d63077e797e6325b972de087206e9
Felix Schlüter <felix.schluter@icecube.wisc.edu>
2025-02-18 17:01:16 +0100

    Add new script to RNO-G test

--- NuRadioReco/test/RNO_G/test_read_rnog_data.sh
+++ NuRadioReco/test/RNO_G/test_read_rnog_data.sh
@@ -7,3 +7,4 @@ mkdir -p tests/data/station23/run325
 mv station23_run325.root tests/data/station23/run325/combined.root
 
 python3 NuRadioReco/examples/RNOG/read_rnog.py tests/data/station23/run325/combined.root test.nur
+python3 NuRadioReco/examples/RNOG/data_analysis_example.py tests/data/station23/run325/combined.root
