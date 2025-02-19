c29c2e6e5d783694b060411ca460cf9c0928fc25
Felix Schlüter <felix.schluter@icecube.wisc.edu>
2025-02-19 13:51:42 +0100

    Add example to simulate the effective temperature of the ice at the antenna

---
+++ NuRadioMC/examples/simulate_effective_ice_temperature/ice_integrator.py
@@ -0,0 +1,258 @@
+from NuRadioMC.utilities import attenuation, medium
+from NuRadioReco.utilities import units
+
+import os
+import h5py
+import numpy as np
+import sys
+import matplotlib.pyplot as plt
+from scipy.interpolate import interp1d
+import json
+
+try:
+    from AntPosCal.ice_model import icemodel
+except ImportError:
+    print("Import of \"AntPosCal.ice_model import icemodel\" failed. "
+          "Please install this repo https://github.com/RNO-G/antenna-positioning")
+    sys.exit()
+
+try:
+    import radiopropa
+except ImportError:
+    # You might need to install the branch ice_model/exponential_polynomial
+    print("Import of \"radiopropa\" failed. "
+          "Please install this repo https://github.com/nu-radio/RadioPropa")
+
+
+z_min = -3000
+
+def get_angles_rhos(file):
+    """ Getting a simulated ray-traced path from a hdf5 file
+
+    Parameters
+    ----------
+    file : str
+        path to the hdf5 file
+
+    Returns
+    -------
+    theta0: float
+        starting angle of the ray in degrees
+    x: np.array
+        x-coordinates of the ray
+    z: np.array
+        z-coordinates of the ray
+    distance: np.array
+        distance along the ray (in meters)
+    power: np.array
+        power fraction along the ray (1 if no reflection, <1 if reflection).
+        does not include the attenuation factor
+    have_reflection: bool
+        True if the ray has a reflection at the surface, False if it has not hit a reflection layer
+    """
+    f = h5py.File(file)
+    d = f['Trajectory3D']
+
+    unique_sn, counts = np.unique(d["SN"], return_counts=True)
+    if len(unique_sn) > 1:
+        assert counts[0] > counts[1], "Something went wrong, thre reflected ray should be longer than throughpassing one"
+
+    have_reflection = len(unique_sn) > 1
+    # select only the reflected ray, not the throughpassing one (through the surface layer)
+    d = d[d["SN"] == d["SN"][0]]
+
+    # ray path
+    x = d['X']
+    z = d['Z']
+
+    # starting angle from first step
+    dx = x[1] - x[0]
+    dz = z[1] - z[0]
+    theta = abs(np.arctan(dx / dz))
+
+    distance = d['D']
+    power = (d['Ax']**2 + d['Ay']**2 + d['Az']**2)
+
+    return theta, x, z, distance, power, have_reflection
+
+
+def get_trace_file(theta, z_antenna, freq=403 * 1e6, in_file=False, filename='__output_tempfile.h5'):
+    """
+    Perfrom ray tracing from an antenna at z_antenna in the direction defined by theta in the Z-X plane.
+    The ray tracing is stopped when the ray reaches the surface (actually a layer 10m above the surface)
+    or the maximum depth of the ice model.
+    """
+
+    # use fifth order polynomial ice model here, I checked that index of refraction is 1. at 0+epsilon and 1.27 at 0-epsilon
+    ice = icemodel.greenland_poly5()
+    iceModelScalar = ice.get_ice_model_radiopropa().get_scalar_field()
+
+    # simulation setup
+    sim = radiopropa.ModuleList()
+    sim.add(radiopropa.PropagationCK(iceModelScalar, 1E-8, .001, 1.))
+
+    # add a discontinuity
+    firnLayer = radiopropa.Discontinuity(
+        radiopropa.Plane(
+            radiopropa.Vector3d(0, 0, -0.001), radiopropa.Vector3d(0, 0, 1)),
+        iceModelScalar.getValue(radiopropa.Vector3d(0, 0, -0.001)), 1.)
+
+    # add a reflective layer at the surface
+    reflective = radiopropa.ReflectiveLayer(radiopropa.Plane(radiopropa.Vector3d(0, 0, -0.001), radiopropa.Vector3d(0, 0, 1)),1)
+    sim.add(firnLayer)
+
+    # Define the surfaces at which the ray tracing is stopped
+    obs2 = radiopropa.Observer()
+    obsz2 = radiopropa.ObserverSurface(radiopropa.Plane(radiopropa.Vector3d(0, 0, z_min), radiopropa.Vector3d(0, 0, 1)))
+    obs2.add(obsz2)
+    obs2.setDeactivateOnDetection(True)
+    sim.add(obs2)
+
+    obs3 = radiopropa.Observer()
+    obsz3 = radiopropa.ObserverSurface(radiopropa.Plane(radiopropa.Vector3d(0, 0, 10), radiopropa.Vector3d(0, 0, 1)))
+    obs3.add(obsz3)
+    obs3.setDeactivateOnDetection(True)
+    sim.add(obs3)
+
+    if in_file:
+        # Output
+        if os.path.isfile(filename):
+            os.remove(filename)
+
+        output = radiopropa.HDF5Output(filename, radiopropa.Output.Trajectory3D)
+        output.setLengthScale(radiopropa.meter)
+        #output.enable(radiopropa.Output.CurrentAmplitudeColumn)
+        output.enable(radiopropa.Output.SerialNumberColumn)
+        sim.add(output)
+
+    # Source - Antenna at z_antenna at which the ray tracing is started
+    source = radiopropa.Source()
+    source.add(radiopropa.SourcePosition(radiopropa.Vector3d(0, 0, z_antenna)))
+    source.add(radiopropa.SourceAmplitude(1))
+    source.add(radiopropa.SourceFrequency(freq))
+    z = np.cos(theta / units.deg * radiopropa.deg)
+    x = np.sin(theta / units.deg * radiopropa.deg)
+    source.add(radiopropa.SourceDirection(radiopropa.Vector3d(x, 0 , z)))
+    sim.setShowProgress(False)
+
+    if in_file:
+        sim.run(source, 1)
+        return filename
+    else:
+        ray = source.getCandidate()
+        sim.run(ray, True)
+        # Do something with the ray ...
+
+
+def get_trace(theta, z_antenna, freq=403 * 1e6):
+    """ Wrapper around get_trace_file """
+    file = get_trace_file(theta, z_antenna, in_file=True, freq=freq)
+    return get_angles_rhos(file)
+
+
+def temperature_integral(zenith, z_antenna, freq=400 * units.MHz, model="GL3"):
+    """ Return the integral of the effective temperature along the ray path.
+
+    This integral takes into accout the attenuation of the signal along the ray path.
+    The effective temperature is calculated as the sum of the temperature at each
+    point along the ray path weighted by the attenuation factor at that point (emissivity).
+
+    Parameters
+    ----------
+    zenith : float
+        Zenith angle of the ray
+    z_antenna : float
+        Depth of the antenna in meters
+    freq : float
+        Frequency of the signal
+    model : str
+        Ice attenuation model to use
+
+    Returns
+    -------
+    eff_temperature : float
+        Effective temperature at the antenna
+    """
+    # get the track for respective zenith direction
+    # (distance vs depth)
+    _, radius, depth_raytracing, distance_raytracing, reflected_power, _ = get_trace(zenith, z_antenna)
+
+    # get the temperature at each of the track points
+    depth_interp = interp1d(distance_raytracing, depth_raytracing, fill_value="extrapolate")
+    power_interp = interp1d(distance_raytracing, reflected_power, fill_value="extrapolate")
+
+    distance = np.linspace(0, 30000, 300000) * units.m
+    d_distance = distance[1] - distance[0]
+
+    depth = depth_interp(distance)
+    ref = power_interp(distance)
+    # manually set cap stuff
+    ref[depth < z_min] = 0
+    depth[depth < z_min] = z_min
+
+    l_att = attenuation.get_attenuation_length(depth, frequency=freq, model=model)
+    meaned_l_att = np.cumsum(l_att) / np.cumsum(np.ones_like(distance))
+
+    att_factor = np.exp(-distance / meaned_l_att)
+
+    assert model.startswith("GL"), "Only the Greenland ice model is supported for now (because the GRIP temperature model is hardcoded.)"
+    temp_env = attenuation.get_grip_temperature(depth)  # already in kelvin
+
+    return np.sum((ref * att_factor * temp_env / l_att * d_distance))
+
+
+def get_eff_temperature(z_antenna=-100, n_theta=100, plot=False, fname=None):
+    import time
+    t0 = time.time()
+    thetas = np.linspace(0, np.pi, n_theta)
+
+    eff_temperatures = []
+    for theta in thetas:
+        eff_temperatures.append(float(temperature_integral(theta, z_antenna)))
+
+    data = {
+        "z_antenna": z_antenna,
+        "theta": thetas.tolist(),
+        "eff_temperature": eff_temperatures
+    }
+
+    if fname is None:
+        fname = f"eff_temperature_{z_antenna}_ntheta{n_theta}.json"
+
+    with open(fname, "w") as f:
+        json.dump(data, f, indent=4)
+    print(time.time() - t0)
+
+    if plot:
+        fig, ax = plt.subplots()
+        ax.plot(np.rad2deg(thetas), eff_temperatures)
+        ax.set_xlabel("theta / deg")
+        ax.set_ylabel("temperature / K")
+        plt.show()
+
+def plot_ray_paths(z_antenna=-100, n_theta=100):
+    thetas = np.linspace(0, np.pi, n_theta)
+
+    import matplotlib as mpl
+    cmap = plt.get_cmap('plasma')
+
+    norm = mpl.colors.Normalize(vmin=0, vmax=180)
+    sm = mpl.cm.ScalarMappable(norm=norm, cmap=cmap)
+
+    fig, ax = plt.subplots()
+    for theta in thetas:
+        _, x, z, _, _, _ = get_trace(theta, z_antenna)
+        ax.plot(x, z, color=sm.to_rgba(np.rad2deg(theta)), alpha=0.8)
+    cb = plt.colorbar(sm, ax=ax, pad=0.02)
+    cb.set_label("theta / deg")
+
+    ax.set_xlabel("x / m")
+    ax.set_ylabel("z / m")
+    ax.set_xlim(-10, 1000)
+    fig.tight_layout()
+    plt.savefig("plot.pdf")
+
+
+if __name__ == "__main__":
+
+    get_eff_temperature(fname="eff_temperature_grip_GL3_400MHz.json")
