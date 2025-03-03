Fixed bugs and scaling and implemented Sara's implementation

NuRadioReco/utilities/fluence_rice_dist_estimator.py
@@ -8,6 +8,8 @@ from NuRadioReco.utilities import fft
 import logging
 logger = logging.getLogger('NuRadioReco.fluence_rice_dist_estimator')
 
+import matplotlib.pyplot as plt
+
 
 # def amp_spec(trace, dt_sec=1.e-9):
 # 	from scipy.fft import fft,fftfreq
@@ -54,14 +56,14 @@ def get_noise_fluence_estimators(trace, times, t_peak, f_low=30*units.MHz, f_hig
 	#times = np.arange(0, len(trace)*dt, dt)
 	dt = times[1] - times[0]
 	signal_start, signal_stop = t_peak - spacing_noise_signal - window_length_tot/2, t_peak + spacing_noise_signal + window_length_tot/2
-	list_ffts_squared=[]  
+	list_ffts_squared = []
+
 	# Generate Tukey window
-	print()
-	print(window_length_tot)
 	window = tukey_window(int(window_length_tot / dt), relative_taper_width)
 
 	#loop over the trace defining noise windows (excluding the signal window)
-	noise_start=0
+	noise_start = times[0]
+	count = 0
 	while noise_start < np.max(times):
 		
 		noise_stop = noise_start + window_length_tot
@@ -77,12 +79,12 @@ def get_noise_fluence_estimators(trace, times, t_peak, f_low=30*units.MHz, f_hig
 			#applying the Tukey window
 			if len(time_trace_clipped) != len(window): # This is prbabaly not the best solution, but it will run
 				window = tukey_window(len(time_trace_clipped), relative_taper_width)
+				print("Window length is not the same as the trace length. This is not optimal.")
 			windowed_trace = time_trace_clipped * window
 
 			#calculating the spectrum and frequencies
 			frequencies_window = np.fft.rfftfreq(len(windowed_trace), d=dt)
-			spectrum_window = fft.time2freq(windowed_trace, 1/dt)
-			#spec_wind, ff_wind = amp_spec(windowed_trace, dt_sec=dt_ns*1.e-9)
+			spectrum_window = np.abs(fft.time2freq(windowed_trace, 1/dt))
 
 			#masking the frequencies outside the initial frequency bandwidt
 			mask_freq = np.all([frequencies_window >= f_low, frequencies_window <= f_high], axis=0)
@@ -96,6 +98,9 @@ def get_noise_fluence_estimators(trace, times, t_peak, f_low=30*units.MHz, f_hig
 
 		else:
 			print("Your peak is at zero or negative time...! ") 
+			count += 1
+			if count > 1000:
+				break # find better solution
 
 	list_ffts_squared = np.array(list_ffts_squared, dtype=float)
 	
@@ -127,7 +132,7 @@ def get_signal_fluence_estimators(trace, times, t_peak, noise_estimators, f_low=
 
 	#calculating the spectrum and frequencies
 	frequencies_window = np.fft.rfftfreq(len(windowed_trace), d=dt)
-	spectrum_window = fft.time2freq(windowed_trace, 1/dt) #amp_spec(windowed_trace, dt_sec=dt_ns*1.e-9)
+	spectrum_window = np.abs(fft.time2freq(windowed_trace, 1/dt))
 
 	#masking the frequencies outside the initial frequency bandwidth
 	mask_freq = np.all([frequencies_window >= f_low, frequencies_window <= f_high], axis=0)

NuRadioReco/utilities/trace_utilities.py
@@ -14,6 +14,11 @@ conversion_factor_integrated_signal = scipy.constants.c * scipy.constants.epsilo
 
 from NuRadioReco.utilities.fluence_rice_dist_estimator import get_signal_fluence_estimators, get_noise_fluence_estimators
 
+import sys
+sys.path.append("/home/mravn/fluence-and-uncertainty-estimation-based-on-rice-distribution")
+from signal_estimator import get_signal_fluence_estimators as get_signal_fluence_estimators_sara
+from noise_estimator import get_noise_fluence_estimators as get_noise_fluence_estimators_sara
+
 def get_efield_antenna_factor(station, frequencies, channels, detector, zenith, azimuth, antenna_pattern_provider):
     """
     Returns the antenna response to a radio signal coming from a specific direction
@@ -147,29 +152,76 @@ def get_electric_field_energy_fluence(electric_field_trace, times, signal_window
                 f_low = 30 * units.MHz,
                 f_high = 80 * units.MHz,
                 window_length_tot = sum(signal_window_mask) * dt,
-                relative_taper_width = 0.142857143
+                relative_taper_width = 0.142857143,
+                )
+            
+            #sample frequency (after the windowing) in MHz
+            delta_f = frequencies_window[1] - frequencies_window[0]
+
+            #to convert the amplitudes squared into energy fluence units
+            conversion_factor = conversion_factor_integrated_signal #scipy.constants.epsilon_0 * scipy.constants.c / scipy.constants.e
+
+            #correcting for selecting positive frequencies
+            one_side_spectrum_corr_factor = 1 #np.sqrt(2)
+
+            #get the fluence of the trace summing up the frequency estimators and converting in eV/m^2
+            fluence_freq = np.sum(estimators) * ((dt * one_side_spectrum_corr_factor) **2) * delta_f * conversion_factor
+
+            #get the variance of the trace fluence summing up the frequency variances and converting in (eV/m^2)^2
+            fluence_freq_variance = np.sum(variances) * (((dt * one_side_spectrum_corr_factor) **2) * delta_f * conversion_factor) **2
+
+            #get the fluence uncertainty as the root square of the variance
+            fluence_freq_error = np.sqrt(fluence_freq_variance)
+
+            signal_energy_fluence[i_pol] = fluence_freq
+            signal_energy_fluence_error[i_pol] = fluence_freq_error
+
+    elif method == "rice_disttribution_sara":
+        signal_energy_fluence = np.zeros(len(electric_field_trace))
+        signal_energy_fluence_error = np.zeros(len(electric_field_trace))
+        for i_pol in range(len(electric_field_trace)):
+            noise_estimators, frequencies_window = get_noise_fluence_estimators_sara(
+                time_trace = electric_field_trace[i_pol,:],
+                t_peak_ns = np.mean(times[signal_window_mask]), # This is not necessarily the peak time
+                f_low_MHz = 30, #* units.MHz,
+                f_high_MHz = 80, #* units.MHz,
+                spacing_noise_signal_ns = 20,
+                window_length_tot_ns = sum(signal_window_mask) * dt,
+                relative_taper_width = 0.142857143,
+                dt_ns=dt,
+                use_median_value = True
+                )
+            estimators, variances = get_signal_fluence_estimators_sara(
+                time_trace = electric_field_trace[i_pol,:],
+                t_peak_ns = np.mean(times[signal_window_mask]),
+                noise_estimators = noise_estimators,
+                f_low_MHz = 30,# * units.MHz,
+                f_high_MHz = 80,# * units.MHz,
+                window_length_tot_ns = sum(signal_window_mask) * dt,
+                relative_taper_width = 0.142857143,
+                dt_ns=dt,
                 )
 
-        #sample frequency (after the windowing) in MHz
-        delta_f = frequencies_window[1] - frequencies_window[0]
+            #sample frequency (after the windowing) in MHz
+            delta_f = frequencies_window[1] - frequencies_window[0]
 
-        #to convert the amplitudes squared into energy fluence units
-        conversion_factor = scipy.constants.epsilon_0 * scipy.constants.c / scipy.constants.e 
+            #to convert the amplitudes squared into energy fluence units
+            conversion_factor = conversion_factor_integrated_signal #scipy.constants.epsilon_0 * scipy.constants.c / scipy.constants.e
 
-        #correcting for selecting positive frequencies
-        one_side_spectrum_corr_factor = np.sqrt(2)
+            #correcting for selecting positive frequencies
+            one_side_spectrum_corr_factor = np.sqrt(2)
 
-        #get the fluence of the trace summing up the frequency estimators and converting in eV/m^2
-        fluence_freq = np.sum(estimators) * ((dt * one_side_spectrum_corr_factor) **2) * delta_f * conversion_factor
+            #get the fluence of the trace summing up the frequency estimators and converting in eV/m^2
+            fluence_freq = np.sum(estimators) * ((dt * one_side_spectrum_corr_factor) **2) * delta_f * conversion_factor
 
-        #get the variance of the trace fluence summing up the frequency variances and converting in (eV/m^2)^2
-        fluence_freq_variance = np.sum(variances) * (((dt * one_side_spectrum_corr_factor) **2) * delta_f * conversion_factor) **2
+            #get the variance of the trace fluence summing up the frequency variances and converting in (eV/m^2)^2
+            fluence_freq_variance = np.sum(variances) * (((dt * one_side_spectrum_corr_factor) **2) * delta_f * conversion_factor) **2
+            
+            #get the fluence uncertainty as the root square of the variance
+            fluence_freq_error = np.sqrt(fluence_freq_variance)
 
-        #get the fluence uncertainty as the root square of the variance
-        fluence_freq_error = np.sqrt(fluence_freq_variance)
-
-        signal_energy_fluence[i_pol] = fluence_freq
-        signal_energy_fluence_error[i_pol] = fluence_freq_error
+            signal_energy_fluence[i_pol] = fluence_freq * 1000
+            signal_energy_fluence_error[i_pol] = fluence_freq_error * 1000
 
     if return_error:
         return signal_energy_fluence, signal_energy_fluence_error