599ac46b35e9aa0d0b1de03d1e62223437f3ea08
Felix Schlüter <felix.schluter@icecube.wisc.edu>
2025-03-07 10:53:24 +0100

    Fix test

--- NuRadioReco/test/RNO_G/test_read_rnog_data.sh
+++ NuRadioReco/test/RNO_G/test_read_rnog_data.sh
@@ -6,4 +6,4 @@ wget https://rnog-data.zeuthen.desy.de/rnog_share/forced_triggers/station23_run3
 mkdir -p tests/data/station23/run325
 mv station23_run325.root tests/data/station23/run325/combined.root
 
-python3 NuRadioReco/examples/RNOG/data_analysis_example.py tests/data/station23/run325/combined.root
+python3 NuRadioReco/examples/RNOG/data_analysis_example.py tests/data/station23/run325/combined.root --outputfile tests/data/station23/run325/processed.nur
