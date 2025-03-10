62ecbbcb839a81ed7606bf326f27ca0a028f3ce7
Mitja Desmet <mitja.desmet@vub.be>
2025-03-10 16:59:23 +0100

    Add module to generate and add galactic noise to efields

---
+++ NuRadioReco/modules/efieldGalacticNoiseAdder.py
@@ -0,0 +1,153 @@
+from NuRadioReco.modules.channelGalacticNoiseAdder import *
+
+
+logger = logging.getLogger('NuRadioReco.efieldGalacticNoiseAdder')
+
+
+class efieldGalacticNoiseAdder(channelGalacticNoiseAdder):
+    """
+    Class that simulates the noise produced by galactic radio emission
+
+    Uses the pydgsm package (https://github.com/telegraphic/pygdsm), which provides
+    radio background data based on Oliveira-Costa et al. (2008) (https://arxiv.org/abs/0802.1525)
+    and Zheng et al. (2016) (https://arxiv.org/abs/1605.04920)
+
+    The radio sky model is evaluated on a number of points above the horizon
+    folded with the antenna response. Since evaluating every frequency individually
+    would be too slow, the model is evaluated for a few frequencies and the log10
+    of the brightness temperature is interpolated in between.
+
+    This module is largely equivalent to the channelGalacticNoiseAdder, but operates on electric fields
+    instead of Channel objects. Note that as of March 2025, ice properties are not taken into account in
+    this module.
+    """
+
+    def __init__(self):
+        super().__init__()
+
+    @register_run()
+    def run(
+            self,
+            event,
+            station,
+            detector,
+            passband=None,
+    ):
+
+        """
+        Adds noise resulting from galactic radio emission to the field traces
+
+        Parameters
+        ----------
+        event: Event object
+            The event containing the station to whose channels noise shall be added
+        station: Station object
+            The station whose channels noise shall be added to
+        detector: Detector object
+            The detector description
+        passband: list of float, optional
+            Lower and upper bound of the frequency range in which noise shall be
+            added. The default (no passband specified) is [10, 1000] MHz
+        """
+        if self.__noise_temperatures is None: # check if .begin has been called, give helpful error message if not
+            msg = "efieldGalacticNoiseAdder was not initialized correctly. Maybe you forgot to call `.begin()`?"
+            logger.error(msg)
+            raise ValueError(msg)
+
+        # check that if all channels field.get_frequencies() are identical
+        last_freqs = None
+        for field in station.get_electric_fields():
+            if (not last_freqs is None) and (
+                    not np.allclose(last_freqs, field.get_frequencies(), rtol=0, atol=0.1 * units.MHz)):
+                logger.error("The frequencies of each field must be the same, but they are not!")
+                return
+            last_freqs = field.get_frequencies()
+
+        freqs = last_freqs
+        d_f = freqs[2] - freqs[1]
+
+        # If we cache the antenna pattern, we need to make sure that the frequencies have not changed
+        # between stations. If they have, we need to clear the cache.
+        if self.__caching:
+            if self.__freqs is None:
+                self.__freqs = freqs
+            else:
+                if not np.allclose(self.__freqs, freqs, rtol=0, atol=0.01 * units.MHz):
+                    self.__freqs = freqs
+                    self._get_cached_antenna_response.cache_clear()
+                    logger.warning(
+                        "Frequencies have changed. Clearing antenna response cache. "
+                        "(If this happens often, something might be wrong...")
+
+        if passband is None:
+            passband = [10 * units.MHz, 1000 * units.MHz]
+
+        passband_filter = (freqs > passband[0]) & (freqs < passband[1])
+
+        site_latitude, site_longitude = detector.get_site_coordinates(station.get_id())
+        station_time = station.get_station_time()
+
+        local_coordinates = get_local_coordinates((site_latitude, site_longitude), station_time, self.__n_side)
+
+        # n_ice = ice.get_refractive_index(-0.01, detector.get_site(station.get_id()))
+        n_air = ice.get_refractive_index(depth=1, site=detector.get_site(station.get_id()))
+        c_vac = scipy.constants.c * units.m / units.s
+
+        field_spectra = {}
+        for field in station.get_electric_fields():
+            field_spectra[field.get_unique_identifier()] = field.get_frequency_spectrum()
+
+        for i_pixel in range(healpy.pixelfunc.nside2npix(self.__n_side)):
+            azimuth = local_coordinates[i_pixel].az.rad
+            zenith = np.pi / 2. - local_coordinates[i_pixel].alt.rad
+
+            if zenith > 90. * units.deg:
+                continue
+
+            temperature_interpolator = scipy.interpolate.interp1d(
+                self.__interpolation_frequencies, np.log10(self.__noise_temperatures[:, i_pixel]), kind='quadratic')
+            noise_temperature = np.power(10, temperature_interpolator(freqs[passband_filter]))
+
+            # calculate spectral radiance of radio signal using rayleigh-jeans law
+            spectral_radiance = (2. * (scipy.constants.Boltzmann * units.joule / units.kelvin)
+                                 * freqs[passband_filter] ** 2 * noise_temperature * self.solid_angle / c_vac ** 2)
+            spectral_radiance[np.isnan(spectral_radiance)] = 0
+
+            # calculate radiance per energy bin
+            spectral_radiance_per_bin = spectral_radiance * d_f
+
+            # calculate electric field per energy bin from the radiance per bin
+            efield_amplitude = np.sqrt(
+                spectral_radiance_per_bin / (c_vac * scipy.constants.epsilon_0 * (
+                        units.coulomb / units.V / units.m))) / d_f
+
+            # assign random phases to electric field
+            noise_spectrum = np.zeros((3, freqs.shape[0]), dtype=np.complex128)
+            phases = self.__random_generator.uniform(0, 2. * np.pi, len(spectral_radiance))
+
+            noise_spectrum[1][passband_filter] = np.exp(1j * phases) * efield_amplitude
+            noise_spectrum[2][passband_filter] = np.exp(1j * phases) * efield_amplitude
+
+            channel_noise_spec = np.zeros_like(noise_spectrum)
+
+            for field in station.get_electric_fields():
+                field_pos = field.get_position()
+                dt = geometryUtilities.get_time_delay_from_direction(
+                    zenith, azimuth, field_pos, n=n_air
+                )
+                delta_phases = -2 * np.pi * freqs[passband_filter] * dt
+
+                # add random polarizations and phase to electric field
+                polarizations = self.__random_generator.uniform(0, 2. * np.pi, len(spectral_radiance))
+
+                channel_noise_spec[1][passband_filter] = noise_spectrum[1][passband_filter] * np.exp(
+                    1j * delta_phases) * np.cos(polarizations)
+                channel_noise_spec[2][passband_filter] = noise_spectrum[2][passband_filter] * np.exp(
+                    1j * delta_phases) * np.sin(polarizations)
+
+                # add noise spectrum from this to field freq spectrum
+                field_spectra[field.get_unique_identifier()] += np.sum(channel_noise_spec, axis=0)
+
+        # Store updated fields spectra
+        for field in station.get_electric_fields():
+            field.set_frequency_spectrum(field_spectra[field.get_unique_identifier()], "same")
