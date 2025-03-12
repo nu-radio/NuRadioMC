61402762f752bedee2056d87f20b5604ee3f11ec
philipp.laub@fau.de <philipp.laub@fau.de>
2025-03-12 14:35:17 +0100

    make warning more specific

--- NuRadioReco/utilities/geometryUtilities.py
+++ NuRadioReco/utilities/geometryUtilities.py
@@ -300,7 +300,7 @@ def fresnel_factors_and_signal_zenith(detector, station, channel_id, zenith):
 
     position = detector.get_relative_position(station.get_id(), channel_id)
     if position[2] < -3 * units.m:
-        logger.warning("This function might return inaccurate results. Consider using raytracing instead")
+        logger.warning("This function might return inaccurate results for deep in-ine antennas. Consider using raytracing instead.")
 
     # first check case if signal comes from above
     if (zenith <= 0.5 * np.pi) and station.is_cosmic_ray() and (position[2] <= 0):
