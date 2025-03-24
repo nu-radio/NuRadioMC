eecb13cae884a72b760ec7d239e3bb200f672d81
Sjoerd Bouma <sjoerdbouma@xs4all.nl>
2025-03-24 10:05:15 +0100

    improve/fix docstrings in io/LOFAR

--- NuRadioReco/modules/io/LOFAR/__init__.py
+++ NuRadioReco/modules/io/LOFAR/__init__.py
@@ -1,1 +1,10 @@
+"""
+Package with modules related to the read-in of LOFAR (.root) data files
 
+Most users will only be interested in the `readLOFARData.readLOFARData` class,
+which directly converts the raw LOFAR data to the NuRadioReco `Event <NuRadioReco.framework.event.Event>`
+structure. The modules `rawTBBio`, `rawTBBio_metadata` and `rawTBBio_utilities` contain several
+helper functions and classes that enable this, but it shouldn't generally be necessary to interact
+with these if you're just trying to read in LOFAR data.
+
+"""
