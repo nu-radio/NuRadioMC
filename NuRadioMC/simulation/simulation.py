f3fe2e795f65902b0d6ec71d6b4486417d0b81d4
Felix Schlüter <felix.schluter@icecube.wisc.edu>
2025-03-25 15:12:48 +0100

    Replace channelSignalReconstructor with custom function

--- NuRadioMC/simulation/simulation.py
+++ NuRadioMC/simulation/simulation.py
@@ -517,7 +517,7 @@ def apply_det_response_sim(
         detector_simulation_filter_amp(evt, sim_station, det)
 
     if config['speedup']['amp_per_ray_solution']:
-        channelSignalReconstructor.run(evt, sim_station, det)
+        _calculate_amp_per_ray_solution(sim_station)
 
     if time_logger is not None:
         time_logger.stop_time('detector response (sim)')
@@ -1874,3 +1874,21 @@ class simulation:
     @integrated_channel_response.setter
     def integrated_channel_response(self, value):
         self._integrated_channel_response = value
+
+def _calculate_amp_per_ray_solution(station):
+    """ Calculate the max amplitude and time of the ray solutions
+
+    We are using this function instead of the channelSignalReconstructor
+    to save time.
+
+    Parameters
+    ----------
+    station : NuRadioReco.framework.sim_station.SimStation
+        the station object
+    """
+    for channel in station.iter_channels():
+        times = channel.get_times()
+        trace = channel.get_trace()
+        h = trace_utilities.get_hilbert_envelope(trace)
+        channel[chp.signal_time] = times[np.argmax(h)]
+        channel[chp.maximum_amplitude_envelope] = h.max()
