001179659e4bb8a6e725679ac71736ee23e84913
Felix Schlüter <felix.schluter@icecube.wisc.edu>
2025-03-27 17:05:42 +0100

    Reimplement function calculate_vrms_from_temperature which apparently got lost when resolving some merge conflict. Expand module doc-string

--- NuRadioReco/utilities/signal_processing.py
+++ NuRadioReco/utilities/signal_processing.py
@@ -1,6 +1,18 @@
 """
 This module contains various functions for signal processing, such as filtering,
-delaying, and upsampling traces.
+delaying, and upsampling traces. Examples include:
+- `half_hann_window`
+- `resample`
+- `upsampling_fir`
+- `get_filter_response`
+- `butterworth_filter_trace`
+- `apply_butterworth`
+- `delay_trace`
+
+It also contains functions to calculate the electric field or voltage amplitude
+from a given noise temperature and bandwidth:
+- `get_electric_field_from_temperature`
+- `calculate_vrms_from_temperature`
 """
 
 from NuRadioReco.utilities import units, geometryUtilities as geo_utl, fft
@@ -418,6 +430,49 @@ def get_electric_field_from_temperature(frequencies, noise_temperature, solid_an
     efield_amplitude = np.sqrt(spectral_radiance_per_bin / (c_vac * epsilon_0)) / d_f
 
     return efield_amplitude
+
+
+def calculate_vrms_from_temperature(temperature, bandwidth=None, response=None, impedance=50 * units.ohm, freqs=None):
+    """ Helper function to calculate the noise vrms from a given noise temperature and bandwidth.
+
+    For details see https://en.wikipedia.org/wiki/Johnson%E2%80%93Nyquist_noise
+    (sec. "Maximum transfer of noise power") or our wiki
+    https://nu-radio.github.io/NuRadioMC/NuRadioMC/pages/HDF5_structure.html
+
+    Parameters
+    ----------
+    temperature: float
+        The noise temperature of the channel in Kelvin
+    bandwidth: float or tuple of 2 floats (list of 2 floats) (default: None)
+        If single float, this argument is interpreted as the effective bandwidth. If tuple, the argument is
+        interpreted as the lower and upper frequency of the bandwidth. Can be `None` if `response` is specified.
+    response: `NuRadioReco.detector.response.Response` (default: None)
+        If not None, the response of the channel is taken into account to calculate the noise vrms.
+    impedance: float (default: 50)
+        Electrical impedance of the channel in Ohm.
+    freqs: array_like (default: None -> np.arange(0, 2500, 0.1) * units.MHz)
+        Frequencies at which the response is evaluated. Only used if `response` is not None.
+
+    Returns
+    -------
+    vrms_per_channel: float
+        The vrms of the channel
+    """
+    if bandwidth is None and response is None:
+        raise ValueError("Please specify bandwidth or response")
+
+    if impedance > 1000 * units.ohm:
+        logger.warning(f"Impedance is {impedance / units.ohm:.2f} Ohm, did you forget to specify the unit?")
+
+    # (effective) bandwidth, i.e., \Delta f in equation
+    if response is None:
+        if not isinstance(bandwidth, (float, int)):
+            bandwidth = bandwidth[1] - bandwidth[0]
+    else:
+        freqs = freqs or np.arange(0, 2500, 0.1) * units.MHz
+        bandwidth = np.trapz(np.abs(response(freqs)) ** 2, freqs)
+
+    return (temperature * impedance * bandwidth * constants.k * units.joule / units.kelvin) ** 0.5
 
 
 def get_efield_antenna_factor(station, frequencies, channels, detector, zenith, azimuth, antenna_pattern_provider):
