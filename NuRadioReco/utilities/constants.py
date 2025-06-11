cd1f327fe44cbbcce05108130c36b81508953a97
Felix Schlüter <30903175+fschlueter@users.noreply.github.com>
2025-06-11 13:45:08 +0200

    Update constants.py: changed J -> joule

--- NuRadioReco/utilities/constants.py
+++ NuRadioReco/utilities/constants.py
@@ -6,7 +6,7 @@ from NuRadioReco.utilities import units
 c = scipy_constants.c * units.m / units.s
 
 # Boltzmann constant
-k_B = scipy_constants.k * units.J / units.K
+k_B = scipy_constants.k * units.joule / units.K
 
 e_mass = scipy_constants.physical_constants['electron mass energy equivalent in MeV'][0] * units.MeV
 mu_mass = scipy_constants.physical_constants['muon mass energy equivalent in MeV'][0] * units.MeV
