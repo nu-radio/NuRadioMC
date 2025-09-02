4f10b538c52b8a3730408eb6068d5023e6fbabe0
Felix Schlüter <30903175+fschlueter@users.noreply.github.com>
2025-09-02 09:39:27 +0200

    Refactor

--- NuRadioReco/detector/RNO_G/db_mongo_read.py
+++ NuRadioReco/detector/RNO_G/db_mongo_read.py
@@ -158,7 +158,7 @@ class Database(object):
             logger.error("Set invalid time for database. Time has to be of type datetime.datetime")
             raise TypeError("Set invalid time for database. Time has to be of type datetime.datetime")
 
-        if time < datetime.datetime(2024,11,1,0,0,0):
+        if time < datetime.datetime(2024, 11, 1):
             logger.error("Set invalid time for database. Time has to be after 01.11.2024 (1st november 2024), before that the database was not used to describe the detector.")
             raise ValueError("Set invalid time for database. Time has to be after 01.11.2024 (1st november 2024), before that the database was not used to describe the detector.")
 
