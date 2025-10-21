0d82510722f7bf2539d13311111546fcdd2adf06
sanyukta13 <sanyukta@icecube.wisc.edu>
2025-10-20 19:28:37 -0500

    added timing info for _calculate_block_offsets and fit_block_offsets

--- NuRadioReco/modules/RNO_G/channelBlockOffsetFitter.py
+++ NuRadioReco/modules/RNO_G/channelBlockOffsetFitter.py
@@ -313,6 +313,7 @@ def fit_block_offsets(
         Class that uses this function to automatically remove the block offsets for all
         channels in a station.
     """
+    start_time = time.perf_counter()
     dt = 1. / sampling_rate
     spectrum = fft.time2freq(trace, sampling_rate)
     frequencies = np.fft.rfftfreq(len(trace), dt)
@@ -388,6 +389,8 @@ def fit_block_offsets(
     if return_trace:
         output_trace = trace - np.repeat(block_offsets, block_size)
         return block_offsets, output_trace
+    end_time = time.perf_counter()
+    logger.info(f"Fitted block offsets in {end_time - start_time:.2f} seconds for a voltage trace using fit_block_offsets.")
 
     return block_offsets
 
@@ -423,7 +426,7 @@ def _calculate_block_offsets(traces, block_size=128, func=np.median, return_trac
         Function that uses an FFT to do an out-of-band removal of block offsets.
         This is generally much more accurate than this function.
     """
-
+    start_time = time.perf_counter()
     num_samples = traces.shape[-1]
     n_cuncks = num_samples // block_size
 
@@ -437,5 +440,8 @@ def _calculate_block_offsets(traces, block_size=128, func=np.median, return_trac
     if return_trace:
         output_traces = traces - np.repeat(block_offsets, block_size, axis=-1)
         return block_offsets, output_traces
+    
+    end_time = time.perf_counter()
+    logger.info(f"Fitted block offsets in {end_time - start_time:.2f} seconds for {num_samples} voltage traces using _calculate_block_offsets.")
 
     return block_offsets
