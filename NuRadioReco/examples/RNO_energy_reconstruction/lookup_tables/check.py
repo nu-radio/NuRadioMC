fb68813bc15c746e70d5ee3e18bb1d6aff7b6245
Gursharan Singh <gursharan2803.singh@gmail.com>
2026-02-01 12:26:25 +0100

    Stop tracking lookup tables (keep locally)

--- NuRadioReco/examples/RNO_energy_reconstruction/lookup_tables/check.py
+++
@@ -1,6 +0,0 @@
-import pickle
-
-with open("lookup_table_60.p", "rb") as f:
-    data = pickle.load(f)
-
-print(data)
--- NuRadioReco/examples/RNO_energy_reconstruction/lookup_tables_old_2/check.py
+++
@@ -1,6 +0,0 @@
-import pickle
-
-with open("lookup_table_greenland_80.p", "rb") as f:
-    data = pickle.load(f)
-
-print(data)
