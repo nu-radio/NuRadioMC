8e028c7db5a32922c19eba363e335908f0f84f41
Gursharan Singh <gursharan2803.singh@gmail.com>
2026-02-04 17:20:14 +0100

    added_detector_db_functionality

---
+++ NuRadioReco/examples/RNO_energy_reconstruction/T00_generate_events.py
@@ -0,0 +1,42 @@
+from NuRadioMC.EvtGen.generator import generate_eventlist_cylinder
+from NuRadioReco.utilities import units
+import numpy as np
+
+# Generate event list for 100 events at 10^19 eV
+n_events = 1000
+
+# Define cylindrical volume
+# Radius and height of cylinder around detector
+zmin = -4 * units.km
+zmax = 0 * units.km
+rmin = 0 * units.km
+rmax = 3 * units.km
+
+# Energy range
+Emin = 1e19 * units.eV
+Emax = 1e20 * units.eV  # Fixed energy
+
+# Flavor
+flavor = [12]  # electron neutrino
+
+# Generate the event list
+print(f"Generating {n_events} neutrino events at E = 10^19 eV...")
+generate_eventlist_cylinder(
+    filename="1e19_1e20.hdf5",
+    n_events=n_events,
+    Emin=Emin,
+    Emax=Emax,
+    volume={
+        'fiducial_rmin': rmin,
+        'fiducial_rmax': rmax,
+        'fiducial_zmin': zmin,
+        'fiducial_zmax': zmax
+    },
+    flavor=flavor,
+    n_events_per_file=None,
+    start_event_id=0,
+    deposited=True
+)
+
+print(f"Event list saved to: 1e19_1e20.hdf5")
+print(f"Number of events: {n_events}")
