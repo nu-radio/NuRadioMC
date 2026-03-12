487d4153f6986856c0a00660ad10cd5393bcecd3
Nicolai Weitkemper <nicolai.weitkemper@tu-dortmund.de>
2026-03-12 19:01:59 +0100

    fix: only check git hash in dev versions

--- NuRadioReco/utilities/version.py
+++ NuRadioReco/utilities/version.py
@@ -8,34 +8,40 @@ import logging
 logger = logging.getLogger("NuRadioReco.utilities.version")
 
 
-def get_git_commit_hash(path):
+def get_git_commit_hash(module) -> str:
     try:
-        gitproc = Popen(['git', 'show'], stdout=PIPE, cwd=path)
-        (stdout, stderr) = gitproc.communicate()
-        h = stdout.decode('utf-8').split('\n')[0].split()[1]
-        check = re.compile(r"^[a-f0-9]{40}(:.+)?$", re.IGNORECASE)
-        if(not check.match(h)):
-            logger.error("NuRadioMC version could not be determined, returning None")
+        if not module.__version__.endswith('-dev'):
+            # distribution versions won't have a Git repository;
+            # eagerly return to not waste time
             return "none"
+
+        path = os.path.dirname(module.__file__)
+
+        gitproc = Popen(['git', 'rev-parse', 'HEAD'], stdout=PIPE, cwd=path)
+        (stdout, stderr) = gitproc.communicate()
+        h = stdout.decode('utf-8').strip()
+        check = re.compile(r"^[a-f0-9]{40}(:.+)?$", re.IGNORECASE)
+        assert check.match(h)
     except:
+        logger.error("NuRadioMC/Reco version could not be determined, returning None")
         return "none"
     return h
 
-path = os.path.dirname(NuRadioMC.__file__)
-NuRadioMC_hash = get_git_commit_hash(path)
+
+# preload in case the source changes during execution
+_NuRadioMC_hash = get_git_commit_hash(NuRadioMC)
+_NuRadioReco_hash = get_git_commit_hash(NuRadioReco)
 
 
-def get_NuRadioMC_commit_hash():
+def get_NuRadioMC_commit_hash() -> str:
     """
     returns the hash of the current commit of the NuRadioMC git repository
     """
-    return NuRadioMC_hash
+    return _NuRadioMC_hash
 
-path = os.path.dirname(NuRadioReco.__file__)
-NuRadioReco_hash = get_git_commit_hash(path)
 
-def get_NuRadioReco_commit_hash():
+def get_NuRadioReco_commit_hash() -> str:
     """
     returns the hash of the current commit of the NuRadioReco git repository
     """
-    return NuRadioReco_hash
+    return _NuRadioReco_hash
