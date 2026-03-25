853f282c992bf364351c83f70ff0e565acc849b2
Felix Schlüter <felix.schluter@icecube.wisc.edu>
2026-03-25 16:09:54 +0100

    Improve rnog detector class: interface for private function + export function

--- NuRadioReco/detector/RNO_G/rnog_detector.py
+++ NuRadioReco/detector/RNO_G/rnog_detector.py
@@ -246,11 +246,11 @@ class Detector():
             # When not querying the entire description at once,
             # we need to make sure that the buffer is updated
             # for all stations before exporting.
-            self._query_all = True
+            self.logger.info("Query entire detector description at once before exporting (this might take a while) ...")
             for station_id in self.__buffered_stations:
                 # remove everything (could be handled smarter ...)
                 self.__buffered_stations[station_id] = {}
-                self._query_station_information(station_id)
+                self._query_station_information(station_id, all=True)
 
         periods = {}
         for station_id in self.__buffered_stations:
@@ -507,7 +507,7 @@ class Detector():
 
             for station_id, need_update in update_buffer_for_station.items():
                 if need_update and self.has_station(station_id):
-                    self._query_station_information(station_id)
+                    self._query_station_information(station_id, all=self._query_all)
 
         # Return when buffer is not empty. This has to come first ...
         for station_id in self.__buffered_stations:
@@ -578,7 +578,7 @@ class Detector():
         self.logger.debug(f"Station {station_id} not commissioned!")
         return False
 
-    def _query_station_information(self, station_id):
+    def _query_station_information(self, station_id, all):
         """
         Query information about a specific station from the database via the db_mongo_read interface.
         You can query only information from the station_list collection (all=False) or the complete
@@ -601,7 +601,8 @@ class Detector():
 
         self.logger.info(
             f"Query information for station {station_id} at {self.get_detector_time()}")
-        if self._query_all:
+
+        if all:
             station_information = self.__db.get_complete_station_information(
                 station_id, measurement_signal_chain=self.signal_chain_measurement_name)
         else:
