211f1d3a0dd458483b63ca4492d8e1e3744d88e9
MartinRavn <martin@ravn.dk>
2026-04-23 19:48:02 +0000

    Updated calculate_channel example script

--- NuRadioMC/simulation/examples/A02calculate_channel.py
+++ NuRadioMC/simulation/examples/A02calculate_channel.py
@@ -8,6 +8,7 @@ import NuRadioMC.simulation.time_logger
 from NuRadioMC.utilities import medium
 import NuRadioReco.framework.radio_shower
 import NuRadioReco.modules.channelBandPassFilter
+import NuRadioReco.modules.channelReadoutWindowCutter
 from NuRadioReco.framework.parameters import showerParameters as shp
 from NuRadioReco.utilities import units
 from datetime import datetime
@@ -33,17 +34,18 @@ det.update(datetime.now())
 cfg = sim.get_config("config.yaml")
 
 # set the ice model
-ice = medium.get_ice_model('southpole_simple')
+ice = NuRadioMC.utilities.medium.get_ice_model(cfg['propagation']['ice_model'])
 # set the propagation module
 # it is important to pass the detector object to the propagator for an accurate calculation of the attenuation length
 # (if the detector is availble, the sampling rate is used to determine the maximum frequency for an efficient interpolation
 # of the attenuation length, otherwise the internal sampling rate of the simulation is used which is much higher, this would 
 # lead to inaccuracies at low frequencies)
-propagator = propagation.get_propagation_module("analytic")(ice, detector=det, config=cfg)
+propagator = propagation.get_propagation_module(cfg['propagation']['module'])(ice, detector=det, config=cfg)
 
 # set the station id and channel id
 sid = 101
 cid = 1
+trace_start_times = 7000 * units.ns
 
 
 
@@ -61,8 +63,7 @@ showers.append(shower)
 
 # calculate the electric fields at the observer positions from the showers
 sim_station = sim.calculate_sim_efield(showers, sid, cid,
-                         det, propagator, ice, cfg,
-                         time_logger=time_logger)
+                         det, propagator, ice, cfg)
 
 # now let's apply the detector response (antennas and signal chain)
 
@@ -74,25 +75,26 @@ def detector_simulation_filter_amp(evt, station, det):
     channelBandPassFilter.run(evt, station, det, passband=[0, 500 * units.MHz],
                                 filter_type='butter', order=10)
 
-# applies the detector response to the electric fields (the antennas are defined
-# in the json detector description file)
-sim.apply_det_response_sim(sim_station, det, cfg, detector_simulation_filter_amp,
-                           time_logger=time_logger)
-
-
 # calculate the signal in each channel after antenna and detector response
 evt = NuRadioReco.framework.event.Event(0, 0)
 stn = NuRadioReco.framework.station.Station(sid)
 stn.set_sim_station(sim_station)
 evt.set_station(stn)
-sim.apply_det_response(evt, det, cfg, detector_simulation_filter_amp, add_noise=False)
+sim.apply_det_response(evt, det, cfg, detector_simulation_filter_amp, add_noise=False, channel_ids=[cid])
+
+# Cut to user defined readout window:
+channelReadoutWindowCutter = NuRadioReco.modules.channelReadoutWindowCutter.channelReadoutWindowCutter()
+channelReadoutWindowCutter.run(evt, stn, det, trace_start_times=np.repeat(trace_start_times, len(stn.get_channel_ids())))
+
+# Resample to detector sampling rate:
+for i, channel in enumerate(stn.iter_channels()):
+    channel.resample(det.get_sampling_frequency(stn.get_id(), channel.get_id()))
+
 
 import matplotlib.pyplot as plt
-# let's plot the results
+# let's plot the result for the channel we simulated:
 fig, (ax, ax2) = plt.subplots(1,2)
 for i, channel in enumerate(stn.iter_channels()):
-    if channel.get_id() != cid:
-        continue
     print(channel.get_id())
     trace = channel.get_trace()
     ax.plot(channel.get_times(), trace/units.V, f"-C{i}", label=f'channel id {channel.get_id()}')
@@ -102,18 +104,6 @@ for i, channel in enumerate(stn.iter_channels()):
     ax2.plot(channel.get_frequencies()/units.MHz, np.abs(channel.get_frequency_spectrum()/units.V*units.MHz),
              f"-C{i}",label=f'channel id {channel.get_id()}')
 
-    # alternatively, we can sum the traces of the sim channels and we should get equivalent results
-    sim_channel_sum = None
-    for sim_channel in sim_station.get_channels_by_channel_id(channel.get_id()):
-        if sim_channel_sum is None:
-            sim_channel_sum = sim_channel
-        else:
-            sim_channel_sum += sim_channel
-    if sim_channel_sum is not None:
-        ax.plot(sim_channel_sum.get_times(), sim_channel_sum.get_trace()/units.V, f"--C{i+1}",
-                label=f'channel id {channel.get_id()}')
-        ax2.plot(sim_channel_sum.get_frequencies()/units.MHz, np.abs(sim_channel_sum.get_frequency_spectrum()/units.V*units.MHz),
-             f"--C{i+1}",label=f'channel id {channel.get_id()}')
     ax2.set_xlabel('Frequency (MHz)')
     ax2.set_ylabel('Voltage (V/MHz)')
 ax.legend()
