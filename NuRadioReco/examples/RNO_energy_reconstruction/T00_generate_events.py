bc27f82a05be3a5bee258842644c1f1006e016a5
Gursharan singh <73589333+Gursharan2803@users.noreply.github.com>
2026-06-23 16:22:09 +0200

    Refactor event generation to use fixed parameters
    Removed command line argument parsing for event generation and set a fixed number of events to 1000. Updated output filename to 'generate.hdf5'.

--- NuRadioReco/examples/RNO_energy_reconstruction/T00_generate_events.py
+++ NuRadioReco/examples/RNO_energy_reconstruction/T00_generate_events.py
@@ -1,27 +1,9 @@
 from NuRadioMC.EvtGen.generator import generate_eventlist_cylinder
 from NuRadioReco.utilities import units
 import numpy as np
-import argparse
-
- 
-parser = argparse.ArgumentParser(
-    description='Generation of neutrino events in a cylindrical volume around the station. The generated events will be stored in an HDF5 file, which can be used as input for the simulation step.'
-)
-parser.add_argument(
-    'Number_of_events',
-    type=int,
-    help='Number of events to generate.'
-)
-parser.add_argument(
-    'output_file',
-    type=str,
-    help='Name of the .nur file the simulated events will be written into.'
-)
-
-args = parser.parse_args()
 
 # Generate event list for 1000 events at 10^19 eV
-n_events = args.Number_of_events
+n_events = 1000
 
 # Define cylindrical volume centered on the station
 zmin = - 2.7 * units.km   # ~-504 m depth
@@ -38,7 +20,7 @@ flavor = [12]  # electron neutrino
 
 # Generate the event list
 generate_eventlist_cylinder(
-    filename=args.output_file,
+    filename=generate.hdf5,
     n_events=n_events,
     Emin=Emin,
     Emax=Emax,
