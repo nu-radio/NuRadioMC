032030bf17e553520fb6f6dfa49c9a8789904645
Sjoerd Bouma <sjoerdbouma@xs4all.nl>
2026-09-21 15:59:05 +0200

    include full error message on download failure

--- NuRadioReco/utilities/dataservers.py
+++ NuRadioReco/utilities/dataservers.py
@@ -64,17 +64,17 @@ def download_from_dataserver(remote_path, target_path, unpack_tarball=True, data
                 r.raise_for_status()
                 requests_status = r.status_code
                 break
-            except requests.exceptions.HTTPError:
-                logger.warning(f"HTTP Error for {dataserver}. Does the file {remote_path} exist on the server?")
+            except requests.exceptions.HTTPError as err:
+                logger.warning(f"HTTP Error for {dataserver}. Does the file {remote_path} exist on the server?", exc_info=err)
                 pass
-            except requests.exceptions.ConnectionError:
-                logger.warning(f"Error Connecting to {dataserver}. Maybe you don't have internet... or the server is down?")
+            except requests.exceptions.ConnectionError as err:
+                logger.warning(f"Error Connecting to {dataserver}. Maybe you don't have internet... or the server is down?", exc_info=err)
                 pass
-            except requests.exceptions.Timeout:
-                logger.warning(f"Timeout Error for {dataserver}.")
+            except requests.exceptions.Timeout as err:
+                logger.warning(f"Timeout Error for {dataserver}.", exc_info=err)
                 pass
             except requests.exceptions.RequestException as err:
-                logger.warning(f"An unusual error for {dataserver} occurred: {err}")
+                logger.warning(f"An unusual error for {dataserver} occurred:", exc_info=err)
                 pass
 
             logger.warning("problem downloading file {} from {}. Let's see if there is another server.".format(target_path, URL))
